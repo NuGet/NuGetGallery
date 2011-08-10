29e991bff87d52bc98663654b9b327fa5ff576a1
Drew Miller <ego@anglicangeek.com>
2011-08-10 11:08:55 -0700

    fixing bad merge during rebase

--- Website/Services/IPackageService.cs
+++ Website/Services/IPackageService.cs
@@ -7,15 +7,10 @@ namespace NuGetGallery {
             IPackage nugetPackage,
             User currentUser);
 
-<<<<<<< HEAD
-        Package FindByIdAndVersion(
-            string id,
-=======
         PackageRegistration FindPackageRegistrationById(string id);
         
         Package FindPackageByIdAndVersion(
             string id, 
->>>>>>> refactored, fixed (added missing data), and added tests for PackageService.CreatePackage
             string version = null);
 
         IEnumerable<Package> GetLatestVersionOfPublishedPackages();
