c06dc80bc647212d7c471ea0bbaeffac153a4151
Drew Miller <ego@anglicangeek.com>
2011-08-11 01:48:34 -0700

    removed data directory transform from migrator scripts since we aren't use an MDF anymore

--- Scripts/Get-ConnectionString.ps1
+++ Scripts/Get-ConnectionString.ps1
@@ -1,10 +1,9 @@
-function Get-ConnectionString($configPath, $connectionStringName, $dataDirectory) 
+function Get-ConnectionString($configPath, $connectionStringName) 
 {
   $config = [xml](get-content $configPath)
   
   $connectionString = ($config.configuration.connectionStrings.add | where { $_.name -eq $connectionStringName }).connectionString
   
-  $connectionString = $connectionString.Replace("|DataDirectory|", $dataDirectory)
   $connectionString = $connectionString.Replace("=", "%3D")
   $connectionString = $connectionString.Replace(";", "%3B")
 
