2ec60696db7510c1377d2fdc88f23ee8d91f2c1e
Drew Miller <ego@anglicangeek.com>
2011-08-11 01:50:35 -0700

    removed unused usings from create database build task

--- Website/BuildTasks/CreateDatabase.cs
+++ Website/BuildTasks/CreateDatabase.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Build.Utilities;
+﻿using System.Data.SqlClient;
 using Microsoft.Build.Framework;
-using System.Data.SqlClient;
+using Microsoft.Build.Utilities;
 
 namespace NuGetGallery
 {
