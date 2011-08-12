654f08d537827d12873790ae83047e445e91df64
Drew Miller <ego@anglicangeek.com>
2011-08-11 21:38:47 -0700

    added migration for a user's API key (wip #12)

---
+++ Website/Migrations/20110811213000_AddApiKeyColumn.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Migrator.Framework;
+
+namespace NuGetGallery.Data.Migrations {
+    [Migration(20110811210300)]
+    public class AddApiKeyColumnMigration : Migration {
+        public override void Up() {
+            Database.AddColumn("Users", "ApiKey", DbType.Guid, 16, ColumnProperty.NotNull | ColumnProperty.Unique, "newid()");
+        }
+
+        public override void Down() {
+            Database.RemoveColumn("Users", "ApiKey");
+        }
+    }
+}
