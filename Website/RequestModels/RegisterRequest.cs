0f73b4d2f8699a3cd9519738b5d369c69b4d8892
Haacked <haacked@gmail.com>
2011-09-23 14:58:26 -0700

    Updated the register model to validate username and email.
    If we are planning to allow users to login using user name or email, we should make sure
    that one user's username cannot overlap with another user's email.

--- Website/RequestModels/RegisterRequest.cs
+++ Website/RequestModels/RegisterRequest.cs
@@ -6,7 +6,7 @@ namespace NuGetGallery {
     public class RegisterRequest {
         [Required]
         [StringLength(64)]
-        //[RegularExpression(@"(?i)[a-z0-9_-]*", ErrorMessage = "User names may only contain letters, numbers, dashes, and hyphens.")]
+        [RegularExpression(@"(?i)[a-z0-9_-]*", ErrorMessage = "User names may only contain letters, numbers, dashes, and hyphens.")]
         [AdditionalMetadata("Hint", "Choose something unique so others will know which contributions are yours.")]
         public string Username { get; set; }
 
@@ -14,7 +14,7 @@ namespace NuGetGallery {
         [StringLength(255)]
         [DisplayName("Email")]
         [DataType(DataType.EmailAddress)]
-        //[RegularExpression(@"(?i)^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$", ErrorMessage="This doesn't appear to be a valid email address.")]
+        [RegularExpression(@"(?i)^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$", ErrorMessage = "This doesn't appear to be a valid email address.")]
         [AdditionalMetadata("Hint", "Your email will not be public unless you choose to disclose it. It is required to verify your registration and for password retrieval, important notifications, etc.")]
         public string EmailAddress { get; set; }
 
