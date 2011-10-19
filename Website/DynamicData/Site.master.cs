9d9162c35948515939f97c2eaa8fceb3c2c947be
Haacked <haacked@gmail.com>
2011-10-19 14:53:18 -0700

    Updated WebBackgrounder and secured the dbAdmin to only allow members of 'Admins' role.

--- Website/DynamicData/Site.master.cs
+++ Website/DynamicData/Site.master.cs
@@ -1,8 +1,26 @@
-﻿
+﻿using System;
+using System.Net;
+using System.Web.UI;
+using NuGetGallery;
+
 namespace DynamicDataEFCodeFirst
 {
-    public partial class Site : System.Web.UI.MasterPage
+    public partial class Site : MasterPage
     {
+        protected override void OnInit(EventArgs e)
+        {
+            if (!Page.User.Identity.IsAuthenticated)
+            {
+                Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                Response.End();
+            }
 
+            if (!Page.User.IsInRole(Const.AdminRoleName))
+            {
+                Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                Response.End();
+            }
+            base.OnInit(e);
+        }
     }
 }
