97e78c93f684b72f9a319e3a6e7357160f868a5b
jeffhandley <jeff@jeffhandley.com>
2011-11-08 11:16:59 -0800

    Issue #191 Adding another index to improve 'Manage My Packages' perf, especially when the owner has lots of packages

---
+++ Website/Migrations/201111081908453_MyPackagesIndexes.cs
@@ -0,0 +1,18 @@
+namespace NuGetGallery.Migrations.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class MyPackagesIndexes : DbMigration
+    {
+        public override void Up()
+        {
+            // CreateIndex doesn't support INCLUDE
+            Sql("CREATE NONCLUSTERED INDEX [IX_PackageRegistrationOwners_UserKey] ON [dbo].[PackageRegistrationOwners] ([UserKey]) INCLUDE ([PackageRegistrationKey])");
+        }
+        
+        public override void Down()
+        {
+            DropIndex(table: "PackageRegistrationOwners", name: "IX_PackageRegistrationOwners_UserKey");
+        }
+    }
+}
