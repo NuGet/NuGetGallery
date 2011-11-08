c08eb98cebff55f9efdc011d9a2e166467215f22
jeffhandley <git@jeffhandley.com>
2011-11-08 00:52:00 -0800

    Added an index to improve Display Package perf

---
+++ Website/Migrations/201111080816426_DisplayPackageIndexes.cs
@@ -0,0 +1,47 @@
+namespace NuGetGallery.Migrations.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class DisplayPackageIndexes : DbMigration
+    {
+        public override void Up()
+        {
+            // CreateIndex does not support INCLUDE
+            Sql(@"CREATE NONCLUSTERED INDEX [IX_Packages_PackageRegistrationKey] ON [dbo].[Packages] 
+                (
+                    [PackageRegistrationKey] ASC
+                )
+                INCLUDE ( [Key],
+                [Copyright],
+                [Created],
+                [Description],
+                [DownloadCount],
+                [ExternalPackageUrl],
+                [HashAlgorithm],
+                [Hash],
+                [IconUrl],
+                [IsLatest],
+                [LastUpdated],
+                [LicenseUrl],
+                [Published],
+                [PackageFileSize],
+                [ProjectUrl],
+                [RequiresLicenseAcceptance],
+                [Summary],
+                [Tags],
+                [Title],
+                [Version],
+                [FlattenedAuthors],
+                [FlattenedDependencies],
+                [IsLatestStable],
+                [Listed],
+                [IsPrerelease],
+                [ReleaseNotes]) WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]");
+        }
+        
+        public override void Down()
+        {
+            DropIndex(table: "Packages", name: "IX_Packages_PackageRegistrationKey");
+        }
+    }
+}
