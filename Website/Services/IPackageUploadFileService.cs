016a5862ef57fe6c6a0a21a2676485b78b730eeb
Drew Miller (anglicangeek) <ego@anglicangeek.com>
2011-11-10 15:46:46 -0800

    added package upload file service and first test

---
+++ Website/Services/IPackageUploadFileService.cs
@@ -0,0 +1,11 @@
+﻿using NuGet;
+
+namespace NuGetGallery
+{
+    public interface IPackageUploadFileService
+    {
+        void DeleteUploadedFile(User user);
+        ZipPackage GetUploadedFile(User user);
+        void SaveUploadedFile(User user, IPackageMetadata package);
+    }
+}
