855c2211a7d13abd36240c88989eaf29492c361c
Drew Miller (anglicangeek) <ego@anglicangeek.com>
2011-11-10 23:31:32 -0800

    re-ordered constants

--- Website/Const.cs
+++ Website/Const.cs
@@ -4,6 +4,9 @@ namespace NuGetGallery
     public static class Const
     {
         public const string AdminRoleName = "Admins";
+        public const int DefaultPackageListPageSize = 20;
+        public const string DefaultPackageListSortOrder = "package-download-count";
+        public const int DefaultPasswordResetTokenExpirationHours = 24;
         public const int MaxEmailSubjectLength = 255;
         public const string PackageContentType = "application/zip";
         public const string PackageFileExtension = ".nupkg";
@@ -12,10 +15,7 @@ namespace NuGetGallery
         public const string PackagesFolderName = "packages";
         public const string Sha1HashAlgorithmId = "SHA1";
         public const string Sha512HashAlgorithmId = "SHA512";
-        public const int DefaultPackageListPageSize = 20;
-        public const string DefaultPackageListSortOrder = "package-download-count";
-        public const int DefaultPasswordResetTokenExpirationHours = 24;
-        public const string UploadsFolderName = "uploads";
         public const string UploadFileNameTemplate = "{0}{1}";
+        public const string UploadsFolderName = "uploads";
     }
 }
