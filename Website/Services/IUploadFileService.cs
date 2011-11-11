3f4cb0b43f0ecd39746729db17f11a9798828d90
Drew Miller (anglicangeek) <ego@anglicangeek.com>
2011-11-10 16:48:56 -0800

    added delete to the upload file service with tests

---
+++ Website/Services/IUploadFileService.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using NuGet;
+
+namespace NuGetGallery
+{
+    public interface IUploadFileService
+    {
+        void DeleteUploadFile(int userKey);
+        
+        ZipPackage GetUploadFile(User user);
+        
+        void SaveUploadFile(
+            int userKey,
+            Stream packageFileStream);
+    }
+}
