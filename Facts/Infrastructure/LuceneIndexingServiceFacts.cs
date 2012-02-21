27e1d648d781055ef935c497aecec4410cb8e817
pranavkm <prkrishn@hotmail.com>
2012-02-21 12:10:54 -0800

    Fix for issue #400. Tweaking Lucene index to boost by DownloadCount

--- Facts/Infrastructure/LuceneIndexingServiceFacts.cs
+++ Facts/Infrastructure/LuceneIndexingServiceFacts.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using Moq;
 using Xunit;
 using Xunit.Extensions;
 
@@ -11,19 +14,62 @@ namespace NuGetGallery.Infrastructure
         [InlineData(new object[] { "NUnit", new string[0] })]
         [InlineData(new object[] { "SisoDb", new[] { "Siso", "Db" } })]
         [InlineData(new object[] { "EntityFramework", new[] { "Entity", "Framework" } })]
-        [InlineData(new object[] { "Sys-netFX", new[] { "Sys-net", "FX" } })]
-        [InlineData(new object[] { "Sys-netX", new[] { "Sys-net" } })]
+        [InlineData(new object[] { "Sys-netFX", new[] { "Sys", "net", "FX" } })]
         [InlineData(new object[] { "xUnit", new string[0] })]
         [InlineData(new object[] { "jQueryUI", new[] { "jQuery", "UI" } })]
+        [InlineData(new object[] { "jQuery-UI", new[] { "jQuery", "UI" } })]
         [InlineData(new object[] { "NuGetPowerTools", new[] { "NuGet", "Power", "Tools" } })]
-        [InlineData(new object[] { "microsoft-web-helpers", new string[0] })]
+        [InlineData(new object[] { "microsoft-web-helpers", new[] { "microsoft", "web", "helpers" } })]
+        [InlineData(new object[] { "EntityFramework.sample", new[] { "Entity", "Framework", "sample" } })]
         public void CamelCaseTokenizer(string term, IEnumerable<string> tokens)
         {
             // Act
-            var result = LuceneIndexingService.CamelCaseTokenize(term);
+            var result = LuceneIndexingService.TokenizeId(term);
 
             // Assert
             Assert.Equal(tokens, result);
+        }
+
+        [Fact]
+        public void UpdateIndexCreatesIndexDirectoryIfNotPresent()
+        {
+            // Arrange
+            var indexingService = new Mock<LuceneIndexingService>() { CallBase = true };
+            indexingService.Setup(s => s.CreateContext()).Returns<DbContext>(null);
+            indexingService.Setup(s => s.GetPackages(null, null)).Returns(new List<PackageIndexEntity> { new PackageIndexEntity() });
+            indexingService.Setup(s => s.GetIndexCreationTime()).Returns<DateTime?>(null);
+            indexingService.Setup(s => s.GetLastWriteTime()).Returns<DateTime?>(null);
+
+            indexingService.Setup(s => s.WriteIndex(true, It.IsAny<List<PackageIndexEntity>>())).Verifiable();
+            indexingService.Setup(s => s.UpdateLastWriteTime()).Verifiable();
+            indexingService.Setup(s => s.EnsureIndexDirectory()).Verifiable();
+
+            // Act
+            indexingService.Object.UpdateIndex();
+
+            // Assert
+            indexingService.Verify();
+        }
+
+        [Fact]
+        public void UpdateIndexRecreatesIndexIfOld()
+        {
+            // Arrange
+            var lastWriteTime = DateTime.UtcNow.AddMinutes(-3);
+            var indexingService = new Mock<LuceneIndexingService>() { CallBase = true };
+            indexingService.Setup(s => s.CreateContext()).Returns<DbContext>(null);
+            indexingService.Setup(s => s.GetPackages(null, lastWriteTime)).Returns(new List<PackageIndexEntity>());
+            indexingService.Setup(s => s.GetIndexCreationTime()).Returns(DateTime.UtcNow.AddDays(-3).AddMinutes(-1));
+            indexingService.Setup(s => s.GetLastWriteTime()).Returns(lastWriteTime);
+
+            indexingService.Setup(s => s.ClearLuceneDirectory()).Verifiable();
+            indexingService.Setup(s => s.UpdateLastWriteTime()).Verifiable();
+
+            // Act
+            indexingService.Object.UpdateIndex();
+
+            // Assert
+            indexingService.Verify();
         }
     }
 }
