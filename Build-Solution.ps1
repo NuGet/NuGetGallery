cc3d4203f5aa331655f5106a2af61047953600fb
anglicangeek <ego@anglicangeek.com>
2012-02-20 23:09:42 -0800

    removed update and revert database support form build scripts; EF 4.3 will have it's own scripts in the NuGet console, and we auto-update during app start, so they weren't really needed. (fixes #221)

--- Build-Solution.ps1
+++ Build-Solution.ps1
@@ -1,13 +1,7 @@
 param($connectionString = "")
 
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path
-. (join-path $scriptPath Scripts\Get-ConnectionString.ps1)
-
-if ($connectionString.Trim() -eq "") 
-{
-  $connectionString = Get-ConnectionString -configPath (join-path $scriptPath Website\web.config) -connectionStringName NuGetGallery
-}
 
 $projFile = join-path $scriptPath Scripts\NuGetGallery.msbuild
  
-& "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $projFile /p:DbConnection=$connectionString /t:FullBuild
+& "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $projFile /t:FullBuild
