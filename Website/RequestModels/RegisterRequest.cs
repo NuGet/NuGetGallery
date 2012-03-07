69692a84f8d1334cda8fd9f6c949dbcfb258d4d3
anglicangeek <ego@anglicangeek.com>
2012-03-07 13:48:17 -0800

    changed username validation to require alphanumeric and start and end of usernames (fixes #450)

--- Website/RequestModels/RegisterRequest.cs
+++ Website/RequestModels/RegisterRequest.cs
@@ -15,7 +15,7 @@ namespace NuGetGallery
 
         [Required]
         [StringLength(64)]
-        [RegularExpression(@"(?i)[a-z0-9_.-]*", ErrorMessage = "User names may only contain letters, numbers, dashes, and hyphens.")]
+        [RegularExpression(@"(?i)[a-z0-9][a-z0-9_.-]+[a-z0-9]", ErrorMessage = "User names must start and end with a letter or number, and may only contain letters, numbers, underscores, periods, and hyphens in between.")]
         [Hint("Choose something unique so others will know which contributions are yours.")]
         public string Username { get; set; }
 
