798069521fe1b19262b92199a285b7d50a8c567e
Jeff Handley <jeff.handley@microsoft.com>
2012-03-09 23:22:25 -0800

    Tweaking the EnableDynamicHttpCompression.cmd to use EXIT /B 0.

--- Scripts/EnableDynamicHttpCompression.cmd
+++ Scripts/EnableDynamicHttpCompression.cmd
@@ -4,4 +4,4 @@ REM Enable Dynamic HTTP Compression for application/* mime types.
 %windir%\system32\inetsrv\appcmd.exe set config /section:urlCompression /doDynamicCompression:True /commit:apphost
 %windir%\system32\inetsrv\appcmd.exe set config /section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='application/*',enabled='True']" /commit:apphost
 
-EXIT 0
+EXIT /B 0
