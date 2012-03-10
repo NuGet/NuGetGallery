94f0b7860d8afcdb6ddb4a7cce4712c582f5777a
Jeff Handley <jeff.handley@microsoft.com>
2012-03-09 16:54:15 -0800

    Ensure that EnableDynamicHttpCompression.cmd has an EXIT 0 to force the indication of success.

--- Scripts/EnableDynamicHttpCompression.cmd
+++ Scripts/EnableDynamicHttpCompression.cmd
@@ -3,3 +3,5 @@
 REM Enable Dynamic HTTP Compression for application/* mime types.
 %windir%\system32\inetsrv\appcmd.exe set config /section:urlCompression /doDynamicCompression:True /commit:apphost
 %windir%\system32\inetsrv\appcmd.exe set config /section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='application/*',enabled='True']" /commit:apphost
+
+EXIT 0
