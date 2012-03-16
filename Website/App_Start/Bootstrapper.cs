422040883e7a5c821ba5f47fbdf031cd03b091f3
anglicangeek <ego@anglicangeek.com>
2012-03-16 16:20:50 -0700

    fixed bootstrapping code so that it doen't execute out-of-order after the recent move to PostApplicationStart

--- Website/App_Start/Bootstrapper.cs
+++ Website/App_Start/Bootstrapper.cs
@@ -1,14 +1,37 @@
-﻿using System.Data.Entity.Migrations;
+﻿using System;
+using System.Data.Entity.Migrations;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Elmah;
 using Elmah.Contrib.Mvc;
+using NuGetGallery.Jobs;
 using NuGetGallery.Migrations;
+using WebBackgrounder;
 
-[assembly: WebActivator.PostApplicationStartMethod(typeof(NuGetGallery.Bootstrapper), "Start")]
+[assembly: WebActivator.PreApplicationStartMethod(typeof(NuGetGallery.Bootstrapper), "Start")]
+[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(NuGetGallery.Bootstrapper), "Stop")]
 namespace NuGetGallery
 {
     public static class Bootstrapper
     {
+        private static readonly HttpApplication _elmahHttpApplication = new ElmahSignalScopeHttpApplication();
+        private static JobManager _jobManager;
+
+        private static JobManager CreateJobManager()
+        {
+            var jobs = new IJob[] { 
+                new UpdateStatisticsJob(TimeSpan.FromSeconds(10), () => new EntitiesContext(), timeout: TimeSpan.FromMinutes(5)),
+                new WorkItemCleanupJob(TimeSpan.FromDays(1), () => new EntitiesContext(), timeout: TimeSpan.FromDays(4)),
+                new LuceneIndexingJob(TimeSpan.FromMinutes(10), timeout: TimeSpan.FromMinutes(2)),
+            };
+
+            var jobCoordinator = new WebFarmJobCoordinator(new EntityWorkItemRepository(() => new EntitiesContext()));
+            var manager = new JobManager(jobs, jobCoordinator);
+            manager.Fail(e => ErrorLog.GetDefault(null).Log(new Error(e)));
+            return manager;
+        }
+
         public static void Start()
         {
             UpdateDatabase();
@@ -20,6 +43,14 @@ namespace NuGetGallery
             GlobalFilters.Filters.Add(new ElmahHandleErrorAttribute());
 
             ValueProviderFactories.Factories.Add(new HttpHeaderValueProviderFactory());
+            _jobManager = CreateJobManager();
+            _jobManager.Start();
+        }
+
+        public static void Stop()
+        {
+            _jobManager.Dispose();
+            _elmahHttpApplication.Dispose();
         }
 
         private static void UpdateDatabase()
@@ -33,6 +64,15 @@ namespace NuGetGallery
             {
                 Settings.SeedDatabase(context);
             }
+        }
+
+        /// <summary>
+        /// Elmah requires an HttpApplication for its API. It uses it to determine when 
+        /// to dispose of its signal instance. At this point, we don't have an HttpApplication 
+        /// so I'll just create a stub one and control its lifecycle here.
+        /// </summary>
+        private class ElmahSignalScopeHttpApplication : HttpApplication
+        {
         }
     }
 }
