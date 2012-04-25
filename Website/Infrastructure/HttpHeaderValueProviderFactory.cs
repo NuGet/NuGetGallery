9c75e854cf79e3b5c0c8a3ba62c707bdaa52091e
pranavkm <prkrishn@hotmail.com>
2012-04-24 22:29:28 -0700

    #409: Fix HttpHeaderValueProvider to work for API routes when hosted in a subdiectory

--- Website/Infrastructure/HttpHeaderValueProviderFactory.cs
+++ Website/Infrastructure/HttpHeaderValueProviderFactory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace NuGetGallery
 {
@@ -8,9 +7,9 @@ namespace NuGetGallery
         public override IValueProvider GetValueProvider(ControllerContext controllerContext)
         {
             var request = controllerContext.RequestContext.HttpContext.Request;
-            
+
             // Use this value provider only if a route is located under "API"
-            if (request.Path.TrimStart('/').StartsWith("api", StringComparison.OrdinalIgnoreCase))
+            if (controllerContext.Controller is ApiController)
                 return new HttpHeaderValueProvider(request, "ApiKey");
             return null;
         }
