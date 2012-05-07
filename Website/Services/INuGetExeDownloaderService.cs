8c2ed475af346c604bdfa64c9c1fdc40b0421bcf
Pranav K <prkrishn@hotmail.com>
2012-05-07 00:03:54 -0700

    Adding support for /api/v2/NuGet.exe route that returns the exe corresponding to the latest NuGet.CommandLine package

---
+++ Website/Services/INuGetExeDownloaderService.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using NuGet;
+
+namespace NuGetGallery
+{
+    public interface INuGetExeDownloaderService
+    {
+        ActionResult CreateNuGetExeDownloadActionnResult();
+
+        void UpdateExecutable(IPackage package);
+    }
+}
--- Website/Services/PackageFileService.cs
+++ Website/Services/PackageFileService.cs
@@ -8,40 +8,24 @@ namespace NuGetGallery
     public class PackageFileService : IPackageFileService
     {
         private readonly IFileStorageService fileStorageSvc;
-        
+
         public PackageFileService(
             IFileStorageService fileStorageSvc)
         {
             this.fileStorageSvc = fileStorageSvc;
         }
 
-        private static string BuildFileName(
-            string id, 
-            string version)
-        {
-            return String.Format(CultureInfo.InvariantCulture, Constants.PackageFileSavePathTemplate, id, version, Constants.NuGetPackageFileExtension);
-        }
-
         public ActionResult CreateDownloadPackageActionResult(Package package)
         {
-            if (package == null)
-                throw new ArgumentNullException("package");
-            if (package.PackageRegistration == null 
-                || String.IsNullOrWhiteSpace(package.PackageRegistration.Id) 
-                || String.IsNullOrWhiteSpace(package.Version))
-                throw new ArgumentException("The package is missing required data.", "package");
-            
-            var fileName = BuildFileName(
-                package.PackageRegistration.Id,
-                package.Version);
+            var fileName = BuildFileName(package);
 
             return fileStorageSvc.CreateDownloadFileActionResult(
-                Constants.PackagesFolderName, 
+                Constants.PackagesFolderName,
                 fileName);
         }
 
         public void DeletePackageFile(
-            string id, 
+            string id,
             string version)
         {
             if (String.IsNullOrWhiteSpace(id))
@@ -52,7 +36,7 @@ namespace NuGetGallery
             var fileName = BuildFileName(id, version);
 
             fileStorageSvc.DeleteFile(
-                Constants.PackagesFolderName, 
+                Constants.PackagesFolderName,
                 fileName);
         }
 
@@ -60,23 +44,49 @@ namespace NuGetGallery
             Package package,
             Stream packageFile)
         {
-            if (package == null)
-                throw new ArgumentNullException("package");
             if (packageFile == null)
                 throw new ArgumentNullException("packageFile");
-            if (package.PackageRegistration == null 
-                || String.IsNullOrWhiteSpace(package.PackageRegistration.Id) 
-                || String.IsNullOrWhiteSpace(package.Version))
-                throw new ArgumentException("The package is missing required data.", "package");
 
-            var fileName = BuildFileName(
-                package.PackageRegistration.Id,
-                package.Version);
+            var fileName = BuildFileName(package);
 
             fileStorageSvc.SaveFile(
-                Constants.PackagesFolderName, 
-                fileName, 
+                Constants.PackagesFolderName,
+                fileName,
                 packageFile);
+        }
+
+
+        public Stream DownloadPackageFile(Package package)
+        {
+            var fileName = BuildFileName(package);
+
+            return fileStorageSvc.GetFile(
+                Constants.PackagesFolderName,
+                fileName);
+        }
+
+        private static string BuildFileName(
+            string id,
+            string version)
+        {
+            return String.Format(CultureInfo.InvariantCulture, Constants.PackageFileSavePathTemplate, id, version, Constants.NuGetPackageFileExtension);
+        }
+
+
+        private static string BuildFileName(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+            if (package.PackageRegistration == null
+                || String.IsNullOrWhiteSpace(package.PackageRegistration.Id)
+                || String.IsNullOrWhiteSpace(package.Version))
+            {
+                throw new ArgumentException("The package is missing required data.", "package");
+            }
+
+            return BuildFileName(package.PackageRegistration.Id, package.Version);
         }
     }
 }
