683d7c4f2f7bfa19780610797d5a5a86ed81b5b7
Pranav K <prkrishn@hotmail.com>
2012-06-05 22:09:02 -0700

    Modifying binding in ContactOwnersViewModel

--- Website/ViewModels/ContactOwnersViewModel.cs
+++ Website/ViewModels/ContactOwnersViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace NuGetGallery
 {
+    [Bind(Include="Message")]
     public class ContactOwnersViewModel
     {
         public string PackageId { get; set; }
+
         public IEnumerable<User> Owners { get; set; }
+        
         [Required(ErrorMessage = "Please enter a message.")]
         [StringLength(4000)]
         public string Message { get; set; }
