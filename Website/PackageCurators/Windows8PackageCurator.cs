01da1970f7867d50cde2d641b5d743bfb86f38e5
anurse <andrew@andrewnurse.net>
2012-06-18 15:20:32 -0700

    Added unit tests for Win8 Curator and tweaked Tag-based Curator

--- Website/PackageCurators/Windows8PackageCurator.cs
+++ Website/PackageCurators/Windows8PackageCurator.cs
@@ -5,21 +5,22 @@ using System.Web;
 
 namespace NuGetGallery
 {
-public class Windows8PackageCurator : TagBasedPackageCurator
-{
-    protected override IEnumerable<string> RequiredTags
+    public class Windows8PackageCurator : TagBasedPackageCurator
     {
-        get {
-            yield return "winrt";
-            yield return "win8";
-            yield return "windows8";
-            yield return "winjs";
+        protected override IEnumerable<string> RequiredTags
+        {
+            get
+            {
+                yield return "winrt";
+                yield return "win8";
+                yield return "windows8";
+                yield return "winjs";
+            }
+        }
+
+        protected override string CuratedFeedName
+        {
+            get { return "windows8-packages"; }
         }
     }
-
-    protected override string CuratedFeedName
-    {
-        get { return "Win8"; }
-    }
-}
 }
