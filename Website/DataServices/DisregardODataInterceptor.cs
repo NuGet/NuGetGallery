1cbda45831076756d915b250b6b46c1f8e927539
Pranav K <prkrishn@hotmail.com>
2012-07-08 15:50:19 -0700

    Modifying feed service to use Lucene search

---
+++ Website/DataServices/DisregardODataInterceptor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace NuGetGallery
+{
+    public class DisregardODataInterceptor : ExpressionVisitor
+    {
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            var methodsToIgnore = new HashSet<string>(new[] { "Take", "Skip", "OrderBy", "ThenBy" }, StringComparer.Ordinal);
+            var method = node.Method;
+            if ((method.DeclaringType == typeof(Queryable)) && methodsToIgnore.Contains(method.Name))
+            {
+                // The expression is of the format Queryable.OrderBy(<Expression>, <Order-by-params>). To avoid performing the 
+                // method, we ignore it, traversing the passed in expression instead.
+                return Visit(node.Arguments[0]);
+            }
+            return base.VisitMethodCall(node);
+        }
+    }
+}
