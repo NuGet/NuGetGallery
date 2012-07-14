d1a54a02928e8ad4841cb00e7eacd466c771ba10
pranavkm <prkrishn@hotmail.com>
2012-07-13 18:18:55 -0700

    * Modifying Lucene to store latest and latest stable packages for   prerelease filtering * Fixing bug where using multiple IndexWriters would lock the index * Improving search perf by adding boosts to fields at index time * Removing the need to recreate index by updating the index when   aggregate download counts are updated.

---
+++ Website/Services/SearchFilter.cs
@@ -0,0 +1,32 @@
+﻿
+namespace NuGetGallery
+{
+    public class SearchFilter
+    {
+        public string SearchTerm { get; set; }
+
+        public int Skip { get; set; }
+
+        public int Take { get; set; }
+
+        public bool IncludePrerelease { get; set; }
+
+        public SortProperty SortProperty { get; set; }
+
+        public SortDirection SortDirection { get; set; }
+    }
+
+    public enum SortProperty
+    {
+        Relevance,
+        DownloadCount,
+        DisplayName,
+        Recent,
+    }
+
+    public enum SortDirection
+    {
+        Descending,
+        Ascending,
+    }
+}
