66b33adb41bc2302dbc6ad67824c44d1cc25b55d
Rob Thijssen <rthijssen@gmail.com>
2012-08-08 17:37:15 +0100

    Implemented UserDisplayViewModel to tidy up the UserDisplay view. (Implemented suggestions from pranavkm (https://github.com/NuGet/NuGetGallery/pull/562))

---
+++ Website/ViewModels/UserDisplayViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NuGetGallery
+{
+    public class UserDisplayViewModel
+    {
+        public bool AllowUserRegistrations
+        {
+            get
+            {
+                return string.IsNullOrWhiteSpace(ConfigurationManager.AppSettings.Get("Ldap:Uri"));
+            }
+        }
+
+        public string UsernameOrDisplayName
+        {
+            get
+            {
+                var user = DependencyResolver.Current.GetService<IUserService>().FindByUsername(HttpContext.Current.User.Identity.Name);
+                return !string.IsNullOrWhiteSpace(user.DisplayName)
+                           ? user.DisplayName
+                           : user.Username;
+            } 
+        } 
+    }
+}
