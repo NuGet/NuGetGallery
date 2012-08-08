39cf07130a369721c79c533f48065b9fd0c7f3b2
pranavkm <prkrishn@hotmail.com>
2012-08-08 15:45:59 -0700

    Fix for #546. Suppress previous animations before executing current

--- Website/Scripts/stats.js
+++ Website/Scripts/stats.js
@@ -1,53 +1,55 @@
-﻿function getStats() {
+﻿function getStats(currData) {
+    currData = currData || {};
     $.get('/Stats', function(data) {
         var section = $('section.aggstats');
         section.show();
-        update(data, 'UniquePackages');
-        update(data, 'Downloads');
-        update(data, 'TotalPackages');
+        update(data, currData, 'UniquePackages');
+        update(data, currData, 'Downloads');
+        update(data, currData, 'TotalPackages');
     })
-    setTimeout(getStats, 30000);
+    setTimeout(function() { getStats(currData) }, 30000);
 }
 
-function update(data, key) {
+function update(data, currData, key) {
+    var currentValue = currData[key] || '';
     var value = data[key].toString();
     var self = $('#' + key);
-    var currentValue = $.trim(self.text().replace(/\s/g, ''));
+    
     if (currentValue != value) {
+        currData[key] = value;
         var length = value.length;
         var currLength = currentValue.length;
         var items = self.children('span');
 
         if (currLength > length) {
-            while (items.length > length) {
-                items.first().remove();
-                items = self.children('span');
-            }
+            items.slice(0, currLength - length).remove();
+            items = items.slice(currLength - length);
         }
-        else if (currLength < length) {
+
+        if (currLength) {
+            // Do not animate the first time around.
+            $.each(value.split('').reverse(), function (i, e) {
+                var c = (i <= currLength) ? currentValue.charAt(currLength - i - 1) : '';
+                if (c != e) {
+                    var el = $(items[length - i - 1]);
+                    animateEl(el, e);
+                }
+            });
+        }
+        if (currLength < length) {
             var i;
             for (i = currLength; i < length; i++) {
                 self.prepend('<span>' + value.charAt(length - i - 1) + '</span>');
             }
             items = self.children('span');
-            currentValue = $.trim(self.text().replace(/\s/g, ''));
-            currLength = currentValue.length;
         }
-
-        $.each(value.split('').reverse(), function (i, e) {
-            var c = (i < currLength) ? currentValue.charAt(currLength - i - 1) : '';
-            if (c != e) {
-                var el = $(items[length - i - 1]);
-                animateEl(el, e);
-            }
-        });
     }
+}
 
-    function animateEl(el, v) {
-        v = v || '';
-        var parent = el.parent();
-        el.animate({ top: 0.3 * parseInt(parent.height()) }, 350, 'linear', function () {
-            $(this).html(v).css({ top: -0.8 * parseInt(parent.height()) }).animate({ top: 0 }, 350, 'linear')
-        });
-    }
+function animateEl(el, v) {
+    v = v || '';
+    var parent = el.parent();
+    el.stop(true, true).animate({ top: 0.3 * parseInt(parent.height()) }, 350, 'linear', function () {
+        $(this).html(v).css({ top: -0.8 * parseInt(parent.height()) }).animate({ top: 0 }, 350, 'linear')
+    });
 }
