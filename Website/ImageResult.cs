b4446dd61c688c93f077dbd925dd0fd36b91464f
dotnetjunky <me@yahoo.com>
2012-10-03 23:08:49 -0700

    Package explorer to the cloud.

--- Website/ImageResult.cs
+++ Website/ImageResult.cs
@@ -1,15 +1,14 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Web;
 using System.Web.Mvc;
 
 namespace NuGetGallery
 {
-
-	public class ImageResult : ActionResult
-	{
-		public ImageResult(Stream imageStream, string contentType)
-		{
+    public class ImageResult : ActionResult
+    {
+        public ImageResult(Stream imageStream, string contentType)
+        {
             if (imageStream == null)
             {
                 throw new ArgumentNullException("imageStream");
@@ -20,37 +19,37 @@ namespace NuGetGallery
                 throw new ArgumentNullException("contentType");
             }
 
-			this.ImageStream = imageStream;
-			this.ContentType = contentType;
-		}
+            this.ImageStream = imageStream;
+            this.ContentType = contentType;
+        }
 
-		public Stream ImageStream { get; private set; }
-		public string ContentType { get; private set; }
+        public Stream ImageStream { get; private set; }
+        public string ContentType { get; private set; }
 
-		public override void ExecuteResult(ControllerContext context)
-		{
+        public override void ExecuteResult(ControllerContext context)
+        {
             if (context == null)
             {
                 throw new ArgumentNullException("context");
             }
 
-			HttpResponseBase response = context.HttpContext.Response;
+            HttpResponseBase response = context.HttpContext.Response;
 
-			response.ContentType = this.ContentType;
+            response.ContentType = this.ContentType;
 
-			byte[] buffer = new byte[4096];
-			while (true)
-			{
-				int read = this.ImageStream.Read(buffer, 0, buffer.Length);
+            byte[] buffer = new byte[4096];
+            while (true)
+            {
+                int read = this.ImageStream.Read(buffer, 0, buffer.Length);
                 if (read == 0)
                 {
                     break;
                 }
 
-				response.OutputStream.Write(buffer, 0, read);
-			}
+                response.OutputStream.Write(buffer, 0, read);
+            }
 
-			response.End();
-		}
-	}
+            response.End();
+        }
+    }
 }
