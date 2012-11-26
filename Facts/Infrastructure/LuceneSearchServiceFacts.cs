3973750f49ea5ba8d8f7f58555c83088904e2f01
Tim Lovell-Smith <tilovell@server>
2012-11-26 11:05:10 -0800

    Search enhancement - make 'Indexing and Search' unit testable. The first unit test is FAILing to find the word 'awesome' in the package description.

---
+++ Facts/Infrastructure/LuceneSearchServiceFacts.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using NuGetGallery.Services;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using Xunit.Extensions;
+
+namespace NuGetGallery.Infrastructure
+{
+    public class LuceneSearchServiceFacts
+    {
+        [Fact]
+        public void IndexAndSearchAPackage()
+        {
+            var packageSource = new Mock<IPackageSource>();
+            packageSource.Setup(x => x.GetPackagesForIndexing(null)).Returns(
+                new List<Package>
+                {
+                    new Package
+                    {
+                        Key = 3,
+                        PackageRegistrationKey = 1,
+                        PackageRegistration = new PackageRegistration
+                        {
+                            Id = "Package #1",
+                        },
+                        Title = "Package #1 4.2.0",
+                        Description = "Package #1 is an awesome package",
+                        Listed = true,
+                        IsLatest = true,
+                        FlattenedAuthors = "",
+                    }
+                }.AsQueryable());
+
+            var luceneIndexingService = new LuceneIndexingService(packageSource.Object);
+            luceneIndexingService.UpdateIndex();
+
+            var luceneSearchService = new LuceneSearchService();
+            int totalHits = 0;
+            var searchFilter = new SearchFilter
+            {
+                Skip = 0,
+                Take = 10,
+                SearchTerm = "awesome",
+            };
+
+            var results = luceneSearchService.Search(
+                packageSource.Object.GetPackagesForIndexing(null), 
+                searchFilter, 
+                out totalHits).ToList();
+
+            Assert.Single(results);
+            Assert.Equal(3, results[0].Key);
+            Assert.Equal(1, results[0].Key);
+        }
+    }
+}
---
+++ Website/Services/PackageSource.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NuGetGallery.Services
+{
+    public class PackageSource : IPackageSource
+    {
+        private readonly IEntityRepository<Package> _packageSet;
+
+        public PackageSource(EntitiesContext entitiesContext)
+        {
+            _packageSet = new EntityRepository<Package>(entitiesContext);
+        }
+
+        [Ninject.Inject]
+        public PackageSource(IEntityRepository<Package> packageRepo)
+        {
+            _packageSet = packageRepo;
+        }
+
+        public IQueryable<Package> GetPackagesForIndexing(DateTime? newerThan)
+        {
+            var collection = _packageSet.GetAll()
+                .Where(x => x.IsLatest || x.IsLatestStable); // which implies that x.IsListed by the way!
+
+            if (newerThan.HasValue)
+            {
+                // Retrieve the Latest and LatestStable version of packages if any package for that registration changed since we last updated the index.
+                // We need to do this because some attributes that we index such as DownloadCount are values in the PackageRegistration table that may
+                // update independent of the package.
+                return collection.Where(x => x.PackageRegistration.Packages.Any(p2 => p2.LastUpdated > newerThan));
+            }
+            else
+            {
+                return collection;
+            }
+        }
+    }
+}
