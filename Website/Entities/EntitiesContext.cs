487f44c2bde4fe114ff97d58bdec1257f7238509
Tim Lovell-Smith <tilovell@server>
2013-02-13 11:46:40 -0800

    Satisfy Code Analysis.

--- Website/Entities/EntitiesContext.cs
+++ Website/Entities/EntitiesContext.cs
@@ -38,7 +38,7 @@ namespace NuGetGallery
         {
             if (ReadOnly)
             {
-                throw new ReadOnlyModeException("SaveChanges unavailable: the gallery is currently in read only mode, with limited service. Please try again later.");
+                throw new ReadOnlyModeException("Save changes unavailable: the gallery is currently in read only mode, with limited service. Please try again later.");
             }
 
             return base.SaveChanges();
