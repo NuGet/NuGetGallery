e2973333743f03bddb3ee6cd75f60bd729dc1867
Tim Lovell-Smith <tilovell@server>
2013-02-15 14:32:08 -0800

    Elmah log now works on top of AzureEntityList.

--- Website/Infrastructure/TableErrorLog.cs
+++ Website/Infrastructure/TableErrorLog.cs
@@ -1,23 +1,73 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using Elmah;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table.DataServices;
+using Microsoft.WindowsAzure.Storage.Table;
 
 namespace NuGetGallery.Infrastructure
 {
-        public class ErrorEntity : TableServiceEntity
+        public class ErrorEntity : ITableEntity
         {
             public string SerializedError { get; set; }
 
+            string ITableEntity.ETag
+            {
+                get;
+                set;
+            }
+
+            string ITableEntity.PartitionKey
+            {
+                get;
+                set;
+            }
+
+            string ITableEntity.RowKey
+            {
+                get;
+                set;
+            }
+
+            DateTimeOffset ITableEntity.Timestamp
+            {
+                get;
+                set;
+            }
+
+            public long LogicalIndex
+            {
+                get
+                {
+                    // Chop off leading "Page_"
+                    long page = Int64.Parse(((ITableEntity)this).PartitionKey.Substring(5));
+                    long offset = Int32.Parse(((ITableEntity)this).RowKey);
+                    return page + offset;
+                }
+            }
+
             public ErrorEntity() { }
+
             public ErrorEntity(Error error)
-                : base(string.Empty, (DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks).ToString("d19", CultureInfo.InvariantCulture))
             {
                 this.SerializedError = ErrorXml.EncodeString(error);
+            }
+
+            void ITableEntity.ReadEntity(IDictionary<string, EntityProperty> properties, Microsoft.WindowsAzure.Storage.OperationContext operationContext)
+            {
+                this.SerializedError = properties["SerializedError"].StringValue;
+            }
+
+            IDictionary<string, EntityProperty> ITableEntity.WriteEntity(OperationContext operationContext)
+            {
+                return new Dictionary<string, EntityProperty>
+                {
+                    { "SerializedError", EntityProperty.GeneratePropertyForString(this.SerializedError) }
+                };
             }
         }
 
@@ -25,71 +75,61 @@ namespace NuGetGallery.Infrastructure
         {
             public const string TableName = "ElmahErrors";
 
-            private string connectionString;
+            private readonly string _connectionString;
+            private readonly AzureEntityList<ErrorEntity> _entityList;
+
+            public TableErrorLog(IDictionary config)
+            {
+                _connectionString = (string)config["connectionString"] ?? RoleEnvironment.GetConfigurationSettingValue((string)config["connectionStringName"]);
+                _entityList = new AzureEntityList<ErrorEntity>(_connectionString, TableName);
+            }
+
+            public TableErrorLog(string connectionString)
+            {
+                _connectionString = connectionString;
+                _entityList = new AzureEntityList<ErrorEntity>(connectionString, TableName);
+            }
+
+            public TableErrorLog()
+            {
+                _entityList = new AzureEntityList<ErrorEntity>(_connectionString, TableName);
+            }
 
             public override ErrorLogEntry GetError(string id)
             {
-                return new ErrorLogEntry(
-                    this, 
-                    id, 
-                    ErrorXml.DecodeString(
-                        CloudStorageAccount.Parse(connectionString)
-                                           .CreateCloudTableClient()
-                                           .GetTableServiceContext()
-                                           .CreateQuery<ErrorEntity>(TableName)
-                                           .Where(e => e.PartitionKey == string.Empty && e.RowKey == id)
-                                           .Single()
-                                           .SerializedError));
+                long pos = Int64.Parse(id, CultureInfo.InvariantCulture);
+                var error = _entityList[pos];
+                Debug.Assert(id == pos.ToString(CultureInfo.InvariantCulture));
+                return new ErrorLogEntry(this, id, ErrorXml.DecodeString(error.SerializedError));
             }
 
             public override int GetErrors(int pageIndex, int pageSize, IList errorEntryList)
             {
-                var count = 0;
-                var errors = CloudStorageAccount.Parse(connectionString)
-                                                .CreateCloudTableClient()
-                                                .GetTableServiceContext()
-                                                .CreateQuery<ErrorEntity>(TableName).Where(e => e.PartitionKey == string.Empty)
-                                                .Take((pageIndex + 1) * pageSize)
-                                                .ToList()
-                                                .Skip(pageIndex * pageSize);
-                foreach (var error in errors)
+                // A little math is required since the AzureEntityList is in ascending order
+                // And we want to retrieve entries in descending order
+                long queryOffset = _entityList.Count64 - ((pageIndex+1) * pageSize);
+                if (queryOffset < 0)
                 {
-                    errorEntryList.Add(new ErrorLogEntry(this, error.RowKey, ErrorXml.DecodeString(error.SerializedError)));
-                    count += 1;
+                    pageSize += (int)queryOffset;
+                    queryOffset = 0;
                 }
-                return count;
+
+                // And since that range was in ascending, flip it to descending.
+                var results = _entityList.GetRange(queryOffset, pageSize).Reverse();
+                foreach (var error in results)
+                {
+                    string id = error.LogicalIndex.ToString(CultureInfo.InvariantCulture);
+                    errorEntryList.Add(new ErrorLogEntry(this, id, ErrorXml.DecodeString(error.SerializedError)));
+                }
+
+                return _entityList.Count;
             }
 
             public override string Log(Error error)
             {
                 var entity = new ErrorEntity(error);
-                var context = CloudStorageAccount.Parse(connectionString)
-                                                 .CreateCloudTableClient()
-                                                 .GetTableServiceContext();
-                context.AddObject(TableName, entity);
-                context.SaveChangesWithRetries();
-                return entity.RowKey;
-            }
-
-            public TableErrorLog(IDictionary config)
-            {
-                connectionString = (string)config["connectionString"] ?? RoleEnvironment.GetConfigurationSettingValue((string)config["connectionStringName"]);
-                Initialize();
-            }
-
-            public TableErrorLog(string connectionString)
-            {
-                this.connectionString = connectionString;
-                Initialize();
-            }
-
-            void Initialize()
-            {
-                CloudStorageAccount.Parse(connectionString)
-                                   .CreateCloudTableClient()
-                                   .GetTableReference(TableName)
-                                   .CreateIfNotExists();
+                long pos = _entityList.Add(entity);
+                return pos.ToString(CultureInfo.InvariantCulture);
             }
         }
-
 }
