73a21ccab74b7d566da694f638c76e11a7acb5d6
anurse <andrew@andrewnurse.net>
2013-02-15 10:59:00 -0800

    Reworked compilation to use the same packaging model as the Gallery.

--- Build-Solution.ps1
+++ Build-Solution.ps1
@@ -1,4 +1,8 @@
-$scriptPath = Split-Path $MyInvocation.MyCommand.Path
-$projFile = join-path $scriptPath DeployScripts\NuGetGalleryOps.msbuild
- 
-& "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $projFile /t:Build
+param(
+    [Parameter(Mandatory=$false)][string]$Configuration = "Release")
+
+$MyPath = Split-Path $MyInvocation.MyCommand.Path
+$ScriptsDir = Join-Path $MyPath Scripts
+
+& $ScriptsDir\Restore-Packages.ps1
+& "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" "$ScriptsDir\NuGetOperations.msbuild" /t:Build /p:"Configuration=$Configuration"
