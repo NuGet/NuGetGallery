1ef20fe38f9828af2473e6abfdfa8f851da54485
bhuvak <bhuvak@llive.com>
2013-03-08 19:18:58 -0800

    Readme
    1. Added Readme.md file to tests.
    2. Updated the RunTests script.

--- FunctionalTests/Scripts/RunSpecificTests.cmd
+++ FunctionalTests/Scripts/RunSpecificTests.cmd
@@ -1,3 +1,12 @@
-set GalleryUrl=%1
-set criteria=%2
-"%VS110COMNTOOLS%..\IDE\mstest.exe" /testcontainer:"..\NuGetGallery.FunctionalTests\bin\Debug\NuGetGallery.FunctionalTests.dll" /testsettings:"..\Local.testsettings" /test:%2
+Set Criteria = %1
+Set GalleryUrl = %2
+
+IF "%Criteria%" == "" (
+ECHO "Specify a search string to filter tests. Example :RunSpecificTests.cmd Download. You can optionally specify the environment that you want to point to as second parameter."
+exit /b
+)
+IF "%GalleryUrl%"=="" (
+Set GalleryUrl=http://nugetgallery-bvts.cloudapp.net/
+) 
+
+"%VS110COMNTOOLS%..\IDE\mstest.exe" /testcontainer:"..\NuGetGallery.FunctionalTests\bin\Debug\NuGetGallery.FunctionalTests.dll" /testsettings:"..\Local.testsettings" /test:"%Criteria%"
