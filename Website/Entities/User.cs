e03b4532af9c0f58d1b39787ee751ccb57d777a5
anurse <andrew@andrewnurse.net>
2013-03-19 11:45:01 -0700

    Revert "Issue #875. Add a DB index to boost performance of the common 'user by authenticated username' query. Limit max username size and other string sizes for Users."
    This reverts commit cc09ad19f5926820975b76c351c53038b1fb202a.

--- Website/Entities/User.cs
+++ Website/Entities/User.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace NuGetGallery
 {
@@ -21,25 +20,12 @@ namespace NuGetGallery
         }
 
         public Guid ApiKey { get; set; }
-
-        [StringLength(256)]
         public string EmailAddress { get; set; }
-
-        [StringLength(256)]
         public string UnconfirmedEmailAddress { get; set; }
-
-        [StringLength(256)]
         public string HashedPassword { get; set; }
-
-        [StringLength(16)]
         public string PasswordHashAlgorithm { get; set; }
-
         public virtual ICollection<EmailMessage> Messages { get; set; }
-
-        [StringLength(64)]
-        [Required]
         public string Username { get; set; }
-
         public virtual ICollection<Role> Roles { get; set; }
         public bool EmailAllowed { get; set; }
 
@@ -48,12 +34,8 @@ namespace NuGetGallery
             get { return !String.IsNullOrEmpty(EmailAddress); }
         }
 
-        [StringLength(32)]
         public string EmailConfirmationToken { get; set; }
-
-        [StringLength(32)]
         public string PasswordResetToken { get; set; }
-
         public DateTime? PasswordResetTokenExpirationDate { get; set; }
         public int Key { get; set; }
 
