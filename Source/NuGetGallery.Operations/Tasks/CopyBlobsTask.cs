c1b8f6022136d14d9a0f6c968b713c6348e30f39
anurse <andrew@andrewnurse.net>
2013-04-03 12:35:37 -0700

    Minor fixes

---
+++ Source/NuGetGallery.Operations/Tasks/CopyBlobsTask.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using NuGetGallery.Operations.Common;
+
+namespace NuGetGallery.Operations
+{
+    [Command("copyblobs", "Copies blobs from a source to a destination", AltName = "cpb")]
+    public class CopyBlobsTask : OpsTask
+    {
+        [Option("Connection string to the source storage server", AltName = "ss")]
+        public CloudStorageAccount SourceStorage { get; set; }
+
+        [Option("Container containing the blobs to copy", AltName = "sc")]
+        public string SourceContainer { get; set; }
+
+        [Option("Connection string to the destination storage server", AltName = "ds")]
+        public CloudStorageAccount DestinationStorage { get; set; }
+
+        [Option("Container to copy the blobs to", AltName = "dc")]
+        public string DestinationContainer { get; set; }
+
+        [Option("Prefix of source blobs to copy. If not specified, copies all blobs", AltName = "p")]
+        public string Prefix { get; set; }
+
+        [Option("If specified, overwrite existing blobs. Otherwise, blobs that exist will be ignored (this is a name check ONLY)", AltName = "w")]
+        public bool Overwrite { get; set; }
+
+        public override void ValidateArguments()
+        {
+            base.ValidateArguments();
+
+            ArgCheck.Required(SourceStorage, "SourceStorage");
+            ArgCheck.Required(SourceContainer, "SourceContainer");
+            ArgCheck.Required(DestinationStorage, "DestinationStorage");
+            ArgCheck.Required(DestinationContainer, "DestinationContainer");
+        }
+
+        public override void ExecuteCommand()
+        {
+            var sourceClient = SourceStorage.CreateCloudBlobClient();
+            var sourceContainer = sourceClient.GetContainerReference(SourceContainer);
+            if (!sourceContainer.Exists())
+            {
+                Log.Warn("No blobs in container!");
+                return;
+            }
+
+            var destClient = DestinationStorage.CreateCloudBlobClient();
+            var destContainer = destClient.GetContainerReference(DestinationContainer);
+            destContainer.CreateIfNotExists();
+
+            Log.Info("Collecting blob names in {0} to copy to {1}", SourceStorage.Credentials.AccountName, DestinationStorage.Credentials.AccountName);
+            var blobs = sourceContainer.ListBlobs(Prefix ?? String.Empty, useFlatBlobListing: true, blobListingDetails: BlobListingDetails.None)
+                                       .OfType<CloudBlockBlob>()
+                                       .ToList();
+            var count = blobs.Count;
+            int index = 0;
+            foreach (var blob in blobs)
+            {
+                index++;
+                var destBlob = destContainer.GetBlockBlobReference(blob.Name);
+
+                var percentage = (int)(((double)index / (double)count) * 100);
+                if (!destBlob.Exists() || Overwrite)
+                {
+                    Log.Info("[{1}/{2}] ({3}%) Started Async Copy of {0}.", blob.Name, index, count, percentage);
+                    if (!WhatIf)
+                    {
+                        destBlob.StartCopyFromBlob(blob);
+                    }
+                }
+                else
+                {
+                    Log.Info("[{1}/{2}] ({3}%) Skipped {0}.", blob.Name, index, count, percentage);
+                }
+            }
+
+            Log.Info("Copies started. Run checkblobcopies with similar parameters to wait on blob copy completion");
+        }
+    }
+}
--- Source/NuGetGallery.Operations/Tasks/SynchronizePackageBackupsTask.cs
+++ Source/NuGetGallery.Operations/Tasks/SynchronizePackageBackupsTask.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
+using NuGetGallery.Operations.Common;
 
 namespace NuGetGallery.Operations
 {
@@ -40,102 +41,20 @@ namespace NuGetGallery.Operations
                     DestinationStorage = CurrentEnvironment.MainStorage;
                 }
             }
-        }
-
-        string DownloadPackageBackupFromSource(string packageBackupBlobFileName)
-        {
-            var cloudClient = SourceStorage.CreateCloudBlobClient();
-
-            var sourcePackageBackupsBlobContainer = Util.GetPackageBackupsBlobContainer(cloudClient);
-
-            var downloadPath = Path.Combine(_tempFolder, packageBackupBlobFileName);
-
-            var blob = sourcePackageBackupsBlobContainer.GetBlockBlobReference(packageBackupBlobFileName);
-            blob.DownloadToFile(downloadPath);
-
-            return downloadPath;
-        }
-
-        IEnumerable<string> GetDestinationPackageBackupBlobFileNames()
-        {
-            var destinationBlobClient = DestinationStorage.CreateCloudBlobClient();
-
-            var destinationPackageBackupsBlobContainer = Util.GetPackageBackupsBlobContainer(destinationBlobClient);
-
-            return destinationPackageBackupsBlobContainer.ListBlobs().Select(bi => bi.Uri.Segments.Last());
-        }
-        
-        IEnumerable<string> GetSourcePackageBackupBlobFileNames()
-        {
-            var sourceBlobClient = SourceStorage.CreateCloudBlobClient();
-
-            var sourcePackageBackupsBlobContainer = Util.GetPackageBackupsBlobContainer(sourceBlobClient);
-
-            return sourcePackageBackupsBlobContainer.ListBlobs().Select(bi => bi.Uri.Segments.Last());
+            ArgCheck.RequiredOrConfig(SourceStorage, "SourceStorage");
+            ArgCheck.RequiredOrConfig(DestinationStorage, "DestinationStorage");
         }
         
         public override void ExecuteCommand()
         {
-            var sourcePackageBackupBlobFileNames = GetSourcePackageBackupBlobFileNames();
-            var destinationPackageBackupBlobFileNames = GetDestinationPackageBackupBlobFileNames();
-
-            Log.Trace("Getting source package backups to sync; this will take some time.");
-            var sourcePackageBackupBlobFileNamesToBackUp = sourcePackageBackupBlobFileNames.Except(destinationPackageBackupBlobFileNames).ToList();
-
-            var totalCount = sourcePackageBackupBlobFileNamesToBackUp.Count;
-            var processedCount = 0;
-            Log.Trace(
-                "Sync'ing {0} package backups from storage account '{1}' to storage account '{2}'.",
-                totalCount,
-                SourceStorage.Credentials.AccountName,
-                DestinationStorage.Credentials.AccountName);
-
-            Parallel.ForEach(sourcePackageBackupBlobFileNamesToBackUp, new ParallelOptions { MaxDegreeOfParallelism = 20 }, packageBackupBlobFileName =>
-            {
-                try
-                {
-                    var downloadPath = DownloadPackageBackupFromSource(packageBackupBlobFileName);
-
-                    UploadPackageBackupToDestination(
-                        packageBackupBlobFileName,
-                        downloadPath);
-
-                    File.Delete(downloadPath);
-
-                    Interlocked.Increment(ref processedCount);
-                    Log.Info(
-                        "Copied package backup '{0}' ({1} of {2}).",
-                        packageBackupBlobFileName,
-                        processedCount,
-                        totalCount);
-
-                }
-                catch (Exception ex)
-                {
-                    Interlocked.Increment(ref processedCount);
-                    Log.Error(
-                        "Error copying package backup '{0}' ({1} of {2}): {3}",
-                        packageBackupBlobFileName,
-                        processedCount,
-                        totalCount,
-                        ex.Message);
-                }
-            });
-        }
-
-        void UploadPackageBackupToDestination(
-            string packageBackupBlobFileName,
-            string downloadPath)
-        {
-            var client = DestinationStorage.CreateCloudBlobClient();
-
-            var destinationPackageBackupsBlobContainer = Util.GetPackageBackupsBlobContainer(client);
-
-            var blob = destinationPackageBackupsBlobContainer.GetBlockBlobReference(packageBackupBlobFileName);
-            
-            blob.UploadFile(downloadPath);
-            blob.Properties.ContentType = "application/zip";
-            blob.SetProperties();
+            new CopyBlobsTask() {
+                SourceStorage = SourceStorage,
+                SourceContainer = "packagebackups",
+                DestinationStorage = DestinationStorage,
+                DestinationContainer = "packagebackups",
+                WhatIf = WhatIf,
+                Overwrite = false
+            }.Execute();
         }
     }
 }
