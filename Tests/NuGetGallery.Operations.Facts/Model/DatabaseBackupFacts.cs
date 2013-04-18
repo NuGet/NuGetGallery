9addd8bdee38762d3fab8b936eb0195e0f9af73e
anurse <andrew@andrewnurse.net>
2013-04-17 17:21:46 -0700

    Started on tests for database backup code

--- Tests/NuGetGallery.Operations.Facts/Model/DatabaseBackupFacts.cs
+++ Tests/NuGetGallery.Operations.Facts/Model/DatabaseBackupFacts.cs
@@ -11,7 +11,31 @@ namespace NuGetGallery.Operations.Model
     {
         public class TheConstructor
         {
-            
+            [Fact]
+            public void GivenAnOldStyleTimeStamp_ItCorrectlyParsesIt()
+            {
+                // Arrange
+                var expected = new DateTimeOffset(new DateTime(2013, 04, 13, 15, 10, 00), TimeSpan.Zero);
+
+                // Act
+                var backup = new DatabaseBackup("server", "Backup_20130413151000");
+
+                // Assert
+                Assert.Equal(expected, backup.Timestamp);
+            }
+
+            [Fact]
+            public void GivenANewStyleTimeStamp_ItCorrectlyParsesIt()
+            {
+                // Arrange
+                var expected = new DateTimeOffset(new DateTime(2013, 04, 13, 15, 10, 00), TimeSpan.Zero);
+
+                // Act
+                var backup = new DatabaseBackup("server", "Backup_2013Apr13_1510_UTC");
+
+                // Assert
+                Assert.Equal(expected, backup.Timestamp);
+            }
         }
     }
 }
