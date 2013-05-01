66a23921589de9ec8176f54788f98bca0da6de1f
anurse <andrew@andrewnurse.net>
2013-05-01 11:07:53 -0700

    Added SemanticVersion ToNormalizedString extension method

--- Website/SemanticVersionExtensions.cs
+++ Website/SemanticVersionExtensions.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Web;
-using Microsoft.Internal.Web.Utils;
 using NuGet;
 
 namespace NuGetGallery
