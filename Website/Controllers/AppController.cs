23459d6968d818fb2136f0497585d146f133e54f
anurse <andrew@andrewnurse.net>
2013-05-07 12:53:25 -0700

    Added command executor to AppController base class

--- Website/Controllers/AppController.cs
+++ Website/Controllers/AppController.cs
@@ -1,13 +1,21 @@
 ﻿using System.Security.Principal;
 using System.Web.Mvc;
+using NuGetGallery.Commands;
 
 namespace NuGetGallery
 {
     public abstract partial class AppController : Controller
     {
+        public CommandExecutor Executor { get; protected set; }
+
         protected virtual IIdentity Identity
         {
             get { return User.Identity; }
+        }
+
+        protected AppController(CommandExecutor executor)
+        {
+            Executor = executor;
         }
 
         protected virtual T GetService<T>()
