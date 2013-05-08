4e2250b141cdb19166ecdae6fb873da5dd20012d
Tim Lovell-Smith <tilovell@server>
2013-05-08 10:37:29 -0700

    Fixes #452 - fix the regexes for usernames and email addresses to be compatible with javascript. Javascript does not support (?i) like .net, so these regex validations fail to load.

--- Website/ViewModels/EditProfileViewModel.cs
+++ Website/ViewModels/EditProfileViewModel.cs
@@ -8,8 +8,7 @@ namespace NuGetGallery
         [StringLength(255)]
         [Display(Name = "Email")]
         [DataType(DataType.EmailAddress)]
-        [RegularExpression(
-            @"(?i)^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$",
+        [RegularExpression(RegisterRequest.EmailValidationRegex,
             ErrorMessage = "This doesn't appear to be a valid email address.")]
         public string EmailAddress { get; set; }
 
