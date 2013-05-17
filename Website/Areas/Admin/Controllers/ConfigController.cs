a5db5071ec2cf8df4a7cc69e45f067b37cabd782
anurse <andrew@andrewnurse.net>
2013-05-17 10:54:08 -0700

    Added config view to admin dashboard

---
+++ Website/Areas/Admin/Controllers/ConfigController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace NuGetGallery.Areas.Admin.Controllers
+{
+    public partial class ConfigController : AdminControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public ConfigController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public virtual ActionResult Index()
+        {
+            var dict = (from p in typeof(IConfiguration).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        where p.CanRead
+                        select p)
+                       .ToDictionary(p => p.Name, p => p.GetValue(_config));
+
+            return View(dict);
+        }
+    }
+}
