d3a2889c1d561edb1adae40de47dbb0a7c5e8aa2
unknown <johtaylo@JTDEVLAP.redmond.corp.microsoft.com>
2013-06-03 17:28:25 -0700

    removed current month from last6months

--- Source/NuGetGallery.Operations/Scripts/DownloadReport_Last6Months.sql
+++ Source/NuGetGallery.Operations/Scripts/DownloadReport_Last6Months.sql
@@ -4,8 +4,13 @@ FROM Fact_Download
 INNER JOIN Dimension_Date ON Dimension_Date.Id = Fact_Download.Dimension_Date_Id
 WHERE Dimension_Date.[Date] >= 
 	DATETIMEFROMPARTS(
-		DATEPART(year, DATEADD(month, -6, GETDATE())),
-		DATEPART(month, DATEADD(month, -6, GETDATE())),
+		DATEPART(year, DATEADD(month, -7, GETDATE())),
+		DATEPART(month, DATEADD(month, -7, GETDATE())),
+		1, 0, 0, 0, 0)
+AND Dimension_Date.[Date] <
+	DATETIMEFROMPARTS(
+		DATEPART(year, GETDATE()),
+		DATEPART(month, GETDATE()),
 		1, 0, 0, 0, 0)
 GROUP BY Dimension_Date.[Year], Dimension_Date.MonthOfYear
 ORDER BY Dimension_Date.[Year], Dimension_Date.MonthOfYear
