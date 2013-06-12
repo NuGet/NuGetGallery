6f8679967d801272b65600a45b33b8334c52e86e
johnataylor <johtaylo@microsoft.com>
2013-06-12 14:39:10 -0700

    initial commit

---
+++ Website/Helpers/PackageHelper.cs
@@ -0,0 +1,11 @@
+﻿
+namespace NuGetGallery
+{
+    public static class PackageHelper
+    {
+        public static string ParseTags(string tags)
+        {
+            return tags.Replace(',', ' ').Replace("  ", " ");
+        }
+    }
+}
