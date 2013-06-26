ff7a510aba4cdf37567075e8d9e57faf4736000d
anurse <andrew@andrewnurse.net>
2013-06-26 11:20:16 -0700

    Added a little JavaScript to reduce spurious contact support requests due to package bugs.

---
+++ Website/Helpers/EnumHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace NuGetGallery.Helpers
+{
+    public static class EnumHelper
+    {
+        private static readonly ConcurrentDictionary<Tuple<Type, string>, string> _descriptionMap = new ConcurrentDictionary<Tuple<Type, string>, string>();
+
+        public static string GetEnumDescription<TEnum>(TEnum value)
+        {
+            return _descriptionMap.GetOrAdd(Tuple.Create(typeof(TEnum), value.ToString()), key =>
+            {
+                FieldInfo fi = typeof(TEnum).GetType().GetField(value.ToString());
+                DescriptionAttribute attr = fi.GetCustomAttribute<DescriptionAttribute>();
+                if (attr != null)
+                {
+                    return attr.Description;
+                }
+                else
+                {
+                    return value.ToString();
+                }
+            });
+        }
+    }
+}
