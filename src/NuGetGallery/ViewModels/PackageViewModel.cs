9380082f7cb6cf260f98eac0a008e4da7034a173
murilogr <t-mrocha@microsoft.com>
2013-07-03 16:25:15 -0700

    Update attribute name to be consistent with database

--- src/NuGetGallery/ViewModels/PackageViewModel.cs
+++ src/NuGetGallery/ViewModels/PackageViewModel.cs
@@ -30,13 +30,13 @@ namespace NuGetGallery
                 && !package.HideLicenseReport)
             {
                 var report = package.LicenseReports.OrderByDescending(r => r.CreatedUtc).FirstOrDefault();
-                SonatypeReportUrl = report.ReportUrl;
+                ReportUrl = report.ReportUrl;
                 LicensesNames = report.Licenses.Select(p => p.Name);
             }
             else
             {
                 HideLicenseReport = true;
-                SonatypeReportUrl = null;
+                ReportUrl = null;
                 LicensesNames = null;
             }
         }
@@ -48,7 +48,7 @@ namespace NuGetGallery
         public string LicenseUrl { get; set; }
         public Boolean HideLicenseReport { get; set; }
         public IEnumerable<string> LicensesNames { get; set; }
-        public string SonatypeReportUrl { get; set; }
+        public string ReportUrl { get; set; }
         public DateTime LastUpdated { get; set; }
         public bool LatestVersion { get; set; }
         public bool LatestStableVersion { get; set; }
