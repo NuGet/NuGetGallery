6347fc3034eac14ec702c84f3eea4c3b1a064827
anurse <andrew@andrewnurse.net>
2013-07-03 12:38:05 -0700

    Fixed FxCop errors

--- Website/Areas/Admin/Models/LuceneInfoModel.cs
+++ Website/Areas/Admin/Models/LuceneInfoModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using NuGetGallery.Configuration;
@@ -19,19 +20,19 @@ namespace NuGetGallery.Areas.Admin.Models
             // Less than a KB?
             if (IndexSize < 1024)
             {
-                return IndexSize.ToString("0") + "b";
+                return IndexSize.ToString("0", CultureInfo.CurrentCulture) + "b";
             }
             // Less than an MB?
             else if (IndexSize < (1024 * 1024))
             {
-                return (IndexSize / (double)1024).ToString("0.00") + "KB";
+                return (IndexSize / (double)1024).ToString("0.00", CultureInfo.CurrentCulture) + "KB";
             }
             // Less than a GB?
             else if (IndexSize < (1024 * 1024 * 1024))
             {
-                return (IndexSize / (double)(1024 * 1024)).ToString("0.00") + "MB";
+                return (IndexSize / (double)(1024 * 1024)).ToString("0.00", CultureInfo.CurrentCulture) + "MB";
             }
-            return (IndexSize / (double)(1024 * 1024 * 1024)).ToString("0.00") + "GB";
+            return (IndexSize / (double)(1024 * 1024 * 1024)).ToString("0.00", CultureInfo.CurrentCulture) + "GB";
         }
     }
 }
