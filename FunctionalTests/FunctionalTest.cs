4caa8c8122dfb49cabbad906597429729eafda36
anurse <andrew@andrewnurse.net>
2013-07-11 14:55:12 -0700

    Stashing

---
+++ FunctionalTests/FunctionalTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.PhantomJS;
+
+namespace FunctionalTests
+{
+    public abstract class FunctionalTest : IDisposable
+    {
+        public IWebDriver Driver { get; set; }
+        public Uri SiteRoot { get; set; }
+
+        public FunctionalTest()
+        {
+            SiteRoot = new Uri("https://www.nuget.org");
+
+            Driver = new PhantomJSDriver();
+        }
+
+        public string Resolve(string relative, string query = null)
+        {
+            return new UriBuilder(SiteRoot)
+            {
+                Path = relative,
+                Query = query
+            }.Uri.AbsoluteUri;
+        }
+
+        public void Dispose()
+        {
+            Driver.Quit();
+        }
+    }
+}
---
+++ FunctionalTests/TheHomePage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using Xunit;
+
+namespace FunctionalTests
+{
+    public class TheHomePage : FunctionalTest
+    {
+        [Fact]
+        public void ShouldRenderSuccessfully()
+        {
+            Driver.Navigate().GoToUrl(SiteRoot);
+            Assert.True(Driver
+                .FindElements(By.TagName("h1"))
+                .Any(e =>
+                    e.Text.Equals("What is NuGet?", StringComparison.Ordinal)));
+        }
+
+        [Fact]
+        public void ShouldHaveNoBrokenImages()
+        {
+            Driver.Navigate().GoToUrl(SiteRoot);
+            Assert.False(Driver
+                .FindElements(By.TagName("img"))
+                .Any(i =>
+                    i.Size.IsEmpty ||
+                    (i.Size.Height == 0 && i.Size.Width == 0)));
+        }
+    }
+}
