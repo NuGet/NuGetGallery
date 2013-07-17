49eb1a3eebca6cab5f102478037fbaa66ce5abd4
anurse <andrew@andrewnurse.net>
2013-07-17 14:47:15 -0700

    Renamed code symbols that reference "Sonatype" to just refer to "License Reports"

--- src/NuGetGallery/ViewModels/PackageViewModel.cs
+++ src/NuGetGallery/ViewModels/PackageViewModel.cs
@@ -29,13 +29,13 @@ namespace NuGetGallery
                 && !package.HideLicenseReport)
             {
                 var report = package.LicenseReports.OrderByDescending(r => r.CreatedUtc).FirstOrDefault();
-                ReportUrl = report.ReportUrl;
+                LicenseReportUrl = report.ReportUrl;
                 LicensesNames = report.Licenses.Select(p => p.Name);
             }
             else
             {
                 HideLicenseReport = true;
-                ReportUrl = null;
+                LicenseReportUrl = null;
                 LicensesNames = null;
             }
         }
@@ -47,7 +47,7 @@ namespace NuGetGallery
         public string LicenseUrl { get; set; }
         public Boolean HideLicenseReport { get; set; }
         public IEnumerable<string> LicensesNames { get; set; }
-        public string ReportUrl { get; set; }
+        public string LicenseReportUrl { get; set; }
         public DateTime LastUpdated { get; set; }
         public bool LatestVersion { get; set; }
         public bool LatestStableVersion { get; set; }
