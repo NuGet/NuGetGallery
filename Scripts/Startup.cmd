b89c6838ee68cad2f689c270e3515fa43ec11a46
anurse <andrew@andrewnurse.net>
2013-07-18 10:01:46 -0700

    Remove old scripts and move new Deployment scripts

--- Scripts/Startup.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Unrestricted -NoProfile -NoLogo "& .\Startup.ps1" >> Startup.out.log
-
-REM Always exit successfully, otherwise Azure might become very sad. And no-one wants a sad Azure.
-exit /b 0
