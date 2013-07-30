1c552535ff9f541bc00c8fd150619798596d7483
anurse <andrew@andrewnurse.net>
2013-07-30 10:55:09 -0700

    Added missing task files

--- Build-Solution.ps1
+++ Build-Solution.ps1
@@ -2,6 +2,6 @@ param($connectionString = "")
 
 $scriptPath = Split-Path $MyInvocation.MyCommand.Path
 
-$projFile = join-path $scriptPath Scripts\NuGetGallery.msbuild
+$projFile = join-path $scriptPath scripts\frontend\NuGetGallery.msbuild
  
-& "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $projFile /t:CIBuild /v:M
+& "$(get-content env:windir)\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" $projFile /t:ReleaseBuild /v:M
---
+++ src/NuGetGallery.Operations/Tasks/Backups/BackupPackageFileTask.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using NuGetGallery.Operations.Common;
+
+namespace NuGetGallery.Operations
+{
+    [Command("backuppackagefile", "Back up a specific package file", AltName = "bpf", MaxArgs = 0)]
+    public class BackupPackageFileTask : PackageVersionTask
+    {
+        public override void ExecuteCommand()
+        {
+            var blobClient = CreateBlobClient();
+
+            var packageBackupsBlobContainer = Util.GetPackageBackupsBlobContainer(blobClient);
+            var backupFileName = Util.GetPackageBackupFileName(
+                PackageId,
+                PackageVersion,
+                PackageHash);
+            var backupPackageBlob = packageBackupsBlobContainer.GetBlockBlobReference(backupFileName);
+            if (backupPackageBlob.Exists())
+            {
+                Log.Info("Skipped {0} {1}: backup already exists", PackageId, PackageVersion);
+                return;
+            }
+
+            var packagesBlobContainer = Util.GetPackagesBlobContainer(blobClient);
+            var packageFileBlob = Util.GetPackageFileBlob(
+                packagesBlobContainer,
+                PackageId,
+                PackageVersion);
+            var packageFileName = Util.GetPackageFileName(
+                PackageId,
+                PackageVersion);
+            var downloadedPackageFilePath = Path.Combine(Util.GetTempFolder(), packageFileName);
+
+            Log.Trace("Downloading package file '{0}' to temporary file '{1}'.", packageFileName, downloadedPackageFilePath);
+            if (!WhatIf)
+            {
+                packageFileBlob.DownloadToFile(downloadedPackageFilePath);
+            }
+
+            Log.Trace("Uploading package file backup '{0}' from temporary file '{1}'.", backupFileName, downloadedPackageFilePath);
+            if (!WhatIf)
+            {
+                backupPackageBlob.UploadFile(downloadedPackageFilePath);
+                backupPackageBlob.Properties.ContentType = "application/zip";
+                backupPackageBlob.SetProperties();
+            }
+
+            Log.Trace("Deleting temporary file '{0}'.", downloadedPackageFilePath);
+            if (!WhatIf)
+            {
+                File.Delete(downloadedPackageFilePath);
+            }
+            Log.Info("Backed Up {0} {1}", PackageId, PackageVersion);
+        }
+    }
+}
