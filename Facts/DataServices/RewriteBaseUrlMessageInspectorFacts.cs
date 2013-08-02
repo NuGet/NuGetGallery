b964ee8aa9731b6d111ed0c31e8836650f0acba9
anurse <andrew@andrewnurse.net>
2013-08-02 11:38:37 -0700

    Add unit tests and more robust replacement logic

---
+++ Facts/DataServices/RewriteBaseUrlMessageInspectorFacts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace NuGetGallery
+{
+    public class RewriteBaseUrlMessageInspectorFacts
+    {
+        public class TheRewriteUrlPathMethod
+        {
+            [Theory]
+            [InlineData("/api/v2/curated-feed", "/api/v2/curated-feeds/foo", "foo")]
+            [InlineData("/api/v2/curated-feed/Packages", "/api/v2/curated-feeds/foo/Packages", "foo")]
+            [InlineData("/api/v2/curated-feed/Packages/jQuery/abc/123", "/api/v2/curated-feeds/foo/Packages/jQuery/abc/123", "foo")]
+            [InlineData("/api/v2/curated-feed/", "/api/v2/curated-feeds/foo/", "foo")]
+            [InlineData("/api/v2/curated-feed/Packages/", "/api/v2/curated-feeds/foo/Packages/", "foo")]
+            [InlineData("/api/v2/curated-feed/Packages/jQuery/abc/123/", "/api/v2/curated-feeds/foo/Packages/jQuery/abc/123/", "foo")]
+            public void CorrectlyRewritesURLsToCuratedFeeds(string start, string rewritten, string curatedFeedName)
+            {
+                Assert.Equal(rewritten, RewriteBaseUrlMessageInspector.RewriteUrlPath(start, curatedFeedName));
+            }
+        }
+    }
+}
