54ba0293979e535f1850504e6699cfeed7afb40f
anurse <andrew@andrewnurse.net>
2013-08-05 16:49:46 -0700

    Restored views and got the site to run

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,17 +4,12 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Outercurve Foundation")]
 [assembly: AssemblyProduct("NuGet Gallery")]
 [assembly: AssemblyCopyright("\x00a9 Outercurve Foundation. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
-[assembly: AssemblyInformationalVersion("0.3.0-rc")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
