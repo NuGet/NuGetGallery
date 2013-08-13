4331af987de56f988f242e7e1c00064b6269aa2e
anurse <andrew@andrewnurse.net>
2013-08-13 14:06:22 -0700

    Stashing stuff

--- src/NuGetGallery.Core/Entities/PackageLicense.cs
+++ src/NuGetGallery.Core/Entities/PackageLicense.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
-namespace NuGetGallery.Entities
+namespace NuGetGallery
 {
     public class PackageLicense
     {
---
+++ src/NuGetGallery/Configuration/FeatureConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace NuGetGallery.Configuration
+{
+    public class FeatureConfiguration
+    {
+        /// <summary>
+        /// Gets a boolean indicating if license reports are enabled.
+        /// </summary>
+        [DefaultValue(false)] // Default: Disabled
+        [DisplayName("Friendly License Names")]
+        [Description("Displays reports on license data")]
+        public bool FriendlyLicenses { get; private set; }
+    }
+}
