59e82239c2eea81a5c25c30bb9d79ab00e250251
Tim Lovell-Smith <tilovell@server>
2013-08-22 12:52:20 -0700

    Re-use the DataServiceHostFactory instead of creating and maintaining several instances, since it seems to have no interesting stateful behavior.

--- src/NuGetGallery/Diagnostics/NinjectGlimpseServiceLocator.cs
+++ src/NuGetGallery/Diagnostics/NinjectGlimpseServiceLocator.cs
@@ -4,7 +4,6 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Web;
 using Glimpse.Core.Configuration;
 using Glimpse.Core.Framework;
 using Ninject;
