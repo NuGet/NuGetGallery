e3cec2fe9c23d81c53f2f2bbb9a83201ced58be2
Tim Lovell-Smith <tilovell@server>
2013-08-22 12:26:53 -0700

    Manually Reimplement glimpse reflection to only look for extensions in Gliimpse*.dll not *.dll to cut down on assembly reflection costs at app startup.

--- src/NuGetGallery/Diagnostics/NinjectGlimpseServiceLocator.cs
+++ src/NuGetGallery/Diagnostics/NinjectGlimpseServiceLocator.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Web;
+using Glimpse.Core.Configuration;
 using Glimpse.Core.Framework;
 using Ninject;
 
@@ -15,11 +19,56 @@ namespace NuGetGallery.Diagnostics
 
             // Glimpse interprets an empty collection to mean: I'm overriding your defaults and telling you NOT to load anythig
             // However, we want an empty collection to indicate to Glimpse that it should use the default. Returning null does that.
-            if (!ninjectResources.Any())
+            if (ninjectResources.Any())
             {
-                return null;
+                return ninjectResources;
             }
-            return ninjectResources;
+
+            var ignoredTypes = new HashSet<Type>();
+            var configSection = ConfigurationManager.GetSection("glimpse") as Section;
+            if (configSection != null)
+            {
+                foreach (var t in configSection.RuntimePolicies.IgnoredTypes.Cast<TypeElement>().Select(te => te.Type))
+                {
+                    ignoredTypes.Add(t); 
+                }
+            }
+
+            string dir = configSection.DiscoveryLocation;
+            if (String.IsNullOrEmpty(dir))
+            {
+                var setupInformation = AppDomain.CurrentDomain.SetupInformation;
+                if (!string.Equals(setupInformation.ShadowCopyFiles, "true", StringComparison.OrdinalIgnoreCase))
+                {
+                    dir = AppDomain.CurrentDomain.BaseDirectory;
+                }
+                else
+                {
+                    dir = Path.Combine(setupInformation.CachePath, setupInformation.ApplicationName);
+                }
+            }
+
+            // "To eliminate all start up assembly scanning you'll want to return collections when GetAllInstances<T>() is called with a T of: IClientScript, IInspector, IResource, ITab, IDisplay, IRuntimePolicy and ISerializationConverter."
+            var results = new List<T>();
+            foreach (string f in Directory.GetFiles(dir, "Glimpse*.dll", SearchOption.AllDirectories))
+            {
+                var assembly = Assembly.LoadFrom(f);
+                Type[] types;
+                try
+                {
+                    types = assembly.GetTypes();
+                }
+                catch (ReflectionTypeLoadException exception)
+                {
+                    types = exception.Types.Where(x => x != null).ToArray();
+                }
+
+                results.AddRange(types
+                    .Where(t => ((typeof(T).IsAssignableFrom(t) && !t.IsInterface) && !t.IsAbstract) && !ignoredTypes.Contains(t))
+                    .Select(t => (T)Activator.CreateInstance(t)));
+            }
+
+            return results;
         }
 
         public T GetInstance<T>() where T : class
