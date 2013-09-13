ac8561953265f28a8a1a3cb5264f1c31c935e420
anurse <andrew@andrewnurse.net>
2013-09-13 15:54:25 -0700

    Clean up JsonApiController facts

---
+++ tests/NuGetGallery.Facts/Framework/Fakes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetGallery.Framework
+{
+    public static class Fakes
+    {
+        public static readonly FakeUser User = new FakeUser("testUser");
+        public static readonly FakeUser Admin = new FakeUser("testAdmin", Constants.AdminRoleName);
+        public static readonly FakeUser Owner = new FakeUser("testPackageOwner");
+        public static readonly PackageRegistration Package = new PackageRegistration()
+        {
+            Id = "FakePackage",
+            Owners = new List<User>() { Owner.User },
+            Packages = new List<Package>() {
+                new Package() { Version = "1.0" },
+                new Package() { Version = "2.0" }
+            }
+        };
+
+        public class FakeUser {
+            public string UserName { get; private set; }
+            public IIdentity Identity { get; private set; }
+            public IPrincipal Principal { get; private set; }
+            public User User { get; private set; }
+
+            public FakeUser(string userName, params string[] roles) {
+                UserName = userName;
+                Identity = new GenericIdentity(UserName);
+                Principal = new GenericPrincipal(Identity, roles);
+                User = new User()
+                {
+                    Username = UserName
+                };
+            }
+        }
+    }
+}
---
+++ tests/NuGetGallery.Facts/Framework/UnitTestBindings.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Moq;
+using Ninject;
+using Ninject.Activation;
+using Ninject.MockingKernel;
+using Ninject.MockingKernel.Moq;
+using Ninject.Modules;
+using Ninject.Planning.Bindings;
+
+namespace NuGetGallery.Framework
+{
+    internal class UnitTestBindings : NinjectModule
+    {
+        internal static IKernel CreateContainer()
+        {
+            return new TestKernel(new UnitTestBindings());
+        }
+
+        public override void Load()
+        {
+            Bind<HttpContextBase>()
+                .ToMethod(_ =>
+                {
+                    var mockContext = new Mock<HttpContextBase>();
+                    mockContext.Setup(c => c.User).Returns(Fakes.User.Principal);
+                    mockContext.Setup(c => c.Request.Url).Returns(new Uri("https://nuget.local/"));
+                    mockContext.Setup(c => c.Request.ApplicationPath).Returns("/");
+                    mockContext.Setup(c => c.Response.ApplyAppPathModifier(It.IsAny<string>())).Returns<string>(s => s);
+                    return mockContext.Object;
+                })
+                .InSingletonScope();
+
+            Bind<IPackageService>()
+                .ToMethod(_ =>
+                {
+                    var mockService = new Mock<IPackageService>();
+                    mockService
+                        .Setup(p => p.FindPackageRegistrationById(Fakes.Package.Id))
+                        .Returns(Fakes.Package);
+                    return mockService.Object;
+                })
+                .InSingletonScope();
+
+            Bind<IUserService>()
+                .ToMethod(_ =>
+                {
+                    var mockService = new Mock<IUserService>();
+                    mockService.Setup(u => u.FindByUsername(Fakes.User.UserName)).Returns(Fakes.User.User);
+                    mockService.Setup(u => u.FindByUsername(Fakes.Owner.UserName)).Returns(Fakes.Owner.User);
+                    mockService.Setup(u => u.FindByUsername(Fakes.Admin.UserName)).Returns(Fakes.Admin.User);
+                    return mockService.Object;
+                });
+        }
+
+        private class TestKernel : MoqMockingKernel
+        {
+            public TestKernel(params NinjectModule[] modules) : base(new NinjectSettings(), modules) { }
+        }
+    }
+}
