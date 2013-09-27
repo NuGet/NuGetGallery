39a31f45eae3db91eeae9f8fdf63f54e1a65f5e9
johnataylor <johtaylo@microsoft.com>
2013-09-26 17:11:10 -0700

    analyzers and new camel case split

---
+++ src/NuGetGallery.Core/Search/TokenizingHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NuGetGallery
+{
+    public class TokenizingHelper
+    {
+        public static IEnumerable<string> CamelCaseSplit(string term)
+        {
+            if (term.Length == 0)
+            {
+                yield break;
+            }
+
+            if (term.Length == 1)
+            {
+                yield return term;
+                yield break;
+            }
+
+            string word = term[0].ToString();
+            bool lastIsUpper = Char.IsUpper(term[0]);
+
+            for (int i = 1; i < term.Length; i++)
+            {
+                bool currentIsUpper = Char.IsUpper(term[i]);
+
+                if (lastIsUpper == false && currentIsUpper == true)
+                {
+                    yield return word;
+                    word = string.Empty;
+                }
+
+                word += term[i];
+
+                lastIsUpper = currentIsUpper;
+            }
+
+            yield return word;
+            yield break;
+        }
+    }
+}
