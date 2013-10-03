9acf91fd181ae471f0052e01b93737ddf90f77a3
johnataylor <johtaylo@microsoft.com>
2013-10-03 16:51:34 -0700

    version analyzer

---
+++ src/NuGetGallery.Core/Search/VersionAnalyzer.cs
@@ -0,0 +1,13 @@
+﻿using Lucene.Net.Analysis;
+using System.IO;
+
+namespace NuGetGallery
+{
+    public class VersionAnalyzer : Analyzer
+    {
+        public override TokenStream TokenStream(string fieldName, TextReader reader)
+        {
+            return new SemanticVersionFilter(new KeywordTokenizer(reader));
+        }
+    }
+}
