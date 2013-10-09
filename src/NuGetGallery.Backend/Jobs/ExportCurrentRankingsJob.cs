6ddec0e7cac57d01bc29996ced0418b20eac1296
johnataylor <johtaylo@microsoft.com>
2013-10-08 17:55:25 -0700

    new export current rankings task and job

---
+++ src/NuGetGallery.Backend/Jobs/ExportCurrentRankingsJob.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Data.SqlClient;
+using NuGetGallery.Operations;
+
+namespace NuGetGallery.Backend.Jobs
+{
+    [Export(typeof(WorkerJob))]
+    public class ExportCurrentRankingsJob : WorkerJob
+    {
+        public override TimeSpan Period
+        {
+            get
+            {
+                return TimeSpan.FromHours(12);
+            }
+        }
+
+        public override void RunOnce()
+        {
+            Logger.Info("Starting export current rankings task.");
+            ExecuteTask(new ExportCurrentRankingsTask
+            {
+                ConnectionString = new SqlConnectionStringBuilder(Settings.WarehouseConnectionString),
+                StorageAccount = Settings.MainStorage,
+                WhatIf = Settings.WhatIf
+            });
+            Logger.Info("Finished export current rankings task.");
+        }
+    }
+}
---
+++ src/NuGetGallery.Operations/Scripts/Ranking_Overall.sql
@@ -0,0 +1,12 @@
+﻿
+SELECT Dimension_Package.PackageId, SUM(DownloadCount) 'Downloads'
+FROM Fact_Download
+INNER JOIN Dimension_Package ON Dimension_Package.Id = Fact_Download.Dimension_Package_Id
+INNER JOIN Dimension_Date ON Dimension_Date.Id = Fact_Download.Dimension_Date_Id
+INNER JOIN Dimension_Operation ON Dimension_Operation.Id = Fact_Download.Dimension_Operation_Id
+WHERE Dimension_Date.[Date] >= CONVERT(DATE, DATEADD(day, -42, GETDATE()))
+  AND Dimension_Date.[Date] < CONVERT(DATE, GETDATE())
+  AND Dimension_Package.PackageListed = 1
+  AND Dimension_Operation.Operation = 'Install'
+GROUP BY Dimension_Package.PackageId
+ORDER BY SUM(DownloadCount) DESC
