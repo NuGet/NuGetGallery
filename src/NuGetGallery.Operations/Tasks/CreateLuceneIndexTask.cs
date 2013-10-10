041d67e38e111fb31236febeeda00006e8edf985
johnataylor <johtaylo@microsoft.com>
2013-10-10 12:18:02 -0700

    rebase and added Index Task stub

---
+++ src/NuGetGallery.Operations/Tasks/CreateLuceneIndexTask.cs
@@ -0,0 +1,21 @@
+﻿using NuGetGallery.Operations.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Threading;
+
+namespace NuGetGallery.Operations
+{
+    [Command("createluceneindex", "Create the Lucene Index from the Gallery database", IsSpecialPurpose = false)]
+    public class CreateLuceneIndexTask : DatabaseTask
+    {
+        public override void ExecuteCommand()
+        {
+            Log.Info("Create Lucene Index begin");
+
+            Log.Info("Create Lucene Index end");
+        }
+    }
+}
