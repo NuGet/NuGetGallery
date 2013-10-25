7b995d061e8f65835b65cb4ce5569c93f49c577d
johnataylor <johtaylo@microsoft.com>
2013-10-25 12:27:27 -0700

    delete and index analysis

---
+++ src/NuGetGallery.Core/Search/IndexAnalyzer.cs
@@ -0,0 +1,55 @@
+﻿using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace NuGetGallery
+{
+    public static class IndexAnalyzer
+    {
+        public static string Analyze(PackageSearcherManager searcherManager)
+        {
+            if ((DateTime.UtcNow - searcherManager.WarmTimeStampUtc) > TimeSpan.FromMinutes(1))
+            {
+                searcherManager.MaybeReopen();
+            }
+
+            IndexSearcher searcher = searcherManager.Get();
+
+            try
+            {
+                IndexReader indexReader = searcher.IndexReader;
+
+                JObject report = new JObject();
+                report.Add("NumDocs", indexReader.NumDocs());
+
+                if (indexReader.CommitUserData != null)
+                {
+                    JObject commitUserdata = new JObject();
+                    foreach (KeyValuePair<string, string> userData in indexReader.CommitUserData)
+                    {
+                        commitUserdata.Add(userData.Key, userData.Value);
+                    }
+                    report.Add("CommitUserData", commitUserdata);
+                }
+
+                JArray segments = new JArray();
+                foreach (ReadOnlySegmentReader segmentReader in indexReader.GetSequentialSubReaders())
+                {
+                    JObject segmentInfo = new JObject();
+                    segmentInfo.Add("segment", segmentReader.SegmentName);
+                    segmentInfo.Add("documents", segmentReader.NumDocs());
+                    segments.Add(segmentInfo);
+                }
+                report.Add("Segments", segments);
+
+                return report.ToString();
+            }
+            finally
+            {
+                searcherManager.Release(searcher);
+            }
+        }
+    }
+}
