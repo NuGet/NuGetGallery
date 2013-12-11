229c373d307965d1d1ea1870e047cd0c87265b3c
anurse <andrew@andrewnurse.net>
2013-12-10 19:47:55 -0800

    Removed build-fix hacks so I have to actually fix the issues later

--- src/Services/Jobs/NuGet.Services.Jobs.Facts/Infrastructure/JobDispatcherFacts.cs
+++ src/Services/Jobs/NuGet.Services.Jobs.Facts/Infrastructure/JobDispatcherFacts.cs
@@ -1,74 +1,74 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Moq;
-//using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
 
-//namespace NuGet.Services.Jobs
-//{
-//    public class JobDispatcherFacts
-//    {
-//        public class TheDispatchMethod
-//        {
-//            [Fact]
-//            public async Task GivenNoJobWithName_ItThrowsUnknownJobException()
-//            {
-//                // Arrange
-//                var dispatcher = new JobDispatcher(Enumerable.Empty<JobDescription>());
-//                var invocation = new Invocation(Guid.NewGuid(), "flarg", "test", new Dictionary<string, string>());
-//                var context = new InvocationContext(new InvocationRequest(invocation), queue: null, logCapture: null);
+namespace NuGet.Services.Jobs
+{
+    public class JobDispatcherFacts
+    {
+        public class TheDispatchMethod
+        {
+            [Fact]
+            public async Task GivenNoJobWithName_ItThrowsUnknownJobException()
+            {
+                // Arrange
+                var dispatcher = new JobDispatcher(Enumerable.Empty<JobDescription>());
+                var invocation = new Invocation(Guid.NewGuid(), "flarg", "test", new Dictionary<string, string>());
+                var context = new InvocationContext(new InvocationRequest(invocation), queue: null, logCapture: null);
 
-//                // Act/Assert
-//                var ex = await AssertEx.Throws<UnknownJobException>(() => dispatcher.Dispatch(context));
-//                Assert.Equal("flarg", ex.JobName);
-//            }
+                // Act/Assert
+                var ex = await AssertEx.Throws<UnknownJobException>(() => dispatcher.Dispatch(context));
+                Assert.Equal("flarg", ex.JobName);
+            }
 
-//            [Fact]
-//            public async Task GivenJobWithName_ItCreatesAnInvocationAndInvokesJob()
-//            {
-//                // Arrange
-//                var jobImpl = new Mock<JobBase>();
-//                var job = new JobDescription("test", "blarg", () => jobImpl.Object);
+            [Fact]
+            public async Task GivenJobWithName_ItCreatesAnInvocationAndInvokesJob()
+            {
+                // Arrange
+                var jobImpl = new Mock<JobBase>();
+                var job = new JobDescription("test", "blarg", () => jobImpl.Object);
 
-//                var dispatcher = new JobDispatcher(new[] { job });
-//                var invocation = new Invocation(Guid.NewGuid(), "Test", "test", new Dictionary<string, string>());
-//                var context = new InvocationContext(new InvocationRequest(invocation), queue: null, logCapture: null);
+                var dispatcher = new JobDispatcher(new[] { job });
+                var invocation = new Invocation(Guid.NewGuid(), "Test", "test", new Dictionary<string, string>());
+                var context = new InvocationContext(new InvocationRequest(invocation), queue: null, logCapture: null);
 
-//                jobImpl.Setup(j => j.Invoke(It.IsAny<InvocationContext>()))
-//                   .Returns(Task.FromResult(InvocationResult.Completed()));
+                jobImpl.Setup(j => j.Invoke(It.IsAny<InvocationContext>()))
+                   .Returns(Task.FromResult(InvocationResult.Completed()));
 
 
-//                // Act
-//                var result = await dispatcher.Dispatch(context);
+                // Act
+                var result = await dispatcher.Dispatch(context);
 
-//                // Assert
-//                Assert.Equal(InvocationStatus.Completed, result.Status);
-//            }
+                // Assert
+                Assert.Equal(InvocationStatus.Completed, result.Status);
+            }
 
-//            [Fact]
-//            public async Task GivenJobWithName_ItReturnsResponseContainingInvocationAndResult()
-//            {
-//                // Arrange
-//                var jobImpl = new Mock<JobBase>();
-//                var job = new JobDescription("test", "blarg", () => jobImpl.Object);
+            [Fact]
+            public async Task GivenJobWithName_ItReturnsResponseContainingInvocationAndResult()
+            {
+                // Arrange
+                var jobImpl = new Mock<JobBase>();
+                var job = new JobDescription("test", "blarg", () => jobImpl.Object);
                 
-//                var ex = new Exception();
-//                var dispatcher = new JobDispatcher(new[] { job });
-//                var invocation = new Invocation(Guid.NewGuid(), "Test", "test", new Dictionary<string, string>());
-//                var context = new InvocationContext(new InvocationRequest(invocation), queue: null, logCapture: null);
+                var ex = new Exception();
+                var dispatcher = new JobDispatcher(new[] { job });
+                var invocation = new Invocation(Guid.NewGuid(), "Test", "test", new Dictionary<string, string>());
+                var context = new InvocationContext(new InvocationRequest(invocation), queue: null, logCapture: null);
 
-//                jobImpl.Setup(j => j.Invoke(It.IsAny<InvocationContext>()))
-//                   .Returns(Task.FromResult(InvocationResult.Completed()));
+                jobImpl.Setup(j => j.Invoke(It.IsAny<InvocationContext>()))
+                   .Returns(Task.FromResult(InvocationResult.Completed()));
 
-//                // Act
-//                var result = await dispatcher.Dispatch(context);
+                // Act
+                var result = await dispatcher.Dispatch(context);
 
-//                // Assert
-//                Assert.Equal(InvocationStatus.Completed, result.Status);
-//                Assert.Null(result.Exception);
-//            }
-//        }
-//    }
-//}
+                // Assert
+                Assert.Equal(InvocationStatus.Completed, result.Status);
+                Assert.Null(result.Exception);
+            }
+        }
+    }
+}
