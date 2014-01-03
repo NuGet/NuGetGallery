28fd9030e7931d7127ee070d140162da9def6199
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-01-03 16:55:37 +0900

    Fix duplicated BOMs.

--- src/NuGetGallery.Operations/Tasks/BlobBackupFailedException.cs
+++ src/NuGetGallery.Operations/Tasks/BlobBackupFailedException.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 
 namespace NuGetGallery.Operations.Tasks
 {
--- tests/NuGetGallery.Facts/TestUtils/MockExtensions.cs
+++ tests/NuGetGallery.Facts/TestUtils/MockExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Moq.Language.Flow;
-﻿using System.Threading.Tasks;
-﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using System.Linq;
 using Moq;
 using NuGetGallery.Authentication;
