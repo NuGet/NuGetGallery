7cf25a7cb4b7061093a9f600f117bca58a4d4a2d
johnataylor <johtaylo@microsoft.com>
2014-03-10 18:59:18 -0700

    added pack cmd

--- src/MetadataClient/Arguments.cs
+++ src/MetadataClient/Arguments.cs
@@ -3,6 +3,11 @@ using System.IO;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using PowerArgs;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.IO.Packaging;
+using System.Net.Mime;
 
 namespace MetadataClient
 {
@@ -66,6 +71,43 @@ namespace MetadataClient
         public void RenameOwner(RenameOwnerArgs args)
         {
             throw new NotImplementedException();
+        }
+
+        [ArgActionMethod]
+        public void Pack(PackArgs args)
+        {
+            string filename = args.Nuspec;
+
+            FileStream stream = new FileStream(filename, FileMode.Open);
+            XDocument nuspec = XDocument.Load(stream);
+
+            IXmlNamespaceResolver resolver = CreateNamespaceResolver();
+
+            string id = nuspec.XPathSelectElement("nuget:package/nuget:metadata/nuget:id", resolver).Value;
+            string version = nuspec.XPathSelectElement("nuget:package/nuget:metadata/nuget:version", resolver).Value;
+
+            Uri partUri = PackUriHelper.CreatePartUri(new Uri("/" + id + ".nuspec", UriKind.Relative));
+
+            string packagePath = id + "." + version + ".nupkg";
+
+            using (Package package = Package.Open(packagePath, FileMode.Create))
+            {
+                PackagePart packagePart = package.CreatePart(partUri, MediaTypeNames.Text.Xml);
+
+                using (XmlWriter writer = XmlWriter.Create(packagePart.GetStream(), new XmlWriterSettings { Indent = true }))
+                {
+                    nuspec.WriteTo(writer);
+                }
+            }
+        }
+
+        static string Nuget = "http://schemas.microsoft.com/packaging/2012/06/nuspec.xsd";
+
+        static IXmlNamespaceResolver CreateNamespaceResolver()
+        {
+            XmlNamespaceManager namespaceManager = new XmlNamespaceManager(new NameTable());
+            namespaceManager.AddNamespace("nuget", Nuget);
+            return namespaceManager;
         }
     }
 }
