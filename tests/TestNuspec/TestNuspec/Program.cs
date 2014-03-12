d89133039cb7740fe5e5399e24d34112c064f512
johnataylor <johtaylo@microsoft.com>
2014-03-11 18:34:47 -0700

    add testnuspec

---
+++ tests/TestNuspec/TestNuspec/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNuspec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
