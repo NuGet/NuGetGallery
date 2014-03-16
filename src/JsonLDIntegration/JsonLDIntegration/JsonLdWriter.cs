a5aed6f51045bfa34d066cfdef9000b1c92d777c
johnataylor <johtaylo@microsoft.com>
2014-03-15 22:52:33 -0700

    added support for RDF lists

--- src/JsonLDIntegration/JsonLDIntegration/JsonLdWriter.cs
+++ src/JsonLDIntegration/JsonLDIntegration/JsonLdWriter.cs
@@ -1,7 +1,9 @@
 ﻿using JsonLD.Core;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using VDS.RDF;
 
 namespace JsonLDIntegration
@@ -45,8 +47,56 @@ namespace JsonLDIntegration
 
         public event RdfWriterWarning Warning;
 
+        const string First = "http://www.w3.org/1999/02/22-rdf-syntax-ns#first";
+        const string Rest = "http://www.w3.org/1999/02/22-rdf-syntax-ns#rest";
+        const string Nil = "http://www.w3.org/1999/02/22-rdf-syntax-ns#nil";
+
+        static IDictionary<INode, List<INode>> GetLists(IGraph graph)
+        {
+            INode first = graph.CreateUriNode(new Uri(First));
+            INode rest = graph.CreateUriNode(new Uri(Rest));
+            INode nil = graph.CreateUriNode(new Uri(Nil));
+
+            IEnumerable<Triple> ends = graph.GetTriplesWithPredicateObject(rest, nil);
+
+            IDictionary<INode, List<INode>> lists = new Dictionary<INode, List<INode>>();
+
+            foreach (Triple end in ends)
+            {
+                List<INode> list = new List<INode>();
+
+                Triple iterator = graph.GetTriplesWithSubjectPredicate(end.Subject, first).First();
+
+                INode head = iterator.Subject;
+
+                while (true)
+                {
+                    list.Add(iterator.Object);
+
+                    IEnumerable<Triple> restTriples = graph.GetTriplesWithPredicateObject(rest, iterator.Subject);
+
+                    if (restTriples.Count() == 0)
+                    {
+                        break;
+                    }
+
+                    iterator = graph.GetTriplesWithSubjectPredicate(restTriples.First().Subject, first).First();
+
+                    head = iterator.Subject;
+                }
+
+                list.Reverse();
+
+                lists.Add(head, list);
+            }
+
+            return lists;
+        }
+
         JToken MakeExpandedForm(IGraph graph)
         {
+            IDictionary<INode, List<INode>> lists = GetLists(graph);
+
             IDictionary<string, JObject> subjects = new Dictionary<string, JObject>();
 
             foreach (Triple triple in graph.Triples)
@@ -85,7 +135,14 @@ namespace JsonLDIntegration
                 }
                 else
                 {
-                    objects.Add(MakeObject(triple.Object));
+                    if (lists.ContainsKey(triple.Object))
+                    {
+                        objects.Add(MakeList(lists[triple.Object]));
+                    }
+                    else
+                    {
+                        objects.Add(MakeObject(triple.Object));
+                    }
                 }
             }
 
@@ -96,6 +153,18 @@ namespace JsonLDIntegration
             }
 
             return result;
+        }
+
+        static JToken MakeList(List<INode> nodes)
+        {
+            JArray list = new JArray();
+
+            foreach (INode node in nodes)
+            {
+                list.Add(MakeObject(node));
+            }
+
+            return new JObject { { "@list", list } };
         }
 
         static JToken MakeObject(INode node)
