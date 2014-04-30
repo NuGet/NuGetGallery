273ca17a5ce41df68f5b8c7b24d9370d895a6dd9
johnataylor <johtaylo@microsoft.com>
2014-04-30 15:19:28 -0700

    Update DownloadReport_NuGetClientVersion.sql
    We need to update this code too if we want to see new versions of NuGet client. (This is intentional to offer a little protection from false versions appearing in results.)

--- src/NuGetGallery.Operations/Scripts/DownloadReport_NuGetClientVersion.sql
+++ src/NuGetGallery.Operations/Scripts/DownloadReport_NuGetClientVersion.sql
@@ -7,6 +7,6 @@ WHERE Dimension_Date.[Date] >= CONVERT(DATE, DATEADD(day, -42, GETDATE()))
   AND Dimension_Date.[Date] < CONVERT(DATE, GETDATE())
   AND Dimension_UserAgent.ClientCategory = 'NuGet'
   AND Dimension_UserAgent.ClientMajorVersion <= 2
-  AND Dimension_UserAgent.ClientMinorVersion <= 7
+  AND Dimension_UserAgent.ClientMinorVersion <= 8
 GROUP BY Dimension_UserAgent.ClientMajorVersion, Dimension_UserAgent.ClientMinorVersion
 ORDER BY Dimension_UserAgent.ClientMajorVersion, Dimension_UserAgent.ClientMinorVersion
