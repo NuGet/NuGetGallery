5b44bb7f4aecabf15d8d61b6739bc9649a67f4e8
Dan Liu <danliu@microsoft.com>
2014-05-23 15:20:50 -0700

    Cleanup test - checkin1 1. Combine the two UploadAndDownloadPackage tests. 2. Delete the never run UploadAndDownloadTestBombPackage test 3. Other minor fixes

--- tests/NuGetGallery.FunctionalTests/ClientIntegrationTests/NuGetCoreTests.cs
+++ tests/NuGetGallery.FunctionalTests/ClientIntegrationTests/NuGetCoreTests.cs
@@ -9,11 +9,12 @@ namespace NuGetGallery.FunctionalTests
     [TestClass]
     public class NuGetCoreTests : GalleryTestBase
     {       
-        [Description("Downloads a package from the server and validates that the file is present in the local disk"), TestMethod]
+        [TestMethod]
+        [Description("Downloads a package from the server and validates that the file is present in the local disk")]
         [Priority(0)]
         public void DownloadPackageWithNuGetCoreTest()
         {
-            string packageId = Constants.TestPackageId; //try to down load a pre-defined test package.            
+            string packageId = Constants.TestPackageId; //try to down load a pre-defined test package - BaseTestPackage.            
             AssertAndValidationHelper.DownloadPackageAndVerify(packageId);
         }
 
