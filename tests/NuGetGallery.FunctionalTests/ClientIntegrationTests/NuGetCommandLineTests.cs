0fafcd79086001be31557f0bff2fa85ac37d5ea7
Dan Liu <danliu@microsoft.com>
2014-05-23 15:41:48 -0700

    add files back.

--- tests/NuGetGallery.FunctionalTests/ClientIntegrationTests/NuGetCommandLineTests.cs
+++ tests/NuGetGallery.FunctionalTests/ClientIntegrationTests/NuGetCommandLineTests.cs
@@ -13,23 +13,6 @@ namespace NuGetGallery.FunctionalTests.ClientIntegrationTests
     [TestClass]
     public class NugetCommandLineTests : GalleryTestBase
     {
-        private TestContext testContextInstance;
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
         [TestMethod]
         [Description("Downloads a package using NuGet.exe and checks if the package file is present in the output dir")]
         [Priority(0)]
@@ -40,7 +23,6 @@ namespace NuGetGallery.FunctionalTests.ClientIntegrationTests
            int exitCode = CmdLineHelper.InstallPackage(packageId, UrlHelper.V2FeedRootUrl, Environment.CurrentDirectory);
            Assert.IsTrue((exitCode == 0), "The package install via Nuget.exe didnt suceed properly. Check the logs to see the process error and output stream");
            Assert.IsTrue(ClientSDKHelper.CheckIfPackageInstalled(packageId), "Package install failed. Either the file is not present on disk or it is corrupted. Check logs for details");
-
         }
 
         [TestMethod]
@@ -52,11 +34,11 @@ namespace NuGetGallery.FunctionalTests.ClientIntegrationTests
         }
 
         [TestMethod]
-        [Description("Creates a test package with minclientversion tag and pushes it to the server using Nuget.exe")]
+        [Description("Creates a test package with minclientversion tag and .cs name. Pushes it to the server using Nuget.exe and then download via ClientSDK")]
         [Priority(0)]
         public void UploadAndDownLoadPackageWithMinClientVersion()
         {
-            string packageId = DateTime.Now.Ticks.ToString() + testContextInstance.TestName;
+            string packageId = DateTime.Now.Ticks.ToString() + "PackageWithDotCsNames.Cs";
             string version = "1.0.0";
             string packageFullPath = PackageCreationHelper.CreatePackageWithMinClientVersion(packageId, version, "2.3");        
             int exitCode = CmdLineHelper.UploadPackage(packageFullPath, UrlHelper.V2FeedPushSourceUrl);
@@ -69,45 +51,7 @@ namespace NuGetGallery.FunctionalTests.ClientIntegrationTests
                 File.Delete(packageFullPath);
                 Directory.Delete(Path.GetFullPath(Path.GetDirectoryName(packageFullPath)), true);
             }
-            // Increase the thread.sleep time as a workaround for packages appearing in search index 1-2 mins after being uploaded
-            System.Threading.Thread.Sleep(3 * 60 * 1000);
             AssertAndValidationHelper.DownloadPackageAndVerify(packageId);
         }
-
-        [TestMethod]
-        [Description("Creates a test package which ends with .cs and pushes it to the server using Nuget.exe")]
-        [Priority(1)]
-        public void UploadAndDownLoadPackageWithDotCsNames()
-        {
-            string packageId = DateTime.Now.Ticks.ToString() +  "UploadAndDownLoadPackageWithDotCsNames.Cs";
-            AssertAndValidationHelper.UploadNewPackageAndVerify(packageId);
-            // Add a thread.sleep as a workaround for packages appearing in search index 1-2 mins after being uploaded
-            Thread.Sleep(3 * 60 * 1000);
-            AssertAndValidationHelper.DownloadPackageAndVerify(packageId);
-        }
-
-        [TestMethod]
-        [Description("Creates a test bomb package and pushes it using Nuget.exe. Downloads the package again to make sure it works fine.")]
-        [Priority(2)]
-        [Ignore] //This method is marked ignore as we don't want it to be run in regular runs. It will be run only when required.
-        public void UploadAndDownLoadTestBombPackage()
-        {
-            string packageId = DateTime.Now.Ticks.ToString() + testContextInstance.TestName;
-            string version = "1.0.0";
-            string packageFullPath = PackageCreationHelper.CreateGalleryTestBombPackage(packageId);
-            int exitCode = CmdLineHelper.UploadPackage(packageFullPath, UrlHelper.V2FeedPushSourceUrl);
-            Assert.IsTrue((exitCode == 0), "The package upload via Nuget.exe didnt suceed properly. Check the logs to see the process error and output stream");
-            Assert.IsTrue(ClientSDKHelper.CheckIfPackageVersionExistsInSource(packageId, version, UrlHelper.V2FeedRootUrl), "Package {0} is not found in the site {1} after uploading.", packageId, UrlHelper.V2FeedRootUrl);
-
-            //Delete package from local disk so once it gets uploaded
-            if (File.Exists(packageFullPath))
-            {
-                File.Delete(packageFullPath);
-                Directory.Delete(Path.GetFullPath(Path.GetDirectoryName(packageFullPath)), true);
-            }
-
-            AssertAndValidationHelper.DownloadPackageAndVerify(packageId);
-        }
-
     }
 }
