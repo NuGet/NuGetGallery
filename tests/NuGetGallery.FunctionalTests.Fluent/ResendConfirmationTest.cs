9d5ce6665bd32a382e731530825fb8f49cb30e54
Dan Liu <danliu@microsoft.com>
2014-05-28 15:23:38 -0700

    Fix fluent tests - part 1

--- tests/NuGetGallery.FunctionalTests.Fluent/ResendConfirmationTest.cs
+++ tests/NuGetGallery.FunctionalTests.Fluent/ResendConfirmationTest.cs
@@ -19,7 +19,19 @@ namespace NuGetGallery.FunctionalTests.Fluent
         [Description("Covers scenarios around the Resend Confirmation link.")]
         public void ResendConfirmation()
         {
+            I.LogOn(EnvironmentSettings.TestAccountName, EnvironmentSettings.TestAccountPassword);
 
+            // Go to account confirmation page
+            I.Open(UrlHelper.BaseUrl + "account/confirmationrequired");
+            // Click the (change) link
+            I.Open(UrlHelper.BaseUrl + "account/edit");
+            I.Expect.Count(0).Of("h1:contains('404')");
+            
+            // Go back to account confirmation page and click the Send Confirmation Email link
+            I.Open(UrlHelper.BaseUrl + "account/confirmationrequired");
+            I.Click("input[value='Send Confirmation Email']");
+            I.Expect.Count(1).Of("h2:contains('Confirmation Email Sent!')");
+            I.Expect.Count(0).Of("h1:contains('404')");
         }
     }
 }
