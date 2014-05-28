8cbf657c48acd8f0241918f5bfcd0c74519603fb
Dan Liu <danliu@microsoft.com>
2014-05-28 13:45:00 -0700

    update searchUI test

--- tests/NuGetGallery.FunctionalTests.Fluent/SearchUITest.cs
+++ tests/NuGetGallery.FunctionalTests.Fluent/SearchUITest.cs
@@ -20,6 +20,7 @@ namespace NuGetGallery.FunctionalTests.Fluent
         public void SearchUI()
         {
             // This test made more sense back when our search box expanded and contracted.
+            // Now the width of the search box is 650. Update the test, so it verifies the width is large enough.
 
             // Go to the front page.
             I.Open(UrlHelper.BaseUrl);
@@ -27,14 +28,14 @@ namespace NuGetGallery.FunctionalTests.Fluent
             // Click in the box
             I.Click("#searchBoxInput", 3, 3);
             I.Wait(1);
-            I.Expect.True(() => (I.Find("#searchBoxInput")().Width > 200));
+            I.Expect.True(() => (I.Find("#searchBoxInput")().Width > 600));
             I.Type("fred");
             I.Wait(1);
-            I.Expect.True(() => (I.Find("#searchBoxInput")().Width > 200));
+            I.Expect.True(() => (I.Find("#searchBoxInput")().Width > 600));
 
             // Click out of the box
             I.Click("img[alt='Manage NuGet Packages Dialog Window']", 3, 3);
-            I.Expect.True(() => (I.Find("#searchBoxInput")().Width > 200));
+            I.Expect.True(() => (I.Find("#searchBoxInput")().Width > 600));
             I.Wait(1);
         }
     }
