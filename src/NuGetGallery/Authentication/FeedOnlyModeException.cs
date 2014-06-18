d787aaea482f640daff6a86a98a69e13c5448ebe
Deepak Aravindakshan <daravind@microsoft.com>
2014-06-18 14:30:45 -0700

    Added a period at the end of error message

--- src/NuGetGallery/Authentication/FeedOnlyModeException.cs
+++ src/NuGetGallery/Authentication/FeedOnlyModeException.cs
@@ -5,7 +5,7 @@ namespace NuGetGallery.Authentication
     [Serializable]
     public class FeedOnlyModeException : Exception
     {
-        public const string FeedOnlyModeError = "Illegal request! Running on Feed Only mode. User Registration or authentication is disallowed";
+        public const string FeedOnlyModeError = "Illegal request! Running on Feed Only mode. User Registration or authentication is disallowed.";
         public FeedOnlyModeException() { }
 
         public FeedOnlyModeException(string message) : base(message) { }
