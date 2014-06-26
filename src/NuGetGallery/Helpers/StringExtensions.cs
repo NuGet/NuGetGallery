b5b3b07dc9fb413584a85fe9a3e74f94545f9d3f
Robert Muehsig <robert.muehsig@code-developer.de>
2014-06-26 22:29:57 +0200

    String Abbreviate for Wordbreaking
    Issues: #1721 #1722 #1723

---
+++ src/NuGetGallery/Helpers/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NuGetGallery.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string[] SplitSafe(this string s, char[] separator, StringSplitOptions stringSplitOptions)
+        {
+            if (s == null)
+            {
+                return new string[0];
+            }
+
+            return s.Split(separator, stringSplitOptions);
+        }
+
+        public static string Abbreviate(this string text, int length)
+        {
+            if (text.Length <= length)
+            {
+                return text;
+            }
+
+            char[] delimiters = { ' ', '.', ',', ':', ';' };
+            int index = text.LastIndexOfAny(delimiters, length - 3);
+
+            if (index > (length / 2))
+            {
+                return text.Substring(0, index) + "...";
+            }
+
+            return text.Substring(0, length - 3) + "...";
+        }
+
+
+    }
+}
