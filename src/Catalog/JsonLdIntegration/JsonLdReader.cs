3a598087cdee99f3b82b2ea6952c7493e3087274
Scott Blomquist <sblom@microsoft.com>
2014-07-10 10:51:08 -0700

    Add support for specific JTokenTypes in the JsonLd -> RDF reader.

--- src/Catalog/JsonLdIntegration/JsonLdReader.cs
+++ src/Catalog/JsonLdIntegration/JsonLdReader.cs
@@ -80,6 +80,21 @@ namespace NuGet.Services.Metadata.Catalog.JsonLDIntegration
                                     {
                                         datatype = datatypeJToken.ToString();
                                     }
+                                    else
+                                    {
+                                        switch (value.Type)
+                                        {
+                                            case JTokenType.Boolean:
+                                                datatype = "http://www.w3.org/2001/XMLSchema#boolean";
+                                                break;
+                                            case JTokenType.Float:
+                                                datatype = "http://www.w3.org/2001/XMLSchema#double";
+                                                break;
+                                            case JTokenType.Integer:
+                                                datatype = "http://www.w3.org/2001/XMLSchema#integer";
+                                                break;
+                                        }
+                                    }
                                     if (!HandleTriple(handler, subject, property.Name, value.ToString(), datatype)) return;
                                 }
                             }
