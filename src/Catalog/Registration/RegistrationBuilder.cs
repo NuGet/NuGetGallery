0bed078c6c6061e4ffec6c31088ad50abfec83b7
johnataylor <johtaylo@microsoft.com>
2014-07-10 18:26:08 -0700

    initial commit on registration

---
+++ src/Catalog/Registration/RegistrationBuilder.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json.Linq;
+using NuGet.Services.Metadata.Catalog;
+using NuGet.Services.Metadata.Catalog.Persistence;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using VDS.RDF;
+
+namespace NuGet.Services.Metadata.Catalog.Registration
+{
+    public class RegistrationBuilder
+    {
+        SortedList<string, Entry> _entries = new SortedList<string,Entry>();
+        Storage _storage;
+        int _segmentSize;
+
+        JObject _segmentFrame;
+        JObject _segmentIndexFrame;
+
+        bool _verbose;
+ 
+        public RegistrationBuilder(Storage storage, int segmentSize, bool verbose = false)
+        {
+            _segmentSize = segmentSize;
+            _storage = storage;
+
+            _segmentFrame = JObject.Parse((new StreamReader(@"context\Registration.json").ReadToEnd()));
+            _segmentFrame["@type"] = "http://schema.nuget.org/catalog#Segment";
+            _segmentIndexFrame = JObject.Parse((new StreamReader(@"context\Registration.json").ReadToEnd()));
+            _segmentIndexFrame["@type"] = "http://schema.nuget.org/catalog#SegmentIndex";
+
+            _verbose = verbose;
+        }
+        public void Add(Entry entry, Func<Entry, string> key)
+        {
+            _entries.Add(key(entry), entry);
+        }
+
+        public void Add(Entry entry)
+        {
+            Add(entry, (e) => e.Id);
+        }
+
+        public async Task Commit()
+        {
+            Uri segmentIndexUri = _storage.ResolveUri("segment_index.json");
+            SegmentIndex segmentIndex = new SegmentIndex(segmentIndexUri);
+
+            SortedList<string, Entry> batch = new SortedList<string, Entry>();
+            foreach (var item in _entries)
+            {
+                batch.Add(item.Key, item.Value);
+                if (batch.Count == _segmentSize)
+                {
+                    await AddSegment(segmentIndex, batch);
+                    batch.Clear();
+                }
+            }
+            await AddSegment(segmentIndex, batch);
+
+            IGraph graph = segmentIndex.ToGraph();
+
+            await _storage.Save(segmentIndexUri, new StringStorageContent(Utils.CreateJson(graph, _segmentIndexFrame), "application/json"));
+
+            if (_verbose)
+            {
+                Console.WriteLine(segmentIndexUri);
+            }
+        }
+
+        async Task AddSegment(SegmentIndex segmentIndex, SortedList<string, Entry> batch)
+        {
+            if (batch.Count == 0)
+            {
+                return;
+            }
+
+            Uri segmentUri = _storage.ResolveUri(segmentIndex.GetNextSegmentName());
+
+            Segment segment = new Segment();
+            segment.Uri = segmentUri;
+
+            foreach (var item in batch)
+            {
+                Segment.SegmentEntry segmentEntry = new Segment.SegmentEntry();
+                segmentEntry.Uri = item.Value.Uri;
+                segmentEntry.Id = item.Value.Id;
+                segmentEntry.Version = item.Value.Version;
+                segmentEntry.Description = item.Value.Description;
+
+                segment.Entries.Add(segmentEntry);
+            }
+
+            IGraph graph = segment.ToGraph();
+
+            await _storage.Save(segmentUri, new StringStorageContent(Utils.CreateJson(graph, _segmentFrame), "application/json"));
+
+            if (_verbose)
+            {
+                Console.WriteLine(segmentUri);
+            }
+
+            SegmentIndex.SegmentSummary segmentSummary = new SegmentIndex.SegmentSummary();
+            segmentSummary.Uri = segmentUri;
+            segmentSummary.Lowest = batch.First().Key;
+            segmentSummary.Highest = batch.Last().Key;
+            segmentSummary.Count = batch.Count;
+
+            segmentIndex.Segments.Add(segmentSummary);
+        }
+    }
+}
---
+++ src/Catalog/Registration/SegmentIndex.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VDS.RDF;
+
+namespace NuGet.Services.Metadata.Catalog.Registration
+{
+    public class SegmentIndex
+    {
+        IList<SegmentSummary> _segments = new List<SegmentSummary>();
+        public Uri Uri { get; private set; }
+        public int SegmentNumber { get; private set; }
+        public IList<SegmentSummary> Segments { get { return _segments; } }
+        public SegmentIndex(Uri uri)
+        {
+            Uri = uri;
+            SegmentNumber = 0;
+        }
+
+        public SegmentIndex(IGraph graph)
+        {
+            FromGraph(graph);
+        }
+
+        public string GetNextSegmentName()
+        {
+            return "segment_" + (SegmentNumber++).ToString() + ".json";
+        }
+
+        public IGraph ToGraph()
+        {
+            IGraph graph = new Graph();
+
+            graph.NamespaceMap.AddNamespace("rdf", new Uri("http://www.w3.org/1999/02/22-rdf-syntax-ns#"));
+            graph.NamespaceMap.AddNamespace("catalog", new Uri("http://schema.nuget.org/catalog#"));
+
+            INode subject = graph.CreateUriNode(Uri);
+
+            graph.Assert(subject, graph.CreateUriNode("rdf:type"), graph.CreateUriNode("catalog:SegmentIndex"));
+
+            graph.Assert(subject, graph.CreateUriNode("catalog:segmentNumber"), graph.CreateLiteralNode(SegmentNumber.ToString(), new Uri("http://www.w3.org/2001/XMLSchema#integer")));
+
+            foreach (SegmentSummary entry in Segments)
+            {
+                graph.Assert(subject, graph.CreateUriNode("catalog:entry"), graph.CreateUriNode(entry.Uri));
+                graph.Merge(entry.ToGraph(), true);
+            }
+
+            return graph;
+        }
+
+        void FromGraph(IGraph graph)
+        {
+            graph.NamespaceMap.AddNamespace("rdf", new Uri("http://www.w3.org/1999/02/22-rdf-syntax-ns#"));
+            graph.NamespaceMap.AddNamespace("catalog", new Uri("http://schema.nuget.org/catalog#"));
+
+            INode subject = graph.GetTriplesWithPredicateObject(graph.CreateUriNode("rdf:type"), graph.CreateUriNode("catalog:SegmentIndex")).First().Subject;
+
+            Uri = ((IUriNode)subject).Uri;
+            
+            SegmentNumber = int.Parse(((ILiteralNode)graph.GetTriplesWithSubjectPredicate(subject, graph.CreateUriNode("catalog:segmentNumber")).First().Object).Value);
+
+            foreach (Triple segment in graph.GetTriplesWithSubjectPredicate(subject, graph.CreateUriNode("catalog:entry")))
+            {
+                Segments.Add(new SegmentSummary(segment, graph));
+            }
+        }
+
+        public class SegmentSummary
+        {
+            public Uri Uri { get; set; }
+            public string Lowest { get; set; }
+            public string Highest { get; set; }
+            public int Count { get; set; }
+            public SegmentSummary()
+            {
+            }
+            public SegmentSummary(Triple segment, IGraph graph)
+            {
+                FromGraph(segment, graph);
+            }
+            public IGraph ToGraph()
+            {
+                IGraph graph = new Graph();
+
+                graph.NamespaceMap.AddNamespace("catalog", new Uri("http://schema.nuget.org/catalog#"));
+
+                INode subject = graph.CreateUriNode(Uri);
+
+                graph.Assert(subject, graph.CreateUriNode("catalog:lowest"), graph.CreateLiteralNode(Lowest));
+                graph.Assert(subject, graph.CreateUriNode("catalog:highest"), graph.CreateLiteralNode(Highest));
+                graph.Assert(subject, graph.CreateUriNode("catalog:count"), graph.CreateLiteralNode(Count.ToString(), new Uri("http://www.w3.org/2001/XMLSchema#integer")));
+
+                return graph;
+            }
+
+            void FromGraph(Triple segment, IGraph graph)
+            {
+                graph.NamespaceMap.AddNamespace("catalog", new Uri("http://schema.nuget.org/catalog#"));
+
+                Uri = ((IUriNode)segment.Object).Uri;
+                Lowest = graph.GetTriplesWithSubjectPredicate(segment.Object, graph.CreateUriNode("catalog:lowest")).First().Object.ToString();
+                Highest = graph.GetTriplesWithSubjectPredicate(segment.Object, graph.CreateUriNode("catalog:highest")).First().Object.ToString();
+                Count = int.Parse(((ILiteralNode)graph.GetTriplesWithSubjectPredicate(segment.Object, graph.CreateUriNode("catalog:count")).First().Object).Value);
+            }
+        }
+    }
+}
