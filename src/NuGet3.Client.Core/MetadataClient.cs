f3f176466d15d29c94043fbe95b84225cbbab313
Scott Blomquist <sblom@microsoft.com>
2014-07-29 08:55:01 -0700

    Segment search implementation.

--- src/NuGet3.Client.Core/MetadataClient.cs
+++ src/NuGet3.Client.Core/MetadataClient.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,19 +20,44 @@ namespace NuGet3.Client
             this.serviceClient = serviceClient;
         }
 
-        public PackageMetadata FindPackage(string packageId, string packageVersion)
+        public async Task<string> FindPackage(string packageId, string packageVersion)
         {
-            return new PackageMetadata();
+            HttpClient hc = new HttpClient();
+            string index = await hc.GetStringAsync(serviceClient.AllPackages);
+            JObject indexJson = JObject.Parse(index);
+
+            var segments = ((JArray)indexJson["entry"]).OrderBy(page => (string)page["lowest"], StringComparer.InvariantCultureIgnoreCase).Select(j => (JObject)j).ToArray();
+
+            int i;
+            for (i = 0; i < segments.Count(); ++i)
+            {
+                if (string.Compare((string)segments[i]["lowest"], packageId, true, CultureInfo.InvariantCulture) > 0)
+                {
+                    break;
+                }
+            }
+
+            if (i == 0)
+            {
+                return null;
+            }
+
+            string segment = await hc.GetStringAsync((string)segments[i - 1]["url"]);
+            JObject segmentJson = JObject.Parse(segment);
+            foreach (var entry in segmentJson["entry"])
+            {
+                if (string.Compare((string)entry["id"],packageId,true,CultureInfo.InvariantCulture) == 0)
+                {
+                    return (string)entry["url"];
+                }
+            }
+
+            return null;
         }
 
         public PackageMetadata GetPackageMetadata(string packageUri)
         {
             return new PackageMetadata();
-        }
-
-        public string GetPackageUri(string p1, string p2)
-        {
-            return "";
         }
     }
 }
