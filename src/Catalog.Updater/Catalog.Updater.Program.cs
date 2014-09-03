8329f6f74bbff6bc6534db9f1a17fddc08dadeba
deepakaravindr <daravind@microsoft.com>
2014-09-03 11:11:20 -0700

    Added the generic jobRunner to run any jobBase. Also, changed the Program on each exe to simply do the following:             var job = new Job();             JobRunner.Run(job, args).Wait(); The jobRunner handles arguments to the runner and job. Also, handles errors and such

--- src/Catalog.Updater/Catalog.Updater.Program.cs
+++ src/Catalog.Updater/Catalog.Updater.Program.cs
@@ -10,73 +10,8 @@ namespace Catalog.Updater
     {
         static void Main(string[] args)
         {
-            bool runContinuously = true;
-            if (args.Length > 0 && String.Equals(args[0], "dbg", StringComparison.OrdinalIgnoreCase))
-            {
-                args = args.Skip(1).ToArray();
-                Debugger.Launch();
-            }
-
-            if (args.Length > 0 && String.Equals(args[0], "once", StringComparison.OrdinalIgnoreCase))
-            {
-                args = args.Skip(1).ToArray();
-                runContinuously = false;
-            }
-
-            // Construct the job object. This initializes the logger(s) alone, and makes the jobName available
             var job = new Job();
-            job.Logger.Log(TraceLevel.Warning, "Started...");
-            job.Logger.Log(TraceLevel.Warning, "Running " + (runContinuously ? " continuously..." : " once..."));
-
-            try
-            {
-                // Get the args passed in or provided as an env variable based on jobName as a dictionary of <string argName, string argValue>
-                var jobArgsDictionary = JobConfigManager.GetJobArgsDictionary(job.Logger, args, job.JobName);
-
-                // Try and get the sleep duration provided, if any. Default is 5000 milliSeconds
-                string sleepDurationString;
-                int sleepDuration = 5000;
-                if (jobArgsDictionary.TryGetValue(JobArgumentNames.Sleep, out sleepDurationString))
-                {
-                    if (!Int32.TryParse(sleepDurationString, out sleepDuration))
-                    {
-                        job.Logger.Log(TraceLevel.Warning, "SleepDuration is not a valid integer. SleepDuration should be in milliseconds and a valid integer");
-                    }
-                }
-                job.Logger.Log(TraceLevel.Warning, "SleepDuration is {0}", sleepDuration);
-
-                // Initialize the job once with everything needed. Logger(s) are already initialized
-                if (!job.Init(jobArgsDictionary))
-                {
-                    // If the job could not be initialized successfully, STOP!
-                    job.Logger.Log(TraceLevel.Error, "Exiting. The job could not be initialized successfully with the arguments passed");
-                    return;
-                }
-
-                // Run the job now
-                do
-                {
-                    job.Run().Wait();
-                    // Wait for <sleepDuration> milliSeconds and run the job again
-                    Thread.Sleep(sleepDuration);
-                } while (runContinuously);
-            }
-            catch (AggregateException ex)
-            {
-                var innerEx = ex.InnerExceptions.Count > 0 ? ex.InnerExceptions[0] : null;
-                if (innerEx != null)
-                {
-                    job.Logger.Log(TraceLevel.Error, innerEx.ToString());
-                }
-                else
-                {
-                    job.Logger.Log(TraceLevel.Error, ex.ToString());
-                }
-            }
-            catch (Exception ex)
-            {
-                job.Logger.Log(TraceLevel.Error, ex.ToString());
-            }
+            JobRunner.Run(job, args).Wait();
         }
     }
 }
---
+++ src/NuGet.Jobs.Common/JobRunner.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NuGet.Jobs.Common
+{
+    public static class JobRunner
+    {
+        /// <summary>
+        /// This is a static method to run a job whose args are passed in
+        /// By default,
+        ///     a) The job will be run continuously in a while loop. Could be overridden using 'once' argument
+        ///     b) The sleep duration between each run when running continuously is 5000 milliSeconds. Could be overridden using '-Sleep' argument
+        /// </summary>
+        /// <param name="job">Job to run</param>
+        /// <param name="args">Args contains args to the job runner like (dbg, once and so on) and for the job itself</param>
+        /// <returns></returns>
+        public static async Task Run(JobBase job, string[] args)
+        {
+            bool runContinuously = true;
+            if (args.Length > 0 && String.Equals(args[0], "dbg", StringComparison.OrdinalIgnoreCase))
+            {
+                args = args.Skip(1).ToArray();
+                Debugger.Launch();
+            }
+
+            if (args.Length > 0 && String.Equals(args[0], "once", StringComparison.OrdinalIgnoreCase))
+            {
+                args = args.Skip(1).ToArray();
+                runContinuously = false;
+            }
+
+            job.Logger.Log(TraceLevel.Warning, "Started...");
+            job.Logger.Log(TraceLevel.Warning, "Running " + (runContinuously ? " continuously..." : " once..."));
+
+            try
+            {
+                // Get the args passed in or provided as an env variable based on jobName as a dictionary of <string argName, string argValue>
+                var jobArgsDictionary = JobConfigManager.GetJobArgsDictionary(job.Logger, args, job.JobName);
+
+                // Try and get the sleep duration provided, if any. Default is 5000 milliSeconds
+                string sleepDurationString;
+                int sleepDuration = 5000;
+                if (jobArgsDictionary.TryGetValue(JobArgumentNames.Sleep, out sleepDurationString))
+                {
+                    if (!Int32.TryParse(sleepDurationString, out sleepDuration))
+                    {
+                        job.Logger.Log(TraceLevel.Warning, "SleepDuration is not a valid integer. SleepDuration should be in milliseconds and a valid integer");
+                    }
+                }
+                job.Logger.Log(TraceLevel.Warning, "SleepDuration is {0}", sleepDuration);
+
+                // Initialize the job once with everything needed. Logger(s) are already initialized
+                if (!job.Init(jobArgsDictionary))
+                {
+                    // If the job could not be initialized successfully, STOP!
+                    job.Logger.Log(TraceLevel.Error, "Exiting. The job could not be initialized successfully with the arguments passed");
+                    return;
+                }
+
+                // Run the job now
+                do
+                {
+                    await job.Run();
+                    // Wait for <sleepDuration> milliSeconds and run the job again
+                    Thread.Sleep(sleepDuration);
+                } while (runContinuously);
+            }
+            catch (AggregateException ex)
+            {
+                var innerEx = ex.InnerExceptions.Count > 0 ? ex.InnerExceptions[0] : null;
+                if (innerEx != null)
+                {
+                    job.Logger.Log(TraceLevel.Error, innerEx.ToString());
+                }
+                else
+                {
+                    job.Logger.Log(TraceLevel.Error, ex.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                job.Logger.Log(TraceLevel.Error, ex.ToString());
+            }
+
+        }
+    }
+}
