3c454bf9d0f7817322951aea3923b2d1c3c07360
juste <juste@microsoft.com>
2014-10-30 17:31:45 -0700

    CatalogIndexReader

--- src/Catalog/CatalogIndexEntry.cs
+++ src/Catalog/CatalogIndexEntry.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NuGet.Canton
+namespace NuGet.Services.Metadata.Catalog
 {
     public class CatalogIndexEntry : IComparable<CatalogIndexEntry>
     {
