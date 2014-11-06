f3b5e27c9eadad1eaa91e2fa5a5e556f1666d573
deepakaravindr <daravind@microsoft.com>
2014-11-05 17:53:43 -0800

    Test console application for configuring trace listener via app.config

---
+++ src/Tests.AppConfig.AzureJobTraceListener/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Tests.AppConfig.AzureJobTraceListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.TraceInformation("Program Information");
+            Trace.TraceWarning("Program Warning");
+            Trace.TraceError("Program error");
+            Trace.Close();
+        }
+    }
+}
