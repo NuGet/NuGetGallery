a154ca5c90823af4a1357f9a20185f223a5fb62e
johnataylor <johtaylo@microsoft.com>
2014-11-12 14:19:24 -0800

    fixed page fetch criteria in CommitCollector

--- src/Catalog/CommitCollector.cs
+++ src/Catalog/CommitCollector.cs
@@ -23,7 +23,7 @@ namespace NuGet.Services.Metadata.Catalog
 
             IEnumerable<CatalogItem> rootItems = root["items"]
                 .Select(item => new CatalogItem(item))
-                .Where(item => item.CommitTimeStamp > front.Value && item.CommitTimeStamp <= back.Value)
+                .Where(item => item.CommitTimeStamp > front.Value)
                 .OrderBy(item => item.CommitTimeStamp);
 
             bool acceptNextBatch = false;
