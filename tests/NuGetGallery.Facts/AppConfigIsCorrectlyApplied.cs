465a222cf016185b38173fb43449429e8303d01f
Maarten Balliauw <maarten@maartenballiauw.be>
2015-03-18 11:25:58 +0100

    Fixed failing tests

--- tests/NuGetGallery.Facts/AppConfigIsCorrectlyApplied.cs
+++ tests/NuGetGallery.Facts/AppConfigIsCorrectlyApplied.cs
@@ -16,11 +16,11 @@ namespace NuGetGallery
         [Fact]
         public void VerifyBindingRedirectToMvc4IsWorking()
         {
-            // System.Web.Mvc should be binding redirected from version 3.0.0.0 to 4.0.0.0 just like in our actual app.
+            // System.Web.Mvc should be binding redirected from version 3.0.0.0 to 4.0.0.1 just like in our actual app.
             string typeName = "System.Web.Mvc.Controller, System.Web.Mvc, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
             Type resolvedType = Type.GetType(typeName);
             Version runtimeVersion = resolvedType.Assembly.GetName().Version;
-            Assert.Equal(new Version("4.0.0.0"), runtimeVersion);
+            Assert.Equal(new Version("4.0.0.1"), runtimeVersion);
         }
     }
 }
