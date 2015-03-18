1dfa1c387122e8b1acd74dfcab10e0032d5a67eb
johnataylor <johtaylo@microsoft.com>
2015-03-18 11:57:42 -0700

    update indexing package

--- src/Ng/SearchIndexFromCatalogCollector.cs
+++ src/Ng/SearchIndexFromCatalogCollector.cs
@@ -9,10 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Runtime.Versioning;
 using System.Threading.Tasks;
 
 namespace Ng
