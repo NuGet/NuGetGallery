99445311aff5ba556f049c721380dc022ed36f22
xavierdecoster <xavier.decoster@outlook.com>
2015-03-23 15:52:09 -0700

    added common metadata

--- src/NuGet.Indexing/Properties/AssemblyInfo.cs
+++ src/NuGet.Indexing/Properties/AssemblyInfo.cs
@@ -1,7 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NuGet.Indexing")]
 [assembly: AssemblyDescription("Lucene-based Search Indexing tools for NuGet")]
 [assembly: InternalsVisibleTo("NuGet.Indexing.Facts")]
+
+[assembly: AssemblyCompany("Outercurve Foundation")]
+[assembly: AssemblyProduct("NuGet Services")]
+[assembly: AssemblyCopyright("\x00a9 Outercurve Foundation. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
