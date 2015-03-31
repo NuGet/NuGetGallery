be74497b56d6df36bb6916d875d739f42f00d0c3
Ranjini Mathrubootham <ranjinim@microsoft.com>
2015-03-31 13:06:37 -0700

    CreateWarehouseReports

---
+++ Stats.CreateWarehouseReports/scripts/DownloadReport_RecentPopularityByPackage.sql
@@ -0,0 +1,9 @@
+﻿SELECT Dimension_Package.PackageVersion, SUM(DownloadCount) 'Downloads'
+FROM Fact_Download
+INNER JOIN Dimension_Package ON Dimension_Package.Id = Fact_Download.Dimension_Package_Id
+INNER JOIN Dimension_Date ON Dimension_Date.Id = Fact_Download.Dimension_Date_Id
+WHERE Dimension_Date.[Date] >= CONVERT(DATE, DATEADD(day, -42, GETDATE()))
+  AND Dimension_Date.[Date] < CONVERT(DATE, GETDATE())
+  AND Dimension_Package.PackageId = @PackageId
+GROUP BY Dimension_Package.PackageVersion
+ORDER BY SUM(DownloadCount) DESC
---
+++ Stats.CreateWarehouseReports/scripts/DownloadReport_RecentPopularityDetail.sql
@@ -0,0 +1,21 @@
+﻿
+SELECT TOP(500) 
+	Dimension_Package.PackageId,
+	Dimension_Package.PackageVersion,
+	Dimension_Package.PackageTitle,
+	Dimension_Package.PackageDescription,
+	Dimension_Package.PackageIconUrl,
+	SUM(DownloadCount) 'Downloads'
+FROM Fact_Download
+INNER JOIN Dimension_Package ON Dimension_Package.Id = Fact_Download.Dimension_Package_Id
+INNER JOIN Dimension_Date ON Dimension_Date.Id = Fact_Download.Dimension_Date_Id
+WHERE Dimension_Date.[Date] >= CONVERT(DATE, DATEADD(day, -42, GETDATE()))
+  AND Dimension_Date.[Date] < CONVERT(DATE, GETDATE())
+  AND Dimension_Package.PackageListed = 1
+GROUP BY 
+	Dimension_Package.PackageId, 
+	Dimension_Package.PackageVersion,
+	Dimension_Package.PackageTitle,
+	Dimension_Package.PackageDescription,
+	Dimension_Package.PackageIconUrl
+ORDER BY SUM(DownloadCount) DESC
