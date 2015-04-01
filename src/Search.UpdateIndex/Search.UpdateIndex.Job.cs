40852ce248fe23718b9dee2d5f0e6a801498c777
Ranjini Mathrubootham <ranjinim@microsoft.com>
2015-04-01 09:13:25 -0700

    Search.UpdateIndex

---
+++ src/Search.UpdateIndex/Search.UpdateIndex.Job.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NuGet.Jobs.Common;
+using System.Data.SqlClient;
+using Microsoft.WindowsAzure.Storage;
+using NuGet.Indexing;
+using System.Diagnostics;
+using Lucene.Net.Store;
+using System.IO;
+
+namespace Search.UpdateIndex
+{
+    /// <summary>
+    /// This job is to update lucene index out of the package database.
+    /// </summary>
+    internal class Job : JobBase
+    {
+        private const string DefaultDataContainerName = "ng-search-data";
+
+        /// <summary>
+        /// The gallery database or the package database
+        /// </summary>
+        private SqlConnectionStringBuilder PackageDatabase { get; set; }
+        /// <summary>
+        /// The storage account in which FrameworksList.FileName can be found
+        /// </summary>
+        private CloudStorageAccount DataStorageAccount { get; set; }
+        /// <summary>
+        /// The container in DataStorageAccount in which FrameworksList.FileName can be found. Default is 'ng-search-data'
+        /// </summary>
+        private string DataContainerName { get; set; }
+        private string IndexFolder { get; set; }
+
+        public override bool Init(IDictionary<string, string> jobArgsDictionary)
+        {
+            PackageDatabase =
+            new SqlConnectionStringBuilder(
+                JobConfigManager.GetArgument(jobArgsDictionary,
+                    JobArgumentNames.PackageDatabase,
+                    EnvironmentVariableKeys.SqlGallery));
+
+            DataStorageAccount =
+                CloudStorageAccount.Parse(
+                    JobConfigManager.GetArgument(jobArgsDictionary,
+                        JobArgumentNames.DataStorageAccount,
+                        EnvironmentVariableKeys.StoragePrimary));
+
+            DataContainerName =
+                JobConfigManager.TryGetArgument(jobArgsDictionary,
+                    JobArgumentNames.DataContainerName);
+
+            if (String.IsNullOrEmpty(DataContainerName))
+            {
+                DataContainerName = DefaultDataContainerName;
+            }
+
+            IndexFolder =
+                JobConfigManager.GetArgument(jobArgsDictionary,
+                    JobArgumentNames.IndexFolder);
+
+            // Initialized successfully, return true
+            return true;
+        }
+
+        public override Task<bool> Run()
+        {
+            FrameworksList frameworksList = new StorageFrameworksList(DataStorageAccount, DataContainerName, FrameworksList.FileName);
+            Lucene.Net.Store.Directory directory = new SimpleFSDirectory(new DirectoryInfo(IndexFolder));
+
+            // Run the task
+            UpdateIndexTask task = new UpdateIndexTask()
+            {
+                SqlConnectionString = PackageDatabase.ConnectionString,
+                StorageAccount = DataStorageAccount,
+                Container = String.IsNullOrEmpty(IndexFolder) ?
+                    (IndexFolder ?? "ng-search") :
+                    null,
+                DataContainer = DataContainerName,
+                Folder = IndexFolder,
+           
+            };
+            task.Execute();
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ src/Stats.CreateWarehouseReports/scripts/DownloadReport_ListInactive.sql
@@ -0,0 +1,10 @@
+﻿
+SELECT DISTINCT Dimension_Package.PackageId
+FROM Dimension_Package
+WHERE Dimension_Package.PackageId NOT IN (
+	SELECT DISTINCT Dimension_Package.PackageId
+	FROM Fact_Download
+	INNER JOIN Dimension_Package ON Dimension_Package.Id = Fact_Download.Dimension_Package_Id
+	INNER JOIN Dimension_Date ON Dimension_Date.Id = Fact_Download.Dimension_Date_Id
+	WHERE Dimension_Date.[Date] >= CONVERT(DATE, DATEADD(day, -42, GETDATE()))
+	  AND Dimension_Date.[Date] < CONVERT(DATE, GETDATE()))
