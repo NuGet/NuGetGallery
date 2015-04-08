75b7ef8f1b9654d9fbb8ee7a0ed53f61c3f6d428
Maarten Balliauw <maarten@maartenballiauw.be>
2015-04-08 15:41:03 +0200

    Updated configuration setting placeholder for storage account key

--- src/Ng/Scripts/Catalog2RegistrationV3.cmd
+++ src/Ng/Scripts/Catalog2RegistrationV3.cmd
@@ -9,7 +9,7 @@
 
 	title #{Jobs.Catalog2Registration.Title}
 
-    start /w Ng.exe catalog2registration -source #{Jobs.Catalog.Source} -contentBaseAddress #{Jobs.Catalog2Registration.ContentBaseAddress} -storageBaseAddress #{Jobs.Catalog2Registration.StorageBaseAddress} -storageType azure -storageAccountName #{Jobs.Catalog2Registration.Registration.StorageAccount.Name} -storageKeyValue #{Jobs.Catalog2Registration.Registration.StorageAccount.Name} -storageContainer #{Jobs.Catalog2Registration.Registration.Container} -verbose true -interval #{Jobs.Catalog2Registration.Interval}
+    start /w Ng.exe catalog2registration -source #{Jobs.Catalog.Source} -contentBaseAddress #{Jobs.Catalog2Registration.ContentBaseAddress} -storageBaseAddress #{Jobs.Catalog2Registration.StorageBaseAddress} -storageType azure -storageAccountName #{Jobs.Catalog2Registration.Registration.StorageAccount.Name} -storageKeyValue #{Jobs.Catalog2Registration.Registration.StorageAccount.Key} -storageContainer #{Jobs.Catalog2Registration.Registration.Container} -verbose true -interval #{Jobs.Catalog2Registration.Interval}
 
 	echo "Finished - Catalog2Registration"
 
