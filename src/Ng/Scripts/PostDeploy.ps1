2e58422d526c29825b85f95220da137b5a25851e
Maarten Balliauw <maarten@maartenballiauw.be>
2015-05-05 11:51:45 +0200

    Refactoring of service install scripts

--- src/Ng/Scripts/PostDeploy.ps1
+++ src/Ng/Scripts/PostDeploy.ps1
@@ -1,3 +1,5 @@
+. "Functions.ps1" 
+
 $serviceNameC2R = $OctopusParameters["Jobs.Catalog2Registration.Service.Name"]
 $serviceNameC2L = $OctopusParameters["Jobs.Catalog2Lucene.Service.Name"]
 $serviceNameC2D = $OctopusParameters["Jobs.Catalog2Dnx.Service.Name"]
@@ -8,29 +10,7 @@ $titleC2D = $OctopusParameters["Jobs.Catalog2Dnx.Title"]
 
 $currentDirectory = [string](Get-Location)
 
-# Install services
-# Uses nssm - from http://nssm.cc/download
-
-## Catalog2RegistrationV3
-$installC2R = "nssm install $serviceNameC2R $currentDirectory\Catalog2RegistrationV3.cmd"
-cmd /C $installC2R 
-Set-Service -Name $serviceNameC2R -DisplayName "$titleC2R - $serviceNameC2R" -Description "Runs Catalog2RegistrationV3." -StartupType Automatic
-sc.exe failure $serviceNameC2R reset= 30 actions= restart/5000 
-
-## Catalog2LuceneV3
-$installC2L = "nssm install $serviceNameC2L $currentDirectory\Catalog2LuceneV3.cmd"
-cmd /C $installC2L 
-Set-Service -Name $serviceNameC2L -DisplayName "$titleC2L - $serviceNameC2L" -Description "Runs Catalog2LuceneV3." -StartupType Automatic
-sc.exe failure $serviceNameC2L reset= 30 actions= restart/5000 
-
-## Catalog2DnxV3
-$installC2D = "nssm install $serviceNameC2D $currentDirectory\Catalog2DnxV3.cmd"
-cmd /C $installC2D 
-Set-Service -Name $serviceNameC2D -DisplayName "$titleC2D - $serviceNameC2D" -Description "Runs Catalog2DnxV3." -StartupType Automatic
-sc.exe failure $serviceNameC2D reset= 30 actions= restart/5000 
-
-
-# Run services
-net start $serviceNameC2R
-net start $serviceNameC2L
-net start $serviceNameC2D
+# Install services	
+Install-NuGetService($serviceNameC2R, $titleC2R, "$currentDirectory\Catalog2RegistrationV3.cmd")
+Install-NuGetService($serviceNameC2L, $titleC2L, "$currentDirectory\Catalog2LuceneV3.cmd")
+Install-NuGetService($serviceNameC2D, $titleC2D, "$currentDirectory\Catalog2DnxV3.cmd")
--- src/Ng/Scripts/PreDeploy.ps1
+++ src/Ng/Scripts/PreDeploy.ps1
@@ -1,3 +1,5 @@
+. "Functions.ps1" 
+
 $serviceNameC2R = $OctopusParameters["Jobs.Catalog2Registration.Service.Name"]
 $serviceNameC2L = $OctopusParameters["Jobs.Catalog2Lucene.Service.Name"]
 $serviceNameC2D = $OctopusParameters["Jobs.Catalog2Dnx.Service.Name"]
@@ -5,19 +7,9 @@ $serviceNameC2D = $OctopusParameters["Jobs.Catalog2Dnx.Service.Name"]
 # Stop and remove services
 
 Write-Host Removing services...
-if (Get-Service $serviceNameC2R -ErrorAction SilentlyContinue)
-{
-    Stop-Service $serviceNameC2R -Force
-    sc.exe delete $serviceNameC2R 
-}
-if (Get-Service $serviceNameC2L -ErrorAction SilentlyContinue)
-{
-    Stop-Service $serviceNameC2L -Force
-    sc.exe delete $serviceNameC2L 
-}
-if (Get-Service $serviceNameC2D -ErrorAction SilentlyContinue)
-{
-    Stop-Service $serviceNameC2D -Force
-    sc.exe delete $serviceNameC2D 
-}
+
+Uninstall-NuGetService($serviceNameC2R)
+Uninstall-NuGetService($serviceNameC2L)
+Uninstall-NuGetService($serviceNameC2D)
+
 Write-Host Removed services.
