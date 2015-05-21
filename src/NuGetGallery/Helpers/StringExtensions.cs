3641439c7fc74c9bedc03cb1cbde5e2b2c3a1ca5
robertmuehsig <robert.muehsig@code-developer.de>
2015-05-21 15:22:18 +0200

    String Abbreviate for Wordbreaking #2232

---
+++ src/NuGetGallery/Helpers/StringExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using System;
+
+namespace NuGetGallery.Helpers
+{
+    public static class StringExtensions
+    {
+        public static string[] SplitSafe(this string s, char[] separator, StringSplitOptions stringSplitOptions)
+        {
+            if (s == null)
+            {
+                return new string[0];
+            }
+
+            return s.Split(separator, stringSplitOptions);
+        }
+
+        public static string Abbreviate(this string text, int length)
+        {
+            if (text.Length <= length)
+            {
+                return text;
+            }
+
+            char[] delimiters = { ' ', '.', ',', ':', ';' };
+            int index = text.LastIndexOfAny(delimiters, length - 3);
+
+            if (index > (length / 2))
+            {
+                return text.Substring(0, index) + "...";
+            }
+
+            return text.Substring(0, length - 3) + "...";
+        }
+
+
+    }
+}
