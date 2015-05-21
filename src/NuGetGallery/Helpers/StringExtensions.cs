5ef30337e6a211e7ef0262544aae9bab110068ac
Xavier Decoster <xavier.decoster@outlook.com>
2015-05-21 20:57:50 +0200

    restored TruncateAtWordBoundary method

--- src/NuGetGallery/Helpers/StringExtensions.cs
+++ src/NuGetGallery/Helpers/StringExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
+using System.Globalization;
 
 namespace NuGetGallery.Helpers
 {
@@ -34,6 +36,17 @@ namespace NuGetGallery.Helpers
             return text.Substring(0, length - 3) + "...";
         }
 
+        public static string TruncateAtWordBoundary(this string input, int length = 300, string ommission = "...", string morText = "")
+        {
+            if (string.IsNullOrEmpty(input) || input.Length < length)
+                return input;
 
+            int nextSpace = input.LastIndexOf(" ", length, StringComparison.Ordinal);
+
+            return string.Format(CultureInfo.CurrentCulture, "{2}{1}{0}",
+                                 morText,
+                                 ommission,
+                                 input.Substring(0, (nextSpace > 0) ? nextSpace : length).Trim());
+        }
     }
 }
