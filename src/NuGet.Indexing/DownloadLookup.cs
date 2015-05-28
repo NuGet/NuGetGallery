f18a03f4f63fdf5cfe216002ee49b6463f4cb945
bhuvak <bhuvak@live.com>
2015-05-28 15:38:20 -0700

    Added tests to DownloadKookUp for good and bad data and incorporated other review comments

--- src/NuGet.Indexing/DownloadLookup.cs
+++ src/NuGet.Indexing/DownloadLookup.cs
@@ -4,6 +4,7 @@
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace NuGet.Indexing
@@ -17,29 +18,60 @@ namespace NuGet.Indexing
         public IDictionary<string, IDictionary<string, int>> Load()
         {
             IDictionary<string, IDictionary<string, int>> result = new Dictionary<string, IDictionary<string, int>>();
-            //The data in downloads.v1.json will be an array of Package records - which has Id, Array of Versions and download count.
-            //[["AutofacContrib.NSubstitute",["2.4.3.700",406],["2.5.0",137]],["Assman.Core",["2.0.7",138]]....
+            // The data in downloads.v1.json will be an array of Package records - which has Id, Array of Versions and download count.
+            // Sample.json : [["AutofacContrib.NSubstitute",["2.4.3.700",406],["2.5.0",137]],["Assman.Core",["2.0.7",138]]....
             using (JsonReader jsonReader = GetReader())
             {
-                jsonReader.Read();
-
-                while (jsonReader.Read())
+                try
                 {
-                    if (jsonReader.TokenType == JsonToken.StartArray)
+                    jsonReader.Read();
+
+                    while (jsonReader.Read())
                     {
-                        JToken record = JToken.ReadFrom(jsonReader);
-                        string id = record[0].ToString().ToLowerInvariant();
-                        IDictionary<string, int> versions = new Dictionary<string, int>();
-                        foreach (JToken token in record)
+                        try
                         {
-                            if (token.Count() == 2)
-                                versions.Add(token[0].ToString().ToLowerInvariant(), token[1].ToObject<int>());
+                            if (jsonReader.TokenType == JsonToken.StartArray)
+                            {
+                                JToken record = JToken.ReadFrom(jsonReader);
+                                string id = record[0].ToString().ToLowerInvariant();
+                                // The second entry in each record should be an array of versions, if not move on to next entry.
+                                // This is a check to safe guard against invalid entries.
+                                if (record.Count() == 2 && record[1].Type != JTokenType.Array)
+                                {
+                                    continue;
+                                }
+                                IDictionary<string, int> versions = new Dictionary<string, int>();
+                                foreach (JToken token in record)
+                                {
+                                    if (token != null && token.Count() == 2)
+                                    {
+                                        string version = token[0].ToString().ToLowerInvariant();
+                                        // Check for duplicate versions before adding.
+                                        if (!versions.ContainsKey(version))
+                                        {
+                                            versions.Add(version, token[1].ToObject<int>());
+                                        }
+                                    }
+                                }
+                                //Check for duplicate Ids before adding to dict.
+                                if (!result.ContainsKey(id))
+                                {
+                                    result.Add(id, versions);
+                                }
+
+                            }
                         }
-                        result.Add(id, versions);
+                        catch (JsonReaderException ex)
+                        {
+                            Trace.TraceInformation("Invalid entry found in downloads.v1.json. Exception Message : {0}", ex.Message);
+                        }
                     }
                 }
+                catch (JsonReaderException ex)
+                {
+                    Trace.TraceError("Data present in downloads.v1.json is invalid. Couldn't get download data. Exception Message : {0}", ex.Message);
+                }
             }
-
             return result;
         }
     }
---
+++ tests/IndexingTests/DownloadLookupTests.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using NuGet.Indexing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IndexingTests
+{
+    public class DownloadLookupTests
+    {
+        [Theory]
+        [InlineData(@"[[""AutofacContrib.NSubstitute"",[""2.4.3.700"",406],[""2.5.0"",137]],[""Assman.Core"",[""2.0.7"",138]]]")]
+        public void DownloadLookUp_ValidJson(string json)
+        {
+            StringDownloadLookup downloadLookup = new StringDownloadLookup(json);
+            IDictionary<string,IDictionary<string,int>> packages = downloadLookup.Load();
+            Assert.True(packages != null & packages.Count() == 2);
+            IDictionary<string,int> versions = new Dictionary<string,int>();
+            Assert.True(packages.TryGetValue("AutofacContrib.NSubstitute".ToLowerInvariant(), out versions));
+            Assert.True(versions.Count() == 2);
+            Assert.True(versions.Values.ToArray()[0] == 406 && versions.Values.ToArray()[1] == 137);
+        }
+
+        [Theory]
+        [InlineData(@"[[""AutofacContrib.NSubstitute"",[""2.4.3.700"",406],[""2.5.0"",137]],[""AutofacContrib.NSubstitute"",[""3"",0],[""2.5"",0]]]")]
+        public void DownloadLookUp_NoExceptionThrownWithDeuplicateKeys(string json)
+        {
+            StringDownloadLookup downloadLookup = new StringDownloadLookup(json);
+            IDictionary<string, IDictionary<string, int>> packages = downloadLookup.Load();
+            // Dict should have only one entry when there are duplicate entries.
+            Assert.True(packages != null & packages.Count() == 1);
+            IDictionary<string, int> versions = new Dictionary<string, int>();
+            Assert.True(packages.TryGetValue("AutofacContrib.NSubstitute".ToLowerInvariant(), out versions));
+            Assert.True(versions.Count() == 2);
+            // When duplicate entries are present,the first one should be taken and the consecutive ones should be ignored.
+            Assert.True(versions.Values.ToArray()[0] == 406 && versions.Values.ToArray()[1] == 137);
+        }
+
+        [Theory]
+        [InlineData(@"[[""AutofacContrib.NSubstitute"",[2.4.3.700,406],[""2.5.0"",137]],[""Assman.Core"",[""2.0.7"",138]]]")]
+        public void DownloadLookUp_InvalidEntriesIgnored(string json)
+        {
+            StringDownloadLookup downloadLookup = new StringDownloadLookup(json);
+            IDictionary<string, IDictionary<string, int>> packages = downloadLookup.Load();
+            // Out of two entries one is invalid. So dict count should be 1.
+            Assert.True(packages != null & packages.Count() == 1);
+            IDictionary<string, int> versions = new Dictionary<string, int>();
+            Assert.False(packages.TryGetValue("AutofacContrib.NSubstitute".ToLowerInvariant(), out versions));
+            Assert.True(packages.TryGetValue("Assman.Core".ToLowerInvariant(), out versions));
+            Assert.True(versions.Count() == 1);
+            // When duplicate entries are present,the first one should be taken and the consecutive ones should be ignored.
+            Assert.True(versions.Values.ToArray()[0] == 138 && versions.Keys.ToArray()[0] == "2.0.7");
+        }
+
+        [Theory]
+        [InlineData(@"[WDSD$%^")]
+        public void DownloadLookUp_InvalidJsonDoesntThrowsException(string json)
+        {
+            StringDownloadLookup downloadLookup = new StringDownloadLookup(json);
+            IDictionary<string, IDictionary<string, int>> packages = downloadLookup.Load();
+            // Invaid json should return empty dictionary.
+            Assert.True(packages != null & packages.Count() == 0);         
+        }
+    }
+
+    public class StringDownloadLookup : DownloadLookup
+    {
+        string _path;
+
+        public override string Path { get { return _path; } }
+
+        public StringDownloadLookup(string json)
+        {
+            _path = json;
+        }
+        protected override JsonReader GetReader()
+        {
+            return new JsonTextReader(new StringReader(Path));
+        }
+    }
+}
