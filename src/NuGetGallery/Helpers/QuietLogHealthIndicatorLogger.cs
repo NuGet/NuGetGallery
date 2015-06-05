2cb41eb18cf5433611cde1b586fc8767adecf923
Maarten Balliauw <maarten@maartenballiauw.be>
2015-06-05 09:34:16 +0200

    Adding CultureInfo.CurrentCulture to string.Format call

--- src/NuGetGallery/Helpers/QuietLogHealthIndicatorLogger.cs
+++ src/NuGetGallery/Helpers/QuietLogHealthIndicatorLogger.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Globalization;
 using System.ServiceModel;
 using NuGet.Services.Search.Client;
 
@@ -13,13 +14,13 @@ namespace NuGetGallery
         public void LogDecreaseHealth(Uri endpoint, int health, Exception exception)
         {
             QuietLog.LogHandledException(
-                new EndpointNotFoundException(string.Format("ServiceDiscovery - Endpoint '{0}' degraded. Current health: {1}. See inner exception for details.", endpoint, health), exception));
+                new EndpointNotFoundException(string.Format(CultureInfo.CurrentCulture, "ServiceDiscovery - Endpoint '{0}' degraded. Current health: {1}. See inner exception for details.", endpoint, health), exception));
         }
 
         public void LogIncreaseHealth(Uri endpoint, int health)
         {
             QuietLog.LogHandledException(
-                new EndpointNotFoundException(string.Format("ServiceDiscovery - Endpoint '{0}' health increased. Current health: {1}.", endpoint, health)));
+                new EndpointNotFoundException(string.Format(CultureInfo.CurrentCulture, "ServiceDiscovery - Endpoint '{0}' health increased. Current health: {1}.", endpoint, health)));
         }
     }
 }
