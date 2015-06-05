380327c4595913295a6f9989a4b58edab423c5bf
Maarten Balliauw <maarten@maartenballiauw.be>
2015-06-05 09:33:14 +0200

    Throw a more specific exception when logging endpoint health in ELMAH

--- src/NuGetGallery/Helpers/QuietLogHealthIndicatorLogger.cs
+++ src/NuGetGallery/Helpers/QuietLogHealthIndicatorLogger.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.ServiceModel;
 using NuGet.Services.Search.Client;
 
 namespace NuGetGallery
@@ -12,13 +13,13 @@ namespace NuGetGallery
         public void LogDecreaseHealth(Uri endpoint, int health, Exception exception)
         {
             QuietLog.LogHandledException(
-                new Exception(string.Format("ServiceDiscovery - Endpoint '{0}' degraded. Current health: {1}. See inner exception for details.", endpoint, health), exception));
+                new EndpointNotFoundException(string.Format("ServiceDiscovery - Endpoint '{0}' degraded. Current health: {1}. See inner exception for details.", endpoint, health), exception));
         }
 
         public void LogIncreaseHealth(Uri endpoint, int health)
         {
             QuietLog.LogHandledException(
-                new Exception(string.Format("ServiceDiscovery - Endpoint '{0}' health increased. Current health: {1}.", endpoint, health)));
+                new EndpointNotFoundException(string.Format("ServiceDiscovery - Endpoint '{0}' health increased. Current health: {1}.", endpoint, health)));
         }
     }
 }
