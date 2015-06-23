8bb62188d0b414ef2af18291afde4c399c152062
Xavier Decoster <xavier.decoster@outlook.com>
2015-06-23 12:58:12 +0200

    fix mvc binding redirect test

--- tests/NuGetGallery.Facts/AppConfigIsCorrectlyApplied.cs
+++ tests/NuGetGallery.Facts/AppConfigIsCorrectlyApplied.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
 using System;
 using System.Configuration;
 using Xunit;
@@ -12,17 +13,17 @@ namespace NuGetGallery
         public void VerifyAppDomainHasConfigurationSettings()
         {
             string value = ConfigurationManager.AppSettings["YourTestsAreNotGoingInsane"];
-            Assert.False(String.IsNullOrEmpty(value), "App.Config not loaded");
+            Assert.False(string.IsNullOrEmpty(value), "App.Config not loaded");
         }
 
         [Fact]
-        public void VerifyBindingRedirectToMvc4IsWorking()
+        public void VerifyBindingRedirectToMvc5IsWorking()
         {
-            // System.Web.Mvc should be binding redirected from version 3.0.0.0 to 4.0.0.1 just like in our actual app.
+            // System.Web.Mvc should be binding redirected from version 3.0.0.0 to 5.2.3.0 just like in our actual app.
             string typeName = "System.Web.Mvc.Controller, System.Web.Mvc, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
             Type resolvedType = Type.GetType(typeName);
             Version runtimeVersion = resolvedType.Assembly.GetName().Version;
-            Assert.Equal(new Version("4.0.0.1"), runtimeVersion);
+            Assert.Equal(new Version("5.2.3.0"), runtimeVersion);
         }
     }
 }
