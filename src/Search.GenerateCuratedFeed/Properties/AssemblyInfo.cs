08385709c81f69678f7bdfb89ab6f79181ce8234
johnataylor <johtaylo@microsoft.com>
2015-06-22 17:36:09 -0700

    added curated feed report pivoted on feed

--- src/Search.GenerateCuratedFeed/Properties/AssemblyInfo.cs
+++ src/Search.GenerateCuratedFeed/Properties/AssemblyInfo.cs
@@ -1,15 +1,18 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CatalogCollector-PackageRegistrationBlobs")]
+[assembly: AssemblyTitle("Search.GenerateCuratedFeed")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyProduct("CatalogCollector-PackageRegistrationBlobs")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Search.GenerateCuratedFeed")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -17,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c077a100-c585-4013-b04f-3cd12327d53d")]
+[assembly: Guid("caa62ebc-9ae1-4ff5-b162-ba2fd979868d")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ src/Search.GenerateCuratedFeed/Search.GenerateCuratedFeed.Job.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NuGet.Jobs.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Search.GenerateCuratedFeed
+{
+    internal class Job : JobBase
+    {
+        public const string DefaultContainerName = "ng-search-data";
+        public const string ReportName = "curatedfeeds.json";
+
+        public SqlConnectionStringBuilder Source { get; set; }
+        public CloudStorageAccount Destination { get; set; }
+        public CloudBlobContainer DestinationContainer { get; set; }
+        public string DestinationContainerName { get; set; }
+        public string OutputDirectory { get; set; }
+
+        public override async Task<bool> Run()
+        {
+            Trace.TraceInformation(String.Format("Generating Curated feed report from {0}/{1}.", Source.DataSource, Source.InitialCatalog));
+
+            using (SqlConnection connection = new SqlConnection(Source.ConnectionString))
+            {
+                connection.Open();
+
+                string sql = LoadResource("Scripts.CuratedFeed.sql");
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                command.CommandType = CommandType.Text;
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                JArray result = JsonHelper.SqlDataReader2Json(reader, "FeedName", "Id");
+
+                Trace.TraceInformation(String.Format("Gathered {0} rows of data.", result.Count));
+
+                await WriteReport(result.ToString(Formatting.None), ReportName, Formatting.None);
+            }
+
+            return true;
+        }
+
+        static string LoadResource(string resourceName)
+        {
+            string name = Assembly.GetExecutingAssembly().GetName().Name;
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name + "." + resourceName);
+            return new StreamReader(stream).ReadToEnd();
+        }
+
+        protected async Task WriteReport(string report, string name, Formatting formatting)
+        {
+            if (!String.IsNullOrEmpty(OutputDirectory))
+            {
+                await WriteToFile(report, name);
+            }
+            else
+            {
+                await DestinationContainer.CreateIfNotExistsAsync();
+                await WriteToBlob(report, name);
+            }
+        }
+
+        public override bool Init(IDictionary<string, string> jobArgsDictionary)
+        {
+
+            Source =
+                new SqlConnectionStringBuilder(
+                    JobConfigManager.GetArgument(jobArgsDictionary,
+                        JobArgumentNames.SourceDatabase,
+                        EnvironmentVariableKeys.SqlGallery));
+
+            OutputDirectory = JobConfigManager.GetArgument(jobArgsDictionary,
+                       JobArgumentNames.OutputDirectory);
+
+            if (string.IsNullOrEmpty(OutputDirectory))
+            {
+                Destination = CloudStorageAccount.Parse(
+                                           JobConfigManager.GetArgument(jobArgsDictionary,
+                                               JobArgumentNames.PrimaryDestination, EnvironmentVariableKeys.StoragePrimary));
+
+                DestinationContainerName = JobConfigManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.DestinationContainerName) ?? DefaultContainerName;
+
+
+                DestinationContainer = Destination.CreateCloudBlobClient().GetContainerReference(DestinationContainerName);
+            }
+
+            return true;
+
+        }
+
+        #region PrivateMembers
+
+        private async Task WriteToFile(string report, string name)
+        {
+            string fullPath = Path.Combine(OutputDirectory, name);
+            string parentDir = Path.GetDirectoryName(fullPath);
+            Trace.TraceInformation(String.Format("Writing report to {0}", fullPath));
+
+            if (!Directory.Exists(parentDir))
+            {
+                Directory.CreateDirectory(parentDir);
+            }
+            if (File.Exists(fullPath))
+            {
+                File.Delete(fullPath);
+            }
+            using (var writer = new StreamWriter(File.OpenWrite(fullPath)))
+            {
+                await writer.WriteAsync(report);
+            }
+
+            Trace.TraceInformation(String.Format("Wrote report to {0}", fullPath));
+        }
+
+        private async Task WriteToBlob(string report, string name)
+        {
+            var blob = DestinationContainer.GetBlockBlobReference(name);
+            Trace.TraceInformation(String.Format("Writing report to {0}", blob.Uri.AbsoluteUri));
+
+            blob.Properties.ContentType = "application/json";
+            await blob.UploadTextAsync(report);
+
+            Trace.TraceInformation(String.Format("Wrote report to {0}", blob.Uri.AbsoluteUri));
+        }
+
+        #endregion PrivateMembers
+    }
+
+}
+
