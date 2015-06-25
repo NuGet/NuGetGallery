453b55895b1e80aa70a92a29022754a1b776fbfe
Xavier Decoster <xavier.decoster@outlook.com>
2015-06-25 16:46:57 +0200

    added a bit more tracing

--- src/NuGetGallery.Cloud/DeployToAzure.ps1
+++ src/NuGetGallery.Cloud/DeployToAzure.ps1
@@ -157,7 +157,9 @@ function ConfigureDiagnostics([string]$roleName)
 
 	$storageContext = New-AzureStorageContext -ConnectionString $diagnosticsConfigurationSetting.value
 
+	Write-Host "Configuring diagnostics for '$OctopusAzureServiceName' (role: $roleName, slot: $OctopusAzureSlot)..."
 	Set-AzureServiceDiagnosticsExtension -ServiceName $OctopusAzureServiceName -Slot $OctopusAzureSlot -DiagnosticsConfigurationPath $config -StorageContext $storageContext -Verbose
+	Write-Host "Configured diagnostics for role $roleName."
 }
 
 CreateOrUpdate
