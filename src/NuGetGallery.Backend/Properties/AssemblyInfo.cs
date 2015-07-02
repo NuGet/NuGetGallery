acb3bb0942f401fad2cf674c4fecc80368d255af
Xavier Decoster <xavier.decoster@outlook.com>
2015-07-02 10:12:03 +0200

    removed obsolete NuGetGallery.Backend code and tests

--- src/NuGetGallery.Backend/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("NuGetGallery.Backend")]
-[assembly: AssemblyDescription("Background worker for performing regular maintainance on the NuGet Gallery.")]
