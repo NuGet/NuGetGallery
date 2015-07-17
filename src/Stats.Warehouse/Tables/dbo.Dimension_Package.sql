b225596440e865a0970d6f79826623fd8f684116
Xavier Decoster <xavier.decoster@outlook.com>
2015-07-17 16:01:20 +0200

    removed obsolete columns from stats warehouse tables

--- src/Stats.Warehouse/Tables/dbo.Dimension_Package.sql
+++ src/Stats.Warehouse/Tables/dbo.Dimension_Package.sql
@@ -2,10 +2,6 @@
     [Id]                 INT            IDENTITY (1, 1) NOT NULL,
     [PackageId]          NVARCHAR (128) NULL,
     [PackageVersion]     NVARCHAR (64)  NULL,
-    [PackageListed]      BIT            DEFAULT ((1)) NOT NULL,
-    [PackageTitle]       NVARCHAR (256) NULL,
-    [PackageDescription] NVARCHAR (MAX) NULL,
-    [PackageIconUrl]     NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_Dimension_Package] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
