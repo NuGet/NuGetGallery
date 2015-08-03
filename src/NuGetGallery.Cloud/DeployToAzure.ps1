2a9f38eefdafb55cef82df05d531a04e0b1a3178
Maarten Balliauw <maarten@maartenballiauw.be>
2015-08-03 17:59:59 +0200

    Workaround https://github.com/Azure/azure-powershell/issues/193

--- src/NuGetGallery.Cloud/DeployToAzure.ps1
+++ src/NuGetGallery.Cloud/DeployToAzure.ps1
@@ -159,13 +159,13 @@ function ConfigureDiagnostics([string]$roleName)
 
 	Write-Host "Configuring diagnostics for '$OctopusAzureServiceName' (role: $roleName, slot: $OctopusAzureSlot)..."
 	
-	$extension = Get-AzureServiceDiagnosticsExtension -ServiceName $OctopusAzureServiceName -Slot $OctopusAzureSlot -ErrorAction SilentlyContinue -ErrorVariable errorVariable
+	$extension = Get-AzureServiceDiagnosticsExtension -ServiceName $OctopusAzureServiceName -Slot $OctopusAzureSlot -Role 'NuGetGallery' -ErrorAction SilentlyContinue -ErrorVariable errorVariable
 	if (!($?)) {
 		Write-Host "Diagnostics error occurred. Details: $errorVariable"
 	} elseif ($extension -ne $null) {
 		Write-Host "Diagnostics already configured. Skipping."
 	} else {
-		Set-AzureServiceDiagnosticsExtension -ServiceName $OctopusAzureServiceName -Slot $OctopusAzureSlot -DiagnosticsConfigurationPath $config -StorageContext $storageContext -Role $roleName -Verbose
+		Set-AzureServiceDiagnosticsExtension -ServiceName $OctopusAzureServiceName -Slot $OctopusAzureSlot -Role 'NuGetGallery' -DiagnosticsConfigurationPath $config -StorageContext $storageContext -Role $roleName -Verbose
 	}
 	
 	Write-Host "Configured diagnostics for role $roleName."
