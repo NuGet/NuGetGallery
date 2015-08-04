f9a355d1b63509c2c420154fa2cf9ca1fa828d26
Xavier Decoster <xavier.decoster@outlook.com>
2015-08-04 22:23:23 +0200

    use sql function to get clientcategory for client dimension

---
+++ src/Stats.Warehouse/Programmability/Functions/dbo.GetClientCategory.sql
@@ -0,0 +1,56 @@
+﻿CREATE FUNCTION [dbo].[GetClientCategory]
+(
+	@ClientName NVARCHAR(128)
+)
+RETURNS NVARCHAR(50)
+WITH SCHEMABINDING
+AS
+BEGIN
+	IF @ClientName IS NULL
+		RETURN ''
+
+    IF	(
+			-- VS NuGet 2.8+
+			CHARINDEX('NuGet VS PowerShell Console', @ClientName) > 0
+		OR	CHARINDEX('NuGet VS Packages Dialog - Solution', @ClientName) > 0
+		OR	CHARINDEX('NuGet VS Packages Dialog', @ClientName) > 0
+
+			-- VS NuGet (pre-2.8)
+		OR	CHARINDEX('NuGet Add Package Dialog', @ClientName) > 0
+        OR	CHARINDEX('NuGet Command Line', @ClientName) > 0
+        OR	CHARINDEX('NuGet Package Manager Console', @ClientName) > 0
+        OR	CHARINDEX('NuGet Visual Studio Extension', @ClientName) > 0
+        OR	CHARINDEX('Package-Installer', @ClientName) > 0
+		)
+        RETURN 'NuGet'
+
+		-- WebMatrix
+    IF	CHARINDEX('WebMatrix', @ClientName) > 0
+        RETURN 'WebMatrix'
+
+	IF	(
+			-- NuGet Package Explorer
+			CHARINDEX('NuGet Package Explorer Metro', @ClientName) > 0
+		OR	CHARINDEX('NuGet Package Explorer', @ClientName) > 0
+		)
+		RETURN 'NuGet Package Explorer'
+
+    IF (
+			-- Browsers
+			CHARINDEX('Mozilla', @ClientName) > 0
+		OR	CHARINDEX('Firefox', @ClientName) > 0
+		OR	CHARINDEX('Opera', @ClientName) > 0
+		OR	CHARINDEX('Chrome', @ClientName) > 0
+		OR	CHARINDEX('Browser', @ClientName) > 0
+		)
+        RETURN 'Browser'
+
+	IF (
+			-- Bots
+			CHARINDEX('Bot', @ClientName) > 0
+		OR	CHARINDEX('bot', @ClientName) > 0
+		)
+        RETURN 'Bot'
+
+    RETURN ''
+END
--- src/Stats.Warehouse/Tables/dbo.Dimension_Client.sql
+++ src/Stats.Warehouse/Tables/dbo.Dimension_Client.sql
@@ -1,15 +1,16 @@
 ﻿CREATE TABLE [dbo].[Dimension_Client]
 (
-	[Id]                 INT           IDENTITY (1, 1) NOT NULL,
-    [ClientName]         VARCHAR (128) NULL,
-    [Major] VARCHAR(50)           NULL,
-    [Minor] VARCHAR(50)           NULL,
-    [Patch] VARCHAR(50) NULL,
-    [ClientVersion] AS CONCAT(ISNULL([Major], '0'), '.', ISNULL([Minor], '0'), '.', ISNULL([Patch], '0')),
+	[Id]					INT				IDENTITY (1, 1) NOT NULL,
+    [ClientName]			VARCHAR (128)	NULL,
+    [Major]					VARCHAR(50)		NULL,
+    [Minor]					VARCHAR(50)		NULL,
+    [Patch]					VARCHAR(50)		NULL,
+	[ClientVersion]			AS CONCAT(ISNULL([Major], '0'), '.', ISNULL([Minor], '0'), '.', ISNULL([Patch], '0')) PERSISTED,
+	[ClientCategory]		AS [dbo].[GetClientCategory] ([ClientName]) PERSISTED,
     CONSTRAINT [PK_Dimension_Client] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (STATISTICS_NORECOMPUTE = ON)
 )
 GO
 
-CREATE UNIQUE NONCLUSTERED INDEX [IX_Dimension_Client_UniqueIndex] ON [dbo].[Dimension_Client] ([ClientName] ASC, [Major] DESC, [Minor] DESC, [Patch] DESC) INCLUDE ([Id], [ClientVersion])
+CREATE UNIQUE NONCLUSTERED INDEX [IX_Dimension_Client_UniqueIndex] ON [dbo].[Dimension_Client] ([ClientName] ASC, [Major] DESC, [Minor] DESC, [Patch] DESC) INCLUDE ([Id], [ClientVersion], [ClientCategory])
 WITH (STATISTICS_NORECOMPUTE = ON)
 GO
