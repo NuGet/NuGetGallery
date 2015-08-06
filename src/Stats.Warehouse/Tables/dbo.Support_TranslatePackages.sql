df191d5d3d5c689559e3e5fbebd4c79ab416eb71
Xavier Decoster <xavier.decoster@outlook.com>
2015-08-06 13:37:32 +0200

    added support table for correcting incorrectly parsed request urls

---
+++ src/Stats.Warehouse/Tables/dbo.Support_TranslatePackages.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Support_TranslatePackages]
+(
+	[CorrectedPackageId]						INT NOT NULL ,
+	[IncorrectPackageId]		NVARCHAR(128) NOT NULL,
+	[IncorrectPackageVersion]	NVARCHAR(128) NOT NULL,
+
+    CONSTRAINT [PK_Support_TranslatePackages] PRIMARY KEY CLUSTERED ([CorrectedPackageId]) WITH (STATISTICS_NORECOMPUTE = ON),
+    CONSTRAINT [FK_Support_TranslatePackages_Dimension_Package] FOREIGN KEY ([CorrectedPackageId]) REFERENCES [Dimension_Package]([Id])
+)
+GO
+
+CREATE INDEX [IX_Support_TranslatePackages] ON [dbo].[Support_TranslatePackages] ([IncorrectPackageId], [IncorrectPackageVersion]) INCLUDE ([CorrectedPackageId])
+GO
