ca7c5313ec8a4235b494bcec5c3da6cf35de9e02
Xavier Decoster <xavier.decoster@outlook.com>
2015-08-07 10:41:48 +0200

    removing the indexed view for now as it's not leveraged due to non-deterministic filter clauses everywhere

--- src/Stats.Warehouse/Programmability/Stored Procedures/dbo.SelectTotalDownloadCountsPerPackageVersion.sql
+++ src/Stats.Warehouse/Programmability/Stored Procedures/dbo.SelectTotalDownloadCountsPerPackageVersion.sql
@@ -3,11 +3,15 @@ AS
 BEGIN
 	SET NOCOUNT ON;
 
-	SELECT	[PackageId],
-			[PackageVersion],
-			SUM([TotalDownloadCount]) AS [TotalDownloadCount]
-	FROM	[dbo].[vwAggregatedDownloads]
-	GROUP BY	[PackageId],
-				[PackageVersion]
+	SELECT	P.[PackageId],
+			P.[PackageVersion],
+			SUM(ISNULL(F.[DownloadCount], 0)) AS [TotalDownloadCount]
+	FROM	[dbo].[Fact_Download] (NOLOCK) AS F
+
+	INNER JOIN	[dbo].[Dimension_Package] AS P (NOLOCK)
+	ON		P.[Id] = F.[Dimension_Package_Id]
+
+	GROUP BY	P.[PackageId],
+				P.[PackageVersion]
 
 END
