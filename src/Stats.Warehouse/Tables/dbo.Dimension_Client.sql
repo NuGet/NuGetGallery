cd9db4950dcd2a258c8e3a56b78fa19dfac88723
Maarten Balliauw <maarten@maartenballiauw.be>
2015-08-31 11:04:54 +0200

    Removed old warehouse report jobs

--- src/Stats.Warehouse/Tables/dbo.Dimension_Client.sql
+++ src/Stats.Warehouse/Tables/dbo.Dimension_Client.sql
@@ -11,9 +11,3 @@
 )
 GO
 
-CREATE UNIQUE NONCLUSTERED INDEX [IX_Dimension_Client_UniqueIndex] ON [dbo].[Dimension_Client] ([ClientName] ASC, [Major] DESC, [Minor] DESC, [Patch] DESC) INCLUDE ([Id], [ClientVersion], [ClientCategory])
-WITH (STATISTICS_NORECOMPUTE = ON)
-GO
-
-CREATE INDEX [IX_Dimension_Client_ClientCategory] ON [dbo].[Dimension_Client] ([ClientCategory]) WITH (STATISTICS_NORECOMPUTE = ON)
-GO
