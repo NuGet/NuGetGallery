b8eeed47e570697398a397a96332152f75e95f83
Xavier Decoster <xavier.decoster@outlook.com>
2015-09-23 09:56:35 +0200

    tests for NuGet.Core and NuGet generic user agents

--- tests/Tests.Stats.ImportAzureCdnStatistics/UserAgentParserFacts.cs
+++ tests/Tests.Stats.ImportAzureCdnStatistics/UserAgentParserFacts.cs
@@ -53,6 +53,8 @@ namespace Tests.Stats.ImportAzureCdnStatistics
             [InlineData("Fiddler", "Fiddler", null, null, null)]
             [InlineData("curl/7.21.0 (x86_64-pc-linux-gnu) libcurl/7.21.0 OpenSSL/0.9.8o zlib/1.2.3.4 libidn/1.18", "curl", "7", "21", "0")]
             [InlineData("Java/1.7.0_51", "Java", "1", "7", "0")]
+            [InlineData("NuGet Core/2.8.6", "NuGet", "2", "8", "6")]
+            [InlineData("NuGet/2.8.6", "NuGet", "2", "8", "6")]
             public void RecognizesCustomClients(string userAgent, string expectedClient, string expectedMajor, string expectedMinor, string expectedPatch)
             {
                 var parsed = _parser.ParseUserAgent(userAgent);
