4710a4e528d42be1dad6c38a624780aa6977e11d
Xavier Decoster <xavier.decoster@outlook.com>
2015-09-24 18:39:05 +0200

    fix missing group by statement

--- src/Stats.Warehouse/Programmability/Stored Procedures/dbo.DownloadReportRecentPopularityDetailByPackage.sql
+++ src/Stats.Warehouse/Programmability/Stored Procedures/dbo.DownloadReportRecentPopularityDetailByPackage.sql
@@ -12,26 +12,19 @@ BEGIN
 			P.PackageVersion,
 			C.ClientCategory,
 			CASE
-				WHEN	C.ClientCategory = 'Script'
-				THEN	'Scripted Downloads'
-				WHEN	C.ClientCategory = 'Browser'
-				THEN	'Browsers'
-				WHEN	C.ClientCategory = 'Mobile'
-				THEN	'Browsers (Mobile)'
-				WHEN	C.ClientName = 'NuGet'
-				THEN	'NuGet.Core-based Downloads'
-				WHEN	C.ClientName = 'NuGet Shim'
-				THEN	'DNX Utility'
+				WHEN	C.ClientCategory = 'Script' THEN 'Scripted Downloads'
+				WHEN	C.ClientCategory = 'Browser' THEN 'Browsers'
+				WHEN	C.ClientCategory = 'Mobile' THEN 'Browsers (Mobile)'
+				WHEN	C.ClientName = 'NuGet' THEN 'NuGet.Core-based Downloads'
+				WHEN	C.ClientName = 'NuGet Shim' THEN 'DNX Utility'
 				ELSE	C.ClientName
 			END AS ClientName,
 			CASE
-				WHEN	C.ClientCategory IN ('Script', 'Browser', 'Mobile')
-				THEN	'0'
+				WHEN	(C.ClientCategory IN ('Script', 'Browser', 'Mobile') OR C.ClientName = 'NuGet Shim') THEN '0'
 				ELSE	C.Major
 			END AS Major,
 			CASE
-				WHEN	C.ClientCategory IN ('Script', 'Browser', 'Mobile')
-				THEN	'0'
+				WHEN	(C.ClientCategory IN ('Script', 'Browser', 'Mobile') OR C.ClientName = 'NuGet Shim') THEN '0'
 				ELSE	C.Minor
 			END AS Minor,
 			O.Operation,
@@ -60,28 +53,21 @@ BEGIN
 
 	GROUP BY
 				P.PackageVersion,
-				CASE
-					WHEN	C.ClientCategory = 'Script'
-					THEN	'Scripted Downloads'
-					WHEN	C.ClientCategory = 'Browser'
-					THEN	'Browsers'
-					WHEN	C.ClientCategory = 'Mobile'
-					THEN	'Browsers (Mobile)'
-					WHEN	C.ClientName = 'NuGet'
-					THEN	'NuGet.Core-based Downloads'
-					WHEN	C.ClientName = 'NuGet Shim'
-					THEN	'DNX Utility'
-					ELSE	C.ClientName
-				END,
 				C.ClientCategory,
 				CASE
-					WHEN	C.ClientCategory IN ('Script', 'Browser', 'Mobile') OR C.ClientName = 'NuGet Shim'
-					THEN	'0'
+					WHEN	C.ClientCategory = 'Script' THEN 'Scripted Downloads'
+					WHEN	C.ClientCategory = 'Browser' THEN 'Browsers'
+					WHEN	C.ClientCategory = 'Mobile' THEN 'Browsers (Mobile)'
+					WHEN	C.ClientName = 'NuGet' THEN 'NuGet.Core-based Downloads'
+					WHEN	C.ClientName = 'NuGet Shim' THEN 'DNX Utility'
+					ELSE	C.ClientName
+				END,
+				CASE
+					WHEN	(C.ClientCategory IN ('Script', 'Browser', 'Mobile') OR C.ClientName = 'NuGet Shim') THEN '0'
 					ELSE	C.Major
 				END,
 				CASE
-					WHEN	C.ClientCategory IN ('Script', 'Browser', 'Mobile') OR C.ClientName = 'NuGet Shim'
-					THEN	'0'
+					WHEN	(C.ClientCategory IN ('Script', 'Browser', 'Mobile') OR C.ClientName = 'NuGet Shim') THEN '0'
 					ELSE	C.Minor
 				END,
 				O.Operation
