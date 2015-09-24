1c893c2c76d5d298e4b6530cd43b72f17efdba09
Xavier Decoster <xavier.decoster@outlook.com>
2015-09-24 18:15:21 +0200

    recognize NuGet Client v3 as NuGet client

--- src/Stats.Warehouse/Programmability/Functions/dbo.IsNuGetClient.sql
+++ src/Stats.Warehouse/Programmability/Functions/dbo.IsNuGetClient.sql
@@ -14,6 +14,7 @@ BEGIN
 			CHARINDEX('NuGet VS PowerShell Console', @ClientName) > 0
 		OR	CHARINDEX('NuGet VS Packages Dialog - Solution', @ClientName) > 0
 		OR	CHARINDEX('NuGet VS Packages Dialog', @ClientName) > 0
+		OR	CHARINDEX('NuGet Client V3', @ClientName) > 0
 
 			-- VS NuGet (pre-2.8)
 		OR	CHARINDEX('NuGet Add Package Dialog', @ClientName) > 0
