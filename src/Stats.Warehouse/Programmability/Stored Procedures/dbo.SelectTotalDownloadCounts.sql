7833ee51dcf193ada0ec9fdb378d861de9e239ac
Maarten Balliauw <maarten@maartenballiauw.be>
2015-09-24 22:39:20 +0200

    Updated download count correction

--- src/Stats.Warehouse/Programmability/Stored Procedures/dbo.SelectTotalDownloadCounts.sql
+++ src/Stats.Warehouse/Programmability/Stored Procedures/dbo.SelectTotalDownloadCounts.sql
@@ -5,6 +5,6 @@ BEGIN
 
 	-- select total # of downloads + correction from gallery database/old warehouse
 	-- 208000000 is an approximate
-	SELECT	SUM(ISNULL(F.[DownloadCount], 0)) + 208000000 AS [Downloads]
+	SELECT	SUM(ISNULL(F.[DownloadCount], 0)) - 51000000 AS [Downloads]
 	FROM	[dbo].[Fact_Download] (NOLOCK) AS F
 END
