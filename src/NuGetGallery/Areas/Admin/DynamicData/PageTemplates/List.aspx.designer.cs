aaf172aeb876cc692e5128374e2bae14b1b5711f
Maarten Balliauw <maarten@maartenballiauw.be>
2015-10-13 07:50:25 +0200

    Update dynamic data pages

--- src/NuGetGallery/Areas/Admin/DynamicData/PageTemplates/List.aspx.designer.cs
+++ src/NuGetGallery/Areas/Admin/DynamicData/PageTemplates/List.aspx.designer.cs
@@ -49,6 +49,24 @@ namespace NuGetGallery {
         protected global::System.Web.DynamicData.DynamicValidator GridViewValidator;
         
         /// <summary>
+        /// SearchPanel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel SearchPanel;
+        
+        /// <summary>
+        /// search control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox search;
+        
+        /// <summary>
         /// FilterRepeater control.
         /// </summary>
         /// <remarks>
