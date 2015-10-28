0e1f63a45c6b0887378dc1833b1725db444a319a
Maarten Balliauw <maarten@maartenballiauw.be>
2015-10-28 08:00:00 +0100

    Update c2dnx job configuration template

--- src/Ng/Scripts/Catalog2DnxV3.cmd
+++ src/Ng/Scripts/Catalog2DnxV3.cmd
@@ -8,9 +8,9 @@ cd Ng
 	set NUGETJOBS_STORAGE_PRIMARY=#{Jobs.ngcatalog2dnx.Storage.Primary}
 
 	title #{Jobs.ngcatalog2dnx.Title}
-
-    start /w Ng.exe catalog2dnx -source #{Jobs.ngcatalog2dnx.Catalog.Source} -contentBaseAddress #{Jobs.ngcatalog2dnx.ContentBaseAddress} -storageType azure -storageAccountName #{Jobs.ngcatalog2dnx.Registration.StorageAccount.Name} -storageKeyValue #{Jobs.ngcatalog2dnx.Registration.StorageAccount.Key} -storageContainer #{Jobs.ngcatalog2dnx.Registration.Container} -verbose true -interval #{Jobs.ngcatalog2dnx.Interval}
-
+	
+    start /w Ng.exe catalog2dnx -source #{Jobs.ngcatalog2dnx.Catalog.Source} -contentBaseAddress #{Jobs.ngcatalog2dnx.ContentBaseAddress} -storageBaseAddress #{Jobs.ngcatalog2dnx.StorageBaseAddress} -storageType azure -storageAccountName #{Jobs.ngcatalog2dnx.StorageAccount.Name} -storageKeyValue #{Jobs.ngcatalog2dnx.StorageAccount.Key} -storageContainer #{Jobs.ngcatalog2dnx.Container} -verbose true -interval #{Jobs.ngcatalog2dnx.Interval}
+	
 	echo "Finished #{Jobs.ngcatalog2dnx.Title}"
 
 	goto Top
