07b818f2a611e8441538152631158ea2d78063d6
Joel Verhagen <jver@microsoft.com>
2015-11-17 13:58:17 -0800

    Add test project for BasicSearch project

---
+++ tests/NuGet.Services.BasicSearchTests/StartupTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using NuGet.Services.BasicSearch;
+
+namespace NuGet.Services.BasicSearchTests
+{
+    using System.Configuration;
+    using System.Net.Http;
+    using Xunit;
+
+    public class StartupTests
+    {
+        [Fact]
+        public async Task Sandbox()
+        {
+        }
+    }
+}
