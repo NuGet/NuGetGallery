d13b5a3e8b8751d786fa26240edd789b7eb16a93
johnataylor <johtaylo@microsoft.com>
2015-11-23 15:24:46 -0800

    remove Owner from CustomSimilarity

--- src/NuGet.Indexing/CustomSimilarity.cs
+++ src/NuGet.Indexing/CustomSimilarity.cs
@@ -8,7 +8,7 @@ namespace NuGet.Indexing
     {
         public override float LengthNorm(string fieldName, int numTerms)
         {
-            if (fieldName == "TokenizedId" || fieldName == "ShingledId" || fieldName == "Owner" || fieldName == "Title")
+            if (fieldName == "TokenizedId" || fieldName == "ShingledId" || fieldName == "Title")
             {
                 return 1;
             }
