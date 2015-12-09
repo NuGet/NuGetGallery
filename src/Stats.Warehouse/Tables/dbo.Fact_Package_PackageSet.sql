9cffee0d21c3720a6a3d59f92adb7670fd708b02
Xavier Decoster <xavier.decoster@outlook.com>
2015-12-09 16:11:59 +0100

    using packageId instead of ID for packageset relationships

--- src/Stats.Warehouse/Tables/dbo.Fact_Package_PackageSet.sql
+++ src/Stats.Warehouse/Tables/dbo.Fact_Package_PackageSet.sql
@@ -2,8 +2,7 @@
 (
 	[Id]						INT				IDENTITY (1, 1) NOT NULL,
 	[Dimension_PackageSet_Id]	INT				NOT NULL,
-	[Dimension_Package_Id]		INT				NOT NULL,
+	[LowercasedPackageId]		NVARCHAR(255)	NOT NULL,
 	CONSTRAINT [PK_Fact_Package_PackageSet] PRIMARY KEY CLUSTERED ([Id] ASC) WITH (STATISTICS_NORECOMPUTE = ON),
-    CONSTRAINT [FK_Fact_Package_PackageSet_Dimension_Package] FOREIGN KEY ([Dimension_Package_Id]) REFERENCES [dbo].[Dimension_Package]([Id]),
     CONSTRAINT [FK_Fact_Package_PackageSet_Dimension_PackageSet] FOREIGN KEY ([Dimension_PackageSet_Id]) REFERENCES [dbo].[Dimension_PackageSet]([Id])
 )
