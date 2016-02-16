0a9bfd780d66b9b403fc02da3574e40161afd3e1
Maarten Balliauw <maarten@maartenballiauw.be>
2016-02-16 19:35:31 +0100

    OData: apply (some) query options even when hijacked

--- src/NuGetGallery/WebApi/QueryResult.cs
+++ src/NuGetGallery/WebApi/QueryResult.cs
@@ -92,6 +92,8 @@ namespace NuGetGallery.WebApi
 
         public IHttpActionResult GetInnerResult()
         {
+            IQueryable queryResults = null;
+
             // If we're counting, don't set the max page size
             if (FormatAsCountResult)
             {
@@ -102,14 +104,31 @@ namespace NuGetGallery.WebApi
             var queryOptions = _queryOptions;
 
             // If the result is already paged, it has already been queried as well.
-            // Remove options that would further limit the resultset.
+            // Make sure we only apply the options that are allowed at this stage.
             if (_isPagedResult && _generateNextLink != null && _totalResults.HasValue)
             {
+                queryResults = _queryable.ToList().AsQueryable();
+
+                if (queryOptions.Filter != null)
+                {
+                    queryResults = queryOptions.Filter.ApplyTo(queryResults, _querySettings);
+                }
+
+                if (queryOptions.OrderBy != null)
+                {
+                    queryResults = queryOptions.OrderBy.ApplyTo(queryResults, _querySettings);
+                }
+
+                // Remove options that would further limit the resultset.
                 queryOptions = null;
             }
+            else
+            {
+                // Apply the query as-is
+                queryResults = ExecuteQuery(_queryable, queryOptions);
+            }
 
-            // Apply the query
-            var queryResults = ExecuteQuery(_queryable, queryOptions);
+            // Determine the resulting query
             var modelQueryResults = queryResults as IQueryable<TModel>; // cast succeeds if querying on model
             var projectedQueryResults = queryResults as IQueryable<IEdmEntityObject>; // cast succeeds in case of projection
             if (queryResults == null)
