fe59a68efa6f70c92444705ddd31cd547bf8c779
Xavier Decoster <xavier.decoster@outlook.com>
2016-03-24 17:07:14 +0100

    Code cleanup
    Removed dead code

--- src/NuGet.Jobs.Common/StorageHelpers.cs
+++ src/NuGet.Jobs.Common/StorageHelpers.cs
@@ -1,28 +1,22 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
 using System.Globalization;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace NuGet.Jobs
 {
     public static class StorageHelpers
     {
-        private const string _contentTypeJson = "application/json";
-        private static readonly string PackageBackupsDirectory = "packages";
-        private static readonly string PackageBlobNameFormat = "{0}.{1}.nupkg";
-        private static readonly string PackageBackupBlobNameFormat = PackageBackupsDirectory + "/{0}/{1}/{2}.nupkg";
+        private const string _packageBackupsDirectory = "packages";
+        private const string _packageBlobNameFormat = "{0}.{1}.nupkg";
+        private const string _packageBackupBlobNameFormat = _packageBackupsDirectory + "/{0}/{1}/{2}.nupkg";
 
         public static string GetPackageBlobName(string id, string version)
         {
             return string.Format(
                 CultureInfo.InvariantCulture,
-                PackageBlobNameFormat,
+                _packageBlobNameFormat,
                 id,
                 version).ToLowerInvariant();
         }
@@ -31,47 +25,10 @@ namespace NuGet.Jobs
         {
             return string.Format(
                 CultureInfo.InvariantCulture,
-                PackageBackupBlobNameFormat,
+                _packageBackupBlobNameFormat,
                 id.ToLowerInvariant(),
                 version.ToLowerInvariant(),
                 WebUtility.UrlEncode(hash));
-        }
-
-        public static CloudBlobDirectory GetBlobDirectory(CloudStorageAccount account, string path)
-        {
-            var client = account.CreateCloudBlobClient();
-            client.DefaultRequestOptions = new BlobRequestOptions
-            {
-                ServerTimeout = TimeSpan.FromMinutes(5)
-            };
-
-            string[] segments = path.Split('/');
-            string containerName;
-            string prefix;
-
-            if (segments.Length < 2)
-            {
-                // No "/" segments, so the path is a container and the catalog is at the root...
-                containerName = path;
-                prefix = string.Empty;
-            }
-            else
-            {
-                // Found "/" segments, but we need to get the first segment to use as the container...
-                containerName = segments[0];
-                prefix = string.Join("/", segments.Skip(1)) + "/";
-            }
-
-            var container = client.GetContainerReference(containerName);
-            var dir = container.GetDirectoryReference(prefix);
-            return dir;
-        }
-
-        public static async Task UploadJsonBlob(CloudBlobContainer container, string blobName, string content)
-        {
-            CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
-            blob.Properties.ContentType = _contentTypeJson;
-            await blob.UploadTextAsync(content);
         }
     }
 }
---
+++ src/Search.GenerateAuxiliaryData/SqlExportArguments.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using NuGet.Jobs;
+
+namespace Search.GenerateAuxiliaryData
+{
+    internal class SqlExportArguments
+    {
+        public string ConnectionString { get; private set; }
+        public CloudStorageAccount Destination { get; private set; }
+        public CloudBlobContainer DestinationContainer { get; private set; }
+        public string DestinationContainerName { get; private set; }
+        public string OutputDirectory { get; private set; }
+        public string Name { get; private set; }
+
+        public SqlExportArguments(IDictionary<string, string> jobArgsDictionary, string defaultContainerName, string defaultName)
+        {
+            var connStrBldr = new SqlConnectionStringBuilder(
+                JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.SourceDatabase, EnvironmentVariableKeys.SqlGallery));
+
+            ConnectionString = connStrBldr.ToString();
+            OutputDirectory = JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.OutputDirectory);
+
+            if (string.IsNullOrEmpty(OutputDirectory))
+            {
+                Destination = CloudStorageAccount.Parse(
+                    JobConfigurationManager.GetArgument(jobArgsDictionary, JobArgumentNames.PrimaryDestination, EnvironmentVariableKeys.StoragePrimary));
+
+                DestinationContainerName = JobConfigurationManager.TryGetArgument(jobArgsDictionary, JobArgumentNames.DestinationContainerName) ?? defaultContainerName;
+                DestinationContainer = Destination.CreateCloudBlobClient().GetContainerReference(DestinationContainerName);
+            }
+
+            Name = defaultName;
+        }
+    }
+}
