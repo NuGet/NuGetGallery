4583d44492bc1db650e1603e6a447b6c1fd134c3
Xavier Decoster <xavier.decoster@outlook.com>
2016-03-24 13:40:31 +0100

    Add deployment scripts for Search.GenerateAuxiliaryData job

---
+++ src/Search.GenerateAuxiliaryData/Scripts/Search.GenerateAuxiliaryData.cmd
@@ -0,0 +1,16 @@
+@echo OFF
+	
+cd bin
+
+:Top
+	echo "Starting job - #{Jobs.search.generateauxiliarydata.Title}"
+	
+	set NUGETJOBS_STORAGE_PRIMARY=#{Jobs.search.generateauxiliarydata.Storage.Primary}
+
+	title #{Jobs.search.generateauxiliarydata.Title}
+
+    start /w search.generateauxiliarydata.exe -SourceDatabase "#{Jobs.search.generateauxiliarydata.SourceDatabase}" -verbose true -sleep #{Jobs.search.generateauxiliarydata.Sleep}
+
+	echo "Finished #{Jobs.search.generateauxiliarydata.Title}"
+
+	goto Top
