4d61dd7ff8889a2e6106706451ec441151378015
Maarten Balliauw <maarten@maartenballiauw.be>
2016-03-31 12:33:34 +0200

    Revert "Try RAMDirectory as backing directory"
    This reverts commit 820ed3716dec312336e93d66972cbebbd6bee6fe.

--- src/NuGet.Indexing/NuGetSearcherManager.cs
+++ src/NuGet.Indexing/NuGetSearcherManager.cs
@@ -105,7 +105,7 @@ namespace NuGet.Indexing
                 searcherManager = new NuGetSearcherManager(
                     indexContainer,
                     loggerFactory.CreateLogger<NuGetSearcherManager>(),
-                    directory ?? new AzureDirectory(storageAccount, indexContainer, new RAMDirectory()),
+                    directory ?? new AzureDirectory(storageAccount, indexContainer),
                     loader ?? new StorageLoader(storageAccount, dataContainer, loggerFactory.CreateLogger<StorageLoader>()));
             }
 
