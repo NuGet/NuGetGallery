be5afbb535ac11965904a72ee9a06ad663c6e629
Maarten Balliauw <maarten@maartenballiauw.be>
2016-05-13 09:03:46 +0200

    Add summary to NuGetAcronymExpansionProvider

--- src/NuGet.Indexing/NuGetAcronymExpansionProvider.cs
+++ src/NuGet.Indexing/NuGetAcronymExpansionProvider.cs
@@ -8,6 +8,30 @@ using Newtonsoft.Json;
 
 namespace NuGet.Indexing
 {
+    /// <summary>
+    /// This class loads a file that contains the acronyms we want to expand.
+    /// 
+    /// This file could look like:
+    /// 
+    /// {
+    ///     "ef": [
+    ///         "ef",
+    ///         "entity framework"
+    ///     ]
+    /// }
+    /// 
+    /// The above file expands "ef" into both "ef" and "entity framework".
+    /// 
+    /// The acronym itself must be repeated in the list of expansions. This
+    /// approach allows us to "replace" acronyms as well. For example, imagine
+    /// we want to treat "mvc5" as just "mvc". the expansion could look like:
+    /// 
+    /// {
+    ///     "mvc5": [
+    ///         "mvc"
+    ///     ]
+    /// }
+    /// </summary>
     public class NuGetAcronymExpansionProvider : IAcronymExpansionProvider
     {
         public static readonly IAcronymExpansionProvider Instance = new NuGetAcronymExpansionProvider();
