26fb16f71125aadbcfb9e3f772726dcdcec7e7f9
Robert Muehsig <robert.muehsig@codeinside.eu>
2016-06-18 12:18:31 +0200

    Invoke Gallery Publishing via MSBuild

--- build.cmd
+++ build.cmd
@@ -1,3 +1,5 @@
+IF "%~1"=="LocalGalleryPublish" GOTO LocalGalleryPublish
+
 :CheckOS
 IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
 
@@ -8,5 +10,10 @@ GOTO END
 :32BIT
 "%programfiles%\MSBuild\14.0\Bin\msbuild.exe" build.msbuild /tv:14.0 /p:VisualStudioVersion=14.0 /p:ToolsVersion=14.0 %*
 GOTO END
+
+:LocalGalleryPublish
+"%programfiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" build.msbuild /t:LocalGalleryPublish /tv:14.0 /p:VisualStudioVersion=14.0 /p:ToolsVersion=14.0 
+GOTO END
+
 
 :END
