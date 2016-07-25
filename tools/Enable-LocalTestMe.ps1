717f80fb8cb7da610fbc31434a41a467a8a464cb
angrifel <angrifel@users.noreply.github.com>
2016-07-25 03:43:11 -0300

    Reordered operations to check for makecert.exe only when needed. Made cosmetic changes to script to improve readability and consistency. (#3158)
    Add an optional extended description…

--- tools/Enable-LocalTestMe.ps1
+++ tools/Enable-LocalTestMe.ps1
@@ -1,20 +1,16 @@
 param([string]$Subdomain="nuget", [string]$SiteName = "NuGet Gallery", [string]$SitePhysicalPath, [string]$MakeCertPath, [string]$AppCmdPath)
 
-if(!(([Security.Principal.WindowsPrincipal]([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))) {
-    throw "This script must be run as an admin."
+function Get-SiteFQDN() {return "$Subdomain.localtest.me"}
+function Get-SiteHttpHost() {return "$(Get-SiteFQDN):80"}
+function Get-SiteHttpsHost() {return "$(Get-SiteFQDN):443"}
+
+function Get-SiteCertificate([string] $Store, [switch] $UseLocalMachine) {
+    [string] $level = if($UseLocalMachine) {'LocalMachine'} else {'CurrentUser'}
+    return @(dir -l "Cert:\$level\$Store" | where {$_.Subject -eq "CN=$(Get-SiteFQDN)"})
 }
 
-if(!$SitePhysicalPath) {
-    $ScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path;
-    $SitePhysicalPath = Join-Path $ScriptRoot "..\src\NuGetGallery"
-}
-if(!(Test-Path $SitePhysicalPath)) {
-    throw "Could not find site at $SitePhysicalPath. Use -SitePhysicalPath argument to specify the path."
-}
-$SitePhysicalPath = Convert-Path $SitePhysicalPath
-
-# Find Windows SDK
-if(!$MakeCertPath) {
+function Find-MakeCert() {
+    # Find Windows SDK
     $SDKVersion = dir 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Microsoft SDKs\Windows' | 
         where { $_.PSChildName -match "v(?<ver>\d+\.\d+)" } | 
         foreach { New-Object System.Version $($matches["ver"]) } |
@@ -32,20 +28,74 @@ if(!$MakeCertPath) {
     if($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
         $xArch = "x64"
     }
-    $MakeCertPath = Join-Path $WinSDKDir "bin\$xArch\makecert.exe"
+
+    [string] $candidatePath = Join-Path $WinSDKDir "bin\$xArch\makecert.exe"
     
     # If registry scan finds wrong path, try the Azure extension's makecert
-    if(!(Test-Path $MakeCertPath)) {
+    if(!(Test-Path $candidatePath)) {
         if($xArch = "x64"){
-            $MakeCertPath = "${env:ProgramFiles(x86)}/Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\Microsoft Azure Data Lake Tools for Visual Studio 2015\2.0.6000.0\CppSDK\SDK\bin\makecert.exe"
+            $candidatePath = "${env:ProgramFiles(x86)}/Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\Microsoft Azure Data Lake Tools for Visual Studio 2015\2.0.6000.0\CppSDK\SDK\bin\makecert.exe"
         } else {
-            $MakeCertPath = "${env:ProgramFiles}/Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\Microsoft Azure Data Lake Tools for Visual Studio 2015\2.0.6000.0\CppSDK\SDK\bin\makecert.exe"
+            $candidatePath = "${env:ProgramFiles}/Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\Microsoft Azure Data Lake Tools for Visual Studio 2015\2.0.6000.0\CppSDK\SDK\bin\makecert.exe"
         }
+    }
+
+    return $candidatePath
+}
+
+function Initialize-SiteCertificate() {
+    Write-Host "Generating a Self-Signed SSL Certificate for $(Get-SiteFQDN)"
+    & $MakeCertPath -r -pe -n "CN=$Subdomain.localtest.me" -b "`"$([DateTime]::Now.ToString("MM\/dd\/yyy"))`"" -e "`"$([DateTime]::Now.AddYears(10).ToString("MM\/dd\/yyy"))`"" -eku '1.3.6.1.5.5.7.3.1' -ss root -sr localMachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12
+
+    [object] $cert = Get-SiteCertificate -Store 'Root' -UseLocalMachine
+    if($cert -eq $null) {
+        throw "Failed to create an SSL Certificate"
+    }
+
+    return $cert
+}
+
+function Invoke-Netsh() {
+    $argStr = $([String]::Join(" ", $args))
+    Write-Verbose "netsh $argStr"
+    $result = netsh @args
+    $parsed = [Regex]::Match($result, ".*Error: (\d+).*")
+    if($parsed.Success) {
+        $err = $parsed.Groups[1].Value
+        if($err -ne "183") {
+            throw $result
+        }
+    } else {
+        Write-Host $result
     }
 }
 
-if(!(Test-Path $MakeCertPath)) {
-    throw "Could not find makecert.exe in $MakeCertPath!"
+if(!(([Security.Principal.WindowsPrincipal]([System.Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"))) {
+    throw "This script must be run as an admin."
+}
+
+if(!$SitePhysicalPath) {
+    $ScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path;
+    $SitePhysicalPath = Join-Path $ScriptRoot "..\src\NuGetGallery"
+}
+if(!(Test-Path $SitePhysicalPath)) {
+    throw "Could not find site at $SitePhysicalPath. Use -SitePhysicalPath argument to specify the path."
+}
+$SitePhysicalPath = Convert-Path $SitePhysicalPath
+
+# Check for a cert
+$siteCert = Get-SiteCertificate -Store 'Root'
+if($siteCert.Length -eq 0) {
+    $siteCert = Get-SiteCertificate -Store 'Root' -UseLocalMachine
+    if($siteCert.Length -eq 0) {
+        Write-Host $MakeCertPath
+        if(!$MakeCertPath) { $MakeCertPath = Find-MakeCert }
+        Write-Host $MakeCertPath
+
+        if(!(Test-Path $MakeCertPath)) {
+            throw "Could not find makecert.exe in $MakeCertPath!"
+        }
+    }
 }
 
 # Find IIS Express
@@ -69,56 +119,29 @@ if(!(Test-Path $AppCmdPath)) {
     throw "Could not find appcmd.exe in $AppCmdPath!"
 }
 
-function Invoke-Netsh() {
-    $argStr = $([String]::Join(" ", $args))
-    Write-Verbose "netsh $argStr"
-    $result = netsh @args
-    $parsed = [Regex]::Match($result, ".*Error: (\d+).*")
-    if($parsed.Success) {
-        $err = $parsed.Groups[1].Value
-        if($err -ne "183") {
-            throw $result
-        }
-    } else {
-        Write-Host $result
-    }
-}
-
 # Enable access to the necessary URLs
 # S-1-1-0 is the unlocalized version for: user=Everyone 
-Invoke-Netsh http add urlacl "url=http://$Subdomain.localtest.me:80/" "sddl=D:(A;;GX;;;S-1-1-0)"
-Invoke-Netsh http add urlacl "url=https://$Subdomain.localtest.me:443/" "sddl=D:(A;;GX;;;S-1-1-0)"
+Invoke-Netsh http add urlacl "url=http://$(Get-SiteHttpHost)/" "sddl=D:(A;;GX;;;S-1-1-0)"
+Invoke-Netsh http add urlacl "url=https://$(Get-SiteHttpsHost)/" "sddl=D:(A;;GX;;;S-1-1-0)"
 
+$SiteFullName = "$SiteName ($(Get-SiteFQDN))"
 echo $AppCmdPath list site $SiteFullName
-$SiteFullName = "$SiteName ($Subdomain.localtest.me)"
 $sites = @(&$AppCmdPath list site $SiteFullName)
 if($sites.Length -gt 0) {
     Write-Warning "Site '$SiteFullName' already exists. Deleting and recreating."
     &$AppCmdPath delete site "$SiteFullName"
 }
 
-&$AppCmdPath add site /name:"$SiteFullName" /bindings:"http://$Subdomain.localtest.me:80,https://$Subdomain.localtest.me:443" /physicalPath:$SitePhysicalPath
+&$AppCmdPath add site /name:"$SiteFullName" /bindings:"http://$(Get-SiteHttpHost),https://$(Get-SiteHttpsHost)" /physicalPath:$SitePhysicalPath
 
-# Check for a cert
-$cert = @(dir -l "Cert:\CurrentUser\Root" | where {$_.Subject -eq "CN=$Subdomain.localtest.me"})
-if($cert.Length -eq 0) {
-    $cert = @(dir -l "Cert:\LocalMachine\Root" | where {$_.Subject -eq "CN=$Subdomain.localtest.me"})
-}
-
-if($cert.Length -eq 0) {
-    Write-Host "Generating a Self-Signed SSL Certificate for $Subdomain.localtest.me"
+if ($siteCert -eq $null) { 
     # Generate one
-    & $MakeCertPath -r -pe -n "CN=$Subdomain.localtest.me" -b `"$([DateTime]::Now.ToString("MM\/dd\/yyy"))`" -e `"$([DateTime]::Now.AddYears(10).ToString("MM\/dd\/yyy"))`" -eku 1.3.6.1.5.5.7.3.1 -ss root -sr localMachine -sky exchange -sp "Microsoft RSA SChannel Cryptographic Provider" -sy 12
-    $cert = @(dir -l "Cert:\LocalMachine\Root" | where {$_.Subject -eq "CN=$Subdomain.localtest.me"})
+    $siteCert = Initialize-SiteCertificate 
 }
 
-if($cert.Length -eq 0) {
-    throw "Failed to create an SSL Certificate"
-}
-
-Write-Host "Using SSL Certificate: $($cert.Thumbprint)"
+Write-Host "Using SSL Certificate: $($siteCert.Thumbprint)"
 
 # Set the Certificate
-Invoke-Netsh http add sslcert hostnameport="$Subdomain.localtest.me:443" certhash="$($cert.Thumbprint)" certstorename=Root appid="{$([Guid]::NewGuid().ToString())}"
+Invoke-Netsh http add sslcert hostnameport="$(Get-SiteHttpsHost)" certhash="$($siteCert.Thumbprint)" certstorename=Root appid="{$([Guid]::NewGuid().ToString())}"
 
-Write-Host "Ready! All you have to do now is go to your website project properties and set 'http://$Subdomain.localtest.me' as your Project URL!"
+Write-Host "Ready! All you have to do now is go to your website project properties and set 'http://$(Get-SiteFQDN)' as your Project URL!"
