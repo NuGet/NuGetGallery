a304924147986267d0050e9cf1b40a2e1a990569
Maarten Balliauw <maarten@balliauw.be>
2016-08-09 19:30:40 +0200

    Package with Unicode characters is not searched correctly using the gallery #3186 (#3188)

--- src/NuGetGallery/Controllers/PackagesController.cs
+++ src/NuGetGallery/Controllers/PackagesController.cs
@@ -11,6 +11,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Caching;
@@ -349,8 +350,11 @@ namespace NuGetGallery
                 var isIndexed = HttpContext.Cache.Get(isIndexedCacheKey) as bool?;
                 if (!isIndexed.HasValue)
                 {
+                    var normalizedRegistrationId = package.PackageRegistration.Id
+                        .Normalize(NormalizationForm.FormC);
+
                     var searchFilter = SearchAdaptor.GetSearchFilter(
-                            "id:\"" + package.PackageRegistration.Id + "\" AND version:\"" + package.Version + "\"",
+                            "id:\"" + normalizedRegistrationId + "\" AND version:\"" + package.Version + "\"",
                             1, null, SearchFilter.ODataSearchContext);
 
                     searchFilter.IncludePrerelease = true;
