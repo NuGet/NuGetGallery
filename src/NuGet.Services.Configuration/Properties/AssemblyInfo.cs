7489bad944fd5daa15d1c0c5dd29c1d15f583fb1
Xavier Decoster <xavier.decoster@outlook.com>
2016-08-31 13:55:31 +0200

    Added NuGet.Services.Configuration

---
+++ src/NuGet.Services.Configuration/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Services.Configuration")]
+[assembly: AssemblyDescription("Shared configuration management NuGet services.")]
+[assembly: AssemblyProduct("NuGet.Services.Configuration")]
+[assembly: AssemblyCompany(".NET Foundation")]
+[assembly: AssemblyCopyright("Copyright © .NET Foundation 2016")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
---
+++ src/NuGet.Services.Configuration/SecretReaderFactory.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Specialized;
+using Microsoft.Extensions.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using NuGet.Services.KeyVault;
+
+namespace NuGet.Services.Configuration
+{
+    public class SecretReaderFactory : ISecretReaderFactory
+    {
+        private string _vaultName;
+        private string _clientId;
+        private string _certificateThumbprint;
+        private string _storeName;
+        private string _storeLocation;
+        private bool _validateCertificate;
+
+        public SecretReaderFactory(IConfigurationRoot config)
+        {
+            _vaultName = config[Constants.KeyVaultVaultNameKey];
+            _clientId = config[Constants.KeyVaultClientIdKey];
+            _certificateThumbprint = config[Constants.KeyVaultCertificateThumbprintKey];
+            _storeName = config[Constants.KeyVaultStoreNameKey];
+            _storeLocation = config[Constants.KeyVaultStoreLocationKey];
+            _validateCertificate = bool.Parse(config[Constants.KeyVaultValidateCertificateKey]);
+        }
+
+        public ISecretReader CreateSecretReader()
+        {
+            if (string.IsNullOrEmpty(_vaultName))
+            {
+                return new EmptySecretReader();
+            }
+
+            var keyVaultConfiguration = new KeyVaultConfiguration(
+                _vaultName,
+                _clientId,
+                _certificateThumbprint,
+                !string.IsNullOrEmpty(_storeName) ? (StoreName)Enum.Parse(typeof(StoreName), _storeName) : StoreName.My,
+                !string.IsNullOrEmpty(_storeLocation) ? (StoreLocation)Enum.Parse(typeof(StoreLocation), _storeLocation) : StoreLocation.LocalMachine,
+                _validateCertificate);
+
+            return new KeyVaultReader(keyVaultConfiguration);
+        }
+
+        public ISecretInjector CreateSecretInjector(ISecretReader secretReader)
+        {
+            return new SecretInjector(secretReader);
+        }
+    }
+}
