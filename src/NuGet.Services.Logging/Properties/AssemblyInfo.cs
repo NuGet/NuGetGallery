8ed938c7da71135f0b5b3ca6c0a308e1e48f856a
Xavier Decoster <xavier.decoster@outlook.com>
2016-08-31 13:41:03 +0200

    Added NuGet.Services.Logging

---
+++ src/NuGet.Services.Logging/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Services.Logging")]
+[assembly: AssemblyDescription("Shared server-side logging component for NuGet services.")]
+[assembly: AssemblyProduct("NuGet.Services.Logging")]
+[assembly: AssemblyCompany(".NET Foundation")]
+[assembly: AssemblyCopyright("Copyright © .NET Foundation 2016")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1823895d-6969-4368-a2b6-765c2488a215")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
