1bf3b1ff94ed005d4dbe9ef6deb2e7f80d4e49e0
Scott Bommarito <scottbom@microsoft.com>
2016-10-20 13:01:44 -0700

    Added extensions to IDictionary<string, string> for ease of use in jobs (#6)

---
+++ src/NuGet.Services.Configuration/DictionaryExtensions.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace NuGet.Services.Configuration
+{
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Gets the value associated with a key or null if there is no value associated with key.
+        /// </summary>
+        /// <param name="dictionary">Dictionary to get value from.</param>
+        /// <param name="key">The key associated with the desired value.</param>
+        /// <returns>The value associated with key in dictionary or null.</returns>
+        public static string GetOrNull(this IDictionary<string, string> dictionary, string key)
+        {
+            string value;
+            dictionary.TryGetValue(key, out value);
+            return value;
+        }
+
+        /// <summary>
+        /// Gets the value associated with a key and converts it into T or null if there is no value associated with key or the value could not be converted into T.
+        /// </summary>
+        /// <typeparam name="T">Type to convert value into.</typeparam>
+        /// <param name="dictionary">Dictionary to get value from.</param>
+        /// <param name="key">The key associated with the desired value.</param>
+        /// <returns>The value associated with key converted into T or null.</returns>
+        public static T? GetOrNull<T>(this IDictionary<string, string> dictionary, string key) where T : struct
+        {
+            string valueString;
+            if (!dictionary.TryGetValue(key, out valueString))
+            {
+                return null;
+            }
+
+            try
+            {
+                return ConfigurationUtility.ConvertFromString<T>(valueString);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the value associated with a key and converts it into T or throws.
+        /// </summary>
+        /// <typeparam name="T">Type to convert value into.</typeparam>
+        /// <param name="dictionary">Dictionary to get value from.</param>
+        /// <param name="key">The key associated with the desired value.</param>
+        /// <returns>The value associated with key converted into T.</returns>
+        /// <exception cref="KeyNotFoundException">Thrown when there is no value associated with key in the dictionary.</exception>
+        /// <exception cref="NotSupportedException">Thrown when a conversion from string to T is impossible.</exception>
+        public static T GetOrThrow<T>(this IDictionary<string, string> dictionary, string key) where T : struct
+        {
+            return ConfigurationUtility.ConvertFromString<T>(dictionary[key]);
+        }
+    }
+}
---
+++ tests/NuGet.Services.Configuration.Tests/DictionaryExtensionsFacts.cs
@@ -0,0 +1,92 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace NuGet.Services.Configuration.Tests
+{
+    public class DictionaryExtensionsFacts
+    {
+        public static IEnumerable<object[]> ValueData => new[]
+        {
+            new object[] {true},
+            new object[] {false},
+            new object[] {-1},
+            new object[] {1259},    
+            new object[] {DateTime.MinValue},
+            new object[] {DateTime.MinValue.AddYears(100).AddDays(50).AddHours(5).AddSeconds(62)} 
+        };
+
+        private struct NoConversionFromStringToThisStruct
+        {
+        }
+
+        [Fact]
+        public void GetOrNullStringReturnsAndDoesNotThrow()
+        {
+            // Arrange
+            const string key = "key";
+            const string value = "value";
+            const string notKey = "notAKey";
+            IDictionary<string, string> dictionary = new Dictionary<string, string>
+            {
+                {key, value}
+            };
+
+            // Act
+            var valueFromDictionary = dictionary.GetOrNull(key);
+            var notFoundFromDictionary = dictionary.GetOrNull(notKey);
+
+            // Assert
+            Assert.Equal(value, valueFromDictionary);
+            Assert.Equal(default(string), notFoundFromDictionary);
+        }
+
+        [Theory]
+        [MemberData(nameof(ValueData))]
+        public void GetOrNullConvertsAndDoesNotThrow<T>(T value) where T : struct
+        {
+            // Arrange
+            const string key = "key";
+            const string notKey = "notAKey";
+            IDictionary<string, string> dictionary = new Dictionary<string, string>
+            {
+                {key, value.ToString()}
+            };
+
+            // Act
+            var valueFromDictionary = dictionary.GetOrNull<T>(key);
+            var notFoundFromDictionary = dictionary.GetOrNull<T>(notKey);
+            var notSupportedFromDictionary = dictionary.GetOrNull<NoConversionFromStringToThisStruct>(key);
+
+            // Assert
+            Assert.True(valueFromDictionary.HasValue);
+            Assert.Equal(value, valueFromDictionary.Value);
+            Assert.False(notFoundFromDictionary.HasValue);
+            Assert.False(notSupportedFromDictionary.HasValue);
+        }
+
+        [Theory]
+        [MemberData(nameof(ValueData))]
+        public void GetOrThrowConvertsValueAndThrows<T>(T value) where T : struct
+        {
+            // Arrange
+            const string key = "key";
+            const string notKey = "notAKey";
+            IDictionary<string, string> dictionary = new Dictionary<string, string>
+            {
+                {key, value.ToString()}
+            };
+
+            // Act
+            var valueFromDictionary = dictionary.GetOrThrow<T>(key);
+
+            // Assert
+            Assert.Equal(value, valueFromDictionary);
+            Assert.Throws<KeyNotFoundException>(() => dictionary.GetOrThrow<T>(notKey));
+            Assert.Throws<NotSupportedException>(() => dictionary.GetOrThrow<NoConversionFromStringToThisStruct>(key));
+        }
+    }
+}
