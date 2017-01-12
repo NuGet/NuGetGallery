c2c32b29f35b97100686c598da2424fda79c2f28
scottbommarito <scottbom@microsoft.com>
2017-01-11 17:34:52 -0800

    removed another parameter name that breaks fortify/fxcop

--- tests/NuGetGallery.Facts/Services/CloudBlobFileStorageServiceFacts.cs
+++ tests/NuGetGallery.Facts/Services/CloudBlobFileStorageServiceFacts.cs
@@ -146,7 +146,7 @@ namespace NuGetGallery
             }
 
             [Theory]
-            [FolderNamesData(includePermissions: true)]
+            [FolderNamesData(true)]
             public async Task WillSetPermissionsForDemandedFolderInBlobContainers(string folderName, bool isPublic)
             {
                 var fakeBlobContainer = new Mock<ICloudBlobContainer>();
