96c46bd4d6e75ea4827198c57d77c61b1aeb8ce1
scottbommarito <scottbom@microsoft.com>
2017-01-20 11:17:32 -0800

    use init.ps1 from servercommon

--- build.ps1
+++ build.ps1
@@ -2,15 +2,16 @@
 param (
     [ValidateSet("debug", "release")]
     [string]$Configuration = 'debug',
-	[ValidateSet("Release","rtm", "rc", "beta", "beta2", "final", "xprivate", "zlocal")]
+    [ValidateSet("Release","rtm", "rc", "beta", "beta2", "final", "xprivate", "zlocal")]
     [string]$ReleaseLabel = 'zlocal',
     [int]$BuildNumber,
     [switch]$SkipRestore,
     [switch]$CleanCache,
-	[string]$SimpleVersion = '1.0.0',
-	[string]$SemanticVersion = '1.0.0-zlocal',
-	[string]$Branch,
-	[string]$CommitSHA
+    [string]$SimpleVersion = '1.0.0',
+    [string]$SemanticVersion = '1.0.0-zlocal',
+    [string]$Branch,
+    [string]$CommitSHA,
+    [string]$BuildBranch = 'sb-common'
 )
 
 # For TeamCity - If any issue occurs, this script fail the build. - By default, TeamCity returns an exit code of 0 for all powershell scripts, even if they fail
@@ -22,17 +23,21 @@ trap {
     exit 1
 }
 
-. "$PSScriptRoot\build\common.ps1"
+if (-not (Test-Path "$PSScriptRoot/build")) {
+    New-Item -Path "$PSScriptRoot/build" -ItemType "directory"
+}
+wget -Uri "https://raw.githubusercontent.com/NuGet/ServerCommon/$BuildBranch/build/init.ps1" -OutFile "$PSScriptRoot/build/init.ps1"
+. "$PSScriptRoot/build/init.ps1" -Branch "$BuildBranch"
 
 Function Clean-Tests {
-	[CmdletBinding()]
-	param()
-	
-	Trace-Log 'Cleaning test results'
-	
-	Remove-Item (Join-Path $PSScriptRoot "Results.*.xml")
+    [CmdletBinding()]
+    param()
+    
+    Trace-Log 'Cleaning test results'
+    
+    Remove-Item (Join-Path $PSScriptRoot "Results.*.xml")
 }
-	
+    
 Write-Host ("`r`n" * 3)
 Trace-Log ('=' * 60)
 
@@ -43,50 +48,50 @@ if (-not $BuildNumber) {
 Trace-Log "Build #$BuildNumber started at $startTime"
 
 $BuildErrors = @()
-	
+    
 Invoke-BuildStep 'Cleaning test results' { Clean-Tests } `
-	-ev +BuildErrors
+    -ev +BuildErrors
 
 Invoke-BuildStep 'Installing NuGet.exe' { Install-NuGet } `
     -ev +BuildErrors
-	
+    
 Invoke-BuildStep 'Clearing package cache' { Clear-PackageCache } `
     -skip:(-not $CleanCache) `
     -ev +BuildErrors
-	
+    
 Invoke-BuildStep 'Clearing artifacts' { Clear-Artifacts } `
     -ev +BuildErrors
 
 Invoke-BuildStep 'Restoring solution packages' { `
-	Install-SolutionPackages -path (Join-Path $PSScriptRoot ".nuget\packages.config") -output (Join-Path $PSScriptRoot "packages") -excludeversion } `
+    Install-SolutionPackages -path (Join-Path $PSScriptRoot ".nuget\packages.config") -output (Join-Path $PSScriptRoot "packages") -excludeversion } `
     -skip:$SkipRestore `
     -ev +BuildErrors
-	
+    
 Invoke-BuildStep 'Set version metadata in AssemblyInfo.cs' { `
-	param($Path, $Version, $Branch, $Commit)
-	Set-VersionInfo -Path $Path -Version $Version -Branch $Branch -Commit $Commit `
-	} `
-	-args (Join-Path $PSScriptRoot "src\NuGetGallery\Properties\AssemblyInfo.cs"), $SimpleVersion, $Branch, $CommitSHA `
-	-ev +BuildErrors
-	
-Invoke-BuildStep 'Set version metadata in AssemblyInfo.cs' { `
-	param($Path, $Version, $Branch, $Commit)
-	Set-VersionInfo -Path $Path -Version $Version -Branch $Branch -Commit $Commit `
-	} `
-	-args (Join-Path $PSScriptRoot "src\NuGetGallery.Core\Properties\AssemblyInfo.cs"), $SimpleVersion, $Branch, $CommitSHA `
-	-ev +BuildErrors
-		
-Invoke-BuildStep 'Building solution' { 
-	param($Configuration, $BuildNumber, $SolutionPath, $SkipRestore)
-	Build-Solution $Configuration $BuildNumber -MSBuildVersion "14" $SolutionPath -SkipRestore:$SkipRestore -MSBuildProperties "/p:MvcBuildViews=true" `
-	} `
-	-args $Configuration, $BuildNumber, (Join-Path $PSScriptRoot "NuGetGallery.sln"), $SkipRestore `
+    param($Path, $Version, $Branch, $Commit)
+    Set-VersionInfo -Path $Path -Version $Version -Branch $Branch -Commit $Commit `
+    } `
+    -args (Join-Path $PSScriptRoot "src\NuGetGallery\Properties\AssemblyInfo.cs"), $SimpleVersion, $Branch, $CommitSHA `
     -ev +BuildErrors
-	
+    
+Invoke-BuildStep 'Set version metadata in AssemblyInfo.cs' { `
+    param($Path, $Version, $Branch, $Commit)
+    Set-VersionInfo -Path $Path -Version $Version -Branch $Branch -Commit $Commit `
+    } `
+    -args (Join-Path $PSScriptRoot "src\NuGetGallery.Core\Properties\AssemblyInfo.cs"), $SimpleVersion, $Branch, $CommitSHA `
+    -ev +BuildErrors
+        
+Invoke-BuildStep 'Building solution' { 
+    param($Configuration, $BuildNumber, $SolutionPath, $SkipRestore)
+    Build-Solution $Configuration $BuildNumber -MSBuildVersion "14" $SolutionPath -SkipRestore:$SkipRestore -MSBuildProperties "/p:MvcBuildViews=true" `
+    } `
+    -args $Configuration, $BuildNumber, (Join-Path $PSScriptRoot "NuGetGallery.sln"), $SkipRestore `
+    -ev +BuildErrors
+    
 Invoke-BuildStep 'Creating artifacts' {
-		New-Package (Join-Path $PSScriptRoot "src\NuGetGallery.Core\NuGetGallery.Core.csproj") -Configuration $Configuration -BuildNumber $BuildNumber -ReleaseLabel $ReleaseLabel -Version $SemanticVersion `
-		-ev +BuildErrors
-	}
+        New-Package (Join-Path $PSScriptRoot "src\NuGetGallery.Core\NuGetGallery.Core.csproj") -Configuration $Configuration -BuildNumber $BuildNumber -ReleaseLabel $ReleaseLabel -Version $SemanticVersion `
+        -ev +BuildErrors
+    }
 
 Trace-Log ('-' * 60)
 
