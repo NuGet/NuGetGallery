5f872a17e60620d682422030c28164e80e01e87a
skofman <skofman@microsoft.com>
2017-01-20 16:50:27 -0800

    PR comments

--- src/NuGetGallery.Core/CredentialTypes.cs
+++ src/NuGetGallery.Core/CredentialTypes.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NuGetGallery
 {
@@ -23,19 +25,18 @@ namespace NuGetGallery
             return type.StartsWith(Password.Prefix, StringComparison.OrdinalIgnoreCase);
         }
 
+        internal static List<string> SupportedCredentialTypes = new List<string> { Password.Sha1, Password.Pbkdf2, Password.V3, ApiKeyV1 };
+
         /// <summary>
-        /// Forward compatibility - we support only the bellow subset of credentials in this code version.
+        /// Forward compatibility - we support only the below subset of credentials in this code version.
         /// Any unrecognized credential will be ignored.
         /// </summary>
         /// <param name="credential"></param>
         /// <returns></returns>
         public static bool IsSupportedCredential(Credential credential)
         {
-            return string.Compare(credential.Type, Password.Pbkdf2, StringComparison.OrdinalIgnoreCase) == 0 ||
-                   string.Compare(credential.Type, Password.Sha1, StringComparison.OrdinalIgnoreCase) == 0 ||
-                   string.Compare(credential.Type, Password.V3, StringComparison.OrdinalIgnoreCase) == 0 ||
-                   string.Compare(credential.Type, ApiKeyV1, StringComparison.OrdinalIgnoreCase) == 0 ||
-                   credential.Type.StartsWith(ExternalPrefix, StringComparison.OrdinalIgnoreCase);
+            return SupportedCredentialTypes.Any(credType => string.Compare(credential.Type, credType, StringComparison.OrdinalIgnoreCase) == 0)
+                    || credential.Type.StartsWith(ExternalPrefix, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
