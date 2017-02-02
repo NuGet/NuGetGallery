eb7a94f6795abe603749994cbeb6833aed32620f
scottbommarito <scottbom@microsoft.com>
2017-02-02 14:52:13 -0800

    use vstest and export in xml

--- tests/Scripts/Configure-FunctionalTests.ps1
+++ tests/Scripts/Configure-FunctionalTests.ps1
@@ -1,3 +1,9 @@
+# Delete leftover tests
+Get-ChildItem $env:Build_SourcesDirectory | Where-Object {$_.Extension -eq '.trx' -Or $_.Name -match 'functionaltests.*.xml'} | ForEach-Object {
+    Remove-Item $_
+}
+
+# Determine the url of the gallery we are testing.
 function GetUrl()
 {
 	param([string] $xmlString)
@@ -14,6 +20,7 @@ if($env:Slot -eq "Production")
 }
 else
 {
+    # Use the Azure management certificate to find the url of the desired slot
 	$dict = New-Object "System.Collections.Generic.Dictionary``2[System.String,System.String]"
 	$dict.Add('x-ms-version', ' 2014-02-01')
 	$uri =  "https://management.core.windows.net/$env:SubscriptionId/services/hostedservices/$env:CloudServiceName" + "?embed-detail=true"
--- tests/Scripts/RunReadonlyModeTests.bat
+++ tests/Scripts/RunReadonlyModeTests.bat
@@ -12,7 +12,7 @@ REM Required Tools
 set msbuild="%PROGRAMFILES(X86)%\MsBuild\14.0\Bin\msbuild"
 set xunit="..\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"
 set nuget="nuget.exe"
-set mstest="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\mstest.exe"
+set vstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 
 REM Clean previous test results
 if exist resultsfile.trx (
@@ -36,14 +36,14 @@ if not "%errorlevel%"=="0" goto failure
 REM Run functional tests
 set testDir="NuGetGallery.FunctionalTests\bin\%config%"
 copy %nuget% %testDir%
-call %xunit% "%testDir%\NuGetGallery.FunctionalTests.dll" -trait "Category=%testCategory%" -teamcity -html functionaltestsreadonly.html
+call %xunit% "%testDir%\NuGetGallery.FunctionalTests.dll" -trait "Category=%testCategory%" -xml functionaltests.readonly.xml
 
 REM Run web UI and load tests
-call %mstest% /TestContainer:"NuGetGallery.WebUITests.ReadOnlyMode\bin\%config%\NuGetGallery.WebUITests.ReadOnlyMode.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfile.trx
+call %vstest% /TestContainer:"NuGetGallery.WebUITests.ReadOnlyMode\bin\%config%\NuGetGallery.WebUITests.ReadOnlyMode.dll" /logger:trx
 if not "%errorlevel%"=="0" goto failure
 
 REM Run Load tests
-call %mstest% /TestContainer:"NuGetGallery.LoadTests\bin\%config%\NuGetGallery.LoadTests.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:loadtests-resultsfile.trx /category:%testCategory%
+call %vstest% /TestContainer:"NuGetGallery.LoadTests\bin\%config%\NuGetGallery.LoadTests.dll" /logger:trx /TestCaseFilter:"TestCategory=%testCategory%"
 if not "%errorlevel%"=="0" goto failure
 
 :success
