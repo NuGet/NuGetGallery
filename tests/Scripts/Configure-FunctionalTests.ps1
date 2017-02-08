9058f75cf942db9a3ca402628dfc089fb20a2bfc
scottbommarito <scottbom@microsoft.com>
2017-02-08 14:39:40 -0800

    use azure resource manager cmdlets

--- tests/Scripts/Configure-FunctionalTests.ps1
+++ tests/Scripts/Configure-FunctionalTests.ps1
@@ -17,22 +17,25 @@ Get-ChildItem "$PSScriptRoot\.." -Recurse | Where-Object {$_.Extension -eq '.trx
 # Determine the url to run the tests against
 if ($Slot -eq "Production")
 {
+    # If the slot is 'Production', then we can assume that we are testing the same url as the service root. 
     $GalleryUrl = $ServiceRoot
 }
 else
 {
     # Use Azure PowerShell cmdlets to find the url of the desired slot
-    try
+    Try
     {
         Write-Host "Logging into Azure as service principal."
+        # Log into Azure using a service principal with a certificate
         $login = Add-AzureRmAccount -ApplicationId "$ApplicationId" -CertificateThumbprint "$AzureCertificateThumbprint" -ServicePrincipal -SubscriptionId "$SubscriptionId" -TenantId "$TenantId"
-        $subscriptionName = $login.Context.Subscription.SubscriptionName
-        Write-Host "Using $subscriptionName subscription."
-        Select-AzureSubscription -SubscriptionName "$subscriptionName"
-        Write-Host "Fetching url of $Slot slot of $CloudServiceName."
-        $GalleryUrl = (Get-AzureDeployment -ServiceName "$CloudServiceName" -Slot "$Slot").Url
+        # Get the resource group name of the cloud service resource
+        $resourceGroupName = (Find-AzureRmResource -ResourceNameEquals "$CloudServiceName").ResourceGroupName
+        # Use the resource group name to construct the id of the slot resource
+        $slotResource = Get-AzureRmResource -Id "/subscriptions/$SubscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.ClassicCompute/domainNames/$CloudServiceName/slots/$Slot"
+        # Get the uri of the slot resource
+        $GalleryUrl = $slotResource.Properties.uri
     }
-    catch [System.Exception]
+    Catch [System.Exception]
     {
         Write-Host "Failed to retrieve URL for testing!"
         Write-Host $_.Exception.Message
