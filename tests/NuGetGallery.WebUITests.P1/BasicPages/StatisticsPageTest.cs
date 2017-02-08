338e2352917fb6e77571fc034b854ae06c81bd33
Joel Verhagen <jver@microsoft.com>
2017-02-08 11:13:20 -0800

    Make the statistics page UI test not depend on a specific page ID

--- tests/NuGetGallery.WebUITests.P1/BasicPages/StatisticsPageTest.cs
+++ tests/NuGetGallery.WebUITests.P1/BasicPages/StatisticsPageTest.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using System.Web.UI;
 using Microsoft.VisualStudio.TestTools.WebTesting;
 using NuGetGallery.FunctionalTests.Helpers;
 
@@ -23,10 +22,10 @@ namespace NuGetGallery.FunctionalTests.WebUITests.BasicPages
         {
             var statsPageRequest = new WebTestRequest(UrlHelper.StatsPageUrl);
 
-            // Checks for the presence of a link to jquery package. It is harded to Jquery for now as there is no API exposed for stats
-            // and also Jquery is going to be one of the top 10 for now.
-            var jQueryPackageValidationRule = AssertAndValidationHelper.GetValidationRuleForHtmlTagInnerText(HtmlTextWriterTag.A.ToString(), HtmlTextWriterAttribute.Href.ToString(), "/packages/EntityFramework/", "EntityFramework");
-            statsPageRequest.ValidateResponse += jQueryPackageValidationRule.Validate;
+            // Checks for the presence of the rank element. This means there is at least one package in the list.
+            var validationRule = AssertAndValidationHelper.GetValidationRuleForFindText(
+                @"<td class=""statistics-rank"">1</td>");
+            statsPageRequest.ValidateResponse += validationRule.Validate;
 
             // Validation rule to check for the default text in stats page.
             var statsPageDefaultTextValidationRule = AssertAndValidationHelper.GetValidationRuleForFindText(Constants.StatsPageDefaultText);
