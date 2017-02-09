3937fd1b04d2b332f673fad0282d99f856897dd2
scottbommarito <scottbom@microsoft.com>
2017-02-08 16:02:52 -0800

    see if vsts supports trx from mstest

--- tests/Scripts/RunP0Tests.bat
+++ tests/Scripts/RunP0Tests.bat
@@ -12,7 +12,7 @@ REM Required Tools
 set msbuild="%PROGRAMFILES(X86)%\MsBuild\14.0\Bin\msbuild"
 set xunit="..\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"
 set nuget="nuget.exe"
-set mstest="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\mstest.exe"
+set mstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\mstest.exe"
 set vstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 
 REM Clean previous test results
@@ -40,7 +40,6 @@ copy %nuget% %testDir%
 call %xunit% "%testDir%\NuGetGallery.FunctionalTests.dll" -trait "Category=%testCategory%" -xml functionaltests.P0.xml
 
 REM Run web UI tests
-call %vstest% "NuGetGallery.WebUITests.P0\bin\%config%\NuGetGallery.WebUITests.P0.dll" /logger:trx
 call %mstest% /TestContainer:"NuGetGallery.WebUITests.P0\bin\%config%\NuGetGallery.WebUITests.P0.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfile.trx
 if not "%errorlevel%"=="0" goto failure
 
