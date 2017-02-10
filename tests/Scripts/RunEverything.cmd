4b99fbf52be57171d9918a2fe7c5acf2d388bea8
Scott Bommarito <scottbom@microsoft.com>
2017-02-10 15:48:34 -0800

    Make functional tests output xml instead of html and use VSTest instead of MSTest (#3526)
    * add script to configure functional tests
    
    * use vstest and export in xml
    
    * indentation fix
    
    * fix indentation in .bat files
    
    * log exception
    
    * use azure service principal with cmdlets
    
    * logging improvements
    
    * get help of add-azurermaccount test
    
    * use "-Tenant" instead of "-TenantId"
    
    * switch back to using "-TenantId"
    
    * add select-azurermcontext call
    
    * Set-AzureRmContext
    
    * Select-AzureSubscription
    
    * get subscription name from login
    
    * subscription name logging
    
    * use azure resource manager cmdlets
    
    * replace http with https in slot resource uri
    
    * "and make sure it is https"
    
    * try using mstest to run web ui tests
    
    * see if vsts supports trx from mstest
    
    * use mstest for web tests and delete artifacts in script
    
    * run all tests if some fail
    
    * xunit version to 2.1.0

--- tests/Scripts/RunEverything.cmd
+++ tests/Scripts/RunEverything.cmd
@@ -11,21 +11,25 @@ set solutionPath="NuGetGallery.FunctionalTests.sln"
 
 REM Required Tools
 set msbuild="%PROGRAMFILES(X86)%\MsBuild\14.0\Bin\msbuild"
-set xunit="..\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"
+set xunit="..\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"
 set nuget="nuget.exe"
-set mstest="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\mstest.exe"
+set mstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\mstest.exe"
+set vstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 
 REM Clean previous test results
+if exist functionaltests.*.xml (
+    del functionaltests.*.xml
+)
 if exist resultsfile.trx (
-	del resultsfile.trx
+    del resultsfile.trx
 )
 if exist TestResults (
-	rd TestResults /S /Q
+    rd TestResults /S /Q
 )
 
 REM Restore packages
 if not exist nuget (
-	call PowerShell -NoProfile -ExecutionPolicy Bypass -File %cd%\Scripts\DownloadLatestNuGetExeRelease.ps1
+    call PowerShell -NoProfile -ExecutionPolicy Bypass -File %cd%\Scripts\DownloadLatestNuGetExeRelease.ps1
 )
 call %nuget% restore "%solutionPath%" -NonInteractive
 if not "%errorlevel%"=="0" goto failure
@@ -38,27 +42,27 @@ REM Run functional tests
 set testDir="NuGetGallery.FunctionalTests\bin\%config%"
 set fluentTestDir="NuGetGallery.FunctionalTests.Fluent\bin\%config%"
 copy %nuget% %testDir%
-call %xunit% "%testDir%\NuGetGallery.FunctionalTests.dll" -teamcity
+call %xunit% "%testDir%\NuGetGallery.FunctionalTests.dll" -xml functionaltests.everything.xml
 if not "%errorlevel%"=="0" set exitCode=-1
 
 copy %nuget% %fluentTestDir%
-call %xunit% "%fluentTestDir%\NuGetGallery.FunctionalTests.Fluent.dll" -teamcity -html functionaltestseverything.html
+call %xunit% "%fluentTestDir%\NuGetGallery.FunctionalTests.Fluent.dll" -xml functionaltests.fluent.xml
 if not "%errorlevel%"=="0" set exitCode=-1
 
 REM Run web UI tests
-call %mstest% /TestContainer:"NuGetGallery.WebUITests.P0\bin\%config%\NuGetGallery.WebUITests.P0.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfileP0.trx
+call %mstest% /TestContainer:"NuGetGallery.WebUITests.P0\bin\%config%\NuGetGallery.WebUITests.P0.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfile.trx
 if not "%errorlevel%"=="0" set exitCode=-1
 
 REM Run web UI tests
-call %mstest% /TestContainer:"NuGetGallery.WebUITests.P1\bin\%config%\NuGetGallery.WebUITests.P1.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfileP1.trx
+call %mstest% /TestContainer:"NuGetGallery.WebUITests.P1\bin\%config%\NuGetGallery.WebUITests.P1.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfile.trx
 if not "%errorlevel%"=="0" set exitCode=-1
 
 REM Run web UI tests
-call %mstest% /TestContainer:"NuGetGallery.WebUITests.P1\bin\%config%\NuGetGallery.WebUITests.P2.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfileP2.trx
+call %mstest% /TestContainer:"NuGetGallery.WebUITests.P2\bin\%config%\NuGetGallery.WebUITests.P2.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfile.trx
 if not "%errorlevel%"=="0" set exitCode=-1
 
 REM Run Load tests
-call %mstest% /TestContainer:"NuGetGallery.LoadTests\bin\%config%\NuGetGallery.LoadTests.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:loadtests-resultsfile.trx /category:%testCategory%
+call %vstest% /TestContainer:"NuGetGallery.LoadTests\bin\%config%\NuGetGallery.LoadTests.dll" /logger:trx /TestCaseFilter:"TestCategory=%testCategory%"
 if not "%errorlevel%"=="0" set exitCode=-1
 
 goto end
--- tests/Scripts/RunP2Tests.bat
+++ tests/Scripts/RunP2Tests.bat
@@ -7,19 +7,24 @@ REM Configuration
 set config=Release
 set testCategory="P2Tests"
 set solutionPath="NuGetGallery.FunctionalTests.sln"
+set exitCode=0
 
 REM Required Tools
 set msbuild="%PROGRAMFILES(X86)%\MsBuild\14.0\Bin\msbuild"
-set xunit="..\packages\xunit.runner.console.2.0.0\tools\xunit.console.exe"
+set xunit="..\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"
 set nuget="nuget.exe"
-set mstest="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\mstest.exe"
+set mstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\mstest.exe"
+set vstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 
 REM Clean previous test results
+if exist functionaltests.*.xml (
+    del functionaltests.*.xml
+)
 if exist resultsfile.trx (
-	del resultsfile.trx
+    del resultsfile.trx
 )
 if exist TestResults (
-	rd TestResults /S /Q
+    rd TestResults /S /Q
 )
 if exist loadtests-resultsfile.trx (
 	del loadtests-resultsfile.trx
@@ -27,7 +32,7 @@ if exist loadtests-resultsfile.trx (
 
 REM Restore packages
 if not exist nuget (
-	call PowerShell -NoProfile -ExecutionPolicy Bypass -File %cd%\Scripts\DownloadLatestNuGetExeRelease.ps1
+    call PowerShell -NoProfile -ExecutionPolicy Bypass -File %cd%\Scripts\DownloadLatestNuGetExeRelease.ps1
 )
 call %nuget% restore "%solutionPath%" -NonInteractive
 if not "%errorlevel%"=="0" goto failure
@@ -39,18 +44,18 @@ if not "%errorlevel%"=="0" goto failure
 REM Run functional tests
 set testDir="NuGetGallery.FunctionalTests\bin\%config%"
 copy %nuget% %testDir%
-call %xunit% "%testDir%\NuGetGallery.FunctionalTests.dll" -trait "Category=%testCategory%" -teamcity -html functionaltestsP2.html
+call %xunit% "%testDir%\NuGetGallery.FunctionalTests.dll" -trait "Category=%testCategory%" -xml functionaltests.P2.xml
+if not "%errorlevel%"=="0" set exitCode=-1
 
 REM Run web UI and load tests
 call %mstest% /TestContainer:"NuGetGallery.WebUITests.P2\bin\%config%\NuGetGallery.WebUITests.P2.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfile.trx
-if not "%errorlevel%"=="0" goto failure
+if not "%errorlevel%"=="0" set exitCode=-1
 
 REM Run Load tests
-call %mstest% /TestContainer:"NuGetGallery.LoadTests\bin\%config%\NuGetGallery.LoadTests.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:loadtests-resultsfile.trx /category:%testCategory%
-if not "%errorlevel%"=="0" goto failure
+call %vstest% /TestContainer:"NuGetGallery.LoadTests\bin\%config%\NuGetGallery.LoadTests.dll" /logger:trx /TestCaseFilter:"TestCategory=%testCategory%"
+if not "%errorlevel%"=="0" set exitCode=-1
 
-:success
-exit 0
+exit /B %exitCode%
 
 :failure
 exit -1
