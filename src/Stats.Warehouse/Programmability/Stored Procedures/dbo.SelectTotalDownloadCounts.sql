59ffd78390c4daf84157ac97883ead0d641bed31
Scott Bommarito <scottbom@microsoft.com>
2017-02-14 16:21:37 -0800

    Hotfix: stats total download counts overflow

--- src/Stats.Warehouse/Programmability/Stored Procedures/dbo.SelectTotalDownloadCounts.sql
+++ src/Stats.Warehouse/Programmability/Stored Procedures/dbo.SelectTotalDownloadCounts.sql
@@ -4,6 +4,6 @@ BEGIN
 	SET NOCOUNT ON;
 
 	-- select total # of downloads + correction for packages that can not be mapped
-	SELECT	SUM(ISNULL(F.[DownloadCount], 0)) - 21000000 AS [Downloads]
+	SELECT	SUM(CAST(ISNULL(F.[DownloadCount], 0) AS BIGINT)) - 21000000 AS [Downloads]
 	FROM	[dbo].[Fact_Download] (NOLOCK) AS F
 END
