6fee629d9198d49b412d1abe289f163d808db17a
Shishir H <shishir.f22@gmail.com>
2017-02-16 12:10:01 -0800

    Fix wording for expiring API keys email notification (#105)
    * Fix sql query to get apikey v2 expiring api keys
    
    * Fix email wording and aggregate the emails by users
    
    * format mail
    
    * Fix resend email logic
    
    * add app host
    
    * format messaging
    
    * Nit fixes
    
    * Nit fixes
    
    * address feedback
    
    * Fix exception logging
    
    * undo app host changes

--- src/Gallery.CredentialExpiration/Models/ExpiredCredentialData.cs
+++ src/Gallery.CredentialExpiration/Models/ExpiredCredentialData.cs
@@ -5,11 +5,17 @@ using System;
 
 namespace Gallery.CredentialExpiration.Models
 {
+    public static class Constants
+    {
+        public const string NonScopedApiKeyDescription = "Full access API key";
+    }
+
     public class ExpiredCredentialData
     {
         public string Type { get; set; }
         public string Username { get; set; }
         public string EmailAddress { get; set; }
+        public string Description { get; set; }
         public DateTimeOffset Created { get; set; }
         public DateTimeOffset Expires { get; set; }
     }
