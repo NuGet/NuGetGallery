03d9a4ae3adbcecc6eca67647b2b0f6d983e3e38
Xavier Decoster <xavier.decoster@outlook.com>
2017-03-01 13:16:18 +0100

    Fix titles of supportrequest notifications jobs

--- src/NuGet.SupportRequests.Notifications/Scripts/OnCallDailyNotification.cmd
+++ src/NuGet.SupportRequests.Notifications/Scripts/OnCallDailyNotification.cmd
@@ -5,7 +5,7 @@ cd bin
 :Top
 	echo "Starting job - NuGet - SupportRequests.Notifications.OnCallDailyNotification.cmd"
 	
-	title #{Jobs.supportrequests.notifications.Title}
+	title "NuGet - SupportRequests.Notifications.OnCallDailyNotification.cmd"
 
 	start /w nuget.supportrequests.notifications.exe -ConsoleLogOnly -Task "OnCallDailyNotification" -SourceDatabase "Server=tcp:vz2xmz8oda.database.windows.net;Database=nuget-prod-supportrequest;Persist Security Info=False;User ID=$$Prod-SupportRequestDBReadOnly-UserName$$;Password=$$Prod-SupportRequestDBReadOnly-Password$$;Connect Timeout=30;Encrypt=True" -PagerDutyAccountName "nuget" -PagerDutyApiKey "$$Prod-PagerDuty-ApiKey$$" -SmtpUri "smtps://nuget:$$Prod-SendGridSMTP-Password$$@smtp.sendgrid.net:587/" -VaultName "#{Deployment.Azure.KeyVault.VaultName}" -ClientId "#{Deployment.Azure.KeyVault.ClientId}" -CertificateThumbprint "#{Deployment.Azure.KeyVault.CertificateThumbprint}" -InstrumentationKey "#{Jobs.supportrequests.notifications.InstrumentationKey}" -verbose true -Once
 
--- src/NuGet.SupportRequests.Notifications/Scripts/WeeklySummaryNotification.cmd
+++ src/NuGet.SupportRequests.Notifications/Scripts/WeeklySummaryNotification.cmd
@@ -5,7 +5,7 @@ cd bin
 :Top
 	echo "Starting job - NuGet - SupportRequests.Notifications.WeeklySummaryNotification.cmd"
 	
-	title #{Jobs.supportrequests.notifications.Title}
+	title "NuGet - SupportRequests.Notifications.WeeklySummaryNotification.cmd"
 
 	start /w nuget.supportrequests.notifications.exe -ConsoleLogOnly -Task "WeeklySummaryNotification" -TargetEmailAddress "#{Jobs.supportrequests.notifications.weeklysummarynotification.TargetEmailAddress}" -SourceDatabase "Server=tcp:vz2xmz8oda.database.windows.net;Database=nuget-prod-supportrequest;Persist Security Info=False;User ID=$$Prod-SupportRequestDBReadOnly-UserName$$;Password=$$Prod-SupportRequestDBReadOnly-Password$$;Connect Timeout=30;Encrypt=True" -PagerDutyAccountName "nuget" -PagerDutyApiKey "$$Prod-PagerDuty-ApiKey$$" -SmtpUri "smtps://nuget:$$Prod-SendGridSMTP-Password$$@smtp.sendgrid.net:587/" -VaultName "#{Deployment.Azure.KeyVault.VaultName}" -ClientId "#{Deployment.Azure.KeyVault.ClientId}" -CertificateThumbprint "#{Deployment.Azure.KeyVault.CertificateThumbprint}" -InstrumentationKey "#{Jobs.supportrequests.notifications.InstrumentationKey}" -verbose true -Once
 
