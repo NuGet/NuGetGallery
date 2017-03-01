7a000f0ee56fc9997d799990b099a7273caac8a1
scottbommarito <scottbom@microsoft.com>
2017-03-01 10:56:37 -0800

    fix readonly functional tests

--- tests/Scripts/RunReadonlyModeTests.bat
+++ tests/Scripts/RunReadonlyModeTests.bat
@@ -7,14 +7,19 @@ REM Configuration
 set config=Release
 set testCategory="ReadonlyModeTests"
 set solutionPath="NuGetGallery.FunctionalTests.sln"
+set exitCode=0
 
 REM Required Tools
 set msbuild="%PROGRAMFILES(X86)%\MsBuild\14.0\Bin\msbuild"
 set xunit="..\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"
 set nuget="nuget.exe"
+set mstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\mstest.exe"
 set vstest="C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 
 REM Clean previous test results
+if exist functionaltests.*.xml (
+    del functionaltests.*.xml
+)
 if exist resultsfile.trx (
     del resultsfile.trx
 )
@@ -37,17 +42,17 @@ REM Run functional tests
 set testDir="NuGetGallery.FunctionalTests\bin\%config%"
 copy %nuget% %testDir%
 call %xunit% "%testDir%\NuGetGallery.FunctionalTests.dll" -trait "Category=%testCategory%" -xml functionaltests.readonly.xml
+if not "%errorlevel%"=="0" set exitCode=-1
 
 REM Run web UI and load tests
-call %vstest% /TestContainer:"NuGetGallery.WebUITests.ReadOnlyMode\bin\%config%\NuGetGallery.WebUITests.ReadOnlyMode.dll" /logger:trx
-if not "%errorlevel%"=="0" goto failure
+call %mstest% /TestContainer:"NuGetGallery.WebUITests.ReadOnlyMode\bin\%config%\NuGetGallery.WebUITests.ReadOnlyMode.dll" /TestSettings:Local.testsettings /detail:stdout /resultsfile:resultsfile.trx
+if not "%errorlevel%"=="0" set exitCode=-1
 
 REM Run Load tests
 call %vstest% /TestContainer:"NuGetGallery.LoadTests\bin\%config%\NuGetGallery.LoadTests.dll" /logger:trx /TestCaseFilter:"TestCategory=%testCategory%"
-if not "%errorlevel%"=="0" goto failure
+if not "%errorlevel%"=="0" set exitCode=-1
 
-:success
-exit 0
+exit /B %exitCode%
 
 :failure
 exit -1
