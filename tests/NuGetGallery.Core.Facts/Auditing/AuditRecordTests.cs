f581446e6655b2f3093c1786eaa3b9491684a0f3
Damon Tivel <dtivel@microsoft.com>
2017-03-23 13:50:55 -0700

    Add unit test for audit record types

---
+++ tests/NuGetGallery.Core.Facts/Auditing/AuditRecordTests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
+using Xunit;
+
+namespace NuGetGallery.Auditing
+{
+    public class AuditRecordTests
+    {
+        [Fact]
+        public void SubclassingTypeSet_HasNotChanged()
+        {
+            var actualAuditRecordTypeNames = typeof(AuditRecord).Assembly.GetTypes()
+                .Where(type => type.IsClass && !type.IsAbstract && type.IsSubclassOf(typeof(AuditRecord)))
+                .Select(type => type.FullName)
+                .OrderBy(typeName => typeName)
+                .ToArray();
+
+            var expectedAuditRecordTypeCount = 4;
+
+            Assert.True(expectedAuditRecordTypeCount == actualAuditRecordTypeNames.Length,
+                $"Audit record types have been {(actualAuditRecordTypeNames.Length > expectedAuditRecordTypeCount ? "added" : "removed")}.  " +
+                $"Please evaluate this change against known {nameof(AuditingService)} implementations.");
+            Assert.Equal("NuGetGallery.Auditing.FailedAuthenticatedOperationAuditRecord", actualAuditRecordTypeNames[0]);
+            Assert.Equal("NuGetGallery.Auditing.PackageAuditRecord", actualAuditRecordTypeNames[1]);
+            Assert.Equal("NuGetGallery.Auditing.PackageRegistrationAuditRecord", actualAuditRecordTypeNames[2]);
+            Assert.Equal("NuGetGallery.Auditing.UserAuditRecord", actualAuditRecordTypeNames[3]);
+        }
+    }
+}
