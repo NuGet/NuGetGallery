b24f0845f9a800e36a881554cceb43be1dfbbe22
Christy Henriksson <chenriks@microsoft.com>
2017-04-11 09:43:39 -0700

    Update flaky assert dependent on system clocks (#3771)
    * Change expiration assert to 2 days

--- tests/NuGetGallery.FunctionalTests/ODataFeeds/V2FeedExtendedTests.cs
+++ tests/NuGetGallery.FunctionalTests/ODataFeeds/V2FeedExtendedTests.cs
@@ -303,7 +303,9 @@ namespace NuGetGallery.FunctionalTests.ODataFeeds
 
             var json = JObject.Parse(responseText);
             var expiration = json.Value<DateTime>("Expires");
-            Assert.True(expiration - DateTime.UtcNow < TimeSpan.FromDays(1), "Verification keys should expire after 1 day.");
+
+            // Verification key should expire in 1 day. Ensure expiration is within 2 days in case client/server clocks differ.
+            Assert.True(expiration - DateTime.UtcNow < TimeSpan.FromDays(2), "Verification keys should expire after 1 day.");
 
             return json.Value<string>("Key");
         }
