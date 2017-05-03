9518ff419a37fda37e8239c42fefe79e8f6d2916
Christy Henriksson <chenriks@microsoft.com>
2017-05-03 11:32:04 -0700

    SecurityPolicies migration fix

--- src/NuGetGallery/Migrations/201704211454424_SecurityPolicies.cs
+++ src/NuGetGallery/Migrations/201704211454424_SecurityPolicies.cs
@@ -1,28 +1,32 @@
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
 namespace NuGetGallery.Migrations
 {
+    using System;
     using System.Data.Entity.Migrations;
     
     public partial class SecurityPolicies : DbMigration
     {
         public override void Up()
         {
-            CreateTable("UserSecurityPolicies", c => new
-            {
-                Key = c.Int(nullable: false, identity: true),
-                Name = c.String(nullable: false, maxLength: 256),
-                UserKey = c.Int(nullable: false),
-                Value = c.String(nullable: true, maxLength: 256)
-            })
-            .PrimaryKey(t => t.Key)
-            .ForeignKey("Users", t => t.UserKey);
+            CreateTable(
+                "dbo.UserSecurityPolicies",
+                c => new
+                    {
+                        Key = c.Int(nullable: false, identity: true),
+                        UserKey = c.Int(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 256),
+                        Value = c.String(),
+                    })
+                .PrimaryKey(t => t.Key)
+                .ForeignKey("dbo.Users", t => t.UserKey, cascadeDelete: true)
+                .Index(t => t.UserKey);
+            
         }
         
         public override void Down()
         {
-            DropTable("UserSecurityPolicies");
+            DropForeignKey("dbo.UserSecurityPolicies", "UserKey", "dbo.Users");
+            DropIndex("dbo.UserSecurityPolicies", new[] { "UserKey" });
+            DropTable("dbo.UserSecurityPolicies");
         }
     }
 }
