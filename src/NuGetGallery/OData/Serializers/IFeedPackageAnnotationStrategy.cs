de0f917c4e7b89227a4c5e44aa1bc7324829f154
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2017-05-04 19:46:39 +0200

    Refactor NuGetEntityTypeSerializer + unit test coverage (#3879)

---
+++ src/NuGetGallery/OData/Serializers/IFeedPackageAnnotationStrategy.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Data.OData;
+using System.Net.Http;
+
+namespace NuGetGallery.OData.Serializers
+{
+    internal interface IFeedPackageAnnotationStrategy
+    {
+        bool CanAnnotate(object entityInstance);
+        void Annotate(HttpRequestMessage request, ODataEntry entry, object entityInstance);
+    }
+}
