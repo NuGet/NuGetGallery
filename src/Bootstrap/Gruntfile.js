69f8aec0d67a639db6476c97d8e6f75edec86e13
Joel Verhagen <jver@microsoft.com>
2017-05-10 09:44:05 -0700

    Remove glyphicons (unused)

--- src/Bootstrap/Gruntfile.js
+++ src/Bootstrap/Gruntfile.js
@@ -275,11 +275,6 @@ module.exports = function (grunt) {
     },
 
     copy: {
-      fonts: {
-        expand: true,
-        src: 'fonts/**',
-        dest: 'dist/'
-      },
       docs: {
         expand: true,
         cwd: 'dist/',
@@ -478,10 +473,10 @@ module.exports = function (grunt) {
   grunt.registerTask('dist-css', ['less-compile', 'autoprefixer:core', 'autoprefixer:theme', 'csscomb:dist', 'cssmin:minifyCore', 'cssmin:minifyTheme']);
 
   // Full distribution task.
-  grunt.registerTask('dist', ['clean:dist', 'dist-css', 'copy:fonts', 'dist-js']);
+  grunt.registerTask('dist', ['clean:dist', 'dist-css', 'dist-js']);
 
   // Default task.
-  grunt.registerTask('default', ['clean:dist', 'copy:fonts', 'test']);
+  grunt.registerTask('default', ['clean:dist', 'test']);
 
   grunt.registerTask('build-glyphicons-data', function () { generateGlyphiconsData.call(this, grunt); });
 
