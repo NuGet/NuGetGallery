12c77d7824628573aec798956c44bd7e802d8e64
Xavier Decoster <xavier.decoster@outlook.com>
2017-05-12 11:23:15 +0200

    code review feedback

--- src/NuGetGallery/Services/PackageService.cs
+++ src/NuGetGallery/Services/PackageService.cs
@@ -325,9 +325,9 @@ namespace NuGetGallery
                 latestPackageVersions = _packageRegistrationRepository.GetAll()
                     .Where(pr => pr.Owners.Where(owner => owner.Username == user.Username).Any())
                     .Select(pr => pr.Packages.OrderByDescending(p => p.Version).FirstOrDefault())
+                    .Where(p => p != null)
                     .Include(p => p.PackageRegistration)
-                    .Include(p => p.PackageRegistration.Owners)
-                    .Where(p => p != null);
+                    .Include(p => p.PackageRegistration.Owners);
             }
             else
             {
