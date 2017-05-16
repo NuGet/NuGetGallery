af1228e26c87e8db2efc564326eef359f0616d2a
Xavier Decoster <xavier.decoster@outlook.com>
2017-05-16 14:37:31 +0200

    Detect if package only differ by metadata and show optimal user-facing error message

--- src/NuGetGallery/Strings.Designer.cs
+++ src/NuGetGallery/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace NuGetGallery {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Strings {
@@ -650,6 +650,15 @@ namespace NuGetGallery {
         public static string PackageIsMissingRequiredData {
             get {
                 return ResourceManager.GetString("PackageIsMissingRequiredData", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Package versions that differ only by metadata cannot be uploaded. A package with ID &apos;{0}&apos; and version &apos;{1}&apos; already exists and cannot be modified..
+        /// </summary>
+        public static string PackageVersionDiffersOnlyByMetadataAndCannotBeModified {
+            get {
+                return ResourceManager.GetString("PackageVersionDiffersOnlyByMetadataAndCannotBeModified", resourceCulture);
             }
         }
         
