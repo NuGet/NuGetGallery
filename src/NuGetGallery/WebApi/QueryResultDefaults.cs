647c95975c335ce90b01cff5edf2196a332b5d63
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2017-05-16 09:20:29 +0200

    Hijack IsLatest(Stable)Version OData filter when semVerLevel=2.0.0 (#3966)

---
+++ src/NuGetGallery/WebApi/QueryResultDefaults.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Web.Http.OData.Query;
+
+namespace NuGetGallery.WebApi
+{
+    public static class QueryResultDefaults
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2211:NonConstantFieldsShouldNotBeVisible")]
+        public static ODataQuerySettings DefaultQuerySettings = new ODataQuerySettings()
+        {
+            HandleNullPropagation = HandleNullPropagationOption.False,
+            EnsureStableOrdering = true,
+            EnableConstantParameterization = false
+        };
+    }
+}
