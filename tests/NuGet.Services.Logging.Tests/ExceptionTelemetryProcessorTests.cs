308e61f1564463826c9ad183bd90253cf0fb6aa0
Damon Tivel <dtivel@microsoft.com>
2017-06-05 09:53:53 -0700

    Reduce exception telemetry noise (#51)

---
+++ tests/NuGet.Services.Logging.Tests/ExceptionTelemetryProcessorTests.cs
@@ -0,0 +1,163 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Web;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace NuGet.Services.Logging.Tests
+{
+    public class ExceptionTelemetryProcessorTests
+    {
+        [Fact]
+        public void Constructor_ThrowsForNullNext()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => new ExceptionTelemetryProcessor(nextProcessor: null, telemetryClient: new TelemetryClient()));
+
+            Assert.Equal("nextProcessor", exception.ParamName);
+        }
+
+        [Fact]
+        public void Constructor_ThrowsForNullTelemetryClient()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => new ExceptionTelemetryProcessor(Mock.Of<ITelemetryProcessor>(), telemetryClient: null));
+
+            Assert.Equal("telemetryClient", exception.ParamName);
+        }
+
+        [Fact]
+        public void Process_ThrowsForNullItem()
+        {
+            using (var test = TelemetryProcessorTest<ExceptionTelemetryProcessor>.Create())
+            {
+                var exception = Assert.Throws<ArgumentNullException>(() => test.Processor.Process(item: null));
+
+                Assert.Equal("item", exception.ParamName);
+            }
+        }
+
+        [Fact]
+        public void Process_CallsNextProcessorForExceptionTelemetryWithNullException()
+        {
+            var telemetry = new ExceptionTelemetry();
+
+            // By default this property is null.
+            // However, assert this fact anyway to guard against an unexpected change in the dependency
+            // which would invalidate the setup for this test.
+            Assert.Null(telemetry.Exception);
+
+            using (var test = TelemetryProcessorTest<ExceptionTelemetryProcessor>.Create())
+            {
+                test.NextProcessor.Setup(x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))));
+
+                test.Processor.Process(telemetry);
+
+                test.NextProcessor.Verify(
+                    x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))),
+                    Times.Once());
+            }
+        }
+
+        [Fact]
+        public void Process_CallsNextProcessorForExceptionTelemetryWithExceptionThatIsNotHttpException()
+        {
+            var telemetry = new ExceptionTelemetry(new NullReferenceException());
+
+            using (var test = TelemetryProcessorTest<ExceptionTelemetryProcessor>.Create())
+            {
+                test.NextProcessor.Setup(x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))));
+
+                test.Processor.Process(telemetry);
+
+                test.NextProcessor.Verify(
+                    x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))),
+                    Times.Once());
+            }
+        }
+
+        [Theory]
+        [InlineData(500)]
+        [InlineData(503)]
+        public void Process_CallsNextProcessorForExceptionTelemetryWithHttpStatusCodeGreaterThanOrEqualTo500(
+            int statusCode)
+        {
+            var telemetry = new ExceptionTelemetry(new HttpException(statusCode, message: "a"));
+
+            using (var test = TelemetryProcessorTest<ExceptionTelemetryProcessor>.Create())
+            {
+                test.NextProcessor.Setup(x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))));
+
+                test.Processor.Process(telemetry);
+
+                test.NextProcessor.Verify(
+                    x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))),
+                    Times.Once());
+            }
+        }
+
+        [Fact]
+        public void Process_TracksNewTraceTelemetryForExceptionTelemetryWithHttpStatusCodeLessThan500()
+        {
+            var telemetry = new ExceptionTelemetry(new HttpException(httpCode: 499, message: "a"));
+
+            using (var test = TelemetryProcessorTest<ExceptionTelemetryProcessor>.Create())
+            {
+                test.Processor.Process(telemetry);
+
+                Assert.IsType<TraceTelemetry>(test.SentTelemetry.Single());
+            }
+        }
+
+        [Fact]
+        public void Process_CallsTelemetryClientWithTraceTelemetryPopulatedFromExceptionTelemetry()
+        {
+            var exception = new HttpException(httpCode: 401, message: "a");
+            var exceptionJson = JObject.FromObject(exception).ToString(Formatting.None);
+            var timestamp = DateTimeOffset.UtcNow;
+            var exceptionTelemetry = new ExceptionTelemetry(exception);
+
+            exceptionTelemetry.Context.Cloud.RoleInstance = "b";
+            exceptionTelemetry.Context.Cloud.RoleName = "c";
+            exceptionTelemetry.Context.GetInternalContext().SdkVersion = "d";
+            exceptionTelemetry.Context.InstrumentationKey = "e";
+            exceptionTelemetry.Context.Operation.Id = "f";
+            exceptionTelemetry.Context.Operation.Name = "g";
+            exceptionTelemetry.Context.Operation.ParentId = "h";
+            exceptionTelemetry.Context.Location.Ip = "127.0.0.1";
+            exceptionTelemetry.Properties.Add("i", "j");
+            exceptionTelemetry.Sequence = "k";
+            exceptionTelemetry.Timestamp = timestamp;
+
+            using (var test = TelemetryProcessorTest<ExceptionTelemetryProcessor>.Create())
+            {
+                test.Processor.Process(exceptionTelemetry);
+
+                var sentTelemetry = test.SentTelemetry.Single() as TraceTelemetry;
+
+                Assert.Equal("a", sentTelemetry.Message);
+                Assert.Equal(SeverityLevel.Warning, sentTelemetry.SeverityLevel);
+                Assert.Equal("b", sentTelemetry.Context.Cloud.RoleInstance);
+                Assert.Equal("c", sentTelemetry.Context.Cloud.RoleName);
+                Assert.Equal("d", sentTelemetry.Context.GetInternalContext().SdkVersion);
+                Assert.Equal("e", sentTelemetry.Context.InstrumentationKey);
+                Assert.Equal("f", sentTelemetry.Context.Operation.Id);
+                Assert.Equal("g", sentTelemetry.Context.Operation.Name);
+                Assert.Equal("h", sentTelemetry.Context.Operation.ParentId);
+                Assert.Equal("127.0.0.1", sentTelemetry.Context.Location.Ip);
+                Assert.Equal("j", sentTelemetry.Properties["i"]);
+                Assert.Equal("k", sentTelemetry.Sequence);
+                Assert.Equal(timestamp, sentTelemetry.Timestamp);
+            }
+        }
+    }
+}
---
+++ tests/NuGet.Services.Logging.Tests/TelemetryProcessorTest.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Moq;
+
+namespace NuGet.Services.Logging.Tests
+{
+    internal sealed class TelemetryProcessorTest<T> : IDisposable
+        where T : ITelemetryProcessor
+    {
+        private readonly TelemetryClient _client;
+        private readonly TelemetryConfiguration _configuration;
+        private bool _isDisposed;
+
+        internal Mock<ITelemetryProcessor> NextProcessor { get; }
+        internal T Processor { get; }
+        internal List<ITelemetry> SentTelemetry { get; }
+
+        internal TelemetryProcessorTest(
+            T processor,
+            Mock<ITelemetryProcessor> nextProcessor,
+            TelemetryClient client,
+            TelemetryConfiguration configuration,
+            List<ITelemetry> sentTelemetry)
+        {
+            Processor = processor;
+            NextProcessor = nextProcessor;
+            _client = client;
+            _configuration = configuration;
+            SentTelemetry = sentTelemetry;
+        }
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                _configuration.Dispose();
+
+                _isDisposed = true;
+                GC.SuppressFinalize(this);
+
+                NextProcessor.Verify();
+            }
+        }
+
+        internal static TelemetryProcessorTest<T> Create()
+        {
+            var sentTelemetry = new List<ITelemetry>();
+            var configuration = new TelemetryConfiguration()
+            {
+                DisableTelemetry = false,
+                InstrumentationKey = Guid.Empty.ToString(),
+                TelemetryChannel = new StubTelemetryChannel()
+                {
+                    OnSend = telemetry => sentTelemetry.Add(telemetry)
+                }
+            };
+            var client = new TelemetryClient(configuration);
+            var nextProcessor = new Mock<ITelemetryProcessor>(MockBehavior.Strict);
+            T processor;
+
+            if (typeof(T) == typeof(RequestTelemetryProcessor))
+            {
+                processor = (T)(object)new RequestTelemetryProcessor(nextProcessor.Object);
+            }
+            else if (typeof(T) == typeof(ExceptionTelemetryProcessor))
+            {
+                processor = (T)(object)new ExceptionTelemetryProcessor(nextProcessor.Object, client);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+
+            return new TelemetryProcessorTest<T>(
+                processor,
+                nextProcessor,
+                client,
+                configuration,
+                sentTelemetry);
+        }
+
+        private sealed class StubTelemetryChannel : ITelemetryChannel
+        {
+            public bool? DeveloperMode { get; set; }
+            public string EndpointAddress { get; set; }
+            public Action<ITelemetry> OnSend { get; set; }
+
+            public void Dispose()
+            {
+            }
+
+            public void Flush()
+            {
+            }
+
+            public void Send(ITelemetry item)
+            {
+                OnSend(item);
+            }
+        }
+    }
+}
