4e6820408f551c581da866738293ccbc48051a5c
Andrei Grigorev <andrey.grigoriev@gmail.com>
2017-06-06 17:27:28 -0700

    Making sure package blob storage URL is properly urlencoded. (#153)
    * Making sure package blob storage URL is properly urlencoded.
    
    * rephrased comments

--- src/Validation.Common/Validators/Vcs/VcsValidator.cs
+++ src/Validation.Common/Validators/Vcs/VcsValidator.cs
@@ -107,10 +107,11 @@ namespace NuGet.Jobs.Validation.Common.Validators.Vcs
         {
             // The VCS service needs a blob storage URL, which the NuGet API does not expose.
             // Build one from a template here.
-            return _packageUrlTemplate
+            // Guarantee all URL transformations (such as URL encoding) are performed.
+            return new Uri(_packageUrlTemplate
                 .Replace("{id}", packageId)
                 .Replace("{version}", packageVersion)
-                .ToLowerInvariant();
+                .ToLowerInvariant()).AbsoluteUri;
         }
     }
 }
