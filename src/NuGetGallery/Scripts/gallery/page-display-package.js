c22aeddc64d07df071539e32353e4c6bcfe1b5fc
Joel Verhagen <jver@microsoft.com>
2017-06-12 09:13:35 -0700

    Address more feedback
    10px padding on transient message
    Add .NET CLI tab
    Add 8px padding below owners

--- src/NuGetGallery/Scripts/gallery/page-display-package.js
+++ src/NuGetGallery/Scripts/gallery/page-display-package.js
@@ -1,19 +1,23 @@
 $(function () {
     'use strict';
-    
+
+    function configureCopyButton(id) {
+        var copyButton = $('#' + id + '-button');
+        copyButton.popover({ trigger: 'manual' });
+
+        copyButton.click(function () {
+            var text = $('#' + id + '-text').text().trim();
+            window.nuget.copyTextToClipboard(text);
+            copyButton.popover('show');
+            setTimeout(function () {
+                copyButton.popover('destroy');
+            }, 1000);
+        });
+    }
+
     window.nuget.configureExpander("hidden-versions", "Show less", "Show more");
     window.nuget.configureExpander("dependency-groups", "Hide", "Show");
 
-    // Copy button.
-    var copyButton = $('#install-script-button');
-    copyButton.popover({ trigger: 'manual' });
-
-    $('#install-script-button').click(function () {
-        var text = $('#install-script-text').text().trim();
-        window.nuget.copyTextToClipboard(text);
-        copyButton.popover('show');
-        setTimeout(function () {
-            copyButton.popover('destroy');
-        }, 1000);
-    });
+    configureCopyButton('package-manager');
+    configureCopyButton('dotnet-cli');
 });
