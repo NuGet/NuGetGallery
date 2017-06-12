39bcbbd0b943e3434169049ff7a7af87d2aec79f
Joel Verhagen <joel.verhagen@gmail.com>
2017-06-12 09:15:14 -0700

    Update banner, transient message, and package details page (#4105)

--- src/NuGetGallery/Scripts/gallery/page-display-package.js
+++ src/NuGetGallery/Scripts/gallery/page-display-package.js
@@ -1,35 +1,23 @@
 $(function () {
     'use strict';
 
-    // Hidden versions.
-    var hiddenVersions = $('#hidden-versions');
-    var showHiddenVersion = $('#show-hidden-versions');
-    var showHiddenVersionText = $('#show-hidden-versions span');
-    var showHiddenVersionIcon = $('#show-hidden-versions i');
-    hiddenVersions.on('hide.bs.collapse', function () {
-        showHiddenVersionText.text('Show more');
-        showHiddenVersionIcon.removeClass('ms-Icon--ChevronUp');
-        showHiddenVersionIcon.addClass('ms-Icon--ChevronDown');
-    });
-    hiddenVersions.on('show.bs.collapse', function () {
-        showHiddenVersionText.text('Show less');
-        showHiddenVersionIcon.removeClass('ms-Icon--ChevronDown');
-        showHiddenVersionIcon.addClass('ms-Icon--ChevronUp');
-    });
-    showHiddenVersion.on('click', function (e) {
-        e.preventDefault();
-    });
+    function configureCopyButton(id) {
+        var copyButton = $('#' + id + '-button');
+        copyButton.popover({ trigger: 'manual' });
 
-    // Copy button.
-    var copyButton = $('#install-script-button');
-    copyButton.popover({ trigger: 'manual' });
+        copyButton.click(function () {
+            var text = $('#' + id + '-text').text().trim();
+            window.nuget.copyTextToClipboard(text);
+            copyButton.popover('show');
+            setTimeout(function () {
+                copyButton.popover('destroy');
+            }, 1000);
+        });
+    }
 
-    $('#install-script-button').click(function () {
-        var text = $('#install-script-text').text().trim();
-        window.nuget.copyTextToClipboard(text);
-        copyButton.popover('show');
-        setTimeout(function () {
-            copyButton.popover('destroy');
-        }, 1000);
-    });
+    window.nuget.configureExpander("hidden-versions", "Show less", "Show more");
+    window.nuget.configureExpander("dependency-groups", "Hide", "Show");
+
+    configureCopyButton('package-manager');
+    configureCopyButton('dotnet-cli');
 });
