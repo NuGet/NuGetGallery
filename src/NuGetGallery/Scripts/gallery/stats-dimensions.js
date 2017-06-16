38a32bb46f581ed5f3e9a21b67a7cf2ba44702e3
Ryu Yu <ryuyu@microsoft.com>
2017-06-16 14:32:23 -0700

    [Redesign]Small Fixes for StatisticsPages (#4200)
    * Fix issue with loading page when we pass bad parameters.
    * move number parsing to common.
    * remove a stray semicolon.
    * Hide chart when nothing to display.
    * Remove encapsulating row to add gutter back.
    * Fix link to package stats.
    * Small spacing nit.

--- src/NuGetGallery/Scripts/gallery/stats-dimensions.js
+++ src/NuGetGallery/Scripts/gallery/stats-dimensions.js
@@ -4,7 +4,7 @@
 
         $("#loading-placeholder").hide();
         // Populate the data table
-        data['reportSize'] = data.Table.length;
+        data['reportSize'] = data.Table != null ? data.Table.length : 0;
 
         $("#report").remove();
 
