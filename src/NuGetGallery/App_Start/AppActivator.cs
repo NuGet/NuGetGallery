e169b33a898b4139680ca8f8d1129a093026f362
scottbommarito <scottbom@microsoft.com>
2017-06-20 11:40:07 -0700

    manage packages page

--- src/NuGetGallery/App_Start/AppActivator.cs
+++ src/NuGetGallery/App_Start/AppActivator.cs
@@ -208,6 +208,10 @@ namespace NuGetGallery
                 .Include("~/Scripts/gallery/page-display-package.js");
             BundleTable.Bundles.Add(displayPackageScriptBundle);
 
+            var managePackagesScriptBundle = new ScriptBundle("~/Scripts/gallery/page-manage-packages.min.js")
+                .Include("~/Scripts/gallery/page-manage-packages.js");
+            BundleTable.Bundles.Add(managePackagesScriptBundle);
+
             var aboutScriptBundle = new ScriptBundle("~/Scripts/gallery/page-about.min.js")
                 .Include("~/Scripts/gallery/page-about.js");
             BundleTable.Bundles.Add(aboutScriptBundle);
