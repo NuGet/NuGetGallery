88ed552d55e64652744d3ae51b1787997e28ac64
Lauren <laurenshen@users.noreply.github.com>
2017-06-27 15:10:19 -0700

    DB schema changes for repo URL and ReadMe (#4283) (#4290)
    * added repository URL and pending readme columns (#4256)
    
    * Added schema changes to DB (#4263)
    
    * Added schema changes to DB
    
    * Fixed indentation and reverted UseIIS
    
    * PR review update
    
    * PR review update
    
    * revert applicationhost.config
    
    * actually revert applicationhost.config
    
    * changed enum to string because db doesn't support enum
    
    * changed bool to nullable
    
    * null (default) is same as unchanged

--- src/NuGetGallery/Migrations/201706262349176_AddRepositoryURL_ReadMe.cs
+++ src/NuGetGallery/Migrations/201706262349176_AddRepositoryURL_ReadMe.cs
@@ -8,16 +8,16 @@ namespace NuGetGallery.Migrations
         public override void Up()
         {
             AddColumn("dbo.Packages", "RepositoryUrl", c => c.String());
-            AddColumn("dbo.Packages", "HasReadMe", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Packages", "HasReadMe", c => c.Boolean());
             AddColumn("dbo.PackageEdits", "RepositoryUrl", c => c.String());
-            AddColumn("dbo.PackageEdits", "ReadmeModified", c => c.Boolean(nullable: false));
+            AddColumn("dbo.PackageEdits", "ReadMeState", c => c.String());
             AddColumn("dbo.PackageHistories", "RepositoryUrl", c => c.String());
         }
         
         public override void Down()
         {
             DropColumn("dbo.PackageHistories", "RepositoryUrl");
-            DropColumn("dbo.PackageEdits", "ReadmeModified");
+            DropColumn("dbo.PackageEdits", "ReadMeState");
             DropColumn("dbo.PackageEdits", "RepositoryUrl");
             DropColumn("dbo.Packages", "HasReadMe");
             DropColumn("dbo.Packages", "RepositoryUrl");
