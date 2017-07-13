ba93cdc195d00250363e3dbf2f5a87984e0ddcd4
Lauren Rosenberg <laurenfay.rosenberg@mail.utoronto.ca>
2017-07-13 13:26:03 -0700

    Fixed URL parsing

--- src/NuGetGallery/Scripts/gallery/async-file-upload.js
+++ src/NuGetGallery/Scripts/gallery/async-file-upload.js
@@ -380,11 +380,12 @@
 			$(".readme-file").hide();
 			$(".readme-write").show();
 		}
+		clearReadMeError();
 	}
 
 	function validateReadMeUrl(url) {
 		var urlRegex = new RegExp("^" + "(?:(?:https?|ftp)://)?" + "(?:\\S+(?::\\S*)?@)?" + "(?:" + "(?!(?:10|127)(?:\\.\\d{1,3}){3})" + "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" + "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" + "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" + "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" + "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" + "|" + "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" + "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" + "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))" + "\\.?" + ")" + "(?::\\d{2,5})?" + "(?:[/?#]\\S*)?" + "$", "i");
-		return validateReadMeFileName(url) && urlRegex.test(url);
+		return validateReadMeFileName(url) && urlRegex.test(url) && url.split(".").length > 2;
 	}
 
 	function validateReadMeFileName(fileName) {
