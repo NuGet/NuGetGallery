ee7d100de9aa884ca9d522b002460d058964bc69
shishirh <shishirh@microsoft.com>
2017-07-20 17:45:50 -0700

    Skeleton code for controllers and views for reserve namespace

---
+++ src/NuGetGallery/Areas/Admin/Controllers/ReserveNamespaceController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NuGetGallery.Areas.Admin.Controllers
+{
+    public class ReserveNamespaceController : AdminControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public virtual JsonResult SearchPrefix(string query)
+        {
+            var prefixList = new string[] { "abc", "abc.*", "Microsoft.*" };
+            var obj = new
+            {
+                Prefixes = prefixList.Select(p => new { Prefix = p })
+            };
+
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
