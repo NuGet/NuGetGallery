352a1139a11ed97fee682d6ef542721ec9da9793
Peter Yu <ryuyu@microsoft.com>
2017-07-26 16:18:36 -0700

    Update to limit character and move to end of current input.

--- src/NuGetGallery/Scripts/gallery/common.js
+++ src/NuGetGallery/Scripts/gallery/common.js
@@ -312,9 +312,20 @@ $(function () {
 
     $(document).on('keydown', function (e) {
         var code = (e.keyCode || e.which);
+        var isValidInputCharacter =
+            ((code >= 48 && code <= 57)           // numbers 0-9
+                || (code >= 64 && code <= 90)     // letters a-z
+                || (code >= 96 && code <= 111)    // numpad
+                || (code >= 186 && code <= 192)   // ; = , - . / `
+                || (code >= 219 && code <= 222))  // [\ ] '
+            && !e.altKey && !e.ctrlKey && !e.metaKey;
 
-        if (code != 9 /*tab*/ && document.activeElement == document.body) {
-            $("#search").focus();
+        if (isValidInputCharacter && document.activeElement == document.body) {
+            var searchbox = $("#search");
+            searchbox.focus();
+            var currInput = searchbox.val();
+            searchbox.val("");
+            searchbox.val(currInput);
         }
     });
 });
