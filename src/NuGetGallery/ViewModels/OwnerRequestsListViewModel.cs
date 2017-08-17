ad5e9eab351cb24e607868d18e83e9decc3af727
scottbommarito <scottbom@microsoft.com>
2017-08-16 17:38:15 -0700

    pr comments

--- src/NuGetGallery/ViewModels/OwnerRequestsListViewModel.cs
+++ src/NuGetGallery/ViewModels/OwnerRequestsListViewModel.cs
@@ -12,15 +12,10 @@ namespace NuGetGallery
         public string Name { get; private set; }
 
         public User CurrentUser { get; private set; }
-
+        
         public OwnerRequestsListViewModel(IEnumerable<PackageOwnerRequest> requests, string name, User currentUser, IPackageService packageService)
-            : this(requests.Select(r => new OwnerRequestsListItemViewModel(r, packageService)).ToArray(), name, currentUser)
         {
-        }
-
-        public OwnerRequestsListViewModel(IEnumerable<OwnerRequestsListItemViewModel> requestItems, string name, User currentUser)
-        {
-            RequestItems = requestItems;
+            RequestItems = requests.Select(r => new OwnerRequestsListItemViewModel(r, packageService)).ToArray();
             Name = name;
             CurrentUser = currentUser;
         }
