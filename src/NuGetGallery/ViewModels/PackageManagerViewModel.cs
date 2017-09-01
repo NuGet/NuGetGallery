7ff0ddd0b2c13aedd56a14d7f41f54eae9193b0c
Loic Sharma <sharma.loic@gmail.com>
2017-09-01 15:49:18 -0700

    Added view models

---
+++ src/NuGetGallery/ViewModels/PackageManagerViewModel.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGetGallery
+{
+    /// <summary>
+    /// The basic model for a client that conforms to the NuGet protocol.
+    /// </summary>
+    public class PackageManagerViewModel
+    {
+        /// <summary>
+        /// The package manager's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A unique identifier that represents this package manager.
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The non-selectable prefix displayed on before the package manager's commands.
+        /// </summary>
+        public string CommandPrefix { get; set; }
+
+        /// <summary>
+        /// A string that represents the command used to install a specific package.
+        /// </summary>
+        public string InstallPackageCommand { get; set; }
+    }
+}
---
+++ src/NuGetGallery/ViewModels/ThirdPartyPackageManagerViewModel.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGetGallery
+{
+    /// <summary>
+    /// A Package Manager that conforms to the NuGet protocol but isn't
+    /// maintained by the NuGet team.
+    /// </summary>
+    public class ThirdPartyPackageManagerViewModel : PackageManagerViewModel
+    {
+        /// <summary>
+        /// The URL that links should be used to contact the package manager's
+        /// maintainers for support.
+        /// </summary>
+        public string ContactUrl { get; set; }
+    }
+}
