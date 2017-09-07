3d7ab393146e4e265b760145747cedeab42ebfe2
Xavier Decoster <xavier.decoster@outlook.com>
2017-09-07 19:55:20 +0200

    Add unit test that scans .cshtml files for usages of UrlHelper

--- src/NuGetGallery/UrlExtensions.cs
+++ src/NuGetGallery/UrlExtensions.cs
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using NuGetGallery.Configuration;
-using NuGetGallery.Configuration.SecretReader;
-using NuGetGallery.Diagnostics;
 using System;
 using System.Globalization;
 using System.Web;
