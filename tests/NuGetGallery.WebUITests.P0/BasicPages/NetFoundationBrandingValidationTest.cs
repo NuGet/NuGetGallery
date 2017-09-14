5fe915c3dae4b2db0efd2d28b422f44b1bf4b1b5
Xavier Decoster <xavier.decoster@outlook.com>
2017-09-14 10:02:26 +0200

    Functional test fix for NetFoundationBrandingValidationTest. (no more relative URL)

--- tests/NuGetGallery.WebUITests.P0/BasicPages/NetFoundationBrandingValidationTest.cs
+++ tests/NuGetGallery.WebUITests.P0/BasicPages/NetFoundationBrandingValidationTest.cs
@@ -25,8 +25,8 @@ namespace NuGetGallery.FunctionalTests.WebUITests.BasicPages
             var pageRequest = new WebTestRequest(UrlHelper.BaseUrl);
             var homePageTextValidationRuleLogo = AssertAndValidationHelper.GetValidationRuleForFindText(@"<a href=""https://www.dotnetfoundation.org"">");
             var homePageTextValidationRuleCopyright = AssertAndValidationHelper.GetValidationRuleForFindText(@"&copy; " + DateTime.UtcNow.Year + " .NET Foundation");
-            var homePageTextValidationRuleTOU = AssertAndValidationHelper.GetValidationRuleForFindText(@"<a href=""/policies/Terms"">Terms of Use</a>");
-            var homePageTextValidationRulePrivacy = AssertAndValidationHelper.GetValidationRuleForFindText(@"<a href=""/policies/Privacy"">Privacy Policy</a>");
+            var homePageTextValidationRuleTOU = AssertAndValidationHelper.GetValidationRuleForFindText($@"<a href=""{UrlHelper.BaseUrl}/policies/Terms"">Terms of Use</a>");
+            var homePageTextValidationRulePrivacy = AssertAndValidationHelper.GetValidationRuleForFindText($@"<a href=""{UrlHelper.BaseUrl}/policies/Privacy"">Privacy Policy</a>");
 
             pageRequest.ValidateResponse += homePageTextValidationRuleLogo.Validate;
             pageRequest.ValidateResponse += homePageTextValidationRuleCopyright.Validate;
