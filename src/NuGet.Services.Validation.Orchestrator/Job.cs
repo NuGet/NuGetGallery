f52b716d889808dbf1cd2ac6c64993b22b5c25bd
Joel Verhagen <jver@microsoft.com>
2017-09-28 15:01:02 -0700

    Add configuration for the VcsValidator and integrate with DI (#219)
    Address https://github.com/NuGet/Engineering/issues/764

--- src/NuGet.Services.Validation.Orchestrator/Job.cs
+++ src/NuGet.Services.Validation.Orchestrator/Job.cs
@@ -5,12 +5,17 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Autofac;
+using Autofac.Core;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.WindowsAzure.Storage;
 using NuGet.Jobs;
+using NuGet.Jobs.Validation.Common;
 using NuGet.Services.Configuration;
+using NuGet.Services.Validation.Vcs;
 
 namespace NuGet.Services.Validation.Orchestrator
 {
@@ -20,7 +25,10 @@ namespace NuGet.Services.Validation.Orchestrator
         private const string ValidateArgument = "Validate";
 
         private const string ConfigurationSectionName = "Configuration";
+        private const string VcsSectionName = "Vcs";
         private const string ValidateOnlyConfigurationKey = nameof(ValidateOnlyConfiguration.ValidateOnly);
+
+        private const string VcsBindingKey = VcsSectionName;
 
         private string _configurationFilename;
         private bool _validateOnly;
@@ -89,14 +97,48 @@ namespace NuGet.Services.Validation.Orchestrator
         private void ConfigureJobServices(IServiceCollection services, IConfigurationRoot configurationRoot)
         {
             services.Configure<ValidationConfiguration>(configurationRoot.GetSection(ConfigurationSectionName));
+            services.Configure<VcsConfiguration>(configurationRoot.GetSection(VcsSectionName));
             services.Configure<ValidateOnlyConfiguration>(configurationRoot);
             services.AddTransient<ConfigurationValidator>();
+            services.AddTransient<VcsValidator>();
         }
 
         private static IServiceProvider CreateProvider(IServiceCollection services)
         {
             var containerBuilder = new ContainerBuilder();
             containerBuilder.Populate(services);
+
+            /// Initialize dependencies for the <see cref="VcsValidator"/>. There is some additional complexity here
+            /// because the implementations require ambiguous types (such as a <see cref="string"/> and a
+            /// <see cref="CloudStorageAccount"/> which there may be more than one configuration of).
+            containerBuilder
+                .Register(c =>
+                {
+                    var vcsConfiguration = c.Resolve<IOptionsSnapshot<VcsConfiguration>>();
+                    var cloudStorageAccount = CloudStorageAccount.Parse(vcsConfiguration.Value.DataStorageAccount);
+                    return cloudStorageAccount;
+                })
+                .Keyed<CloudStorageAccount>(VcsBindingKey);
+
+            containerBuilder
+                .RegisterType<PackageValidationService>()
+                .WithParameter(new ResolvedParameter(
+                    (pi, ctx) => pi.ParameterType == typeof(CloudStorageAccount),
+                    (pi, ctx) => ctx.ResolveKeyed<CloudStorageAccount>(VcsBindingKey)))
+                .WithParameter(new ResolvedParameter(
+                    (pi, ctx) => pi.ParameterType == typeof(string),
+                    (pi, ctx) => ctx.Resolve<IOptionsSnapshot<VcsConfiguration>>().Value.ContainerName))
+                .As<IPackageValidationService>();
+
+            containerBuilder
+                .RegisterType<PackageValidationAuditor>()
+                .WithParameter(new ResolvedParameter(
+                    (pi, ctx) => pi.ParameterType == typeof(CloudStorageAccount),
+                    (pi, ctx) => ctx.ResolveKeyed<CloudStorageAccount>(VcsBindingKey)))
+                .WithParameter(new ResolvedParameter(
+                    (pi, ctx) => pi.ParameterType == typeof(string),
+                    (pi, ctx) => ctx.Resolve<IOptionsSnapshot<VcsConfiguration>>().Value.ContainerName))
+                .As<IPackageValidationAuditor>();
 
             return new AutofacServiceProvider(containerBuilder.Build());
         }
