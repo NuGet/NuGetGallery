9b3626ce28f56621712973fb81382182e4c858fd
Andrei Grigorev <angrigor@microsoft.com>
2017-09-29 10:48:41 -0700

    Documentation update for the GetSharedReadUri

--- src/NuGetGallery.Core/Services/ISimpleCloudBlob.cs
+++ src/NuGetGallery.Core/Services/ISimpleCloudBlob.cs
@@ -32,8 +32,9 @@ namespace NuGetGallery
         /// using the produced URI only (without storage account credentials).
         /// </summary>
         /// <param name="endOfAccess">
-        /// Optional "end of access" timestamp. After the specified time, 
-        /// the returned URI becomes invalid.
+        /// "End of access" timestamp. After the specified time, 
+        /// the returned URI becomes invalid if implementation supports it.
+        /// Null for no time limit.
         /// </param>
         /// <returns>Shared access URI.</returns>
         Uri GetSharedReadUri(DateTimeOffset? endOfAccess);
