046d8291d330aec8abd25f2f9daa7c6526e40604
Ryu Yu <ryuyu@microsoft.com>
2017-10-05 13:43:14 -0700

    Fix caching of static files. (#4776)
    * Update dependcy injection to have proper instancing for storage dependents.
    * Updating comment  DI -> dependency injection.

--- src/NuGetGallery/App_Start/DefaultDependenciesModule.cs
+++ src/NuGetGallery/App_Start/DefaultDependenciesModule.cs
@@ -431,10 +431,18 @@ namespace NuGetGallery
 
             foreach (var dependent in StorageDependent.GetAll(configuration.Current))
             {
-                builder.RegisterType(dependent.ImplementationType)
+                var registration = builder.RegisterType(dependent.ImplementationType)
                     .AsSelf()
-                    .As(dependent.InterfaceType)
-                    .InstancePerLifetimeScope();
+                    .As(dependent.InterfaceType);
+
+                if (dependent.IsSingleInstance)
+                {
+                    registration.SingleInstance();
+                }
+                else
+                {
+                    registration.InstancePerLifetimeScope();
+                }
             }
 
             builder.RegisterInstance(NullReportService.Instance)
@@ -497,13 +505,21 @@ namespace NuGetGallery
                         .Keyed<IFileStorageService>(dependent.BindingKey);
                 }
 
-                builder.RegisterType(dependent.ImplementationType)
+                var registration = builder.RegisterType(dependent.ImplementationType)
                     .WithParameter(new ResolvedParameter(
                        (pi, ctx) => pi.ParameterType == typeof(IFileStorageService),
                        (pi, ctx) => ctx.ResolveKeyed<IFileStorageService>(dependent.BindingKey)))
                     .AsSelf()
-                    .As(dependent.InterfaceType)
-                    .InstancePerLifetimeScope();
+                    .As(dependent.InterfaceType);
+
+                if (dependent.IsSingleInstance)
+                {
+                    registration.SingleInstance();
+                }
+                else
+                {
+                    registration.InstancePerLifetimeScope();
+                }
             }
 
             // when running on Windows Azure, we use a back-end job to calculate stats totals and store in the blobs
@@ -611,12 +627,12 @@ namespace NuGetGallery
             {
                 service = new NullCookieComplianceService();
             }
-            
+
             builder.RegisterInstance(service)
                 .AsSelf()
                 .As<ICookieComplianceService>()
                 .SingleInstance();
-            
+
             // Initialize the service on App_Start to avoid any performance degradation during initial requests.
             var siteName = configuration.GetSiteRoot(true);
             HostingEnvironment.QueueBackgroundWorkItem(async cancellationToken => await service.InitializeAsync(siteName, diagnostics, cancellationToken));
--- src/NuGetGallery/App_Start/StorageDependent.cs
+++ src/NuGetGallery/App_Start/StorageDependent.cs
@@ -17,12 +17,14 @@ namespace NuGetGallery
             string bindingKey,
             string azureStorageConnectionString,
             Type implementationType,
-            Type interfaceType)
+            Type interfaceType,
+            bool isSingleInstance)
         {
             BindingKey = bindingKey;
             AzureStorageConnectionString = azureStorageConnectionString;
             ImplementationType = implementationType;
             InterfaceType = interfaceType;
+            IsSingleInstance = isSingleInstance;
         }
 
         public StorageDependent SetBindingKey(string bindingKey)
@@ -31,7 +33,8 @@ namespace NuGetGallery
                 bindingKey,
                 AzureStorageConnectionString,
                 ImplementationType,
-                InterfaceType);
+                InterfaceType,
+                IsSingleInstance);
         }
 
         /// <summary>
@@ -54,14 +57,20 @@ namespace NuGetGallery
         /// </summary>
         public Type InterfaceType { get; }
 
+        /// <summary>
+        /// Indicates if the implementation type should be declared as a singleton in dependency injection container.
+        /// </summary>
+        public bool IsSingleInstance { get; }
+
         public static StorageDependent Create<TImplementation, TInterface>(
-            string azureStorageConnectionString) where TImplementation : TInterface
+            string azureStorageConnectionString, bool isSingleInstance) where TImplementation : TInterface
         {
             return new StorageDependent(
                 typeof(TImplementation).FullName,
                 azureStorageConnectionString,
                 typeof(TImplementation),
-                typeof(TInterface));
+                typeof(TInterface),
+                isSingleInstance);
         }
 
         /// <summary>
@@ -75,10 +84,10 @@ namespace NuGetGallery
             /// This array must be added to as we implement more services that use <see cref="IFileStorageService"/>.
             var dependents = new[]
             {
-                Create<ContentService, IContentService>(configuration.AzureStorage_Content_ConnectionString),
-                Create<NuGetExeDownloaderService, INuGetExeDownloaderService>(configuration.AzureStorage_NuGetExe_ConnectionString),
-                Create<PackageFileService, IPackageFileService>(configuration.AzureStorage_Packages_ConnectionString),
-                Create<UploadFileService, IUploadFileService>(configuration.AzureStorage_Uploads_ConnectionString),
+                Create<ContentService, IContentService>(configuration.AzureStorage_Content_ConnectionString, isSingleInstance: true),
+                Create<NuGetExeDownloaderService, INuGetExeDownloaderService>(configuration.AzureStorage_NuGetExe_ConnectionString, isSingleInstance: false),
+                Create<PackageFileService, IPackageFileService>(configuration.AzureStorage_Packages_ConnectionString, isSingleInstance: false),
+                Create<UploadFileService, IUploadFileService>(configuration.AzureStorage_Uploads_ConnectionString, isSingleInstance: false),
             };
 
             var connectionStringToBindingKey = dependents
