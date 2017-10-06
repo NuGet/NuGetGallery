da8028c1fb7fe2793213bc89b9a15751f0707254
Scott Bommarito <scottbom@microsoft.com>
2017-10-06 11:22:43 -0700

    Add StorageQueue wrapper for queues (#62)

---
+++ src/NuGet.Services.Storage/AzureStorageQueueMessage.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace NuGet.Services.Storage
+{
+    internal class AzureStorageQueueMessage : StorageQueueMessage
+    {
+        internal CloudQueueMessage Message { get; }
+
+        internal AzureStorageQueueMessage(CloudQueueMessage message)
+            : base(message.AsString)
+        {
+            Message = message;
+        }
+
+        internal AzureStorageQueueMessage(string contents)
+            : base(contents)
+        {
+            Message = new CloudQueueMessage(contents);
+        }
+    }
+}
