26a9a11be28fd66b8c298478dafd673feea3bc2a
skofman <skofman@microsoft.com>
2017-10-11 13:10:25 -0700

    X-NuGet-Protocol-Version for Functional tests

--- tests/NuGetGallery.FunctionalTests/PackageCreation/ApiPushTests.cs
+++ tests/NuGetGallery.FunctionalTests/PackageCreation/ApiPushTests.cs
@@ -97,6 +97,7 @@ namespace NuGetGallery.FunctionalTests.PackageCreation
             {
                 request.Content = new StreamContent(new BarrierStream(package, barrier));
                 request.Headers.Add(Constants.NuGetHeaderApiKey, EnvironmentSettings.TestAccountApiKey);
+                request.Headers.Add(Constants.NuGetHeaderProtocolVersion, Constants.NuGetProtocolVersion);
 
                 using (var response = await client.SendAsync(request))
                 {
