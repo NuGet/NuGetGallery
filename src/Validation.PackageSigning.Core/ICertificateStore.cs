fb03f8c923033145d9664980aea409f00c31bf3e
Loïc Sharma <sharma.loic@gmail.com>
2017-10-19 17:33:45 -0700

    [Package Signing] Add shared Package Signing project (#232)
    Adds a shared project that will be used across different package signing projects. Contains:
    
    * `ICertificateStore` - An implementation be added later
    * `CertificateValidationMessage`/`CertificateValidationMessageSerializer` - the messaged use by the `CertificatesValidator` to kick off `VerifyCertificate` jobs.

---
+++ src/Validation.PackageSigning.Core/ICertificateStore.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace NuGet.Jobs.Validation.PackageSigning
+{
+    /// <summary>
+    /// The class used to <see cref="X509Certificate2"/> store and retrieve certificates.
+    /// </summary>
+    public interface ICertificateStore
+    {
+        /// <summary>
+        /// Check if the store contains the certificate.
+        /// </summary>
+        /// <param name="thumbprint">The certificate's thumbprint.</param>
+        /// <returns>Whether the store contains a certificate that has the given thumbprint.</returns>
+        Task<bool> Exists(string thumbprint);
+
+        /// <summary>
+        /// Load the certificate into memory.
+        /// </summary>
+        /// <param name="thumbprint">The certificate's thumbprint.</param>
+        /// <returns>A certificate whose thumbprint is the given thumbprint.</returns>
+        Task<X509Certificate2> Load(string thumbprint);
+
+        /// <summary>
+        /// Save the certificate to the store.
+        /// </summary>
+        /// <param name="certificate">The certificate to save to the store.</param>
+        /// <returns>A task that completes when the certificate has been saved.</returns>
+        Task Save(X509Certificate2 certificate);
+    }
+}
---
+++ src/Validation.PackageSigning.Core/Messages/CertificateValidationMessageSerializer.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using NuGet.Services.ServiceBus;
+
+namespace NuGet.Jobs.Validation.PackageSigning.Messages
+{
+    public class CertificateValidationMessageSerializer : IBrokeredMessageSerializer<CertificateValidationMessage>
+    {
+        private const string CertificateValidationSchemaName = "CertificateValidationMessageData";
+
+        private IBrokeredMessageSerializer<CertificateValidationMessageData1> _serializer =
+            new BrokeredMessageSerializer<CertificateValidationMessageData1>();
+
+        public IBrokeredMessage Serialize(CertificateValidationMessage message)
+        {
+            return _serializer.Serialize(new CertificateValidationMessageData1
+            {
+                CertificateKey = message.CertificateKey,
+                ValidationId = message.ValidationId,
+                RevalidateRevokedCertificate = message.RevalidateRevokedCertificate
+            });
+        }
+
+        public CertificateValidationMessage Deserialize(IBrokeredMessage message)
+        {
+            var deserializedMessage = _serializer.Deserialize(message);
+
+            return new CertificateValidationMessage
+            {
+                CertificateKey = deserializedMessage.CertificateKey,
+                ValidationId = deserializedMessage.ValidationId,
+                RevalidateRevokedCertificate = deserializedMessage.RevalidateRevokedCertificate
+            };
+        }
+
+        [Schema(Name = CertificateValidationSchemaName, Version = 1)]
+        private struct CertificateValidationMessageData1
+        {
+            public long CertificateKey { get; set; }
+            public Guid ValidationId { get; set; }
+            public bool RevalidateRevokedCertificate { get; set; }
+        }
+    }
+}
