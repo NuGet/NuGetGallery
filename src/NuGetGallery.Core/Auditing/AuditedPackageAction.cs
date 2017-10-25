a5115a8dba3c4c48faca5bb962f7675691836f07
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2017-10-25 19:27:33 +0200

    Get rid of obsolete notifications on package details view (#4902)
    * Remove "pending edit" notifications from package details view
    
    * Remove min-client requirement for push message on package details view
    
    * Mark AuditedPackageAction.UndoEdit as obsolete

--- src/NuGetGallery.Core/Auditing/AuditedPackageAction.cs
+++ src/NuGetGallery.Core/Auditing/AuditedPackageAction.cs
@@ -1,6 +1,8 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+
 namespace NuGetGallery.Auditing
 {
     public enum AuditedPackageAction
@@ -11,6 +13,7 @@ namespace NuGetGallery.Auditing
         List,
         Unlist,
         Edit,
+        [Obsolete("Undo package edit functionality is being retired.")]
         UndoEdit,
         Verify
     }
