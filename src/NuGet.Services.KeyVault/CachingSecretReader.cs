b84a8d89a3355d49823d5a9a600167d7be296712
Loïc Sharma <sharma.loic@gmail.com>
2017-11-08 09:38:28 -0800

    [Package Signing] Added a Scoped Service Bus Message Handler (#82)
    Often times, Service Bus message handlers will need to use some sort of Entity Framework context. This requires that the handlers' callbacks are executed within their own dependency injection scope. This change creates a new generic Service Bus message handler `ScopedMessageHandler` that, upon receiving a message, creates a new dependency injection scope.
    
    This change also makes `CachingSecretReader` thread-safe, as otherwise, the `ScopedMessageHandler` would need a lock.

--- src/NuGet.Services.KeyVault/CachingSecretReader.cs
+++ src/NuGet.Services.KeyVault/CachingSecretReader.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
 
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 namespace NuGet.Services.KeyVault
@@ -10,39 +10,65 @@ namespace NuGet.Services.KeyVault
     public class CachingSecretReader : ISecretReader
     {
         public const int DefaultRefreshIntervalSec = 60 * 60 * 24; // 1 day
-        private readonly int _refreshIntervalSec;
 
         private readonly ISecretReader _internalReader;
-        private readonly Dictionary<string, Tuple<string, DateTime>> _cache;
+        private readonly ConcurrentDictionary<string, CachedSecret> _cache;
+        private readonly TimeSpan _refreshInterval;
 
         public CachingSecretReader(ISecretReader secretReader, int refreshIntervalSec = DefaultRefreshIntervalSec)
         {
-            if (secretReader == null)
-            {
-                throw new ArgumentNullException(nameof(secretReader));
-            }
+            _internalReader = secretReader ?? throw new ArgumentNullException(nameof(secretReader));
+            _cache = new ConcurrentDictionary<string, CachedSecret>();
 
-            _internalReader = secretReader;
-            _cache = new Dictionary<string, Tuple<string, DateTime>>();
-
-            _refreshIntervalSec = refreshIntervalSec;
-        }
-
-        public virtual bool IsSecretOutdated(Tuple<string, DateTime> cachedSecret)
-        {
-            return DateTime.UtcNow.Subtract(cachedSecret.Item2).TotalSeconds >= _refreshIntervalSec;
+            _refreshInterval = TimeSpan.FromSeconds(refreshIntervalSec);
         }
 
         public async Task<string> GetSecretAsync(string secretName)
         {
-            if (!_cache.ContainsKey(secretName) || IsSecretOutdated(_cache[secretName]))
+            if (string.IsNullOrEmpty(secretName))
             {
-                // Get the secret if it is not yet in the cache or it is outdated.
-                var secretValue = await _internalReader.GetSecretAsync(secretName);
-                _cache[secretName] = Tuple.Create(secretValue, DateTime.UtcNow);
+                throw new ArgumentException("Null or empty secret name", nameof(secretName));
             }
 
-            return _cache[secretName].Item1;
+            // If the cache contains the secret and it is not expired, return the cached value.
+            if (_cache.TryGetValue(secretName, out CachedSecret result)
+                && !IsSecretOutdated(result))
+            {
+                return result.Value;
+            }
+
+            // The cache does not contain a fresh copy of the secret. Fetch and cache the secret.
+            var updatedValue = new CachedSecret(await _internalReader.GetSecretAsync(secretName));
+
+            return _cache.AddOrUpdate(secretName, updatedValue, (key, old) => updatedValue)
+                         .Value;
+        }
+
+        private bool IsSecretOutdated(CachedSecret secret)
+        {
+            return (DateTime.UtcNow - secret.CacheTime) >= _refreshInterval;
+        }
+
+        /// <summary>
+        /// A cached secret.
+        /// </summary>
+        private class CachedSecret
+        {
+            public CachedSecret(string value)
+            {
+                Value = value;
+                CacheTime = DateTimeOffset.UtcNow;
+            }
+
+            /// <summary>
+            /// The value of the cached secret.
+            /// </summary>
+            public string Value { get; }
+
+            /// <summary>
+            /// The time at which the secret was cached.
+            /// </summary>
+            public DateTimeOffset CacheTime { get; }
         }
     }
 }
---
+++ tests/NuGet.Services.ServiceBus.Tests/ScopedMessageHandlerFacts.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+
+namespace NuGet.Services.ServiceBus.Tests
+{
+    public class ScopedMessageHandlerFacts
+    {
+        [Fact]
+        public async Task CreatesMessageHandlerUsingScope()
+        {
+            // Arrange
+            var scopeFactoryMock = new Mock<IServiceScopeFactory>();
+            var scopeMock = new Mock<IServiceScope>();
+            var serviceProviderMock = new Mock<IServiceProvider>();
+            var handlerMock = new Mock<IMessageHandler<object>>();
+
+            scopeFactoryMock
+                .Setup(f => f.CreateScope())
+                .Returns(scopeMock.Object);
+
+            scopeMock
+                .SetupGet(s => s.ServiceProvider)
+                .Returns(serviceProviderMock.Object);
+
+            serviceProviderMock
+                .Setup(p => p.GetService(typeof(IMessageHandler<object>)))
+                .Returns(handlerMock.Object);
+
+            var target = new ScopedMessageHandler<object>(scopeFactoryMock.Object);
+
+            // Act - send two empty messages.
+            await target.HandleAsync(new object());
+            await target.HandleAsync(new object());
+
+            // Assert
+            scopeFactoryMock.Verify(f => f.CreateScope(), Times.Exactly(2));
+            serviceProviderMock.Verify(p => p.GetService(typeof(IMessageHandler<object>)), Times.Exactly(2));
+            handlerMock.Verify(h => h.HandleAsync(It.IsAny<object>()), Times.Exactly(2));
+        }
+    }
+}
