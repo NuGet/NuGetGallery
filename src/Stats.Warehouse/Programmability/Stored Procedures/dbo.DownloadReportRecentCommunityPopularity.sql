78d750b06fd4ac3349faf725ce55f0aab5786659
Loïc Sharma <sharma.loic@gmail.com>
2017-11-08 09:15:45 -0800

    [Statistics] Add community popularity reports (#239)
    Adds reports that finds the most popular packages that aren't labelled as "Microsoft", "ASP.NET MVC", or "ASP.NET Web API" packages. The queries were tested against the statistics database.
    
    These reports will be used to fix https://github.com/NuGet/NuGetGallery/issues/3417.
    
    Useful links:
    * [This list](https://gist.githubusercontent.com/loic-sharma/36d9a9cf742df9c85645dd275d6a9d2d/raw/df1d80e33ac6b9ce9cfb2a010f4c5d97fe8c25ea/gistfile1.txt) contains all the packages that will be considered "non-community".
    * [This powershell script](https://gist.github.com/loic-sharma/39c65e3b4d02bac1f52242ef24cf3832) will be used to import non-community packages into the statistics database.
    * [This SQL query](https://gist.github.com/loic-sharma/76090850a76d38c1611ec7b0183bc3ea) is the result of running the powershell script.

---
+++ src/Stats.Warehouse/Programmability/Stored Procedures/dbo.DownloadReportRecentCommunityPopularity.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROCEDURE [dbo].[DownloadReportRecentCommunityPopularity]
+	@ReportGenerationTime DATETIME
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	DECLARE @Cursor DATETIME = (SELECT ISNULL(MAX([Position]), @ReportGenerationTime) FROM [dbo].[Cursors] (NOLOCK) WHERE [Name] = 'GetDirtyPackageId')
+	DECLARE @NonCommunityPackagesId int = (SELECT [Id] FROM [dbo].[Dimension_PackageSet] WHERE [Name] = 'NonCommunityPackages');
+
+	-- Find all packages that have had download facts added in the last 42 days, today inclusive
+	SELECT		TOP 100 P.[PackageId]
+				,SUM(ISNULL(F.[DownloadCount], 0)) AS 'Downloads'
+	FROM		[dbo].[Fact_Download] AS F (NOLOCK)
+
+	INNER JOIN	[dbo].[Dimension_Date] AS D (NOLOCK)
+	ON			F.[Dimension_Date_Id] = D.[Id]
+
+	INNER JOIN	[dbo].[Dimension_Package] AS P (NOLOCK)
+	ON			F.[Dimension_Package_Id] = P.[Id]
+
+	INNER JOIN	Dimension_Client AS C (NOLOCK)
+	ON			C.[Id] = F.[Dimension_Client_Id]
+
+	WHERE		D.[Date] IS NOT NULL
+			AND ISNULL(D.[Date], CONVERT(DATE, '1900-01-01')) >= CONVERT(DATE, DATEADD(day, -42, @ReportGenerationTime))
+			AND ISNULL(D.[Date], CONVERT(DATE, DATEADD(day, 1, @ReportGenerationTime))) <= CONVERT(DATE, @ReportGenerationTime)
+			AND F.[Timestamp] <= @Cursor
+			AND C.ClientCategory NOT IN ('Crawler', 'Unknown')
+			AND NOT (C.ClientCategory = 'NuGet' AND CAST(ISNULL(C.[Major], '0') AS INT) > 10)
+			AND P.[LowercasedPackageId] NOT IN (
+				SELECT [LowercasedPackageId]
+				FROM [dbo].[Fact_Package_PackageSet]
+				WHERE [Dimension_PackageSet_Id] = @NonCommunityPackagesId
+			)
+
+	GROUP BY	P.[PackageId]
+	ORDER BY	[Downloads] DESC
+END
