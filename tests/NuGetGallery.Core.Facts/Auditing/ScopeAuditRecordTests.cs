d53db25651888dc358102a583d5e665c12bb68de
Christy Henriksson <chenriks@microsoft.com>
2017-11-15 16:19:00 -0800

    Add ApiKey owner scope auditing

--- tests/NuGetGallery.Core.Facts/Auditing/ScopeAuditRecordTests.cs
+++ tests/NuGetGallery.Core.Facts/Auditing/ScopeAuditRecordTests.cs
@@ -8,13 +8,14 @@ namespace NuGetGallery.Auditing
     public class ScopeAuditRecordTests
     {
         [Theory]
-        [InlineData(null, null)]
-        [InlineData("", "")]
-        [InlineData("a", "b")]
-        public void Constructor_SetsProperties(string subject, string allowedAction)
+        [InlineData(null, null, null)]
+        [InlineData("", "", "")]
+        [InlineData("a", "b", "c")]
+        public void Constructor_SetsProperties(string owner, string subject, string allowedAction)
         {
-            var entry = new ScopeAuditRecord(subject, allowedAction);
+            var entry = new ScopeAuditRecord(owner, subject, allowedAction);
 
+            Assert.Equal(owner, entry.OwnerUsername);
             Assert.Equal(subject, entry.Subject);
             Assert.Equal(allowedAction, entry.AllowedAction);
         }
