006cc473b3dc6c1dfc293d2b19f52021bc4e936f
Loïc Sharma <sharma.loic@gmail.com>
2017-11-27 15:03:25 -0800

    Revert "Merge pull request #95 from NuGet/dev-revert-breaking"
    This reverts commit 2a61d6473599855a0c76916577af0db24fd74e03, reversing
    changes made to 52874ea0e9d8760dc77953be9d44be52460dc280.

--- src/NuGet.Services.Validation/Entities/EndCertificateStatus.cs
+++ src/NuGet.Services.Validation/Entities/EndCertificateStatus.cs
@@ -4,29 +4,29 @@
 namespace NuGet.Services.Validation
 {
     /// <summary>
-    /// The status for a given <see cref="Certificate"/>.
+    /// The status for a given <see cref="EndCertificate"/>.
     /// </summary>
-    public enum CertificateStatus
+    public enum EndCertificateStatus
     {
         /// <summary>
-        /// The status is unknown if this <see cref="Certificate"/>'s online verification has never completed.
+        /// The status is unknown if this <see cref="EndCertificate"/>'s online verification has never completed.
         /// </summary>
         Unknown = 0,
 
         /// <summary>
-        /// The <see cref="Certificate" /> is valid and has not been revoked.
+        /// The <see cref="EndCertificate" /> is valid and has not been revoked.
         /// </summary>
         Good = 1,
 
         /// <summary>
-        /// The <see cref="Certificate" /> has failed offline validations. This could be for a number of reasons including
+        /// The <see cref="EndCertificate" /> has failed offline validations. This could be for a number of reasons including
         /// an untrusted root or weak hashing algorithm. Anything signed by the certificate should be considered invalid.
         /// </summary>
         Invalid = 2,
 
         /// <summary>
-        /// The <see cref="Certificate"/> has been revoked by the certificate authority. Anything signed by the certificate
-        /// after <see cref="Certificate.RevocationTime"/> should be considered invalid.
+        /// The <see cref="EndCertificate"/> has been revoked by the certificate authority. Anything signed by the certificate
+        /// after <see cref="EndCertificate.RevocationTime"/> should be considered invalid.
         /// </summary>
         Revoked = 3,
     }
