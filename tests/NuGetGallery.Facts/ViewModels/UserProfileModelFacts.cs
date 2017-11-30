3ca798a241a55bc123c9a628feb8dcfc229ee579
Christy Henriksson <chenriks@microsoft.com>
2017-11-30 11:18:06 -0800

    Fix int overflow for user total downloads

---
+++ tests/NuGetGallery.Facts/ViewModels/UserProfileModelFacts.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using NuGetGallery.Framework;
+using Xunit;
+
+namespace NuGetGallery.ViewModels
+{
+    public class UserProfileModelFacts
+    {
+        public class TheTotalDownloadCountProperty : TestContainer
+        {
+            [Fact]
+            public void TotalDownloadCount_DoesNotThrowIntegerOverflow()
+            {
+                // Arrange
+                var controller = GetController<UsersController>();
+                var user = new User("theUser");
+                var packageViewModel = new ListPackageItemViewModel(new Package
+                {
+                    PackageRegistration = new PackageRegistration
+                    {
+                        DownloadCount = int.MaxValue
+                    },
+                    Version = "1.0.0"
+                });
+                var packages = new List<ListPackageItemViewModel> { packageViewModel, packageViewModel };
+
+                // Act
+                var profile = new UserProfileModel(user, packages, 0, 10, controller.Url);
+
+                // Assert
+                long expected = (long)int.MaxValue * 2;
+                Assert.Equal(expected, profile.TotalPackageDownloadCount);
+            }
+        }
+    }
+}
