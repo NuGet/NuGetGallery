402ba5c9c95b5d21d733a7573af21f18e2daa47b
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2017-12-04 12:11:23 +0100

    Edit package is now edit Documentation only (#5095)
    * Minor change to wording of edit page alert.
    
    * Update edit page to support edits to readme only
    
    * Remove unused reset functions from `page-edit-readme.js`

--- src/NuGetGallery/Scripts/gallery/page-edit-readme.js
+++ src/NuGetGallery/Scripts/gallery/page-edit-readme.js
@@ -1,5 +1,163 @@
 ﻿'use strict';
 
+var EditReadmeManager = new function () {
+    var _currVersion;
+    var _viewModel;
+    var _changedState;
+    var _submitUrl;
+    var _cancelUrl;
+    var _submitting;
+    var _submitted = true;
+
+    this.init = function (model, submitUrl, cancelUrl) {
+        _submitting = false;
+        _submitted = false;
+        _submitUrl = submitUrl;
+        _cancelUrl = cancelUrl;
+        _viewModel = model;
+        _changedState = {};
+        bindData(_viewModel);
+
+        $(window).on('beforeunload', confirmLeave);
+
+        $('#verify-submit-button').attr('disabled', 'disabled');
+
+        $('#input-select-version').on('change', function () {
+            document.location = $(this).val();
+        });
+
+        $('input[type="text"], input[type="checkbox"], input[type="url"], input[type="file"], textarea').on('change keydown', function () {
+            $(this).addClass("edited");
+            _changedState[$(this).attr('id')] = true;
+            $('#verify-submit-button').removeAttr('disabled');
+        });
+    }
+
+    this.isEdited = function () {
+        return Object.keys(_changedState).reduce(function (previous, key) { return previous || _changedState[key]; }, false);
+    }
+
+    function confirmLeave() {
+        var message = "";
+        if (_submitting) {
+            message = "Your request is being submitted. Are you sure you want to leave?";
+        }
+
+        if (message !== "") {
+            return message;
+        }
+    }
+
+    function submitAsync(callback, error) {
+        if (EditReadmeManager.isEdited()) {
+            if (!_submitting) {
+                _submitting = true;
+                $.ajax({
+                    url: _submitUrl,
+                    type: 'POST',
+
+                    data: new FormData($('#edit-readme-form')[0]),
+
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+
+                    success: function (model, resultCodeString, fullResponse) {
+                        _submitting = false;
+                        _submitted = true;
+                        if (callback) {
+                            callback(model);
+                        }
+                    },
+
+                    error: handleErrors.bind(this, error)
+                });
+            }
+        } else {
+            if (callback) {
+                callback();
+            }
+        }
+    }
+
+    function cancelEdit() {
+        navigateToPage({ location: _cancelUrl });
+    }
+
+    function navigateToPage(editReadmeResponse) {
+        document.location = editReadmeResponse.location;
+    }
+
+    function displayErrors(errors) {
+        if (errors == null || errors.length < 1) {
+            return;
+        }
+
+        var failureContainer = $("#validation-failure-container");
+        var failureListContainer = document.createElement("div");
+        $(failureListContainer).attr("id", "validation-failure-list");
+        $(failureListContainer).attr("data-bind", "template: { name: 'validation-errors', data: data }");
+        failureContainer.append(failureListContainer);
+        ko.applyBindings({ data: errors }, failureListContainer);
+
+        failureContainer.removeClass("hidden");
+    }
+
+    function handleErrors(errorCallback, model, resultCodeString, fullResponse) {
+        bindData(null);
+
+        _submitting = false;
+        switch (resultCodeString) {
+            case "timeout":
+                displayErrors(["The operation timed out. Please try again."])
+                break;
+            case "abort":
+                displayErrors(["The operation was aborted. Please try again."])
+                break;
+            default:
+                displayErrors(model.responseJSON);
+                break;
+        }
+
+        if ((fullResponse && fullResponse.status >= 500) || (model && model.status >= 500)) {
+            displayErrors(["There was a server error."])
+        }
+
+        if (errorCallback) {
+            errorCallback();
+        }
+    }
+
+    function bindData(model) {
+        
+        if (model == null) {
+            return;
+        }
+
+        var submitContainerElement = document.createElement("div");
+        $(submitContainerElement).attr("id", "submit-block");
+        $(submitContainerElement).attr("class", "collapse in");
+        $(submitContainerElement).attr("aria-expanded", "true");
+        $(submitContainerElement).attr("data-bind", "template: { name: 'submit-package-template', data: data }");
+        $("#submit-package-container").append(submitContainerElement);
+        ko.applyBindings({ data: model }, submitContainerElement);
+
+        $('#verify-cancel-button').on('click', function () {
+            cancelEdit();
+        });
+
+        $('#verify-submit-button').on('click', function () {
+            $('#verify-cancel-button').attr('disabled', 'disabled');
+            $('#verify-submit-button').attr('disabled', 'disabled');
+            $('#verify-submit-button').attr('value', 'Submitting');
+            $('#verify-submit-button').addClass('.loading');
+            submitAsync(navigateToPage);
+        });
+
+        bindReadMeData(model);
+    }
+}
+
 function bindReadMeData(model) {
     $("#import-readme-block").remove();
     $("#readme-collapser-container").addClass("hidden");
