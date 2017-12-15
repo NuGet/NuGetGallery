63517068b5fa391481dd7daabf90eca3f9f8c885
Scott Bommarito <scottbom@microsoft.com>
2017-12-14 16:09:24 -0800

    remove excess whitespace

--- src/NuGetGallery/Controllers/PackagesController.cs
+++ src/NuGetGallery/Controllers/PackagesController.cs
@@ -1640,7 +1640,7 @@ namespace NuGetGallery
                 catch (InvalidPackageException ex)
                 {
                     _telemetryService.TraceException(ex);
-                    
+
                     return Json(400, new[] { ex.Message });
                 }
 
