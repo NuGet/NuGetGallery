aae5c6833bb636df3bb55c064e8f68a858b0d3ee
Svetlana Kofman <skofman@microsoft.com>
2017-12-18 16:55:57 -0800

    PR comments

--- src/NuGetGallery/ViewModels/DeletePackageViewModel.cs
+++ src/NuGetGallery/ViewModels/DeletePackageViewModel.cs
@@ -34,6 +34,6 @@ namespace NuGetGallery
 
         public DeletePackagesRequest DeletePackagesRequest { get; set; }
 
-        public bool IsLocked { get; set; }
+        public bool IsLocked { get; }
     }
 }
