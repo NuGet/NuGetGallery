86cc63122024339419649540a81718d4eb46ce22
Cristina Manu <cmanu@microsoft.com>
2018-01-18 10:41:01 -0800

    fixUploadPackageFromUI

--- tests/NuGetGallery.FunctionalTests.Fluent/ExtensionMethods.cs
+++ tests/NuGetGallery.FunctionalTests.Fluent/ExtensionMethods.cs
@@ -11,7 +11,7 @@ namespace NuGetGallery.FunctionalTests.Fluent
         {
             I.Open(UrlHelper.BaseUrl + "users/account/LogOn");
             I.Expect.Url(x => x.LocalPath.Contains("LogOn"));
-            I.Enter(EnvironmentSettings.TestAccountName).In("#SignIn_UserNameOrEmail");
+            I.Enter(EnvironmentSettings.TestAccountEmail).In("#SignIn_UserNameOrEmail");
             I.Enter(EnvironmentSettings.TestAccountPassword).In("#SignIn_Password");
             I.Click("#signin-link");
             I.Wait(1);
