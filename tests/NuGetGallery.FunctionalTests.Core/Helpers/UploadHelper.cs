63dd7ae2c25611233e1632474854347a3f6d3b71
Scott Bommarito <scottbom@microsoft.com>
2018-01-30 14:31:54 -0800

    sort usings

--- tests/NuGetGallery.FunctionalTests.Core/Helpers/UploadHelper.cs
+++ tests/NuGetGallery.FunctionalTests.Core/Helpers/UploadHelper.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.WebTesting;
-using System.Linq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.WebTesting;
 
 namespace NuGetGallery.FunctionalTests.Helpers
 {
