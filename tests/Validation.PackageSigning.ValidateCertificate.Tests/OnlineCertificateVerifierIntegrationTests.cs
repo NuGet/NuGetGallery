fba9d2dee0b536f5627385157bc53d68ce2374ca
Loïc Sharma <sharma.loic@gmail.com>
2018-02-07 18:19:01 -0800

    [Package Signing] Certificate Validation Testing Infrastructure (#332)
    The focus of this change is to prepare the Validate Certificate job's test project for integration testing:
    
    * Moves common integration testing infrastructure from E&V's test project to the common project
    * Adds a basic integration test to verify a valid codesigning certificate
    
    Progress on https://github.com/NuGet/Engineering/issues/878

---
+++ tests/Validation.PackageSigning.ValidateCertificate.Tests/OnlineCertificateVerifierIntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using NuGet.Services.Validation;
+using Validation.PackageSigning.Core.Tests.Support;
+using Validation.PackageSigning.ValidateCertificate.Tests.Support;
+using Xunit;
+
+namespace Validation.PackageSigning.ValidateCertificate.Tests
+{
+    [Collection(CertificateIntegrationTestCollection.Name)]
+    public class OnlineCertificateVerifierIntegrationTests
+    {
+        private readonly CertificateIntegrationTestFixture _fixture;
+        private readonly OnlineCertificateVerifier _target;
+
+        public OnlineCertificateVerifierIntegrationTests(CertificateIntegrationTestFixture fixture)
+        {
+            _fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+
+            _target = new OnlineCertificateVerifier();
+        }
+
+        [Fact]
+        public async Task ValidCodeSigningCertificate()
+        {
+            // Arrange
+            var certificate = await _fixture.GetSigningCertificateAsync();
+
+            // Act & assert
+            var result = _target.VerifyCodeSigningCertificate(certificate, new X509Certificate2[0]);
+
+            Assert.Equal(EndCertificateStatus.Good, result.Status);
+            Assert.Equal(X509ChainStatusFlags.NoError, result.StatusFlags);
+            Assert.NotNull(result.StatusUpdateTime);
+            Assert.Null(result.RevocationTime);
+        }
+    }
+}
---
+++ tests/Validation.PackageSigning.ValidateCertificate.Tests/Support/CertificateIntegrationTestCollection.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Validation.PackageSigning.Core.Tests.Support;
+using Xunit;
+
+namespace Validation.PackageSigning.ValidateCertificate.Tests.Support
+{
+    [CollectionDefinition(Name)]
+    public class CertificateIntegrationTestCollection : ICollectionFixture<CertificateIntegrationTestFixture>
+    {
+        public const string Name = "Certificate integration test collection";
+    }
+}
