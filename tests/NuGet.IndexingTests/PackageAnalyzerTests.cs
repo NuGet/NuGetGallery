d827421507455b28894c314a04996e9e1088fb05
Joel Verhagen <jver@microsoft.com>
2018-02-16 10:55:02 -0800

    Revert "Fix broken unit tests"
    This reverts commit 752803b6a1a4f2f0b0faabfe1bd9fd44a352b31b.

--- tests/NuGet.IndexingTests/PackageAnalyzerTests.cs
+++ tests/NuGet.IndexingTests/PackageAnalyzerTests.cs
@@ -87,7 +87,7 @@ namespace NuGet.IndexingTests
 
                 yield return new object[]
                 {
-                    "CaseInsensitiveNormalizedVersion",
+                    "CaseInsensitiveVersion",
                     "01.002.0003-ALPHA",
                     new[]
                     {
@@ -99,6 +99,16 @@ namespace NuGet.IndexingTests
                 yield return GetDescriptionTestCase("Description");
                 yield return GetDescriptionTestCase("Summary");
                 yield return GetDescriptionTestCase("Authors");
+                
+                yield return new object[]
+                {
+                    "Owner",
+                    "Microsoft",
+                    new[]
+                    {
+                        new TokenAttributes("microsoft", 0, 9)
+                    }
+                };
 
                 yield return new object[]
                 {
