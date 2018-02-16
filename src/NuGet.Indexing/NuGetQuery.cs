bc96c2abe33a2df6fa88d1531d63c1aa1f33760c
Joel Verhagen <jver@microsoft.com>
2018-02-16 10:54:44 -0800

    Revert "When searching, use the case-insensitive package version field in the Lucene document (#258)"
    This reverts commit de73ffa9964ae0a325a7453cdf75732341fd0907.

--- src/NuGet.Indexing/NuGetQuery.cs
+++ src/NuGet.Indexing/NuGetQuery.cs
@@ -166,12 +166,7 @@ namespace NuGet.Indexing
 
         private static void VersionClause(BooleanQuery query, Analyzer analyzer, IEnumerable<string> values, Occur occur)
         {
-            query.Add(
-                ConstructClauseQuery(
-                    analyzer,
-                    LuceneConstants.CaseInsensitiveNormalizedVersionPropertyName,
-                    values),
-                occur);
+            query.Add(ConstructClauseQuery(analyzer, LuceneConstants.NormalizedVersionPropertyName, values), occur);
         }
 
         private static void TitleClause(BooleanQuery query, Analyzer analyzer, IEnumerable<string> values, Occur occur)
