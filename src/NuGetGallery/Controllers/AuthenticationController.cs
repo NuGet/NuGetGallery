ec6d1b4d0f9bc3f6ebc443e2625c9b694d10d792
shishirh <shishirh@microsoft.com>
2018-03-07 12:49:23 -0800

    ui fixes

--- src/NuGetGallery/Controllers/AuthenticationController.cs
+++ src/NuGetGallery/Controllers/AuthenticationController.cs
@@ -4,11 +4,9 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Net.Mail;
 using System.Security.Claims;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
