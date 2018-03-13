78bfd0f9e42936d736f44c919b3aaac436758e0a
Svetlana Kofman <skofman@microsoft.com>
2018-03-13 09:50:58 -0700

    Add Asia deployment script for Search.GenerateAuxiliaryData (#369)

---
+++ src/Search.GenerateAuxiliaryData/Scripts/Search.GenerateAuxiliaryData.Asia.cmd
@@ -0,0 +1,22 @@
+@echo OFF
+	
+cd bin
+
+:Top
+	echo "Starting job - #{Jobs.Asia.search.generateauxiliarydata.Title}"
+
+	title #{Jobs.Asia.search.generateauxiliarydata.Title}
+
+    start /w search.generateauxiliarydata.exe ^
+	-VaultName "#{Deployment.Azure.KeyVault.VaultName}" ^
+	-ClientId "#{Deployment.Azure.KeyVault.ClientId}" ^
+	-CertificateThumbprint "#{Deployment.Azure.KeyVault.CertificateThumbprint}" ^
+	-PrimaryDestination #{Jobs.Asia.search.generateauxiliarydata.Storage.Primary} ^
+	-PackageDatabase "#{Jobs.search.generateauxiliarydata.PackageDatabase}" ^
+	-StatisticsDatabase "#{Jobs.search.generateauxiliarydata.StatisticsDatabase}" ^
+	-verbose true -Sleep #{Jobs.search.generateauxiliarydata.Sleep} ^
+	-InstrumentationKey "#{Jobs.search.generateauxiliarydata.ApplicationInsightsInstrumentationKey}"
+
+	echo "Finished #{Jobs.search.generateauxiliarydata.Title}"
+
+	goto Top
