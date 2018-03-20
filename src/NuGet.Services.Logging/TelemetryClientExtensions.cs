57581e76b6e0c836113a7195e35b2ce6880b9d9c
Loïc Sharma <sharma.loic@gmail.com>
2018-03-20 15:36:12 -0700

    Add TrackDuration extension (#143)
    Add a `TrackDuration` method. This allows you to easily instrument duration of code.

---
+++ src/NuGet.Services.Logging/TelemetryClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace NuGet.Services.Logging
+{
+    public static class TelemetryClientExtensions
+    {
+        public static IDisposable TrackDuration(
+            this ITelemetryClient telemetry,
+            string metricName,
+            IDictionary<string, string> properties = null)
+        {
+            return new DurationMetric(telemetry, metricName, properties);
+        }
+    }
+}
