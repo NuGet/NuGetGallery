4731bc265fcbd79920385cbfe21c070039b449a1
Scott Bommarito <scottbom@microsoft.com>
2018-03-28 09:52:32 -0700

    style improvements to manage orgs item view

--- src/NuGetGallery/ViewModels/ManageOrganizationsItemViewModel.cs
+++ src/NuGetGallery/ViewModels/ManageOrganizationsItemViewModel.cs
@@ -7,16 +7,47 @@ namespace NuGetGallery
 {
     public class ManageOrganizationsItemViewModel
     {
+        /// <summary>
+        /// The name of this organization.
+        /// </summary>
         public string OrganizationName { get; }
+
+        /// <summary>
+        /// The email address of the organization.
+        /// </summary>
         public string EmailAddress { get; }
+
+        /// <summary>
+        /// Whether or not the current user is an administrator or pending administrator of this organization.
+        /// </summary>
         public bool CurrentUserIsAdmin { get; }
+
+        /// <summary>
+        /// The number of members in this organization.
+        /// </summary>
         public int MemberCount { get; }
+
+        /// <summary>
+        /// The number of packages owned by this organization.
+        /// </summary>
         public int PackagesCount { get; }
+
+        /// <summary>
+        /// Whether or not this is a pending request--the current user is not yet a member of this organization, but can choose to accept or decline a request to become one.
+        /// </summary>
         public bool IsPendingRequest { get; }
+
+        /// <summary>
+        /// Whether or not this is a request for a user account to be transformed into an organization with the current user as the administrator.
+        /// </summary>
         public bool IsPendingTransformRequest { get; }
+
+        /// <summary>
+        /// If this is a pending request, the confirmation token to confirm or decline the request.
+        /// </summary>
         public string ConfirmationToken { get; }
 
-        public ManageOrganizationsItemViewModel(User user, bool isAdmin, IPackageService packageService)
+        private ManageOrganizationsItemViewModel(User user, bool isAdmin, IPackageService packageService)
         {
             OrganizationName = user.Username;
             EmailAddress = user.EmailAddress;
