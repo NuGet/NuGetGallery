5612b49fdcde5043fb213598a1b2e9d9f90eb15b
Cristina Manu <cmanu@microsoft.com>
2018-03-29 17:16:47 -0700

    feedback

--- src/NuGet.Services.Search.Client/Client/ThreadSafeRandom.cs
+++ src/NuGet.Services.Search.Client/Client/ThreadSafeRandom.cs
@@ -12,31 +12,35 @@ namespace NuGet.Services.Search.Client
     public static class ThreadSafeRandom
     {
         private static Random _global = new Random();
+
         [ThreadStatic]
         private static Random _local;
 
         public static int Next()
         {
-            Random inst = _local;
-            if (inst == null)
-            {
-                int seed;
-                lock (_global) seed = _global.Next();
-                _local = inst = new Random(seed);
-            }
+            Random inst = GetLocalInstance();
             return inst.Next();
         }
 
         public static int Next(int min, int max)
         {
+            Random inst = GetLocalInstance();
+            return inst.Next(min, max);
+        }
+
+        static Random GetLocalInstance()
+        {
             Random inst = _local;
             if (inst == null)
             {
                 int seed;
-                lock (_global) seed = _global.Next();
+                lock (_global)
+                {
+                    seed = _global.Next();
+                }
                 _local = inst = new Random(seed);
             }
-            return inst.Next(min, max);
+            return inst;
         }
     }
 }
