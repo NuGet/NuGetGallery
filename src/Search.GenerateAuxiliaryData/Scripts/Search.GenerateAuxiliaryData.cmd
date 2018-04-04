465301aad1ba3789123b1c68e8394549d8529858
Svetlana Kofman <skofman@microsoft.com>
2018-04-04 13:35:52 -0700

    Remove line breaks from script files

--- src/Search.GenerateAuxiliaryData/Scripts/Search.GenerateAuxiliaryData.cmd
+++ src/Search.GenerateAuxiliaryData/Scripts/Search.GenerateAuxiliaryData.cmd
@@ -7,19 +7,8 @@ cd bin
 
 	title #{Jobs.search.generateauxiliarydata.Title}
 
-    start /w search.generateauxiliarydata.exe ^
-	-VaultName "#{Deployment.Azure.KeyVault.VaultName}" ^
-	-ClientId "#{Deployment.Azure.KeyVault.ClientId}" ^
-	-CertificateThumbprint "#{Deployment.Azure.KeyVault.CertificateThumbprint}" ^
-	-PrimaryDestination #{Jobs.search.generateauxiliarydata.Storage.Primary} ^
-	-PackageDatabase "#{Jobs.search.generateauxiliarydata.PackageDatabase}" ^
-	-StatisticsDatabase "#{Jobs.search.generateauxiliarydata.StatisticsDatabase}" ^
-	-AzureCdnCloudStorageAccount "#{Jobs.stats.createazurecdnwarehousereports.AzureCdn.CloudStorageAccount}"
-	-AzureCdnCloudStorageContainerName "#{Jobs.stats.createazurecdnwarehousereports.AzureCdn.CloudStorageContainerName}"
-	-verbose true ^
-	-Sleep #{Jobs.search.generateauxiliarydata.Sleep} ^
-	-InstrumentationKey "#{Jobs.search.generateauxiliarydata.ApplicationInsightsInstrumentationKey}"
-
+    start /w search.generateauxiliarydata.exe -VaultName "#{Deployment.Azure.KeyVault.VaultName}" -ClientId "#{Deployment.Azure.KeyVault.ClientId}" -CertificateThumbprint "#{Deployment.Azure.KeyVault.CertificateThumbprint}" -PrimaryDestination #{Jobs.search.generateauxiliarydata.Storage.Primary} -PackageDatabase "#{Jobs.search.generateauxiliarydata.PackageDatabase}" -StatisticsDatabase "#{Jobs.search.generateauxiliarydata.StatisticsDatabase}" -AzureCdnCloudStorageAccount "#{Jobs.stats.createazurecdnwarehousereports.AzureCdn.CloudStorageAccount}"	-AzureCdnCloudStorageContainerName "#{Jobs.stats.createazurecdnwarehousereports.AzureCdn.CloudStorageContainerName}" -verbose true -Sleep #{Jobs.search.generateauxiliarydata.Sleep} -InstrumentationKey "#{Jobs.search.generateauxiliarydata.ApplicationInsightsInstrumentationKey}"
+	
 	echo "Finished #{Jobs.search.generateauxiliarydata.Title}"
 
 	goto Top
