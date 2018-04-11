dcdf54eb4db796c875da6d83e92f4c84718c302f
Cristina Manu <cmanu@microsoft.com>
2018-04-10 18:40:40 -0700

    Changes for GA

---
+++ src/NuGetGallery/Helpers/ObfuscationHelper.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Web;
+using System.Web.Routing;
+
+namespace NuGetGallery.Helpers
+{
+    public class ObfuscationHelper
+    {
+        public static string ObfuscateRequestUrl(HttpContextBase httpContext, RouteCollection routes)
+        {
+            if (httpContext == null || httpContext.Request == null || httpContext.Request.Url == null || routes == null)
+            {
+                return string.Empty;
+            }
+
+            var route = routes.GetRouteData(httpContext)?.Route as Route;
+            if (route == null)
+            {
+                return string.Empty;
+            }
+            return route.ObfuscateUrlPath(httpContext.Request.Url.AbsolutePath.TrimStart('/'));
+        }
+    }
+}
