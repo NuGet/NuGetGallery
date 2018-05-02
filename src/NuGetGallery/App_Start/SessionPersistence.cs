f08102fdfeb29d6f98f108329a18176b969cb5b4
shishirh <shishirh@microsoft.com>
2018-05-02 13:21:25 -0700

    Add comments

--- src/NuGetGallery/App_Start/SessionPersistence.cs
+++ src/NuGetGallery/App_Start/SessionPersistence.cs
@@ -12,6 +12,11 @@ namespace NuGetGallery
     {
         public static void Setup(IGalleryConfigurationService config)
         {
+            // The machine keys are used for encrypting/decrypting cookies used by ASP.NET, these are usually set by IIS in 'Auto' mode. 
+            // During a deployment to Azure cloud service the same machine key values are set on all the instances of a given cloud service,
+            // thereby providing session persistence across different instances in the same deployment slot. However, across different slots(staging vs production
+            // these session keys are different. Thereby causing the loss of session upon a slot swap. Manually setting these values on role start ensures same
+            // keys are used by all the instances across all the slots of a Azure cloud service. See more analysis here: https://github.com/NuGet/Engineering/issues/1329
             if (config.Current.EnableMachineKeyConfiguration
                 && !string.IsNullOrWhiteSpace(config.Current.MachineKeyDecryption)
                 && !string.IsNullOrWhiteSpace(config.Current.MachineKeyDecryptionKey)
