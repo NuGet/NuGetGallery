eaa6fb385266c57474ebc555aab887004e73f490
Damon Tivel <dtivel@microsoft.com>
2018-05-04 09:43:24 -0700

    Certificates:  hide UI behind feature flag (#5885)
    Resolve https://github.com/NuGet/NuGetGallery/issues/5886.

--- src/NuGetGallery/Services/IContentObjectService.cs
+++ src/NuGetGallery/Services/IContentObjectService.cs
@@ -2,12 +2,14 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Threading.Tasks;
+using NuGetGallery.Services;
 
 namespace NuGetGallery
 {
     public interface IContentObjectService
     {
         ILoginDiscontinuationConfiguration LoginDiscontinuationConfiguration { get; }
+        ICertificatesConfiguration CertificatesConfiguration { get; }
 
         Task Refresh();
     }
