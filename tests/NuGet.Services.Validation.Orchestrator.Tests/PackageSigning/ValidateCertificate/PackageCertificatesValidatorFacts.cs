e6550687f0722d6764729fd5d132e5dbd191eb56
Loïc Sharma <sharma.loic@gmail.com>
2018-05-08 09:01:29 -0700

    Make Certificate Validator gracefully handle repository signed packages (#431)
    The certificate validator expected a single author signature to be present if the package was marked as signed. This is no longer true as the package could be repository signed with no author signature.

--- tests/NuGet.Services.Validation.Orchestrator.Tests/PackageSigning/ValidateCertificate/PackageCertificatesValidatorFacts.cs
+++ tests/NuGet.Services.Validation.Orchestrator.Tests/PackageSigning/ValidateCertificate/PackageCertificatesValidatorFacts.cs
@@ -536,6 +536,70 @@ namespace NuGet.Services.Validation.PackageSigning
 
                 Assert.Equal($"Package signature {signature.Key} is valid but has a timestamp whose end certificate is revoked\r\nParameter name: signature", ex.Message);
             }
+
+            [Fact]
+            public async Task ThrowsIfPackageIsOnlyRepositorySigned()
+            {
+                var validatorStatus = new ValidatorStatus
+                {
+                    ValidationId = ValidationId,
+                    ValidatorName = nameof(PackageCertificatesValidator),
+                    PackageKey = PackageKey,
+                    State = ValidationStatus.Incomplete,
+                    ValidatorIssues = new List<ValidatorIssue>(),
+                };
+
+                var packageSigningState = new PackageSigningState
+                {
+                    PackageKey = PackageKey,
+                    PackageId = PackageId,
+                    PackageNormalizedVersion = PackageNormalizedVersion,
+                    SigningStatus = PackageSigningStatus.Valid
+                };
+
+                var signature = new PackageSignature
+                {
+                    PackageKey = PackageKey,
+                    Status = PackageSignatureStatus.Unknown,
+                    Type = PackageSignatureType.Repository,
+                };
+
+                var timestamp = new TrustedTimestamp
+                {
+                    Value = DateTime.UtcNow.AddDays(-1)
+                };
+
+                var signingCertificate = new EndCertificate
+                {
+                    Status = EndCertificateStatus.Good,
+                    StatusUpdateTime = DateTime.UtcNow.AddSeconds(-1),
+                };
+
+                var timestampCertificate = new EndCertificate
+                {
+                    Status = EndCertificateStatus.Revoked,
+                    StatusUpdateTime = DateTime.UtcNow.AddSeconds(-1),
+                    RevocationTime = DateTime.UtcNow.AddSeconds(-1),
+                };
+
+                packageSigningState.PackageSignatures = new[] { signature };
+                signature.PackageSigningState = packageSigningState;
+                signature.EndCertificate = signingCertificate;
+                signature.TrustedTimestamps = new[] { timestamp };
+                timestamp.PackageSignature = signature;
+                timestamp.EndCertificate = timestampCertificate;
+
+                _validationContext.Mock(
+                    validatorStatuses: new[] { validatorStatus },
+                    packageSigningStates: new[] { packageSigningState },
+                    packageSignatures: new[] { signature },
+                    endCertificates: new[] { signature.EndCertificate, timestampCertificate });
+
+                // Act & Assert
+                var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _target.GetResultAsync(_validationRequest.Object));
+
+                Assert.Equal($"Package with key {validatorStatus.PackageKey} does not have an author signature", ex.Message);
+            }
         }
 
         public class TheStartValidationAsyncMethod : FactsBase
@@ -600,6 +664,52 @@ namespace NuGet.Services.Validation.PackageSigning
                 _validationContext.Mock(
                     validatorStatuses: new[] { validatorStatus },
                     packageSigningStates: new[] { packageSigningState });
+
+                // Act & Assert
+                var actual = await _target.StartAsync(_validationRequest.Object);
+
+                _certificateVerifier.Verify(v => v.EnqueueVerificationAsync(It.IsAny<IValidationRequest>(), It.IsAny<EndCertificate>()), Times.Never);
+                _validationContext.Verify(c => c.SaveChangesAsync(), Times.Once);
+                _telemetryService.Verify(
+                    x => x.TrackDurationToStartPackageCertificatesValidator(It.IsAny<TimeSpan>()),
+                    Times.Never);
+
+                Assert.Equal(ValidationStatus.Succeeded, actual.Status);
+                Assert.Equal(ValidationStatus.Succeeded, validatorStatus.State);
+            }
+
+            [Fact]
+            public async Task OnlyRepositorySignedPackagesAlwaysSucceed()
+            {
+                var validatorStatus = new ValidatorStatus
+                {
+                    ValidationId = ValidationId,
+                    PackageKey = PackageKey,
+                    ValidatorName = nameof(PackageCertificatesValidator),
+                    State = ValidationStatus.NotStarted,
+                    ValidatorIssues = new List<ValidatorIssue>(),
+                };
+
+                var packageSigningState = new PackageSigningState
+                {
+                    PackageKey = PackageKey,
+                    PackageId = PackageId,
+                    PackageNormalizedVersion = PackageNormalizedVersion,
+                    SigningStatus = PackageSigningStatus.Valid
+                };
+
+                var packageSignature = new PackageSignature
+                {
+                    PackageKey = PackageKey,
+                    Status = PackageSignatureStatus.Valid,
+                    Type = PackageSignatureType.Repository,
+                };
+
+                // Arrange
+                _validationContext.Mock(
+                    validatorStatuses: new[] { validatorStatus },
+                    packageSigningStates: new[] { packageSigningState },
+                    packageSignatures: new[] { packageSignature });
 
                 // Act & Assert
                 var actual = await _target.StartAsync(_validationRequest.Object);
