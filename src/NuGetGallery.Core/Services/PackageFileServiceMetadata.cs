8e27918a9d713866906ff911d3d35ca38dcbdba2
cmanu <cristina.manu@microsoft.com>
2018-07-10 10:59:26 -0700

    Refactor CorePackageFileService  (#6148)
    Refactor CorePackageFileService to allow flexibility in metadata.

---
+++ src/NuGetGallery.Core/Services/PackageFileServiceMetadata.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGetGallery
+{
+    public class PackageFileMetadataService : IFileMetadataService
+    {
+        public string FileFolderName => CoreConstants.PackagesFolderName;
+
+        public string FileSavePathTemplate => CoreConstants.PackageFileSavePathTemplate;
+
+        public string FileExtension => CoreConstants.NuGetPackageFileExtension;
+
+        public string ValidationFolderName => CoreConstants.ValidationFolderName;
+
+        public string FileBackupsFolderName => CoreConstants.PackageBackupsFolderName;
+
+        public string FileBackupSavePathTemplate => CoreConstants.PackageFileBackupSavePathTemplate;
+    }
+}
---
+++ tests/NuGetGallery.Facts/Services/PackageFileServiceMetadataFacts.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Xunit;
+namespace NuGetGallery
+{
+    public class PackageFileServiceMetadataFacts
+    {
+        [Fact]
+        public void ValidatePropertyValues()
+        {
+            // Arrange + Act 
+            var data = new PackageFileMetadataService();
+
+            // Assert
+            Assert.Equal("packages", data.FileFolderName);
+            Assert.Equal("{0}.{1}{2}", data.FileSavePathTemplate);
+            Assert.Equal(".nupkg", data.FileExtension);
+            Assert.Equal("validation", data.ValidationFolderName);
+            Assert.Equal("package-backups", data.FileBackupsFolderName);
+            Assert.Equal("{0}/{1}/{2}.{3}", data.FileBackupSavePathTemplate);
+        }
+    }
+}
