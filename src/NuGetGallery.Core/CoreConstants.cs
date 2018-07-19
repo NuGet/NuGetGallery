2519a87743995c4af1fe1995727cd24dec80474d
shishirh <shishirh@microsoft.com>
2018-07-19 14:06:47 -0700

    Add revalidation constant

--- src/NuGetGallery.Core/CoreConstants.cs
+++ src/NuGetGallery.Core/CoreConstants.cs
@@ -30,6 +30,7 @@ namespace NuGetGallery
         public const string PackagesFolderName = "packages";
         public const string UploadsFolderName = "uploads";
         public const string ValidationFolderName = "validation";
+        public const string RevalidationFolderName = "revalidation";
 
         public const string SymbolPackagesFolderName = "symbol-packages";
         public const string NuGetSymbolPackageFileExtension = ".snupkg";
