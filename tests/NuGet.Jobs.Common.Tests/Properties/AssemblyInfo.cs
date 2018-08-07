f33d5fb55e9111f8be5988f6aa554954e13b37e7
Christy Henriksson <chenriks@microsoft.com>
2018-08-07 05:42:44 -0700

    Jobs.Common helpers for NuGet.Services.Sql (#505)

---
+++ tests/NuGet.Jobs.Common.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Jobs.Common.Tests")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ce96428b-8138-4914-9999-2b391797fff8")]
