47a0a4a41305f4ba51349d2f63453793c4213bd5
Christy Henriksson <chenriks@microsoft.com>
2018-08-08 13:07:34 -0700

    Migrate UpdateLicenseReport to JsonConfig (#521)

--- src/UpdateLicenseReports/Scripts/UpdateLicenseReports.cmd
+++ src/UpdateLicenseReports/Scripts/UpdateLicenseReports.cmd
@@ -3,12 +3,15 @@
 cd bin
 
 :Top
-	echo "Starting job - #{Jobs.updatelicensereports.Title}"
+echo "Starting job - #{Jobs.updatelicensereports.Title}"
 	
-	title #{Jobs.updatelicensereports.Title}
+title #{Jobs.updatelicensereports.Title}
 
-    start /w updatelicensereports.exe -VaultName "#{Deployment.Azure.KeyVault.VaultName}" -ClientId "#{Deployment.Azure.KeyVault.ClientId}" -CertificateThumbprint "#{Deployment.Azure.KeyVault.CertificateThumbprint}" -LicenseReportService "#{Jobs.updatelicensereports.LicenseReportServiceUri}" -LicenseReportUser "#{Jobs.updatelicensereports.LicenseReportUser}" -LicenseReportPassword "#{Jobs.updatelicensereports.LicenseReportPassword}" -PackageDatabase "#{Jobs.updatelicensereports.PackageDatabase}" -verbose true -Sleep #{Jobs.updatelicensereports.Sleep} -InstrumentationKey "#{Jobs.updatelicensereports.ApplicationInsightsInstrumentationKey}"
+start /w updatelicensereports.exe -Configuration #{Jobs.updatelicensereports.Configuration}  ^
+    -verbose true  ^
+    -Sleep #{Jobs.updatelicensereports.Sleep}  ^
+    -InstrumentationKey "#{Jobs.updatelicensereports.ApplicationInsightsInstrumentationKey}"
 
-	echo "Finished #{Jobs.updatelicensereports.Title}"
+echo "Finished #{Jobs.updatelicensereports.Title}"
 
-	goto Top
+goto Top
