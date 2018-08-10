487b53008e464a7515c2b656d4b430da2355d259
Xavier Decoster <xadeco@microsoft.com>
2018-08-10 12:50:59 +0200

    Rename test class

--- tests/NuGetGallery.Facts/Security/RequirePackageMetadataCompliancePolicyFacts.cs
+++ tests/NuGetGallery.Facts/Security/RequirePackageMetadataCompliancePolicyFacts.cs
@@ -11,7 +11,7 @@ using Xunit;
 
 namespace NuGetGallery.Security
 {
-    public class RequireMicrosoftPackageCompliancePolicyFacts
+    public class RequirePackageMetadataCompliancePolicyFacts
     {
         public static object[] NonCompliantPackageMemberData
         {
