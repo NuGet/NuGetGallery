1929bce080e3942512dcffb224cdc20845033329
zhhyu <Zhaohua.Yu@microsoft.com>
2018-08-15 17:52:03 -0700

    Update the codes based on review

--- src/NuGetGallery/Services/TyposquattingCheckService.cs
+++ src/NuGetGallery/Services/TyposquattingCheckService.cs
@@ -5,13 +5,21 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace NuGetGallery
 {
     public class TyposquattingCheckService
     {
-        private static readonly char[] SpecialCharacters = {'.', '_', '-'}; 
-        private static readonly Dictionary<char, string> SimilarCharacterDictionary = new Dictionary<char, string>()
+        private static readonly HashSet<char> SpecialCharacters = new HashSet<char>{'.', '_', '-'};
+        private static readonly string SpecialCharactersToString = "[" + new string(SpecialCharacters.ToArray()) + "]";
+        private const char PlaceholderForAlignment = '*';  // This const place holder variable is used for strings alignment 
+
+        /// <summary>
+        /// The following dictionary is built through picking up similar characters manually from wiki unicode page.
+        /// https://en.wikipedia.org/wiki/List_of_Unicode_characters
+        /// </summary>
+        private static readonly IReadOnlyDictionary<char, string> SimilarCharacterDictionary = new Dictionary<char, string>()
         {
             {'a', "AÀÁÂÃÄÅàáâãäåĀāĂăĄąǍǎǞǟǠǡǺǻȀȁȂȃȦȧȺΆΑάαἀἁἂἃἄἅἆἇἈἉἊἋἌἍἎἏӐӑӒӓὰάᾀᾁᾂᾃᾄᾅᾆᾇᾈᾊᾋᾌᾍᾎᾏᾰᾱᾲᾳᾴᾶᾷᾸᾹᾺΆᾼАДад"},
             {'b', "BƀƁƂƃƄƅɃḂḃΒϦЂБВЪЬвъьѢѣҌҍႦႪხҔҕӃӄ" },
@@ -43,43 +51,60 @@ namespace NuGetGallery
             {'8', "Ȣȣ"},
             {'_', ".-" }
         };
-        private static readonly Dictionary<char, char> NormalizedMappingDictionary = GetNormalizedMappingDictionary(SimilarCharacterDictionary);
+        private static readonly IReadOnlyDictionary<char, char> NormalizedMappingDictionary = GetNormalizedMappingDictionary(SimilarCharacterDictionary);
 
-        // TODO: Threshold parameters will be saved in the configuration file. 
-        private const int TyposquattingThreshold1 = 0;
-        private const int TyposquattingThreshold2 = 1;
-        private const int TyposquattingThreshold3 = 2;
-        private const int TyposquattingThresholdInterval1 = 30;
-        private const int TyposquattingThresholdInterval2 = 50;
+        // TODO: Threshold parameters will be saved in the configuration file.
+        // https://github.com/NuGet/Engineering/issues/1645
+        private static List<ThresholdInfo> _thresholdsList = new List<ThresholdInfo>();
 
         // TODO: popular packages checklist will be implemented
-        private List<string> _packageIdCheckList = new List<string>();
+        // https://github.com/NuGet/Engineering/issues/1624
+        private static List<PackageInfo> _packagesCheckList = new List<PackageInfo>();
 
         private class BasicEditDistanceInfo
         {
             public int Distance { get; set; }
-            public char[,] Path { get; set; }
+            public PathInfo[,] Path { get; set; }
+        }  
+        
+        private enum PathInfo
+        {
+            [Description("Match")]
+            MATCH,
+            [Description("Delete")]
+            DELETE,
+            [Description("Substitute")]
+            SUBSTITUTE,
+            [Description("Insert")]
+            INSERT,
         }
 
         public TyposquattingCheckService()
         {
         }
 
-        public TyposquattingCheckService(List<string> packageIdCheckList) : this()
+        public TyposquattingCheckService(List<PackageInfo> packagesCheckList, List<ThresholdInfo> thresholdsList) : this()
         {
-            SetPackageIdCheckList(packageIdCheckList);
+            SetPackageIdCheckList(packagesCheckList);
+            SetThresholdsList(thresholdsList);
         }
 
-        public void SetPackageIdCheckList(List<string> packageIdCheckList)
+        public static void SetPackageIdCheckList(List<PackageInfo> packagesCheckList)
         {
-            _packageIdCheckList = packageIdCheckList;
+            _packagesCheckList = packagesCheckList;
             return;
         }      
+
+        public static void SetThresholdsList(List<ThresholdInfo> thresholdsList)
+        {
+            _thresholdsList = thresholdsList;
+            return;
+        }
         
-        private static Dictionary<char, char> GetNormalizedMappingDictionary(Dictionary<char, string> similarCharacterDictionary)
+        private static Dictionary<char, char> GetNormalizedMappingDictionary(IReadOnlyDictionary<char, string> similarCharacterDictionary)
         {
             Dictionary<char, char> normalizedMappingDictionary = new Dictionary<char, char>();
-            foreach(var item in similarCharacterDictionary)
+            foreach (var item in similarCharacterDictionary)
             {
                 foreach(char c in item.Value)
                 {
@@ -90,23 +115,20 @@ namespace NuGetGallery
             return normalizedMappingDictionary;
         }
 
-        private int GetThreshold(string packageId)
+        private static int GetThreshold(string packageId)
         {
-            if (packageId.Length < TyposquattingThresholdInterval1)
+            foreach (var thresholdInfo in _thresholdsList)
             {
-                return TyposquattingThreshold1;
+                if (packageId.Length >= thresholdInfo.LowerBound && packageId.Length < thresholdInfo.UpperBound)
+                {
+                    return thresholdInfo.Threshold;
+                }
             }
-            else if (packageId.Length >= TyposquattingThresholdInterval1 && packageId.Length < TyposquattingThresholdInterval2)
-            {
-                return TyposquattingThreshold2;
-            }
-            else
-            {
-                return TyposquattingThreshold3;
-            }   
+
+            throw new ArgumentException("There is no predefined typo-squatting threshold for this package Id!");
         }
 
-        private string NormalizeString(string str)
+        private static string NormalizeString(string str)
         {
             StringBuilder normalizedStr = new StringBuilder(str);
             for (int i = 0; i < normalizedStr.Length; i++)
@@ -120,7 +142,7 @@ namespace NuGetGallery
             return normalizedStr.ToString();
         }
 
-        public bool IsUploadedPackageIdTyposquatting(string uploadedPackageId)
+        public static bool IsUploadedPackageIdTyposquatting(string uploadedPackageId, User uploadedPackageOwner)
         {
             if (uploadedPackageId == null)
             {
@@ -131,19 +153,26 @@ namespace NuGetGallery
             uploadedPackageId = NormalizeString(uploadedPackageId);
 
             int countCollision = 0;
-            Parallel.ForEach(_packageIdCheckList, (packageId, loopState) =>
+            Parallel.ForEach(_packagesCheckList, (package, loopState) =>
             {
-                if (IsDistanceLessThanThreshold(uploadedPackageId, packageId, threshold))
+                if (package.Owners.Contains(uploadedPackageOwner.Username))
                 {
-                    Interlocked.Increment(ref countCollision);
-                    loopState.Stop();
+                    return;
                 }
+                else
+                {
+                    if (IsDistanceLessThanThreshold(uploadedPackageId, package.Id, threshold))
+                    {
+                        Interlocked.Increment(ref countCollision);
+                        loopState.Stop();
+                    }
+                }                
             });
 
             return countCollision != 0;
         }
 
-        private bool IsDistanceLessThanThreshold(string str1, string str2, int threshold)
+        private static bool IsDistanceLessThanThreshold(string str1, string str2, int threshold)
         {
             if (str1 == null)
             {
@@ -154,8 +183,8 @@ namespace NuGetGallery
                 throw new ArgumentNullException(nameof(str2));
             }
 
-            string newStr1 = Regex.Replace(str1, "[" + new string(SpecialCharacters) + "]", ""); 
-            string newStr2 = Regex.Replace(str2, "[" + new string(SpecialCharacters) + "]", "");
+            string newStr1 = Regex.Replace(str1, SpecialCharactersToString, string.Empty);
+            string newStr2 = Regex.Replace(str2, SpecialCharactersToString, string.Empty);
             if (Math.Abs(newStr1.Length - newStr2.Length) > threshold)
             {
                 return false;
@@ -164,9 +193,9 @@ namespace NuGetGallery
             return GetDistance(str1, str2, threshold) <= threshold;
         }
 
-        private int GetDistance(string str1, string str2, int threshold)
+        private static int GetDistance(string str1, string str2, int threshold)
         {
-            BasicEditDistanceInfo basicEditDistanceInfo = GetBasicEditDistanceWithPath(str1, str2);
+            var basicEditDistanceInfo = GetBasicEditDistanceWithPath(str1, str2);
             if (basicEditDistanceInfo.Distance <= threshold)
             {
                 return basicEditDistanceInfo.Distance;  
@@ -177,22 +206,25 @@ namespace NuGetGallery
             return refreshedEditDistance;
         }
 
-        private BasicEditDistanceInfo GetBasicEditDistanceWithPath(string str1, string str2)
+        /// <summary>
+        /// The following function is used to calculate the classical edit distance and construct the path in dynamic programming way.
+        /// </summary>
+        private static BasicEditDistanceInfo GetBasicEditDistanceWithPath(string str1, string str2)
         {
             var distances = new int[str1.Length + 1, str2.Length + 1];
-            var path = new char[str1.Length + 1, str2.Length + 1];
+            var path = new PathInfo[str1.Length + 1, str2.Length + 1];
             distances[0, 0] = 0;
-            path[0, 0] = 'M';
+            path[0, 0] = PathInfo.MATCH;
             for (int i = 1; i <= str1.Length; i++)
             {
                 distances[i, 0] = i;
-                path[i, 0] = 'D';
+                path[i, 0] = PathInfo.DELETE;
             }
 
             for (int j = 1; j <= str2.Length; j++)
             {
                 distances[0, j] = j;
-                path[0, j] = 'I';
+                path[0, j] = PathInfo.INSERT;
             }
 
             for (int i = 1; i <= str1.Length; i++)
@@ -202,23 +234,23 @@ namespace NuGetGallery
                     if (str1[i - 1] == str2[j - 1])
                     {
                         distances[i, j] = distances[i - 1, j - 1];
-                        path[i, j] = 'M';
+                        path[i, j] = PathInfo.MATCH;
                     }
                     else
                     {
                         distances[i, j] = distances[i - 1, j - 1] + 1;
-                        path[i, j] = 'S';
+                        path[i, j] = PathInfo.SUBSTITUTE;
 
                         if (distances[i - 1, j] + 1 < distances[i, j])
                         {
                             distances[i, j] = distances[i - 1, j] + 1;
-                            path[i, j] = 'D';
+                            path[i, j] = PathInfo.DELETE;
                         }
 
                         if (distances[i, j - 1] + 1 < distances[i, j])
                         {
                             distances[i, j] = distances[i, j - 1] + 1;
-                            path[i, j] = 'I';
+                            path[i, j] = PathInfo.INSERT;
                         }
                     }
                 }
@@ -231,7 +263,11 @@ namespace NuGetGallery
             };
         }
 
-        private string[] TraceBackAndAlignStrings(char[,] path, string str1, string str2)
+        /// <summary>
+        /// The following function is used to traceback based on the construction path and align two strings.
+        /// Example:  For two strings: "asp.net" "aspnet". After traceback and alignment, we will have aligned strings as "asp.net" "asp*net" ('*' is the placeholder).
+        /// </summary>
+        private static string[] TraceBackAndAlignStrings(PathInfo[,] path, string str1, string str2)
         {
             StringBuilder newStr1 = new StringBuilder(str1);
             StringBuilder newStr2 = new StringBuilder(str2);
@@ -243,20 +279,20 @@ namespace NuGetGallery
             {
                 switch (path[i, j])
                 {
-                    case 'M':
+                    case PathInfo.MATCH:
                         i--;
                         j--;
                         break;
-                    case 'S':
+                    case PathInfo.SUBSTITUTE:
                         i--;
                         j--;
                         break;
-                    case 'D':
-                        newStr2.Insert(j, '*');
+                    case PathInfo.DELETE:
+                        newStr2.Insert(j, PlaceholderForAlignment);
                         i--;
                         break;
-                    case 'I':
-                        newStr1.Insert(i, '*');
+                    case PathInfo.INSERT:
+                        newStr1.Insert(i, PlaceholderForAlignment);
                         j--;
                         break;
                     default:
@@ -266,12 +302,12 @@ namespace NuGetGallery
 
             for (int k = 0; k < i; k++)
             {
-                newStr2.Insert(k, '*');
+                newStr2.Insert(k, PlaceholderForAlignment);
             }
 
             for (int k = 0; k < j; k++)
             {
-                newStr1.Insert(k, '*');
+                newStr1.Insert(k, PlaceholderForAlignment);
             }
 
             alignedStrs[0] = newStr1.ToString();
@@ -280,23 +316,28 @@ namespace NuGetGallery
             return alignedStrs;
         }
 
-        private int RefreshEditDistance(string alignedStr1, string alginedStr2, int basicEditDistance)
+        /// <summary>
+        /// The following function is used to refresh the edit distance based on predefined rules. (Insert/Delete special characters will not account for distance)
+        /// Example:  For two aligned strings: "asp.net" "asp*net" ('*' is the placeholder), we will scan the two strings again and the mapping from '.' to '*' will not account for the distance.
+        ///           So the final distance will be 0 for these two strings "asp.net" "aspnet".
+        /// </summary>
+        private static int RefreshEditDistance(string alignedStr1, string alignedStr2, int basicEditDistance)
         {
-            if (alignedStr1.Length != alginedStr2.Length)
+            if (alignedStr1.Length != alignedStr2.Length)
             {
                 throw new ArgumentException("The lengths of two aligned strings are not same!");
             }
 
             int sameSubstitution = 0;
-            for (int i = 0; i < alginedStr2.Length; i++)
+            for (int i = 0; i < alignedStr2.Length; i++)
             {
-                if (alignedStr1[i] != alginedStr2[i])
+                if (alignedStr1[i] != alignedStr2[i])
                 {
-                    if (alignedStr1[i] == '*' && SpecialCharacters.Contains(alginedStr2[i]))
+                    if (alignedStr1[i] == PlaceholderForAlignment && SpecialCharacters.Contains(alignedStr2[i]))
                     {
                         sameSubstitution += 1;
                     }
-                    else if (SpecialCharacters.Contains(alignedStr1[i]) && alginedStr2[i] == '*')
+                    else if (SpecialCharacters.Contains(alignedStr1[i]) && alignedStr2[i] == PlaceholderForAlignment)
                     {
                         sameSubstitution += 1;
                     }
@@ -309,5 +350,18 @@ namespace NuGetGallery
 
             return basicEditDistance - sameSubstitution;
         }             
+    }
+
+    public class PackageInfo
+    {
+        public string Id { get; set; }
+        public List<string> Owners { get; set; }
+    }
+
+    public class ThresholdInfo
+    {
+        public int LowerBound { get; set; }
+        public int UpperBound { get; set; }
+        public int Threshold { get; set; }
     }
 }
