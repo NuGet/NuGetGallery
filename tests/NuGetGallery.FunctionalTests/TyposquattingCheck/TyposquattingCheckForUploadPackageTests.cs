b59a77766994a0ecd5878011dad1a2b464e60151
zhhyu <Zhaohua.Yu@microsoft.com>
2018-09-28 13:55:47 -0700

    Finish typo-squatting functional tests

---
+++ tests/NuGetGallery.FunctionalTests/TyposquattingCheck/TyposquattingCheckForUploadPackageTests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+using NuGetGallery.FunctionalTests.XunitExtensions;
+
+namespace NuGetGallery.FunctionalTests.TyposquattingCheck
+{
+    public class TyposquattingCheckForUploadPackageTests : GalleryTestBase
+    {
+        private readonly CommandlineHelper _commandlineHelper;
+        private readonly PackageCreationHelper _packageCreationHelper;
+        public TyposquattingCheckForUploadPackageTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+            _commandlineHelper = new CommandlineHelper(TestOutputHelper);
+            _packageCreationHelper = new PackageCreationHelper(testOutputHelper);
+        }
+
+        [TyposquattingTestFact]
+        public async Task UploadNotTyposquattingPackage()
+        {
+            string packageId = DateTime.Now.Ticks + "PackageWithDotCsNames.Cs";
+            string version = "1.0.0";
+            string packageFullPath = await _packageCreationHelper.CreatePackageWithMinClientVersion(packageId, version, "2.3");
+
+            var processResult = await _commandlineHelper.UploadPackageAsync(packageFullPath, UrlHelper.V2FeedPushSourceUrl);
+
+            Assert.True(processResult.ExitCode == 0, Constants.UploadFailureMessage);
+        }
+
+        [TyposquattingTestFact]
+        public async Task UploadTyposquattingPackageAndBlockUser()
+        {
+            var packageId = "newtonsoft-json";
+            string version = "1.0.0";
+            string packageFullPath = await _packageCreationHelper.CreatePackageWithMinClientVersion(packageId, version, "2.3");
+
+            var processResult = await _commandlineHelper.UploadPackageAsync(packageFullPath, UrlHelper.V2FeedPushSourceUrl);
+
+            Assert.True(processResult.ExitCode == 1, Constants.UploadFailureMessage);
+        }
+    }
+}
