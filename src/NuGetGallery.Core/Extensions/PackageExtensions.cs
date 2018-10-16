49c678c93d2688b8270d143357a08043e6588e80
shishirh <shishirh@microsoft.com>
2018-10-16 16:08:46 -0700

    Address feedback

--- src/NuGetGallery.Core/Extensions/PackageExtensions.cs
+++ src/NuGetGallery.Core/Extensions/PackageExtensions.cs
@@ -19,23 +19,5 @@ namespace NuGetGallery.Extensions
                 .OrderByDescending(sp => sp.Created)
                 .FirstOrDefault();
         }
-
-        /// <summary>
-        /// Get the latest(last) uploaded symbols package for the given package, if it is <see cref="PackageStatus.Available"/>.
-        /// </summary>
-        /// <param name="package"><see cref="Package"/> for which latest available symbol package is to be retrieved.</param>
-        /// <returns>The latest symbol package if its status is <see cref="PackageStatus.Available"/>, null otherwise</returns>
-        public static SymbolPackage LatestAvailableSymbolPackage(this Package package)
-        {
-            var latestSymbolPackage = package.LatestSymbolPackage();
-            if (latestSymbolPackage == null || latestSymbolPackage.StatusKey != PackageStatus.Available)
-            {
-                return null;
-            }
-            else
-            {
-                return latestSymbolPackage;
-            }
-        }
     }
 }
