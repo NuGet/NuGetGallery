57a11b798e17acb7a86b77fc2ee15fa7c91886f9
Andrei Grigorev <angrigor@microsoft.com>
2018-10-30 10:20:47 -0700

    Making sure `PackageShouldNotBeSignedUserFixableValidationMessage` does not emit raw HTML.

--- src/NuGetGallery/Services/PackageShouldNotBeSignedUserFixableValidationMessage.cs
+++ src/NuGetGallery/Services/PackageShouldNotBeSignedUserFixableValidationMessage.cs
@@ -1,8 +1,15 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Web;
+
 namespace NuGetGallery
 {
+    /// <summary>
+    /// Represents an error message to be displayed when user uploads a signed package but theirs account
+    /// was not setup to accept signed packages. We want to display some additional text when the error is
+    /// presented on a web page, so this class abuses the messaging a little, no actual HTML is generated.
+    /// </summary>
     public class PackageShouldNotBeSignedUserFixableValidationMessage : IValidationMessage
     {
         public string PlainTextMessage => Strings.UploadPackage_PackageIsSignedButMissingCertificate_CurrentUserCanManageCertificates;
@@ -10,8 +17,9 @@ namespace NuGetGallery
         public bool HasRawHtmlRepresentation => true;
 
         public string RawHtmlMessage 
-            => Strings.UploadPackage_PackageIsSignedButMissingCertificate_CurrentUserCanManageCertificates 
+            => HttpUtility.HtmlEncode(
+                Strings.UploadPackage_PackageIsSignedButMissingCertificate_CurrentUserCanManageCertificates 
                 + " " 
-                + Strings.UploadPackage_PackageIsSignedButMissingCertificate_ManageCertificate;
+                + Strings.UploadPackage_PackageIsSignedButMissingCertificate_ManageCertificate);
     }
 }
