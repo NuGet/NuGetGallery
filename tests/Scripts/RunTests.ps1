b5274278c7ac2b60950246401d3e28429a876cc5
shishirh <shishirh@microsoft.com>
2018-10-31 17:13:38 -0700

    xunit parallel all

--- tests/Scripts/RunTests.ps1
+++ tests/Scripts/RunTests.ps1
@@ -29,7 +29,7 @@ $fullTestCategory = "$($testCategory)Tests"
 $exitCode = 0
 
 $functionalTestsDirectory = "$rootName\NuGetGallery.FunctionalTests\bin\$Config"
-& $xunit "$functionalTestsDirectory\NuGetGallery.FunctionalTests.dll" "-trait" "Category=$fullTestCategory" "-xml" $functionalTestsResults
+& $xunit "$functionalTestsDirectory\NuGetGallery.FunctionalTests.dll" "-trait" "Category=$fullTestCategory" "-xml" $functionalTestsResults "-parallel" "all"
 if ($LastExitCode) {
     $exitCode = 1
 }
