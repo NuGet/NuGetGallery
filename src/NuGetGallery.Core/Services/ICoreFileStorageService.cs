743432396af0d68b19029b4e57ff9231ccf754b3
shishirh <shishirh@microsoft.com>
2018-11-08 14:11:16 -0800

    nit fix

--- src/NuGetGallery.Core/Services/ICoreFileStorageService.cs
+++ src/NuGetGallery.Core/Services/ICoreFileStorageService.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace NuGetGallery
 {
