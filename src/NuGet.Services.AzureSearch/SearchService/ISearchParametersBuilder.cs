553278925b3e7763734e187d11e45c170df5d0ec
Joel Verhagen <jver@microsoft.com>
2018-12-17 16:05:15 -0800

    Add SearchParametersBuilder to build Azure Search SDK parameters for V2 and V3 (#456)
    Progress on https://github.com/NuGet/NuGetGallery/issues/6450
    Progress on https://github.com/NuGet/NuGetGallery/issues/6453
    Progress on https://github.com/NuGet/NuGetGallery/issues/6455

---
+++ src/NuGet.Services.AzureSearch/SearchService/ISearchParametersBuilder.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Azure.Search.Models;
+
+namespace NuGet.Services.AzureSearch.SearchService
+{
+    public interface ISearchParametersBuilder
+    {
+        SearchParameters GetSearchParametersForV2Search(V2SearchRequest request);
+        SearchParameters GetSearchParametersForV3Search(V3SearchRequest request);
+        string GetSearchTextForV2Search(V2SearchRequest request);
+        string GetSearchTextForV3Search(V3SearchRequest request);
+    }
+}
---
+++ src/NuGet.Services.AzureSearch/SearchService/IndexFields.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Newtonsoft.Json.Serialization;
+
+namespace NuGet.Services.AzureSearch.SearchService
+{
+    public static class IndexFields
+    {
+        private static readonly NamingStrategy CamelCaseNamingStrategy = new CamelCaseNamingStrategy();
+
+        public static readonly string LastEdited = CamelCaseNamingStrategy.GetPropertyName(
+            nameof(BaseMetadataDocument.LastEdited),
+            hasSpecifiedName: false);
+
+        public static readonly string Published = CamelCaseNamingStrategy.GetPropertyName(
+            nameof(BaseMetadataDocument.Published),
+            hasSpecifiedName: false);
+
+        public static readonly string SortableTitle = CamelCaseNamingStrategy.GetPropertyName(
+            nameof(BaseMetadataDocument.SortableTitle),
+            hasSpecifiedName: false);
+
+        public static readonly string SemVerLevel = CamelCaseNamingStrategy.GetPropertyName(
+            nameof(BaseMetadataDocument.SemVerLevel),
+            hasSpecifiedName: false);
+
+        public static class Search
+        {
+            public static readonly string SearchFilters = CamelCaseNamingStrategy.GetPropertyName(
+                nameof(SearchDocument.UpdateLatest.SearchFilters),
+                hasSpecifiedName: false);
+        }
+    }
+}
