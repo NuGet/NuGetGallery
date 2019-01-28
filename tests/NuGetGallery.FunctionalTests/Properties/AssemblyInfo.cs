ae9f2d2dec6cbe960472e490ae9b31636a56aae6
shishirh <shishirh@microsoft.com>
2019-01-28 14:54:15 -0800

    nit fix

--- tests/NuGetGallery.FunctionalTests/Properties/AssemblyInfo.cs
+++ tests/NuGetGallery.FunctionalTests/Properties/AssemblyInfo.cs
@@ -17,3 +17,5 @@ using Xunit;
 [assembly: Guid("85e59d16-c60a-4dd1-a970-ea4c6918a7e9")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
