54230e96dd8e6ef829cdfd0b2082240106332762
Loic Sharma <sharma.loic@gmail.com>
2019-01-30 15:06:31 -0800

    Remove unused field

--- src/NuGetGallery.Core/Features/FeatureFlagFileStorageService.cs
+++ src/NuGetGallery.Core/Features/FeatureFlagFileStorageService.cs
@@ -12,13 +12,11 @@ namespace NuGetGallery.Features
     public class FeatureFlagFileStorageService : IFeatureFlagStorageService
     {
         private readonly ICoreFileStorageService _storage;
-        private readonly FeatureFlagOptions _options;
         private readonly JsonSerializer _serializer;
 
-        public FeatureFlagFileStorageService(ICoreFileStorageService storage, FeatureFlagOptions options)
+        public FeatureFlagFileStorageService(ICoreFileStorageService storage)
         {
             _storage = storage ?? throw new ArgumentNullException(nameof(storage));
-            _options = options ?? throw new ArgumentNullException(nameof(options));
             _serializer = new JsonSerializer();
         }
 
