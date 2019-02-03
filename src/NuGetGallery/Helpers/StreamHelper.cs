7aea504e0238ad8c23431d9be74b7e782f1217ca
zhhyu <Zhaohua.Yu@microsoft.com>
2019-02-02 22:49:21 -0800

    Change logic and refactor stream helper

--- src/NuGetGallery/Helpers/StreamHelper.cs
+++ src/NuGetGallery/Helpers/StreamHelper.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,27 +10,16 @@ namespace NuGetGallery.Helpers
 {
     public class StreamHelper
     {
-        private const long BufferSize = 80 * 1024;  // 80 KB
+        public static int BufferSize { get; } = 80 * 1024;  // 80 KB
 
         /// <summary>
-        /// Transform the stream to string given maximum size.
-        /// </summary>
-        /// <param name="stream">stream to be read.</param>
-        /// <param name="maxSize">maximum size.</param>
-        /// <returns>string format of the stream.</returns>
-        public static async Task<string> ReadMaxAsync(Stream stream, long maxSize)
-        {
-            return await ReadMaxAsync(stream, maxSize, encoding: null);
-        }
-
-        /// <summary>
-        /// Transform the stream to string given maximum size and encoding format.
+        /// Read the stream at most maxSize bytes and transform to string.
         /// </summary>
         /// <param name="stream">stream to be read.</param>
         /// <param name="maxSize">maximum size.</param>
         /// <param name="encoding">encoding format.</param>
         /// <returns>string format of the stream.</returns>
-        public static async Task<string> ReadMaxAsync(Stream stream, long maxSize, Encoding encoding)
+        public static async Task<string> ReadMaxAsync(Stream stream, long maxSize, Encoding encoding = null)
         {
             if (stream == null)
             {
@@ -51,22 +39,32 @@ namespace NuGetGallery.Helpers
             using (var memoryStream = new MemoryStream())
             {
                 int bytesRead;
-                var totalBytesRead = 0;
-                var buffer = new byte[BufferSize];
-                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                long totalBytesRead = 0;
+                var buffer = new byte[GetNeededBufferSize(totalBytesRead, maxSize)];
+                while (totalBytesRead < maxSize &&
+                       (bytesRead = await stream.ReadAsync(buffer, 0, GetNeededBufferSize(totalBytesRead, maxSize))) > 0)
                 {
                     totalBytesRead += bytesRead;
-                    if (totalBytesRead > maxSize)
-                    {
-                        throw new InvalidOperationException(string.Format(CultureInfo.CurrentCulture,
-                            Strings.StreamMaxLengthExceeded, maxSize));
-                    }
-
                     await memoryStream.WriteAsync(buffer, 0, bytesRead);
                 }
 
                 return encoding.GetString(memoryStream.ToArray());
             }
+        }
+
+        private static int GetNeededBufferSize(long totalBytesRead, long maxSize)
+        {
+            if (maxSize <= int.MaxValue && maxSize <= BufferSize)
+            {
+                return (int)maxSize;
+            }
+
+            if (maxSize - totalBytesRead <= int.MaxValue && maxSize - totalBytesRead <= BufferSize)
+            {
+                return (int)(maxSize - totalBytesRead);
+            }
+
+            return BufferSize;
         }
     }
 }
--- tests/NuGetGallery.Facts/Helpers/StreamHelperFacts.cs
+++ tests/NuGetGallery.Facts/Helpers/StreamHelperFacts.cs
@@ -6,19 +6,16 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
-using System.Globalization;
 
 namespace NuGetGallery.Helpers
 {
     public class StreamHelperFacts
     {
-        private const int MaxSize = 100;
-
         [Fact]
         public async Task ReadNullStreamThrowException()
         {
             Stream stream = null;
-            var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => StreamHelper.ReadMaxAsync(stream, MaxSize));
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => StreamHelper.ReadMaxAsync(stream, maxSize: 100));
             Assert.Equal(nameof(stream), exception.ParamName);
         }
 
@@ -37,44 +34,38 @@ namespace NuGetGallery.Helpers
         [Theory]
         [InlineData("test content")]
         [InlineData("测试内容")]
-        public async Task ReadValidStream(string inputContent)
+        public async Task CheckContentWhenReadValidStream(string inputContent)
         {
             using (var stream = new MemoryStream(Encoding.UTF8.GetBytes(inputContent)))
             {
-                var content = await StreamHelper.ReadMaxAsync(stream, MaxSize);
+                var content = await StreamHelper.ReadMaxAsync(stream, maxSize: 100);
                 Assert.Equal(inputContent, content);
             }
         }
 
-        [Fact]
-        public async Task ReadStreamWithLessThanMaxSize()
+        [Theory]
+        [InlineData(1, 0, -1)]
+        [InlineData(1, 0, 1)]
+        [InlineData(1, 0, 0)]
+        [InlineData(1, -1, -1)]
+        [InlineData(1, -1, 1)]
+        [InlineData(1, -1, 0)]
+        [InlineData(2, 0, -1)]
+        [InlineData(2, 0, 1)]
+        [InlineData(2, 0, 0)]
+        public async Task CheckContentSizeWithDifferentParameters(int maxSizeMultiplier, int maxSizeOffset, int bytesContentSizeOffset)
         {
-            var byteContent = new byte[MaxSize - 1];
-            using (var stream = new MemoryStream(byteContent))
-            {
-                var content = await StreamHelper.ReadMaxAsync(stream, MaxSize);
-                Assert.Equal(byteContent.Length, Encoding.UTF8.GetBytes(content).Length);
-            }
-        }
+            long maxSize = StreamHelper.BufferSize * maxSizeMultiplier + maxSizeOffset;
+            Assert.True(maxSize < long.MaxValue);
+            var bytesContentSize = maxSize + bytesContentSizeOffset;
+            Assert.True(bytesContentSize < int.MaxValue);
+            var bytesContent = new byte[bytesContentSize];
+            var expectedContentSize = Math.Min(bytesContentSize, maxSize);
 
-        [Fact]
-        public async Task ReadStreamWithMaxSize()
-        {
-            var byteContent = new byte[MaxSize];
-            using (var stream = new MemoryStream(byteContent))
+            using (var stream = new MemoryStream(bytesContent))
             {
-                var content = await StreamHelper.ReadMaxAsync(stream, MaxSize);
-                Assert.Equal(byteContent.Length, Encoding.UTF8.GetBytes(content).Length);
-            }
-        }
-
-        [Fact]
-        public async Task ReadStreamWithLargerThanMaxSizeThrowException()
-        {
-            using (var stream = new MemoryStream(new byte[MaxSize + 1]))
-            {
-                var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => StreamHelper.ReadMaxAsync(stream, MaxSize));
-                Assert.Contains(exception.Message, string.Format(CultureInfo.CurrentCulture, Strings.StreamMaxLengthExceeded, MaxSize));
+                var content = await StreamHelper.ReadMaxAsync(stream, maxSize);
+                Assert.Equal(expectedContentSize, Encoding.UTF8.GetBytes(content).Length);
             }
         }
     }
