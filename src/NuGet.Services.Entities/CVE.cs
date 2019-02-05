2327fc5af3f684c030e7b6c3ef36892d40b4cb42
Xavier Decoster <xadeco@microsoft.com>
2019-02-05 13:59:51 +0100

    Add missing data annotations

--- src/NuGet.Services.Entities/CVE.cs
+++ src/NuGet.Services.Entities/CVE.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NuGet.Services.Entities
@@ -21,11 +22,14 @@ namespace NuGet.Services.Entities
         /// Gets or sets the unique CVE-ID.
         /// </summary>
         [Index(IsUnique = true)]
+        [Required]
         public string CVEId { get; set; }
 
         /// <summary>
         /// Gets or sets the description of the CWE.
         /// </summary>
+        [MaxLength(4000)]
+        [Required]
         public string Description { get; set; }
 
         public virtual ICollection<PackageDeprecation> PackageDeprecations { get; set; }
--- src/NuGet.Services.Entities/CWE.cs
+++ src/NuGet.Services.Entities/CWE.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NuGet.Services.Entities
@@ -21,16 +22,21 @@ namespace NuGet.Services.Entities
         /// Gets or sets the unique CWE-ID.
         /// </summary>
         [Index(IsUnique = true)]
+        [Required]
         public string CWEId { get; set; }
 
         /// <summary>
         /// Gets or sets the name of the CWE.
         /// </summary>
+        [MaxLength(200)]
+        [Required]
         public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets the description of the CWE.
         /// </summary>
+        [MaxLength(600)]
+        [Required]
         public string Description { get; set; }
 
         public virtual ICollection<PackageDeprecation> PackageDeprecations { get; set; }
