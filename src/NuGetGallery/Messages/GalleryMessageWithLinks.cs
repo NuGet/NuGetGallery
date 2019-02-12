14a9fef5504d9addbdc9b000f07cfc66b7ff6a45
shishirh <shishirh@microsoft.com>
2019-02-12 11:26:18 -0800

    refactor some more

---
+++ src/NuGetGallery/Messages/GalleryMessageWithLinks.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+
+namespace NuGetGallery.Messages
+{
+    public class GalleryMessageWithLinks : IGalleryMessage
+    {
+        private string _linkFormat = $"<a src=\"{1}\" target=\"_blank\">{0}</a>";
+        private string _baseMessage;
+        private string[] _plainObjectArguments;
+        private string[] _linkObjectArguments;
+
+        public GalleryMessageWithLinks(string message, LinkMessageFormat[] args)
+        {
+            _baseMessage = message ?? throw new ArgumentNullException(nameof(message));
+
+            _plainObjectArguments = args
+                .Select(x => x.Message)
+                .ToArray();
+
+            _linkObjectArguments = args
+                .Select(x => {
+                    return x.IsUrlMessage
+                        ? string.Format(_linkFormat, x.Message, x.Link)
+                        : x.Message;
+                })
+                .ToArray();
+        }
+
+        public string PlainTextMessage => string.Format(_baseMessage, _plainObjectArguments);
+
+        public bool HasRawHtmlRepresentation => true;
+
+        public string RawHtmlMessage => string.Format(_baseMessage, _linkObjectArguments);
+
+        public override string ToString()
+        {
+            return RawHtmlMessage;
+        }
+    }
+}
---
+++ src/NuGetGallery/Messages/LinkMessageFormat.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGetGallery.Messages
+{
+    public class LinkMessageFormat
+    {
+        public string Message { get; }
+        public string Link { get; }
+        public bool IsUrlMessage { get; }
+
+        /// <summary>
+        /// The place holder object for holding the link formatting for the specified message
+        /// </summary>
+        /// <param name="message">The data to be linked</param>
+        /// <param name="link">The redirection link for the data. Specify this as null if you want to show only data without link</param>
+        public LinkMessageFormat(string message, string link = null)
+        {
+            Message = message;
+            Link = link;
+            IsUrlMessage = !string.IsNullOrEmpty(link);
+        }
+    }
+}
