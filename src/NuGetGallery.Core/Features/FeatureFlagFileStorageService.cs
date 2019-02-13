a365d08dcdb89d212dfcb66ff9f5f8275945aae9
Loic Sharma <sharma.loic@gmail.com>
2019-02-13 08:51:52 -0800

    Address feedback

--- src/NuGetGallery.Core/Features/FeatureFlagFileStorageService.cs
+++ src/NuGetGallery.Core/Features/FeatureFlagFileStorageService.cs
@@ -2,12 +2,14 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
 using NuGet.Services.Entities;
 using NuGet.Services.FeatureFlags;
@@ -31,7 +33,11 @@ namespace NuGetGallery.Features
             _serializer = JsonSerializer.Create(new JsonSerializerSettings
             {
                 Formatting = Formatting.Indented,
-                MissingMemberHandling = MissingMemberHandling.Error
+                MissingMemberHandling = MissingMemberHandling.Error,
+                Converters = new List<JsonConverter>
+                {
+                    new StringEnumConverter()
+                }
             });
         }
 
@@ -61,24 +67,83 @@ namespace NuGetGallery.Features
                 reference.ContentId);
         }
 
-        public async Task<FeatureFlagSaveResult> TrySaveAsync(string flags, string contentId)
+        public async Task<FeatureFlagSaveResult> TrySaveAsync(string flagsJson, string contentId)
         {
             // Ensure the feature flags are valid before saving them.
-            var validationResult = ValidateFlagsOrNull(flags);
-            if (validationResult != null)
+            FeatureFlags flags;
+            try
             {
-                return validationResult;
+                using (var reader = new StringReader(flagsJson))
+                using (var jsonReader = new JsonTextReader(reader))
+                {
+                    flags = _serializer.Deserialize<FeatureFlags>(jsonReader);
+                }
+            }
+            catch (JsonException e)
+            {
+                return FeatureFlagSaveResult.Invalid(e.Message);
             }
 
+            return await TrySaveAsync(flags, contentId);
+        }
+
+        public async Task<bool> TryRemoveUserAsync(User user)
+        {
+            for (var attempt = 0; attempt < MaxRemoveUserAttempts; attempt++)
+            {
+                var reference = await _storage.GetFileReferenceAsync(CoreConstants.Folders.ContentFolderName, CoreConstants.FeatureFlagsFileName);
+
+                FeatureFlags flags;
+                using (var stream = reference.OpenRead())
+                using (var streamReader = new StreamReader(stream))
+                using (var reader = new JsonTextReader(streamReader))
+                {
+                    flags = _serializer.Deserialize<FeatureFlags>(reader);
+                }
+
+                // Don't update the flags if the user isn't listed in any of the flights.
+                if (!flags.Flights.Any(f => f.Value.Accounts.Contains(user.Username, StringComparer.OrdinalIgnoreCase)))
+                {
+                    return true;
+                }
+
+                // The user is listed in the flights. Build a new feature flag object that
+                // no longer contains the user.
+                var result = new FeatureFlags(
+                   flags.Features,
+                   flags.Flights
+                       .ToDictionary(
+                           f => f.Key,
+                           f => RemoveUser(f.Value, user)));
+
+                var saveResult = await TrySaveAsync(result, reference.ContentId);
+                if (saveResult.Type == FeatureFlagSaveResultType.Ok)
+                {
+                    return true;
+                }
+
+                _logger.LogWarning(
+                    0,
+                    "Failed to remove user from feature flags, attempt {Attempt} of {MaxAttempts}...",
+                    attempt + 1,
+                    MaxRemoveUserAttempts);
+            }
+
+            return false;
+        }
+
+        private async Task<FeatureFlagSaveResult> TrySaveAsync(FeatureFlags flags, string contentId)
+        {
             var accessCondition = AccessConditionWrapper.GenerateIfMatchCondition(contentId);
 
             try
             {
                 using (var stream = new MemoryStream())
                 using (var writer = new StreamWriter(stream))
+                using (var jsonWriter = new JsonTextWriter(writer))
                 {
-                    writer.Write(PrettifyJson(flags));
-                    writer.Flush();
+                    _serializer.Serialize(jsonWriter, flags);
+                    jsonWriter.Flush();
                     stream.Position = 0;
 
                     await _storage.SaveFileAsync(CoreConstants.Folders.ContentFolderName, CoreConstants.FeatureFlagsFileName, stream, accessCondition);
@@ -92,47 +157,6 @@ namespace NuGetGallery.Features
             }
         }
 
-        public async Task<bool> TryRemoveUserAsync(User user)
-        {
-            for (var attempt = 0; attempt < MaxRemoveUserAttempts; attempt++)
-            {
-                try
-                {
-                    await MaybeUpdateFlagsAsync((FeatureFlags flags) =>
-                    {
-                        // Don't update the flags if the user isn't listed in any of the flights.
-                        if (!flags.Flights.Any(f => f.Value.Accounts.Contains(user.Username, StringComparer.OrdinalIgnoreCase)))
-                        {
-                            return null;
-                        }
-
-                        // The user is listed in the flights. Build a new feature flag object that
-                        // no longer contains the user.
-                         return new FeatureFlags(
-                            flags.Features,
-                            flags.Flights
-                                .ToDictionary(
-                                    f => f.Key,
-                                    f => RemoveUser(f.Value, user)));
-                    });
-
-                    return true;
-                }
-                catch (StorageException e) when (e.IsPreconditionFailedException())
-                {
-                    _logger.LogWarning(
-                        0,
-                        e,
-                        "Failed to remove user from feature flags due to precondition failed exception, " +
-                        "attempt {Attempt} of {MaxAttempts}...",
-                        attempt + 1,
-                        MaxRemoveUserAttempts);
-                }
-            }
-
-            return false;
-        }
-
         private Flight RemoveUser(Flight flight, User user)
         {
             return new Flight(
@@ -140,56 +164,6 @@ namespace NuGetGallery.Features
                 flight.SiteAdmins,
                 flight.Accounts.Where(a => !a.Equals(user.Username, StringComparison.OrdinalIgnoreCase)).ToList(),
                 flight.Domains);
-        }
-
-        private async Task MaybeUpdateFlagsAsync(Func<FeatureFlags, FeatureFlags> updateAction)
-        {
-            var reference = await _storage.GetFileReferenceAsync(CoreConstants.Folders.ContentFolderName, CoreConstants.FeatureFlagsFileName);
-
-            FeatureFlags flags;
-            using (var stream = reference.OpenRead())
-            using (var streamReader = new StreamReader(stream))
-            using (var reader = new JsonTextReader(streamReader))
-            {
-                flags = _serializer.Deserialize<FeatureFlags>(reader);
-            }
-
-            // The action can cancel the update by returning null.
-            var result = updateAction(flags);
-            if (result == null)
-            {
-                return;
-            }
-
-            var accessCondition = AccessConditionWrapper.GenerateIfMatchCondition(reference.ContentId);
-
-            using (var stream = new MemoryStream())
-            using (var writer = new StreamWriter(stream))
-            using (var jsonWriter = new JsonTextWriter(writer))
-            {
-                _serializer.Serialize(jsonWriter, result);
-                jsonWriter.Flush();
-                stream.Position = 0;
-
-                await _storage.SaveFileAsync(CoreConstants.Folders.ContentFolderName, CoreConstants.FeatureFlagsFileName, stream, accessCondition);
-            }
-        }
-
-        private FeatureFlagSaveResult ValidateFlagsOrNull(string flags)
-        {
-            try
-            {
-                using (var reader = new StringReader(flags))
-                using (var jsonReader = new JsonTextReader(reader))
-                {
-                    _serializer.Deserialize<FeatureFlags>(jsonReader);
-                    return null;
-                }
-            }
-            catch (JsonException e)
-            {
-                return FeatureFlagSaveResult.Invalid(e.Message);
-            }
         }
 
         private string PrettifyJson(string json)
