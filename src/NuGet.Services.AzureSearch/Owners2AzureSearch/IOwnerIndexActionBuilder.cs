80d31c81830533e869a3e613679539e8d90b353f
Joel Verhagen <jver@microsoft.com>
2019-04-06 20:44:03 -0700

    Add OwnerIndexActionBuilder to build to proper set search index changes per ID (#504)
    Progress on https://github.com/NuGet/NuGetGallery/issues/6475

---
+++ src/NuGet.Services.AzureSearch/Owners2AzureSearch/IOwnerIndexActionBuilder.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace NuGet.Services.AzureSearch.Owners2AzureSearch
+{
+    public interface IOwnerIndexActionBuilder
+    {
+        Task<IndexActions> UpdateOwnersAsync(string packageId, string[] owners);
+    }
+}
