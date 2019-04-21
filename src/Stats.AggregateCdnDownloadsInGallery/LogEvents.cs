7f7e349141238a5cb1b6a2a2dc3593cda5a19fc0
cmanu <cristina.manu@microsoft.com>
2019-04-21 16:55:47 -0700

    AggregateStatsDownloads improvements (#745)
    Added telemetry and added logic to not  proceed with updates if the downloads are decreasing.

---
+++ src/Stats.AggregateCdnDownloadsInGallery/LogEvents.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Logging;
+
+namespace Stats.AggregateCdnDownloadsInGallery
+{
+    public static class LogEvents
+    {
+        public static EventId IncorrectIdsInGroupBatch = new EventId(900, "The group batch to be ingested contains multiple package Ids.");
+        public static EventId DownloadCountDecreaseDetected = new EventId(901, "The download count for the package is decreasing.");
+
+    }
+}
