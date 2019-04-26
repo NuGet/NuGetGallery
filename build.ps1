6defb6974373884a765d0085167d607ac5aac541
Loïc Sharma <sharma.loic@gmail.com>
2019-04-26 15:16:39 -0700

    [Azure Search] Sign the assemblies (#509)
    Depends on [NuGetDeployment#900](https://nuget.visualstudio.com/NuGetMicrosoft/_git/NuGetDeployment/pullrequest/900?_a=overview)
    Addresses https://github.com/nuget/nugetgallery/issues/6796

--- build.ps1
+++ build.ps1
@@ -9,7 +9,7 @@ param (
     [string]$SemanticVersion = '1.0.0-zlocal',
     [string]$Branch,
     [string]$CommitSHA,
-    [string]$BuildBranch = '2d8feecabe3aeaed7f5b4d50b9be78c94faf39ec'
+    [string]$BuildBranch = '81aeee183e8a171e4c0fd29fc78de509339808ee'
 )
 
 # For TeamCity - If any issue occurs, this script fails the build. - By default, TeamCity returns an exit code of 0 for all powershell scripts, even if they fail
