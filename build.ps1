39cf711eae9e7e2e5b36e2aed853f8e04591bfd6
zhhyu <Zhaohua.Yu@microsoft.com>
2019-05-01 17:27:52 -0700

    Create NuGet.Services.DatabaseMigration package

--- build.ps1
+++ build.ps1
@@ -97,6 +97,7 @@ Invoke-BuildStep 'Building solution' {
 Invoke-BuildStep 'Creating artifacts' { `
     New-ProjectPackage (Join-Path $PSScriptRoot "src\NuGetGallery.Core\NuGetGallery.Core.csproj") -Configuration $Configuration -Symbols -BuildNumber $BuildNumber -Version $SemanticVersion -PackageId "NuGetGallery.Core$PackageSuffix"
     New-ProjectPackage (Join-Path $PSScriptRoot "src\NuGet.Services.Entities\NuGet.Services.Entities.csproj") -Configuration $Configuration -Symbols -BuildNumber $BuildNumber -Version $SemanticVersion
+    New-ProjectPackage (Join-Path $PSScriptRoot "src\NuGet.Services.DatabaseMigration\NuGet.Services.DatabaseMigration.csproj") -Configuration $Configuration -Symbols -BuildNumber $BuildNumber -Version $SemanticVersion
 } `
 -ev +BuildErrors
 
