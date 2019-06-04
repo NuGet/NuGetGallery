80038f678404d05d52d629b8adfc113263917f26
Peter Yu <ryuyu@microsoft.com>
2019-06-04 14:55:37 -0700

    Remove Referneces to deleted namespace.

--- src/NuGetGallery.Services/PackageManagement/PackageHelper.cs
+++ src/NuGetGallery.Services/PackageManagement/PackageHelper.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using NuGet.Services.Entities;
 using NuGetGallery.Packaging;
-using NuGetGallery.Services.Constants;
 
 namespace NuGetGallery
 {
