e859dc6823b5e0ea338d827da8da1256ad194452
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-05 13:36:20 -0700

    Removed unnecessary imports

--- src/NuGetGallery.Core/GitHub/KiloFormatExtension.cs
+++ src/NuGetGallery.Core/GitHub/KiloFormatExtension.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace NuGetGallery.GitHub
 {
@@ -11,19 +7,29 @@ namespace NuGetGallery.GitHub
         public static string ToKiloFormat(int number)
         {
             if (number >= 1_000_000_000)
-                return new System.Text.StringBuilder((number / 1_000_000_000.0f).ToString("F3")) { [4] = 'G' }.ToString();
+            {
+                return new StringBuilder((number / 1_000_000_000.0f).ToString("F3")) { [4] = 'G' }.ToString();
+            }
+
             if (number >= 100_000_000)
-                return (number / 1000) + "M";
-            if (number >= 10_000_000)
-                return new System.Text.StringBuilder((number / 1000000.0f).ToString("F2")) { [4] = 'M' }.ToString();
+            {
+                return (number / 1_000_000) + "M";
+            }
+
             if (number >= 1_000_000)
-                return new System.Text.StringBuilder((number / 1000000.0f).ToString("F3")) { [4] = 'M' }.ToString();
+            {
+                return new StringBuilder((number / 1_000_000.0f).ToString(number >= 10_000_000 ? "F2" : "F3")) { [4] = 'M' }.ToString();
+            }
+
             if (number >= 100_000)
-                return (number / 1000) + "K";
-            if (number >= 10_000)
-                return new System.Text.StringBuilder((number / 1000.0f).ToString("F2")) { [4] = 'K' }.ToString();
+            {
+                return (number / 1_000) + "K";
+            }
+
             if (number >= 1000)
-                return new System.Text.StringBuilder((number / 1000.0f).ToString("F3")) { [4] = 'K' }.ToString();
+            {
+                return new StringBuilder((number / 1_000.0f).ToString(number >= 10_000 ? "F2" : "F3")) { [4] = 'K' }.ToString();
+            }
 
             return number.ToString("#,0");
         }
