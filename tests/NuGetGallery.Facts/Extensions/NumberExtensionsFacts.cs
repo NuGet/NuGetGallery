2a0e976b1f34efaf541b5d6d3cf457ae58927720
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-07 10:34:07 -0700

    [GitHub Usage] Added tests for ToKiloFormat

--- tests/NuGetGallery.Facts/Extensions/NumberExtensionsFacts.cs
+++ tests/NuGetGallery.Facts/Extensions/NumberExtensionsFacts.cs
@@ -29,5 +29,33 @@ namespace NuGetGallery.Extensions
                 Assert.Equal(expected, actual);
             }
         }
+
+        public class TheToKiloFormatMethod
+        {
+            [Theory]
+            [InlineData(1, "1")]
+            [InlineData(999, "999")]
+            [InlineData(1000, "1.00K")]
+            [InlineData(1990, "1.99K")]
+            [InlineData(1999, "1.99K")]
+            [InlineData(9990, "9.99K")]
+            [InlineData(9999, "9.99K")]
+            [InlineData(10_000, "10.0K")]
+            [InlineData(10_990, "10.9K")]
+            [InlineData(99_990, "99.9K")]
+            [InlineData(100_000, "100K")]
+            [InlineData(100_990, "100K")]
+            [InlineData(100_999, "100K")]
+            [InlineData(999_000, "999K")]
+            [InlineData(999_999, "999K")]
+            [InlineData(1_000_000, "1.00M")]
+            
+            public void FormatsUsingExpectedUnit(int number, string expected)
+            {
+                var actual = NumberExtensions.ToKiloFormat(number);
+
+                Assert.Equal(expected, actual);
+            }
+        }
     }
 }
