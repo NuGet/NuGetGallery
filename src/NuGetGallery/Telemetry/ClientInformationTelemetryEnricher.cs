418a360ce69ceddb973402de467f84be52fd47a4
Cristina Manu <cmanu@microsoft.com>
2019-06-07 15:13:01 -0700

    Missed on file

--- src/NuGetGallery/Telemetry/ClientInformationTelemetryEnricher.cs
+++ src/NuGetGallery/Telemetry/ClientInformationTelemetryEnricher.cs
@@ -21,18 +21,18 @@ namespace NuGetGallery
                 {
                     // ClientVersion is available for NuGet clients starting version 4.1.0-~4.5.0 
                     // Was deprecated and replaced by Protocol version
-                    telemetry.Context.Properties.Add(
+                    telemetry.Context.GlobalProperties.Add(
                         TelemetryService.ClientVersion,
                         httpContext.Request.Headers[GalleryConstants.ClientVersionHeaderName]);
 
-                    telemetry.Context.Properties.Add(
+                    telemetry.Context.GlobalProperties.Add(
                         TelemetryService.ProtocolVersion,
                         httpContext.Request.Headers[GalleryConstants.NuGetProtocolHeaderName]);
 
-                    telemetry.Context.Properties.Add(TelemetryService.ClientInformation, httpContext.GetClientInformation());
+                    telemetry.Context.GlobalProperties.Add(TelemetryService.ClientInformation, httpContext.GetClientInformation());
 
                     // Is the user authenticated or this is an anonymous request?
-                    telemetry.Context.Properties.Add(TelemetryService.IsAuthenticated, httpContext.Request.IsAuthenticated.ToString());
+                    telemetry.Context.GlobalProperties.Add(TelemetryService.IsAuthenticated, httpContext.Request.IsAuthenticated.ToString());
                 }
             }
         }
