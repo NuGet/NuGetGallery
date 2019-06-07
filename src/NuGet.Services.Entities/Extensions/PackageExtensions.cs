4337610e67fa141719d1e49c986d786e120a8830
shishirh <shishirh@microsoft.com>
2019-06-07 12:22:20 -0700

    nit fixes

--- src/NuGet.Services.Entities/Extensions/PackageExtensions.cs
+++ src/NuGet.Services.Entities/Extensions/PackageExtensions.cs
@@ -27,7 +27,7 @@ namespace NuGet.Services.Entities
             return package.GetSymbolsPackagesInReverseOrder()?.FirstOrDefault(sp => sp.StatusKey == PackageStatus.Available);
         }
 
-        public static IOrderedEnumerable<SymbolPackage> GetSymbolsPackagesInReverseOrder(this Package package)
+        private static IOrderedEnumerable<SymbolPackage> GetSymbolsPackagesInReverseOrder(this Package package)
         {
             return package.SymbolPackages?.OrderByDescending(sp => sp.Created);
         }
