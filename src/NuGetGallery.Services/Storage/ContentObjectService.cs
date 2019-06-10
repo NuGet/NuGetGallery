229d17330ec98397f82642bcd29744ceadacad8c
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-10 16:21:21 -0700

    [GH Usage] Reordered imports

--- src/NuGetGallery.Services/Storage/ContentObjectService.cs
+++ src/NuGetGallery.Services/Storage/ContentObjectService.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Newtonsoft.Json;
-using NuGetGallery.GitHub;
-using NuGetGallery.Services;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NuGetGallery.GitHub;
+using NuGetGallery.Services;
 
 namespace NuGetGallery
 {
