5a0764eb2e260fbd4fd6a6d2b92b86b431476b60
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-11 14:29:44 -0700

    [GH Usage] Fix inconsistent code layout

--- src/NuGetGallery.Services/Storage/ContentObjectService.cs
+++ src/NuGetGallery.Services/Storage/ContentObjectService.cs
@@ -49,8 +49,9 @@ namespace NuGetGallery
                await Refresh<TyposquattingConfiguration>(ServicesConstants.ContentNames.TyposquattingConfiguration) ??
                new TyposquattingConfiguration();
 
-            var reposCache = await Refresh<IReadOnlyCollection<RepositoryInformation>>(ServicesConstants.ContentNames.NuGetPackagesGitHubDependencies) ??
-                             Array.Empty<RepositoryInformation>();
+            var reposCache = 
+                await Refresh<IReadOnlyCollection<RepositoryInformation>>(ServicesConstants.ContentNames.NuGetPackagesGitHubDependencies) ??
+                Array.Empty<RepositoryInformation>();
 
             GitHubUsageConfiguration = new GitHubUsageConfiguration(reposCache);
         }
