7b5662378d3a56ca7d88c3173b6d64707bd9a340
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-11 11:11:02 -0700

    [GH Usage] Added license header

--- src/NuGetGallery.Core/GitHub/IGitHubUsageConfiguration.cs
+++ src/NuGetGallery.Core/GitHub/IGitHubUsageConfiguration.cs
@@ -1,4 +1,7 @@
-﻿namespace NuGetGallery.GitHub
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGetGallery.GitHub
 {
     public interface IGitHubUsageConfiguration
     {
