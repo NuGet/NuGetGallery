5ce72e422bd23ec160ec36c9c77ad57ed41c3bf3
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-06-11 14:21:14 -0700

    [GH Usage] Added contextual information about the result of a func call

--- src/NuGetGallery.Core/GitHub/IGitHubUsageConfiguration.cs
+++ src/NuGetGallery.Core/GitHub/IGitHubUsageConfiguration.cs
@@ -6,14 +6,15 @@ namespace NuGetGallery
     public interface IGitHubUsageConfiguration
     {
         /// <summary>
-        /// Returns a NuGetPackageGitHubInformation object that contains the information about a NuGet package.
+        /// Returns the GitHub dependants information about a NuGet package.
+        /// 
         /// If a packageId has no information, the NuGetPackageGitHubInformation's TotalRepos will be 0
         /// and the Repos list will be empty
         /// 
         /// </summary>
         /// <exception cref="System.ArgumentNullException">Thrown when packageId is null</exception>
         /// <param name="packageId">NuGet package id, cannot be null</param>
-        /// <returns></returns>
+        /// <returns>NuGetPackageGitHubInformation that contains the information about a NuGet package.</returns>
         NuGetPackageGitHubInformation GetPackageInformation(string packageId);
     }
 }
