d064212a6013c748b59bb09ba13ba82b23e5db80
Peter Yu <ryuyu@microsoft.com>
2019-06-17 17:30:25 -0700

    Adding emailbuilder facts.

---
+++ tests/AccountDeleter.Facts/EmailBuilderFacts.cs
@@ -0,0 +1,69 @@
+﻿
+
+using Moq;
+using NuGet.Services.Messaging.Email;
+using NuGetGallery.AccountDeleter;
+using NuGetGallery.AccountDeleter.Messengers;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mail;
+using Xunit;
+
+namespace AccountDeleter.Facts
+{
+    public class EmailBuilderFacts
+    {
+        private Mock<IEmailBuilder> _accountDeleteEmailBuilder;
+
+        public EmailBuilderFacts()
+        {
+            _accountDeleteEmailBuilder = new Mock<IEmailBuilder>();
+        }
+
+        [Theory]
+        [InlineData("USERNAME", "test", "test")]
+        [InlineData("Do not replace", "test", "Do not replace")]
+        [InlineData("USERNAME and things", "test", "test and things")]
+        [InlineData("USERNAMEnospaces", "test", "testnospaces")]
+        public void UsernameTokenIsReplacedInBody(string templateString, string username, string expected)
+        {
+            // Setup
+            _accountDeleteEmailBuilder
+                .Setup(adeb => adeb.GetBody(It.IsAny<EmailFormat>()))
+                .Returns(templateString);
+
+            var disposableBuilder = new DisposableEmailBuilder(_accountDeleteEmailBuilder.Object, new EmailRecipients(new List<MailAddress>()), username);
+
+            // Act
+            var resultHtml = disposableBuilder.GetBody(EmailFormat.Html);
+            var resultMd = disposableBuilder.GetBody(EmailFormat.Markdown);
+            var resultText = disposableBuilder.GetBody(EmailFormat.PlainText);
+
+            // Assert
+            Assert.Equal(expected, resultHtml);
+            Assert.Equal(expected, resultMd);
+            Assert.Equal(expected, resultText);
+        }
+
+        [Theory]
+        [InlineData("USERNAME", "test", "test")]
+        [InlineData("Do not replace", "test", "Do not replace")]
+        [InlineData("USERNAME and things", "test", "test and things")]
+        [InlineData("USERNAMEnospaces", "test", "testnospaces")]
+        public void UsernameTokenIsReplacedInSubject(string templateString, string username, string expected)
+        {
+            // Setup
+            _accountDeleteEmailBuilder
+                .Setup(adeb => adeb.GetSubject())
+                .Returns(templateString);
+
+            var disposableBuilder = new DisposableEmailBuilder(_accountDeleteEmailBuilder.Object, new EmailRecipients(new List<MailAddress>()), username);
+
+            // Act
+            var result = disposableBuilder.GetSubject();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
--- tests/AccountDeleter.Facts/EvaluatorFacts.cs
+++ tests/AccountDeleter.Facts/EvaluatorFacts.cs
@@ -23,6 +23,7 @@ namespace AccountDeleter.Facts
 
         public EvaluatorFacts(ITestOutputHelper output)
         {
+            _output = output;
             _packageService = new Mock<IPackageService>();
             _accountDeleteTelemetryService = new Mock<IAccountDeleteTelemetryService>();
 
@@ -59,6 +60,108 @@ namespace AccountDeleter.Facts
 
             // Assert
             Assert.Equal(expectedResult, result);
+        }
+
+        [Fact]
+        public void AlwayRejectEvaulatorReturnsFalse()
+        {
+            // Setup
+            var logger = _loggerFactory.CreateLogger<AlwaysRejectEvaluator>();
+            var testUser = new User();
+
+            var evaluator = new AlwaysRejectEvaluator(logger);
+
+            // Act
+            var result = evaluator.CanUserBeDeleted(testUser);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AlwayAllowEvaulatorReturnsTrue()
+        {
+            // Setup
+            var logger = _loggerFactory.CreateLogger<AlwaysAllowEvaluator>();
+            var testUser = new User();
+
+            var evaluator = new AlwaysAllowEvaluator(logger);
+
+            // Act
+            var result = evaluator.CanUserBeDeleted(testUser);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void AggregateEvaluatorRejectsDuplicateEvalutors()
+        {
+            // Setup
+            var aaeLogger = _loggerFactory.CreateLogger<AlwaysAllowEvaluator>();
+            var areLogger = _loggerFactory.CreateLogger<AlwaysRejectEvaluator>();
+            var aeLogger = _loggerFactory.CreateLogger<AggregateEvaluator>();
+
+            var evaluator1 = new AlwaysAllowEvaluator(aaeLogger);
+            var evaluator2 = new AlwaysRejectEvaluator(areLogger);
+
+            var aggregateEvalutor = new AggregateEvaluator(aeLogger);
+
+            // Act
+            var result1 = aggregateEvalutor.AddEvaluator(evaluator1);
+            var result2 = aggregateEvalutor.AddEvaluator(evaluator1);
+            var result3 = aggregateEvalutor.AddEvaluator(evaluator2);
+
+            // Assert
+            Assert.True(result1);
+            Assert.False(result2);
+            Assert.True(result3);
+        }
+
+        [Fact]
+        public void AggregateEvaluatorRejectsIfAnyRejects()
+        {
+            // Setup
+            var aaeLogger = _loggerFactory.CreateLogger<AlwaysAllowEvaluator>();
+            var areLogger = _loggerFactory.CreateLogger<AlwaysRejectEvaluator>();
+            var aeLogger = _loggerFactory.CreateLogger<AggregateEvaluator>();
+            var testUser = new User();
+
+            var evaluator1 = new AlwaysAllowEvaluator(aaeLogger);
+            var evaluator2 = new AlwaysRejectEvaluator(areLogger);
+
+            var aggregateEvalutor = new AggregateEvaluator(aeLogger);
+            aggregateEvalutor.AddEvaluator(evaluator1);
+            aggregateEvalutor.AddEvaluator(evaluator2);
+
+            // Act
+            var result = aggregateEvalutor.CanUserBeDeleted(testUser);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void AggregateEvaluatorAcceptsIfAllAccept()
+        {
+            // Setup
+            var aaeLogger = _loggerFactory.CreateLogger<AlwaysAllowEvaluator>();
+            var aae2Logger = _loggerFactory.CreateLogger<AlwaysAllowEvaluator>();
+            var aeLogger = _loggerFactory.CreateLogger<AggregateEvaluator>();
+            var testUser = new User();
+
+            var evaluator1 = new AlwaysAllowEvaluator(aaeLogger);
+            var evaluator2 = new AlwaysAllowEvaluator(aae2Logger);
+
+            var aggregateEvalutor = new AggregateEvaluator(aeLogger);
+            aggregateEvalutor.AddEvaluator(evaluator1);
+            aggregateEvalutor.AddEvaluator(evaluator2);
+
+            // Act
+            var result = aggregateEvalutor.CanUserBeDeleted(testUser);
+
+            // Assert
+            Assert.True(result);
         }
     }
 }
