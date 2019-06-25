97bd5544a62f03a49d590cdd3e2a66e0440bea9c
zhhyu <Zhaohua.Yu@microsoft.com>
2019-06-24 19:16:25 -0700

    Clean the version check

---
+++ tests/NuGet.Services.DatabaseMigration.Facts/DatabaseMigrationFacts.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Data.Entity.Migrations;
+using System.Reflection;
+using Xunit;
+
+namespace NuGet.Services.DatabaseMigration.Facts
+{
+    public class DatabaseMigrationFacts
+    {
+        [Fact]
+        public void AssertDbMigratorPrivateFieldNotNull()
+        {
+            var historyRepository = typeof(DbMigrator).
+                GetField("_historyRepository",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Assert.NotNull(historyRepository);
+
+            var connectionField = historyRepository.FieldType.BaseType.GetField(
+                "_existingConnection",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Assert.NotNull(connectionField);
+        }
+    }
+}
