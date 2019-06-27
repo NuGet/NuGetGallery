c6d25ff0d26d3995b759b19d5bbdfc019f8df562
zhhyu <41028779+zhhyu@users.noreply.github.com>
2019-06-27 13:47:15 -0700

    [DB Migration] Fix the package dependency issue and some updates (#7268)
    * Update the DB migration tool
    
    * VerifyDLLVersion
    
    * Update
    
    * Clean the version check
    
    * Verify the type

--- src/DatabaseMigrationTools/Program.cs
+++ src/DatabaseMigrationTools/Program.cs
@@ -13,14 +13,14 @@ namespace NuGetGallery.DatabaseMigrationTools
         {
             var migrationContextFactory = new MigrationContextFactory();
             var job = new Job(migrationContextFactory);
-            JobRunner.RunOnce(job, args).GetAwaiter().GetResult();
+            var exitCode = JobRunner.RunOnce(job, args).GetAwaiter().GetResult();
 
             // Have to use Thread.Sleep() and wait for the logger.
             // "TelemetryConfiguration.Active.TelemetryChannel.Flush()" is not reliable.
             // Hit the issue: https://github.com/Microsoft/ApplicationInsights-dotnet/issues/407
             Thread.Sleep(30000);
 
-            return job.ExitCode;
+            return exitCode;
         }
     }
 }
