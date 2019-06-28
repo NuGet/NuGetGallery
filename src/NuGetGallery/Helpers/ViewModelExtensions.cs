cc0ae13a288146a595f49a9d586d9b16a5bd2d17
Andrei Grigorev <angrigor@microsoft.com>
2019-06-27 17:40:20 -0700

    DisplayPackageViewModel

--- src/NuGetGallery/Helpers/ViewModelExtensions.cs
+++ src/NuGetGallery/Helpers/ViewModelExtensions.cs
@@ -310,6 +310,116 @@ namespace NuGetGallery
             return viewModel;
         }
 
+        public static DisplayPackageViewModel SetupFromPackage(
+            this DisplayPackageViewModel viewModel,
+            Package package,
+            User currentUser,
+            PackageDeprecation deprecation)
+        {
+            ((ListPackageItemViewModel)viewModel).SetupFromPackage(package, currentUser);
+
+            var nuGetVersion = NuGetVersion.Parse(NuGetVersionFormatter.ToFullString(package.Version));
+
+            viewModel.HasSemVer2Version = nuGetVersion.IsSemVer2;
+            viewModel.HasSemVer2Dependency = package.Dependencies.ToList()
+                .Where(pd => !string.IsNullOrEmpty(pd.VersionSpec))
+                .Select(pd => VersionRange.Parse(pd.VersionSpec))
+                .Any(p => (p.HasUpperBound && p.MaxVersion.IsSemVer2) || (p.HasLowerBound && p.MinVersion.IsSemVer2));
+
+            viewModel.Dependencies = new DependencySetsViewModel(package.Dependencies);
+
+            var packageHistory = package
+                .PackageRegistration
+                .Packages
+                .OrderByDescending(p => new NuGetVersion(p.Version))
+                .ToList();
+            var pushedByCache = new Dictionary<User, string>();
+            viewModel.PackageVersions = packageHistory.Select(p => GetPackageHistoryItemViewModel(p, currentUser, GetPushedBy(p, currentUser, pushedByCache))).ToList();
+
+            viewModel.PushedBy = GetPushedBy(package, currentUser, pushedByCache);
+            viewModel.PackageFileSize = package.PackageFileSize;
+
+            viewModel.LatestSymbolsPackage = package.LatestSymbolPackage();
+            viewModel.LatestAvailableSymbolsPackage = viewModel.LatestSymbolsPackage != null && viewModel.LatestSymbolsPackage.StatusKey == PackageStatus.Available
+                ? viewModel.LatestSymbolsPackage
+                : package.LatestAvailableSymbolPackage();
+
+            if (packageHistory.Any())
+            {
+                // calculate the number of days since the package registration was created
+                // round to the nearest integer, with a min value of 1
+                // divide the total download count by this number
+                viewModel.TotalDaysSinceCreated = Convert.ToInt32(Math.Max(1, Math.Round((DateTime.UtcNow - packageHistory.Min(p => p.Created)).TotalDays)));
+                viewModel.DownloadsPerDay = viewModel.TotalDownloadCount / viewModel.TotalDaysSinceCreated; // for the package
+                viewModel.DownloadsPerDayLabel = viewModel.DownloadsPerDay < 1 ? "<1" : viewModel.DownloadsPerDay.ToNuGetNumberString();
+                viewModel.IsDotnetToolPackageType = package.PackageTypes.Any(e => e.Name.Equals("DotnetTool", StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (deprecation != null)
+            {
+                viewModel.AlternatePackageId = deprecation.AlternatePackageRegistration?.Id;
+
+                var alternatePackage = deprecation.AlternatePackage;
+                if (alternatePackage != null)
+                {
+                    // A deprecation should not have both an alternate package registration and an alternate package.
+                    // In case a deprecation does have both, we will hide the alternate package registration's ID in this model.
+                    viewModel.AlternatePackageId = alternatePackage?.Id;
+                    viewModel.AlternatePackageVersion = alternatePackage?.Version;
+                }
+
+                viewModel.CustomMessage = deprecation.CustomMessage;
+            }
+
+            return viewModel;
+        }
+
+        private static DisplayPackageViewModel GetPackageHistoryItemViewModel(
+            Package package,
+            User currentUser,
+            string pushedBy)
+        {
+            var viewModel = new DisplayPackageViewModel();
+
+            ((ListPackageItemViewModel)viewModel).SetupFromPackage(package, currentUser);
+
+            viewModel.Copyright = package.Copyright;
+
+            viewModel.DownloadCount = package.DownloadCount;
+            viewModel.LastEdited = package.LastEdited;
+
+            viewModel.TotalDaysSinceCreated = 0;
+            viewModel.DownloadsPerDay = 0;
+
+            viewModel.PushedBy = pushedBy;
+
+            viewModel.InitializeRepositoryMetadata(package.RepositoryUrl, package.RepositoryType);
+
+            if (PackageHelper.TryPrepareUrlForRendering(package.ProjectUrl, out string projectUrl))
+            {
+                viewModel.ProjectUrl = projectUrl;
+            }
+
+            viewModel.EmbeddedLicenseType = package.EmbeddedLicenseType;
+            viewModel.LicenseExpression = package.LicenseExpression;
+
+            if (PackageHelper.TryPrepareUrlForRendering(package.LicenseUrl, out string licenseUrl))
+            {
+                viewModel.LicenseUrl = licenseUrl;
+
+                var licenseNames = package.LicenseNames;
+                if (!string.IsNullOrEmpty(licenseNames))
+                {
+                    viewModel.LicenseNames = licenseNames.Split(',').Select(l => l.Trim());
+                }
+            }
+
+            viewModel.DeprecationStatus = package.Deprecations.SingleOrDefault()?.Status
+                ?? PackageDeprecationStatus.NotDeprecated;
+
+            return viewModel;
+        }
+
         private static PackageStatusSummary GetPackageStatusSummary(PackageStatus packageStatus, bool listed)
         {
             switch (packageStatus)
@@ -425,6 +535,47 @@ namespace NuGetGallery
             var canEditRequiredSigned = currentUserCanManageRequiredSigner &&
                 (currentUserHasRequiredSignerControl || noOwnerHasRequiredSignerControl);
             return canEditRequiredSigned;
+        }
+
+        private static string GetPushedBy(Package package, User currentUser, Dictionary<User, string> pushedByCache)
+        {
+            var userPushedBy = package.User;
+
+            if (userPushedBy == null)
+            {
+                return null;
+            }
+
+            if (!pushedByCache.ContainsKey(userPushedBy))
+            {
+                // Only show who pushed the package version to users that can see private package metadata
+                if (ActionsRequiringPermissions.DisplayPrivatePackageMetadata.CheckPermissionsOnBehalfOfAnyAccount(currentUser, package) == PermissionsCheckResult.Allowed)
+                {
+                    var organizationsThatUserPushedByBelongsTo =
+                        (package.PackageRegistration?.Owners ?? Enumerable.Empty<User>())
+                            .OfType<Organization>()
+                            .Where(organization => ActionsRequiringPermissions.ViewAccount.CheckPermissions(userPushedBy, organization) == PermissionsCheckResult.Allowed);
+                    if (organizationsThatUserPushedByBelongsTo.Any())
+                    {
+                        // If the user is a member of any organizations that are package owners, only show the user if the current user is a member of the same organization
+                        pushedByCache[userPushedBy] =
+                            organizationsThatUserPushedByBelongsTo.Any(organization => ActionsRequiringPermissions.ViewAccount.CheckPermissions(currentUser, organization) == PermissionsCheckResult.Allowed) ?
+                                userPushedBy?.Username :
+                                organizationsThatUserPushedByBelongsTo.First().Username;
+                    }
+                    else
+                    {
+                        // Otherwise, show the user
+                        pushedByCache[userPushedBy] = userPushedBy?.Username;
+                    }
+                }
+                else
+                {
+                    pushedByCache[userPushedBy] = null;
+                }
+            }
+
+            return pushedByCache[userPushedBy];
         }
     }
 }
--- src/NuGetGallery/ViewModels/DisplayPackageViewModel.cs
+++ src/NuGetGallery/ViewModels/DisplayPackageViewModel.cs
@@ -15,98 +15,17 @@ namespace NuGetGallery
     {
         internal readonly NuGetVersion NuGetVersion;
 
-        public DisplayPackageViewModel(Package package, User currentUser, PackageDeprecation deprecation)
-            : this(package, currentUser, (string)null)
+        public DisplayPackageViewModel()
         {
-            NuGetVersion = NuGetVersion.Parse(FullVersion);
-
-            HasSemVer2Version = NuGetVersion.IsSemVer2;
-            HasSemVer2Dependency = package.Dependencies.ToList()
-                .Where(pd => !string.IsNullOrEmpty(pd.VersionSpec))
-                .Select(pd => VersionRange.Parse(pd.VersionSpec))
-                .Any(p => (p.HasUpperBound && p.MaxVersion.IsSemVer2) || (p.HasLowerBound && p.MinVersion.IsSemVer2));
-
-            Dependencies = new DependencySetsViewModel(package.Dependencies);
-
-            var packageHistory = package
-                .PackageRegistration
-                .Packages
-                .OrderByDescending(p => new NuGetVersion(p.Version))
-                .ToList();
-            PackageVersions = packageHistory.Select(p => new DisplayPackageViewModel(p, currentUser, GetPushedBy(p, currentUser))).ToList();
-
-            PushedBy = GetPushedBy(package, currentUser);
-            PackageFileSize = package.PackageFileSize;
-
-            LatestSymbolsPackage = package.LatestSymbolPackage();
-            LatestAvailableSymbolsPackage = LatestSymbolsPackage != null && LatestSymbolsPackage.StatusKey == PackageStatus.Available
-                ? LatestSymbolsPackage
-                : package.LatestAvailableSymbolPackage();
-
-            if (packageHistory.Any())
-            {
-                // calculate the number of days since the package registration was created
-                // round to the nearest integer, with a min value of 1
-                // divide the total download count by this number
-                TotalDaysSinceCreated = Convert.ToInt32(Math.Max(1, Math.Round((DateTime.UtcNow - packageHistory.Min(p => p.Created)).TotalDays)));
-                DownloadsPerDay = TotalDownloadCount / TotalDaysSinceCreated; // for the package
-                DownloadsPerDayLabel = DownloadsPerDay < 1 ? "<1" : DownloadsPerDay.ToNuGetNumberString();
-                IsDotnetToolPackageType = package.PackageTypes.Any(e => e.Name.Equals("DotnetTool", StringComparison.OrdinalIgnoreCase));
-            }
-
-            if (deprecation != null)
-            {
-                AlternatePackageId = deprecation.AlternatePackageRegistration?.Id;
-
-                var alternatePackage = deprecation.AlternatePackage;
-                if (alternatePackage != null)
-                {
-                    // A deprecation should not have both an alternate package registration and an alternate package.
-                    // In case a deprecation does have both, we will hide the alternate package registration's ID in this model.
-                    AlternatePackageId = alternatePackage?.Id;
-                    AlternatePackageVersion = alternatePackage?.Version;
-                }
-
-                CustomMessage = deprecation.CustomMessage;
-            }
+            // TODO: remove
         }
 
-        private DisplayPackageViewModel(Package package, User currentUser, string pushedBy)
-            : base(package, currentUser)
+        public DisplayPackageViewModel(Package package, User currentUser, PackageDeprecation deprecation)
         {
-            Copyright = package.Copyright;
+            // TODO: remove
+            NuGetVersion = NuGetVersion.Parse(NuGetVersionFormatter.ToFullString(package.Version));
 
-            DownloadCount = package.DownloadCount;
-            LastEdited = package.LastEdited;
-
-            TotalDaysSinceCreated = 0;
-            DownloadsPerDay = 0;
-
-            PushedBy = pushedBy;
-
-            InitializeRepositoryMetadata(package.RepositoryUrl, package.RepositoryType);
-
-            if (PackageHelper.TryPrepareUrlForRendering(package.ProjectUrl, out string projectUrl))
-            {
-                ProjectUrl = projectUrl;
-            }
-
-            EmbeddedLicenseType = package.EmbeddedLicenseType;
-            LicenseExpression = package.LicenseExpression;
-
-            if (PackageHelper.TryPrepareUrlForRendering(package.LicenseUrl, out string licenseUrl))
-            {
-                LicenseUrl = licenseUrl;
-
-                var licenseNames = package.LicenseNames;
-                if (!string.IsNullOrEmpty(licenseNames))
-                {
-                    LicenseNames = licenseNames.Split(',').Select(l => l.Trim());
-                }
-            }
-
-            DeprecationStatus = package.Deprecations.SingleOrDefault()?.Status
-                ?? PackageDeprecationStatus.NotDeprecated;
+            this.SetupFromPackage(package, currentUser, deprecation);
         }
 
         public bool ValidatingTooLong { get; set; }
@@ -117,14 +36,14 @@ namespace NuGetGallery
         public string Copyright { get; set; }
         public string ReadMeHtml { get; set; }
         public DateTime? LastEdited { get; set; }
-        public int DownloadsPerDay { get; private set; }
-        public int TotalDaysSinceCreated { get; private set; }
-        public long PackageFileSize { get; private set; }
-        public SymbolPackage LatestSymbolsPackage { get; private set; }
-        public SymbolPackage LatestAvailableSymbolsPackage { get; private set; }
+        public int DownloadsPerDay { get; set; }
+        public int TotalDaysSinceCreated { get; set; }
+        public long PackageFileSize { get; set; }
+        public SymbolPackage LatestSymbolsPackage { get; set; }
+        public SymbolPackage LatestAvailableSymbolsPackage { get; set; }
 
-        public bool HasSemVer2Version { get; }
-        public bool HasSemVer2Dependency { get; }
+        public bool HasSemVer2Version { get; set; }
+        public bool HasSemVer2Dependency { get; set; }
         public bool IsDotnetToolPackageType { get; set; }
         public bool IsAtomFeedEnabled { get; set; }
         public bool IsPackageDeprecationEnabled { get; set; }
@@ -157,9 +76,9 @@ namespace NuGetGallery
 
         public bool? IsIndexed { get; set; }
 
-        public string DownloadsPerDayLabel { get; private set; }
+        public string DownloadsPerDayLabel { get; set; }
 
-        public string PushedBy { get; private set; }
+        public string PushedBy { get; set; }
 
         public bool IsCertificatesUIEnabled { get; set; }
         public string RepositoryUrl { get; private set; }
@@ -176,50 +95,7 @@ namespace NuGetGallery
         public string AlternatePackageVersion { get; set; }
         public string CustomMessage { get; set; }
 
-        private IDictionary<User, string> _pushedByCache = new Dictionary<User, string>();
-
-        private string GetPushedBy(Package package, User currentUser)
-        {
-            var userPushedBy = package.User;
-
-            if (userPushedBy == null)
-            {
-                return null;
-            }
-
-            if (!_pushedByCache.ContainsKey(userPushedBy))
-            {
-                // Only show who pushed the package version to users that can see private package metadata
-                if (ActionsRequiringPermissions.DisplayPrivatePackageMetadata.CheckPermissionsOnBehalfOfAnyAccount(currentUser, package) == PermissionsCheckResult.Allowed)
-                {
-                    var organizationsThatUserPushedByBelongsTo =
-                        (package.PackageRegistration?.Owners ?? Enumerable.Empty<User>())
-                            .OfType<Organization>()
-                            .Where(organization => ActionsRequiringPermissions.ViewAccount.CheckPermissions(userPushedBy, organization) == PermissionsCheckResult.Allowed);
-                    if (organizationsThatUserPushedByBelongsTo.Any())
-                    {
-                        // If the user is a member of any organizations that are package owners, only show the user if the current user is a member of the same organization
-                        _pushedByCache[userPushedBy] =
-                            organizationsThatUserPushedByBelongsTo.Any(organization => ActionsRequiringPermissions.ViewAccount.CheckPermissions(currentUser, organization) == PermissionsCheckResult.Allowed) ?
-                                userPushedBy?.Username :
-                                organizationsThatUserPushedByBelongsTo.First().Username;
-                    }
-                    else
-                    {
-                        // Otherwise, show the user
-                        _pushedByCache[userPushedBy] = userPushedBy?.Username;
-                    }
-                }
-                else
-                {
-                    _pushedByCache[userPushedBy] = null;
-                }
-            }
-
-            return _pushedByCache[userPushedBy];
-        }
-
-        private void InitializeRepositoryMetadata(string repositoryUrl, string repositoryType)
+        public void InitializeRepositoryMetadata(string repositoryUrl, string repositoryType)
         {
             RepositoryType = RepositoryKind.Unknown;
 
