233b6603168d3bf9efaa4f1d6373d0ab557c5682
Riad Mohamed Gahlouz <t-mogah@microsoft.com>
2019-07-08 17:20:34 -0700

    [GH Usage] Fix typo and extra line

--- src/NuGetGallery/Scripts/gallery/page-display-package.js
+++ src/NuGetGallery/Scripts/gallery/page-display-package.js
@@ -146,7 +146,6 @@ $(function () {
             }, 400, function () {
                 // Add hash (#) to URL when done scrolling (default click behavior)
                 window.location.hash = hash;
-                    
             });
         }
     });
