1c5681f4840d5ef5233de3daf18ecd0387a73920
Loïc Sharma <sharma.loic@gmail.com>
2019-07-29 16:13:59 -0700

    [Azure Search] Nerf exact matches on package id (#612)
    I tested the side-by-side feedback and the top queries as per our Gallery browser telemetry.
    
    This depends on: https://nuget.visualstudio.com/NuGetMicrosoft/_git/NuGetDeployment/pullrequest/1034?_a=overview
    Addresses: https://github.com/nuget/nugetgallery/issues/7375

--- tests/NuGet.Services.AzureSearch.FunctionalTests/Relevancy/V3RelevancyFunctionalTests.cs
+++ tests/NuGet.Services.AzureSearch.FunctionalTests/Relevancy/V3RelevancyFunctionalTests.cs
@@ -35,12 +35,10 @@ namespace NuGet.Services.AzureSearch.FunctionalTests
             // Test that common queries have the most frequently selected results at the top.
             // These results were determined using the "BrowserSearchPage" and "BrowserSearchSelection" metrics
             // on the Gallery's Application Insights telemetry.
-            // TODO: Reduce exact match boost and update "json.net", "redis", and "csv" tests.
-            // See: https://github.com/NuGet/NuGetGallery/issues/7330
             yield return new object[] { "newtonsoft.json", new[] { "newtonsoft.json" } };
             yield return new object[] { "newtonsoft", new[] { "newtonsoft.json" } };
-            yield return new object[] { "json.net", new[] { "json.net", "newtonsoft.json" } };
-            yield return new object[] { "json", new[] { "json", "newtonsoft.json" } };
+            yield return new object[] { "json.net", new[] { "newtonsoft.json" } };
+            yield return new object[] { "json", new[] { "newtonsoft.json" } };
 
             yield return new object[] { "tags:\"aws-sdk-v3\"", new[] { "awssdk.core", "awssdk.s3" } };
 
@@ -58,11 +56,11 @@ namespace NuGet.Services.AzureSearch.FunctionalTests
             yield return new object[] { "dapper", new[] { "dapper" } };
             yield return new object[] { "log4net", new[] { "log4net" } };
             yield return new object[] { "automapper", new[] { "automapper" } };
-            yield return new object[] { "csv", new[] { "csv", "csvhelper" } };
+            yield return new object[] { "csv", new[] { "csvhelper" } };
             yield return new object[] { "bootstrap", new[] { "bootstrap" } };
             yield return new object[] { "moq", new[] { "moq" } };
             yield return new object[] { "serilog", new[] { "serilog" } };
-            yield return new object[] { "redis", new[] { "redis", "stackexchange.redis", "microsoft.extensions.caching.redis" } };
+            yield return new object[] { "redis", new[] { "stackexchange.redis", "microsoft.extensions.caching.redis" } };
         }
 
         [RelevancyTheory]
