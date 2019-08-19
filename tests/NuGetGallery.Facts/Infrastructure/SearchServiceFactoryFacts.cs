NuGet/NuGetGallery

commit 45eec66d923dc176d12cc6d2ecb71beb24c58149

Integrate A/B test service with package search (#7313)
Address https://github.com/NuGet/NuGetGallery/issues/7166

src/NuGetGallery/App_Start/DefaultDependenciesModule.cs
@@ -68,6 +68,11 @@ namespace NuGetGallery
             public const string PreviewSearchClient = "PreviewSearchClientBindingKey";
         }
 
+        public static class ParameterNames
+        {
+            public const string PackagesController_PreviewSearchService = "previewSearchService";
+        }
+
         protected override void Load(ContainerBuilder builder)
         {
             var services = new ServiceCollection();
@@ -801,6 +806,14 @@ namespace NuGetGallery
                     c.Resolve<IMessageService>(),
                     c.Resolve<IMessageServiceConfiguration>()))
                 .As<ISearchSideBySideService>()
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterType<PackagesController>()
+                .WithParameter(new ResolvedParameter(
+                    (pi, ctx) => pi.ParameterType == typeof(ISearchService) && pi.Name == ParameterNames.PackagesController_PreviewSearchService,
+                    (pi, ctx) => ctx.ResolveKeyed<ISearchService>(BindingKeys.PreviewSearchClient)))
+                .As<PackagesController>()
                 .InstancePerLifetimeScope();
         }
 

src/NuGetGallery/Controllers/PackagesController.cs
@@ -90,6 +90,7 @@ namespace NuGetGallery
         private readonly IPackageUpdateService _packageUpdateService;
         private readonly IPackageFileService _packageFileService;
         private readonly ISearchService _searchService;
+        private readonly ISearchService _previewSearchService;
         private readonly IUploadFileService _uploadFileService;
         private readonly IUserService _userService;
         private readonly IEntitiesContext _entitiesContext;
@@ -112,6 +113,7 @@ namespace NuGetGallery
         private readonly ILicenseExpressionSplitter _licenseExpressionSplitter;
         private readonly IFeatureFlagService _featureFlagService;
         private readonly IPackageDeprecationService _deprecationService;
+        private readonly IABTestService _abTestService;
 
         public PackagesController(
             IPackageService packageService,
@@ -120,6 +122,7 @@ namespace NuGetGallery
             IUserService userService,
             IMessageService messageService,
             ISearchService searchService,
+            ISearchService previewSearchService,
             IPackageFileService packageFileService,
             IEntitiesContext entitiesContext,
             IAppConfiguration config,
@@ -141,14 +144,16 @@ namespace NuGetGallery
             ICoreLicenseFileService coreLicenseFileService,
             ILicenseExpressionSplitter licenseExpressionSplitter,
             IFeatureFlagService featureFlagService,
-            IPackageDeprecationService deprecationService)
+            IPackageDeprecationService deprecationService,
+            IABTestService abTestService)
         {
             _packageService = packageService;
             _packageUpdateService = packageUpdateService ?? throw new ArgumentNullException(nameof(packageUpdateService));
             _uploadFileService = uploadFileService;
             _userService = userService;
             _messageService = messageService;
-            _searchService = searchService;
+            _searchService = searchService ?? throw new ArgumentNullException(nameof(searchService));
+            _previewSearchService = previewSearchService ?? throw new ArgumentNullException(nameof(previewSearchService));
             _packageFileService = packageFileService;
             _entitiesContext = entitiesContext;
             _config = config;
@@ -171,6 +176,7 @@ namespace NuGetGallery
             _licenseExpressionSplitter = licenseExpressionSplitter ?? throw new ArgumentNullException(nameof(licenseExpressionSplitter));
             _featureFlagService = featureFlagService ?? throw new ArgumentNullException(nameof(featureFlagService));
             _deprecationService = deprecationService ?? throw new ArgumentNullException(nameof(deprecationService));
+            _abTestService = abTestService ?? throw new ArgumentNullException(nameof(abTestService));
         }
 
         [HttpGet]
@@ -980,8 +986,11 @@ namespace NuGetGallery
 
             SearchResults results;
 
+            var isPreviewSearchEnabled = !string.IsNullOrEmpty(q) && _abTestService.IsPreviewSearchEnabled(GetCurrentUser());
+            var searchService = isPreviewSearchEnabled ? _previewSearchService : _searchService;
+
             // fetch most common query from cache to relieve load on the search service
-            if (string.IsNullOrEmpty(q) && page == 1 && includePrerelease)
+            if (string.IsNullOrEmpty(q) && page == 1 && includePrerelease && !isPreviewSearchEnabled)
             {
                 var cachedResults = HttpContext.Cache.Get("DefaultSearchResults");
                 if (cachedResults == null)
@@ -994,7 +1003,7 @@ namespace NuGetGallery
                         context: SearchFilter.UISearchContext,
                         semVerLevel: SemVerLevelKey.SemVerLevel2);
 
-                    results = await _searchService.Search(searchFilter);
+                    results = await searchService.Search(searchFilter);
 
                     // note: this is a per instance cache
                     HttpContext.Cache.Add(
@@ -1021,7 +1030,7 @@ namespace NuGetGallery
                     context: SearchFilter.UISearchContext,
                     semVerLevel: SemVerLevelKey.SemVerLevel2);
 
-                results = await _searchService.Search(searchFilter);
+                results = await searchService.Search(searchFilter);
             }
 
             int totalHits = results.Hits;
@@ -1040,7 +1049,8 @@ namespace NuGetGallery
                 page - 1,
                 GalleryConstants.DefaultPackageListPageSize,
                 Url,
-                includePrerelease);
+                includePrerelease,
+                isPreviewSearchEnabled);
 
             ViewBag.SearchTerm = q;
 

tests/NuGetGallery.Facts/Controllers/PackagesControllerFacts.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
@@ -11,6 +12,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Moq;
@@ -52,6 +54,7 @@ namespace NuGetGallery
             Mock<HttpContextBase> httpContext = null,
             Stream fakeNuGetPackage = null,
             Mock<ISearchService> searchService = null,
+            Mock<ISearchService> previewSearchService = null,
             Exception readPackageException = null,
             Mock<IPackageFileService> packageFileService = null,
             Mock<IEntitiesContext> entitiesContext = null,
@@ -72,7 +75,8 @@ namespace NuGetGallery
             Mock<ICoreLicenseFileService> coreLicenseFileService = null,
             Mock<ILicenseExpressionSplitter> licenseExpressionSplitter = null,
             Mock<IFeatureFlagService> featureFlagService = null,
-            Mock<IPackageDeprecationService> deprecationService = null)
+            Mock<IPackageDeprecationService> deprecationService = null,
+            Mock<IABTestService> abTestService = null)
         {
             packageService = packageService ?? new Mock<IPackageService>();
             packageUpdateService = packageUpdateService ?? new Mock<IPackageUpdateService>();
@@ -85,7 +89,10 @@ namespace NuGetGallery
             }
             userService = userService ?? new Mock<IUserService>();
             messageService = messageService ?? new Mock<IMessageService>();
+
             searchService = searchService ?? new Mock<ISearchService>();
+
+            previewSearchService = previewSearchService ?? new Mock<ISearchService>();
 
             if (packageFileService == null)
             {
@@ -197,7 +204,10 @@ namespace NuGetGallery
 
             deprecationService = deprecationService ?? new Mock<IPackageDeprecationService>();
 
+            abTestService = abTestService ?? new Mock<IABTestService>();
+
             var diagnosticsService = new Mock<IDiagnosticsService>();
+
             var controller = new Mock<PackagesController>(
                 packageService.Object,
                 packageUpdateService.Object,
@@ -205,6 +215,7 @@ namespace NuGetGallery
                 userService.Object,
                 messageService.Object,
                 searchService.Object,
+                previewSearchService.Object,
                 packageFileService.Object,
                 entitiesContext.Object,
                 configurationService.Current,
@@ -226,7 +237,8 @@ namespace NuGetGallery
                 coreLicenseFileService.Object,
                 licenseExpressionSplitter.Object,
                 featureFlagService.Object,
-                deprecationService.Object);
+                deprecationService.Object,
+                abTestService.Object);
 
             controller.CallBase = true;
             controller.Object.SetOwinContextOverride(Fakes.CreateOwinContext());
@@ -315,6 +327,30 @@ namespace NuGetGallery
                     type, message: new PlainTextOnlyValidationMessage("Something"), warnings: null));
 
             return fakePackageUploadService;
+        }
+
+        public class TheConstructor
+            : TestContainer
+        {
+            /// <summary>
+            /// This test is to verify that the preview search service parameter name for the constructor has the
+            /// expected value. This is necessary because the dependency injection configuration uses the parameter
+            /// name to identify which search service should be preview implementation.
+            /// </summary>
+            [Fact]
+            public void HasPreviewSearchServiceParameter()
+            {
+                var type = typeof(PackagesController);
+
+                var constructors = type.GetConstructors();
+                var constructor = Assert.Single(constructors);
+
+                var parameters = constructor.GetParameters();
+                var parameter = Assert.Single(
+                    parameters,
+                    p => p.Name == DefaultDependenciesModule.ParameterNames.PackagesController_PreviewSearchService);
+                Assert.Equal(typeof(ISearchService), parameter.ParameterType);
+            }
         }
 
         public class TheCancelVerifyPackageAction
@@ -3716,6 +3752,13 @@ namespace NuGetGallery
         public class TheListPackagesMethod
             : TestContainer
         {
+            private readonly Cache _cache;
+
+            public TheListPackagesMethod()
+            {
+                _cache = new Cache();
+            }
+
             [Fact]
             public async Task TrimsSearchTerm()
             {
@@ -3769,6 +3812,83 @@ namespace NuGetGallery
                 var model = result.Model as PackageListViewModel;
                 Assert.Equal(prerel, model.IncludePrerelease);
                 searchService.Verify(x => x.Search(It.Is<SearchFilter>(f => f.IncludePrerelease == prerel)));
+            }
+
+            [Fact]
+            public async Task UsesPreviewSearchServiceWhenABTestServiceSaysSo()
+            {
+                var abTestService = new Mock<IABTestService>();
+                abTestService
+                    .Setup(x => x.IsPreviewSearchEnabled(It.IsAny<User>()))
+                    .Returns(true);
+
+                var searchService = new Mock<ISearchService>();
+                var previewSearchService = new Mock<ISearchService>();
+                previewSearchService
+                    .Setup(s => s.Search(It.IsAny<SearchFilter>()))
+                    .ReturnsAsync(() => new SearchResults(0, DateTime.UtcNow));
+                var controller = CreateController(
+                    GetConfigurationService(),
+                    searchService: searchService,
+                    previewSearchService: previewSearchService,
+                    abTestService: abTestService);
+                controller.SetCurrentUser(TestUtility.FakeUser);
+
+                var result = await controller.ListPackages(new PackageListSearchViewModel { Q = "test", Prerel = true });
+
+                searchService.Verify(x => x.Search(It.IsAny<SearchFilter>()), Times.Never);
+                previewSearchService.Verify(x => x.Search(It.IsAny<SearchFilter>()), Times.Once);
+                abTestService.Verify(
+                    x => x.IsPreviewSearchEnabled(TestUtility.FakeUser),
+                    Times.Once);
+            }
+
+            [Theory]
+            [InlineData(null)]
+            [InlineData("")]
+            [InlineData(" ")]
+            public async Task DoesNotUsePreviewSearchServiceForEmptyQuery(string q)
+            {
+                var abTestService = new Mock<IABTestService>();
+                abTestService
+                    .Setup(x => x.IsPreviewSearchEnabled(It.IsAny<User>()))
+                    .Returns(true);
+
+                var httpContext = new Mock<HttpContextBase>();
+                httpContext.Setup(c => c.Cache).Returns(_cache);
+
+                var searchService = new Mock<ISearchService>();
+                searchService
+                    .Setup(s => s.Search(It.IsAny<SearchFilter>()))
+                    .ReturnsAsync(() => new SearchResults(0, DateTime.UtcNow));
+                var previewSearchService = new Mock<ISearchService>();
+
+                var controller = CreateController(
+                    GetConfigurationService(),
+                    httpContext: httpContext,
+                    searchService: searchService,
+                    previewSearchService: previewSearchService,
+                    abTestService: abTestService);
+                controller.SetCurrentUser(TestUtility.FakeUser);
+
+                var result = await controller.ListPackages(new PackageListSearchViewModel { Q = q, Prerel = true });
+
+                searchService.Verify(x => x.Search(It.IsAny<SearchFilter>()), Times.Once);
+                previewSearchService.Verify(x => x.Search(It.IsAny<SearchFilter>()), Times.Never);
+                abTestService.Verify(
+                    x => x.IsPreviewSearchEnabled(TestUtility.FakeUser),
+                    Times.Never);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                // Clear the cache to avoid test interaction.
+                foreach (DictionaryEntry entry in _cache)
+                {
+                    _cache.Remove((string)entry.Key);
+                }
+
+                base.Dispose(disposing);
             }
         }
 

src/NuGetGallery/ViewModels/PackageListViewModel.cs
@@ -20,7 +20,8 @@ namespace NuGetGallery
             int pageIndex,
             int pageSize,
             UrlHelper url,
-            bool includePrerelease)
+            bool includePrerelease,
+            bool isPreviewSearch)
         {
             // TODO: Implement actual sorting
             IEnumerable<ListPackageItemViewModel> items = packages.ToList().Select(pv => new ListPackageItemViewModel(pv, currentUser));
@@ -41,6 +42,7 @@ namespace NuGetGallery
             LastResultIndex = FirstResultIndex + Items.Count() - 1;
             Pager = pager;
             IncludePrerelease = includePrerelease;
+            IsPreviewSearch = isPreviewSearch;
         }
 
         public int FirstResultIndex { get; }
@@ -62,5 +64,7 @@ namespace NuGetGallery
         public DateTime? IndexTimestampUtc { get; }
 
         public bool IncludePrerelease { get; }
+
+        public bool IsPreviewSearch { get; }
     }
 }

src/NuGetGallery/Views/Shared/ListPackages.cshtml
@@ -61,12 +61,15 @@
     }
 
     <div class="list-packages" role="list">
-        @{ var itemIndex = Model.PageIndex * Model.PageSize; }
+        @{
+            var itemIndex = Model.PageIndex * Model.PageSize;
+            var eventName = Model.IsPreviewSearch ? "preview-search-selection" : "search-selection";
+        }
         @foreach (var package in Model.Items)
         {
             // Only set an item index when there is a search query, for now.
             var thisItemIndex = string.IsNullOrWhiteSpace(Model.SearchTerm) ? null : (int?) itemIndex;
-            @Html.Partial("_ListPackage", package, new ViewDataDictionary { { "itemIndex", thisItemIndex } })
+            @Html.Partial("_ListPackage", package, new ViewDataDictionary { { "itemIndex", thisItemIndex }, { "eventName", eventName } })
             itemIndex++;
         }
     </div>
@@ -88,16 +91,18 @@
         var sincePageLoadCount = 0;
         @if (!string.IsNullOrWhiteSpace(Model.SearchTerm) && (Model.PageIndex == 0 || Model.Items.Count() > 0))
         {
+            var category = Model.IsPreviewSearch ? "preview-search-page" : "search-page";
             var action = Model.IncludePrerelease ? "search-prerel" : "search-stable";
             // Emit an event representing the search page and the page index. This make it easier for the search selection
             // event to be correlated in Google Analytics.
             <text>
-            window.nuget.sendAnalyticsEvent('search-page', '@action', @Html.Raw(Json.Encode(Model.SearchTerm)), @Model.PageIndex);
+            window.nuget.sendAnalyticsEvent('@category', '@action', @Html.Raw(Json.Encode(Model.SearchTerm)), @Model.PageIndex);
             window.nuget.sendAiMetric('BrowserSearchPage', @Model.PageIndex, {
                 SearchTerm: @Html.Raw(Json.Encode(Model.SearchTerm)),
                 IncludePrerelease: '@Model.IncludePrerelease',
                 PageIndex: @Model.PageIndex,
-                TotalCount: @Model.TotalCount
+                TotalCount: @Model.TotalCount,
+                IsPreviewSearch: '@Model.IsPreviewSearch'
             });
             </text>
         }
@@ -142,7 +147,8 @@
                         PackageVersion: data.packageVersion,
                         UseVersion: data.useVersion,
                         SincePageLoadMs: Date.now() - pageLoadTime,
-                        SincePageLoadCount: sincePageLoadCount
+                        SincePageLoadCount: sincePageLoadCount,
+                        IsPreviewSearch: '@Model.IsPreviewSearch'
                     });
 
                     sincePageLoadCount++;

commit dedea69a0761f34ed39d6b48a7a01ab99bbcfb70

[Hotfix] Fix bug causing TempData messages to not work (#7462)
Address https://github.com/NuGet/NuGetGallery/issues/7461
Regressed in https://github.com/NuGet/NuGetGallery/pull/7313

src/NuGetGallery/App_Start/DefaultDependenciesModule.cs
@@ -897,11 +897,10 @@ namespace NuGetGallery
                 .InstancePerLifetimeScope();
 
             builder
-                .RegisterType<PackagesController>()
-                .WithParameter(new ResolvedParameter(
-                    (pi, ctx) => pi.ParameterType == typeof(ISearchService) && pi.Name == ParameterNames.PackagesController_PreviewSearchService,
-                    (pi, ctx) => ctx.ResolveKeyed<ISearchService>(BindingKeys.PreviewSearchClient)))
-                .As<PackagesController>()
+                .Register(c => new SearchServiceFactory(
+                    c.Resolve<ISearchService>(),
+                    c.ResolveKeyed<ISearchService>(BindingKeys.PreviewSearchClient)))
+                .As<ISearchServiceFactory>()
                 .InstancePerLifetimeScope();
         }
 

src/NuGetGallery/Controllers/PackagesController.cs
@@ -89,8 +89,7 @@ namespace NuGetGallery
         private readonly IPackageService _packageService;
         private readonly IPackageUpdateService _packageUpdateService;
         private readonly IPackageFileService _packageFileService;
-        private readonly ISearchService _searchService;
-        private readonly ISearchService _previewSearchService;
+        private readonly ISearchServiceFactory _searchServiceFactory;
         private readonly IUploadFileService _uploadFileService;
         private readonly IUserService _userService;
         private readonly IEntitiesContext _entitiesContext;
@@ -127,8 +126,7 @@ namespace NuGetGallery
             IUploadFileService uploadFileService,
             IUserService userService,
             IMessageService messageService,
-            ISearchService searchService,
-            ISearchService previewSearchService,
+            ISearchServiceFactory searchServiceFactory,
             IPackageFileService packageFileService,
             IEntitiesContext entitiesContext,
             IAppConfiguration config,
@@ -159,8 +157,7 @@ namespace NuGetGallery
             _uploadFileService = uploadFileService;
             _userService = userService;
             _messageService = messageService;
-            _searchService = searchService ?? throw new ArgumentNullException(nameof(searchService));
-            _previewSearchService = previewSearchService ?? throw new ArgumentNullException(nameof(previewSearchService));
+            _searchServiceFactory = searchServiceFactory ?? throw new ArgumentNullException(nameof(searchServiceFactory));
             _packageFileService = packageFileService;
             _entitiesContext = entitiesContext;
             _config = config;
@@ -793,8 +790,9 @@ namespace NuGetGallery
                 }
             }
 
-            var externalSearchService = _searchService as ExternalSearchService;
-            if (_searchService.ContainsAllVersions && externalSearchService != null)
+            var searchService = _searchServiceFactory.GetService();
+            var externalSearchService = searchService as ExternalSearchService;
+            if (searchService.ContainsAllVersions && externalSearchService != null)
             {
                 var isIndexedCacheKey = $"IsIndexed_{package.PackageRegistration.Id}_{package.Version}";
                 var isIndexed = HttpContext.Cache.Get(isIndexedCacheKey) as bool?;
@@ -995,7 +993,7 @@ namespace NuGetGallery
             SearchResults results;
 
             var isPreviewSearchEnabled = _abTestService.IsPreviewSearchEnabled(GetCurrentUser());
-            var searchService = isPreviewSearchEnabled ? _previewSearchService : _searchService;
+            var searchService = isPreviewSearchEnabled ? _searchServiceFactory.GetPreviewService() : _searchServiceFactory.GetService();
 
             // fetch most common query from cache to relieve load on the search service
             if (string.IsNullOrEmpty(q) && page == 1 && includePrerelease)

src/NuGetGallery/Infrastructure/ISearchServiceFactory.cs
@@ -1,0 +1,11 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGetGallery
+{
+    public interface ISearchServiceFactory
+    {
+        ISearchService GetPreviewService();
+        ISearchService GetService();
+    }
+}

src/NuGetGallery/Infrastructure/SearchServiceFactory.cs
@@ -1,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace NuGetGallery
+{
+    public class SearchServiceFactory : ISearchServiceFactory
+    {
+        private readonly ISearchService _searchService;
+        private readonly ISearchService _previewSearchService;
+
+        public SearchServiceFactory(
+            ISearchService searchService,
+            ISearchService previewSearchService)
+        {
+            _searchService = searchService ?? throw new ArgumentNullException(nameof(searchService));
+            _previewSearchService = previewSearchService ?? throw new ArgumentNullException(nameof(previewSearchService));
+        }
+
+        public ISearchService GetService() => _searchService;
+        public ISearchService GetPreviewService() => _previewSearchService;
+    }
+}

src/NuGetGallery/NuGetGallery.csproj
@@ -224,6 +224,7 @@
     <Compile Include="Infrastructure\IABTestService.cs" />
     <Compile Include="Infrastructure\ILuceneDocumentFactory.cs" />
     <Compile Include="Infrastructure\ArrayModelBinder.cs" />
+    <Compile Include="Infrastructure\ISearchServiceFactory.cs" />
     <Compile Include="Infrastructure\LuceneDocumentFactory.cs" />
     <Compile Include="Infrastructure\IHijackSearchServiceFactory.cs" />
     <Compile Include="Infrastructure\Lucene\Correlation\CorrelatingHttpClientHandler.cs" />
@@ -236,6 +237,7 @@
     <Compile Include="Infrastructure\Lucene\ServiceResponse.cs" />
     <Compile Include="Infrastructure\Mail\Messages\SearchSideBySideMessage.cs" />
     <Compile Include="Infrastructure\HijackSearchServiceFactory.cs" />
+    <Compile Include="Infrastructure\SearchServiceFactory.cs" />
     <Compile Include="Migrations\201903020136235_CvesCanBeEmpty.cs" />
     <Compile Include="Migrations\201903020136235_CvesCanBeEmpty.Designer.cs">
       <DependentUpon>201903020136235_CvesCanBeEmpty.cs</DependentUpon>

tests/NuGetGallery.Facts/Controllers/PackagesControllerFacts.cs
@@ -92,8 +92,11 @@ namespace NuGetGallery
             messageService = messageService ?? new Mock<IMessageService>();
 
             searchService = searchService ?? new Mock<ISearchService>();
-
             previewSearchService = previewSearchService ?? new Mock<ISearchService>();
+
+            var searchServiceFactory = new Mock<ISearchServiceFactory>();
+            searchServiceFactory.Setup(x => x.GetService()).Returns(() => searchService.Object);
+            searchServiceFactory.Setup(x => x.GetPreviewService()).Returns(() => previewSearchService.Object);
 
             if (packageFileService == null)
             {
@@ -216,8 +219,7 @@ namespace NuGetGallery
                 uploadFileService.Object,
                 userService.Object,
                 messageService.Object,
-                searchService.Object,
-                previewSearchService.Object,
+                searchServiceFactory.Object,
                 packageFileService.Object,
                 entitiesContext.Object,
                 configurationService.Current,
@@ -330,30 +332,6 @@ namespace NuGetGallery
                     type, message: new PlainTextOnlyValidationMessage("Something"), warnings: null));
 
             return fakePackageUploadService;
-        }
-
-        public class TheConstructor
-            : TestContainer
-        {
-            /// <summary>
-            /// This test is to verify that the preview search service parameter name for the constructor has the
-            /// expected value. This is necessary because the dependency injection configuration uses the parameter
-            /// name to identify which search service should be preview implementation.
-            /// </summary>
-            [Fact]
-            public void HasPreviewSearchServiceParameter()
-            {
-                var type = typeof(PackagesController);
-
-                var constructors = type.GetConstructors();
-                var constructor = Assert.Single(constructors);
-
-                var parameters = constructor.GetParameters();
-                var parameter = Assert.Single(
-                    parameters,
-                    p => p.Name == DefaultDependenciesModule.ParameterNames.PackagesController_PreviewSearchService);
-                Assert.Equal(typeof(ISearchService), parameter.ParameterType);
-            }
         }
 
         public class TheCancelVerifyPackageAction

tests/NuGetGallery.Facts/Infrastructure/SearchServiceFactoryFacts.cs
@@ -1,0 +1,41 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Moq;
+using Xunit;
+
+namespace NuGetGallery
+{
+    public class SearchServiceFactoryFacts
+    {
+        public SearchServiceFactoryFacts()
+        {
+            SearchService = new Mock<ISearchService>();
+            PreviewSearchService = new Mock<ISearchService>();
+
+            Target = new SearchServiceFactory(
+                SearchService.Object,
+                PreviewSearchService.Object);
+        }
+
+        public Mock<ISearchService> SearchService { get; }
+        public Mock<ISearchService> PreviewSearchService { get; }
+        public SearchServiceFactory Target { get; }
+
+        [Fact]
+        public void GetServiceReturnsNonPreview()
+        {
+            var actual = Target.GetService();
+
+            Assert.Same(SearchService.Object, actual);
+        }
+
+        [Fact]
+        public void GetPreviewServiceReturnsPreview()
+        {
+            var actual = Target.GetPreviewService();
+
+            Assert.Same(PreviewSearchService.Object, actual);
+        }
+    }
+}

tests/NuGetGallery.Facts/NuGetGallery.Facts.csproj
@@ -93,6 +93,7 @@
     <Compile Include="Infrastructure\Lucene\GallerySearchServiceFacts.cs" />
     <Compile Include="Helpers\StreamHelperFacts.cs" />
     <Compile Include="Infrastructure\Mail\Messages\SearchSideBySideMessageFacts.cs" />
+    <Compile Include="Infrastructure\SearchServiceFactoryFacts.cs" />
     <Compile Include="Services\ConfigurationIconFileProviderFacts.cs" />
     <Compile Include="Services\AsynchronousDeleteAccountServiceFacts.cs" />
     <Compile Include="Services\FeatureFlagServiceFacts.cs" />