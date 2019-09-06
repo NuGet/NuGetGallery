d9aad5ce005858f4eaab7986cd1f02e0a4c384f8
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2019-09-06 14:17:17 +0200

    Use AppInsights instead of System.Diagnostics.Tracing in DiagnosticsService
    (cherry picked from commit 553c349c7d70054b8d67c8938d71d1b692e8e506)

---
+++ src/NuGetGallery.Services/Diagnostics/GalleryDiagnosticsSource.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Logging;
+
+namespace NuGetGallery.Diagnostics
+{
+    /// <summary>
+    /// Gallery diagnostics source. 
+    /// Trace events (including LogError extension) and Exception events are tracked in ApplicationInsights.
+    /// 
+    /// ILogger implementation based on https://github.com/aspnet/Logging/tree/master/src/Microsoft.Extensions.Logging.TraceSource 
+    /// </summary>
+    public sealed class GalleryDiagnosticsSource : IDiagnosticsSource
+    {
+        private readonly ITelemetryClient _telemetryClient;
+        private readonly string _name;
+
+        public GalleryDiagnosticsSource(string name, ITelemetryClient telemetryClient)
+        {
+            _name = name ?? throw new ArgumentNullException(nameof(name));
+            _telemetryClient = telemetryClient ?? throw new ArgumentNullException(nameof(telemetryClient));
+        }
+
+        /// <summary>
+        /// Write exception to ApplicationInsights.
+        /// </summary>
+        public void ExceptionEvent(Exception exception)
+        {
+            if (exception == null)
+            {
+                throw new ArgumentNullException(nameof(exception));
+            }
+
+            _telemetryClient.TrackException(exception);
+        }
+
+        /// <summary>
+        /// Write a trace event to ApplicationInsights.
+        /// </summary>
+        public void TraceEvent(
+            LogLevel logLevel,
+            EventId eventId,
+            string message,
+            [CallerMemberName] string member = null,
+            [CallerFilePath] string file = null,
+            [CallerLineNumber] int line = 0)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                throw new ArgumentException(
+                    string.Format(CultureInfo.CurrentCulture, ServicesStrings.ParameterCannotBeNullOrEmpty, nameof(message)),
+                    nameof(message));
+            }
+
+            _telemetryClient.TrackTrace(FormatMessage(message, member, file, line), logLevel, eventId);
+        }
+
+        public void PerfEvent(string name, TimeSpan time, IEnumerable<KeyValuePair<string, object>> payload)
+        {
+            // For now, hard-code the number of samples we track to 1000.
+            PerfCounters.AddSample(name, sampleSize: 1000, value: time.TotalMilliseconds);
+
+            // Send the event to the queue
+            MessageQueue.Enqueue(_name, new PerfEvent(name, DateTime.UtcNow, time, payload));
+        }
+
+        private static string FormatMessage(string message, string member, string file, int line)
+        {
+            return string.Format(CultureInfo.CurrentCulture, "[{0}:{1} in {2}] {3}", file, line, member, message);
+        }
+
+        void ILogger.Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var message = formatter(state, exception);
+            TraceEvent(logLevel, eventId, message);
+        }
+
+        bool ILogger.IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        IDisposable ILogger.BeginScope<TState>(TState state)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
