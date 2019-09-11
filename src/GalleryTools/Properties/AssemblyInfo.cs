64131f4f100af687b266f39474acebc7715dfb0a
Svetlana Kofman <skofman@microsoft.com>
2019-09-10 17:30:53 -0700

    Build&Sign config for GalleryTools

--- src/GalleryTools/Properties/AssemblyInfo.cs
+++ src/GalleryTools/Properties/AssemblyInfo.cs
@@ -31,3 +31,16 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-us")]
 
 [assembly: AssemblyMetadata("RepositoryUrl", "https://www.github.com/NuGet/NuGetGallery")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
