5e6c545c86e742142b972e1f09bb3ddece6df6d9
Daniel Jacinto <dajacint@microsoft.com>
2019-09-11 12:47:27 -0700

    Sql fix for AutocompleteDatabasePackage queries.

--- src/NuGetGallery/Queries/AutocompleteDatabasePackageIdsQuery.cs
+++ src/NuGetGallery/Queries/AutocompleteDatabasePackageIdsQuery.cs
@@ -1,33 +1,21 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
-using System.Globalization;
+using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace NuGetGallery
 {
-    public class AutocompleteDatabasePackageIdsQuery
-        : AutocompleteDatabaseQuery, IAutocompletePackageIdsQuery
+    public class AutocompleteDatabasePackageIdsQuery : IAutocompletePackageIdsQuery
     {
-        private const string _partialIdSqlFormat = @"SELECT TOP 30 pr.ID
-FROM Packages p (NOLOCK)
-    JOIN PackageRegistrations pr (NOLOCK) on pr.[Key] = p.PackageRegistrationKey
-WHERE {0} AND pr.ID LIKE {{0}}
-    {1}
-GROUP BY pr.ID
-ORDER BY pr.ID";
+        private readonly IEntitiesContext _entitiesContext;
 
-        private const string _noPartialIdSql = @"SELECT TOP 30 pr.ID
-FROM Packages p (NOLOCK)
-    JOIN PackageRegistrations pr (NOLOCK) on pr.[Key] = p.PackageRegistrationKey
-WHERE  {0} 
-GROUP BY pr.ID
-ORDER BY MAX(pr.DownloadCount) DESC";
-        
-        public AutocompleteDatabasePackageIdsQuery(IEntitiesContext entities)
-            : base(entities)
+        public AutocompleteDatabasePackageIdsQuery(IEntitiesContext entitiesContext)
         {
+            _entitiesContext = entitiesContext ?? throw new ArgumentNullException(nameof(entitiesContext));
         }
 
         public Task<IEnumerable<string>> Execute(
@@ -35,32 +23,42 @@ ORDER BY MAX(pr.DownloadCount) DESC";
             bool? includePrerelease = false,
             string semVerLevel = null)
         {
-            // Create SQL filter on SemVerLevel
-            // By default, we filter out SemVer v2.0.0 package versions.
-            var semVerLevelSqlFilter = "p.[SemVerLevelKey] IS NULL";
-            if (!string.IsNullOrEmpty(semVerLevel))
+            var query = _entitiesContext.Packages
+                .Include(p => p.PackageRegistration);
+            
+            // SemVerLevel filter
+            if (SemVerLevelKey.ForSemVerLevel(semVerLevel) == SemVerLevelKey.SemVer2)
             {
-                var semVerLevelKey = SemVerLevelKey.ForSemVerLevel(semVerLevel);
-                if (semVerLevelKey == SemVerLevelKey.SemVer2)
-                {
-                    semVerLevelSqlFilter = "p.[SemVerLevelKey] = " + SemVerLevelKey.SemVer2;
-                }
+                query = query.Where(p => p.SemVerLevelKey == SemVerLevelKey.SemVer2);
+            }
+            else
+            {
+                query = query.Where(p => !p.SemVerLevelKey.HasValue);
             }
 
-            if (string.IsNullOrWhiteSpace(partialId))
-            {
-                return RunSqlQuery(string.Format(CultureInfo.InvariantCulture, _noPartialIdSql, semVerLevelSqlFilter));
-            }
-
-            var prereleaseFilter = string.Empty;
+            // prerelease filter
             if (!includePrerelease.HasValue || !includePrerelease.Value)
             {
-                prereleaseFilter = "AND p.IsPrerelease = {1}";
+                query = query.Where(p => !p.IsPrerelease);
             }
 
-            var sql = string.Format(CultureInfo.InvariantCulture, _partialIdSqlFormat, semVerLevelSqlFilter, prereleaseFilter);
+            // filters added for partialId
+            if (!string.IsNullOrWhiteSpace(partialId))
+            {
+                query = query.Where(p => p.PackageRegistration.Id.StartsWith(partialId))
+                    .OrderBy(p => p.PackageRegistration.Id);
+            }
+            else
+            {
+                query = query.OrderByDescending(p => p.PackageRegistration.DownloadCount);
+            }
 
-            return RunSqlQuery(sql, partialId + "%", includePrerelease ?? false);
+            // last default filter
+            // this query returns 30 package ids at most
+            var queryResult = query.Take(30).GroupBy(p => p.PackageRegistration.Id).Select(group => group.Key);
+
+            // return the result of the query
+            return Task.FromResult(queryResult.AsEnumerable<string>());
         }
     }
 }
