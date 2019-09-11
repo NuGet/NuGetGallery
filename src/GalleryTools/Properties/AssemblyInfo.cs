03c044c0a98bb08c2b19134869ddd86698564adc
Svetlana Kofman <skofman@microsoft.com>
2019-09-11 13:58:56 -0700

    generate assembly info

--- src/GalleryTools/Properties/AssemblyInfo.cs
+++ src/GalleryTools/Properties/AssemblyInfo.cs
@@ -6,16 +6,12 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("GalleryTools")]
 [assembly: AssemblyCompany(".NET Foundation")]
 [assembly: AssemblyProduct("NuGet Services")]
 [assembly: AssemblyCopyright("\x00a9 .NET Foundation. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 
 #if !PORTABLE
 [assembly: ComVisible(false)]
@@ -30,17 +26,10 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-us")]
 
+[assembly: AssemblyDescription("Tools for performing manual one-time/maintenance operations on the Gallery")]
+
+// The build will automatically inject the following attributes:
+// AssemblyVersion, AssemblyFileVersion, AssemblyInformationalVersion, AssemblyMetadata (for Branch, CommitId, and BuildDateUtc)
+
 [assembly: AssemblyMetadata("RepositoryUrl", "https://www.github.com/NuGet/NuGetGallery")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
