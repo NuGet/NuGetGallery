f5c510eedd2c505c9e5a60ca2c294f46754c1e9b
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2019-09-25 08:41:30 +0200

    Add AppInsights heartbeat support (#308)
    * Upgrade Microsoft.ApplicationInsights v2.3.0 to v2.10.0
    
    * Add AppInsights heartbeat support
    
    * PR feedback

--- src/NuGet.Services.Logging/ApplicationInsights.cs
+++ src/NuGet.Services.Logging/ApplicationInsights.cs
@@ -1,20 +1,47 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Diagnostics;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing;
 
 namespace NuGet.Services.Logging
 {
     public static class ApplicationInsights
     {
+        public static IHeartbeatPropertyManager HeartbeatManager { get; private set; }
+
         public static bool Initialized { get; private set; }
 
         public static void Initialize(string instrumentationKey)
+        {
+            InitializeTelemetryConfiguration(instrumentationKey, heartbeatInterval: null);
+        }
+
+        public static void Initialize(string instrumentationKey, TimeSpan heartbeatInterval)
+        {
+            InitializeTelemetryConfiguration(instrumentationKey, heartbeatInterval);
+        }
+
+        private static void InitializeTelemetryConfiguration(string instrumentationKey, TimeSpan? heartbeatInterval)
         {
             if (!string.IsNullOrWhiteSpace(instrumentationKey))
             {
                 TelemetryConfiguration.Active.InstrumentationKey = instrumentationKey;
                 TelemetryConfiguration.Active.TelemetryInitializers.Add(new TelemetryContextInitializer());
+
+                // Configure heartbeat interval if specified.
+                // When not defined or null, the DiagnosticsTelemetryModule will use its internal defaults (heartbeat enabled, interval of 15 minutes).
+                if (heartbeatInterval.HasValue)
+                {
+                    var heartbeatManager = GetHeartbeatPropertyManager();
+                    if (heartbeatManager != null)
+                    {
+                        heartbeatManager.HeartbeatInterval = heartbeatInterval.Value;
+                    }
+                }
 
                 Initialized = true;
             }
@@ -22,6 +49,38 @@ namespace NuGet.Services.Logging
             {
                 Initialized = false;
             }
+        }
+
+        private static IHeartbeatPropertyManager GetHeartbeatPropertyManager()
+        {
+            if (HeartbeatManager == null)
+            {
+                var telemetryModules = TelemetryModules.Instance;
+
+                try
+                {
+                    foreach (var module in telemetryModules.Modules)
+                    {
+                        if (module is IHeartbeatPropertyManager heartbeatManager)
+                        {
+                            HeartbeatManager = heartbeatManager;
+                        }
+                    }
+                }
+                catch (Exception hearbeatManagerAccessException)
+                {
+                    // An non-critical, unexpected exception occurred trying to access the heartbeat manager.
+                    Trace.TraceError($"There was an error accessing heartbeat manager. Details: {hearbeatManagerAccessException.ToInvariantString()}");
+                }
+
+                if (HeartbeatManager == null)
+                {
+                    // Heartbeat manager unavailable: log warning.
+                    Trace.TraceWarning("Heartbeat manager unavailable");
+                }
+            }
+
+            return HeartbeatManager;
         }
     }
 }
