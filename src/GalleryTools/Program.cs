facde01f8926c841d7aef10c4807078785372dd3
Scott Bommarito <scottbom@microsoft.com>
2019-10-01 16:52:58 -0700

    remove unused refs

--- src/GalleryTools/Program.cs
+++ src/GalleryTools/Program.cs
@@ -2,9 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Data.SqlClient;
 using GalleryTools.Commands;
-using GalleryTools.Utils;
 using Microsoft.Extensions.CommandLineUtils;
 
 namespace GalleryTools
