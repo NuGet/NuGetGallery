e1b5f0ce2c51818a84fb90bd3a33a3ae73ee582c
zhhyu <Zhaohua.Yu@microsoft.com>
2019-10-01 16:54:20 -0700

    Revert "Upgrade to AppInsights v2.10 (#7499)"
    This reverts commit d199584d61d92757081f2f4be3fe39d6aa65860d.

--- tests/NuGetGallery.Facts/Telemetry/DeploymentIdTelemetryEnricherFacts.cs
+++ tests/NuGetGallery.Facts/Telemetry/DeploymentIdTelemetryEnricherFacts.cs
@@ -1,15 +1,12 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
-using Microsoft.ApplicationInsights.Extensibility;
 using Moq;
 using Xunit;
 
-namespace NuGetGallery.Telemetry
+namespace NuGetGallery
 {
     public class DeploymentIdTelemetryEnricherFacts
     {
@@ -17,12 +14,13 @@ namespace NuGetGallery.Telemetry
 
         public DeploymentIdTelemetryEnricherFacts()
         {
-            Telemetry = new TestableTelemetry();
+            Telemetry = new Mock<ITelemetry>();
+            Telemetry.Setup(x => x.Context).Returns(new TelemetryContext());
             Target = new Mock<TestableDeploymentIdTelemetryEnricher>();
             Target.Object.SetDeploymentId("TheDeploymentId");
         }
 
-        public TestableTelemetry Telemetry { get; }
+        public Mock<ITelemetry> Telemetry { get; }
         public Mock<TestableDeploymentIdTelemetryEnricher> Target { get; }
 
         [Fact]
@@ -31,7 +29,7 @@ namespace NuGetGallery.Telemetry
             Target.Object.Initialize(telemetry: null);
 
             Target.Verify(x => x.DeploymentId, Times.Never);
-            Assert.DoesNotContain(PropertyKey, Telemetry.Properties.Keys);
+            Assert.DoesNotContain(PropertyKey, Telemetry.Object.Context.Properties.Keys);
         }
 
         [Fact]
@@ -39,29 +37,29 @@ namespace NuGetGallery.Telemetry
         {
             Target.Object.SetDeploymentId(null);
 
-            Target.Object.Initialize(Telemetry);
+            Target.Object.Initialize(Telemetry.Object);
 
             Target.Verify(x => x.DeploymentId, Times.Never);
-            Assert.DoesNotContain(PropertyKey, Telemetry.Properties.Keys);
+            Assert.DoesNotContain(PropertyKey, Telemetry.Object.Context.Properties.Keys);
         }
 
         [Fact]
         public void DoesNothingWhenDeploymentIdIsAlreadySet()
         {
-            Telemetry.Properties[PropertyKey] = "something else";
+            Telemetry.Object.Context.Properties[PropertyKey] = "something else";
 
-            Target.Object.Initialize(Telemetry);
+            Target.Object.Initialize(Telemetry.Object);
 
             Target.Verify(x => x.DeploymentId, Times.Never);
-            Assert.Equal(Telemetry.Properties[PropertyKey], "something else");
+            Assert.Equal(Telemetry.Object.Context.Properties[PropertyKey], "something else");
         }
 
         [Fact]
         public void SetsDeploymentId()
         {
-            Target.Object.Initialize(Telemetry);
+            Target.Object.Initialize(Telemetry.Object);
 
-            Assert.Equal(Telemetry.Properties[PropertyKey], "TheDeploymentId");
+            Assert.Equal(Telemetry.Object.Context.Properties[PropertyKey], "TheDeploymentId");
         }
 
         public class TestableDeploymentIdTelemetryEnricher : DeploymentIdTelemetryEnricher
@@ -74,35 +72,6 @@ namespace NuGetGallery.Telemetry
             }
 
             internal override string DeploymentId => _deploymentId;
-        }
-
-        public class TestableTelemetry : ITelemetry, ISupportProperties
-        {
-            private readonly IDictionary<string, string>  _properties = new Dictionary<string, string>();
-
-            public DateTimeOffset Timestamp { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-            public TelemetryContext Context => throw new NotImplementedException();
-
-            public IExtension Extension { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-            public string Sequence { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-            public IDictionary<string, string> Properties => _properties;
-
-            public ITelemetry DeepClone()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Sanitize()
-            {
-                throw new NotImplementedException();
-            }
-
-            public void SerializeData(ISerializationWriter serializationWriter)
-            {
-                throw new NotImplementedException();
-            }
         }
     }
 }
