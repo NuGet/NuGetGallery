48d78e7b3fe940fec43377eae6b771097faebe21
Joel Verhagen <jver@microsoft.com>
2019-10-15 17:02:48 -0700

    Add lease service to allow pessimistic concurrency control (#829)
    Progress on https://github.com/NuGet/NuGetGallery/issues/7590

---
+++ src/Validation.Common.Job/Leases/CloudBlobLeaseService.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace NuGet.Jobs.Validation.Leases
+{
+    /// <summary>
+    /// This implementation uses Azure Blob leases which have a minimum lease duration of 15 seconds and a maximum
+    /// duration of 60 seconds. For more information about Azure Storage blob leasing, refer to the documentation:
+    /// https://docs.microsoft.com/en-us/rest/api/storageservices/lease-blob
+    /// </summary>
+    public class CloudBlobLeaseService : ILeaseService
+    {
+        private static readonly TimeSpan MinLeaseTime = TimeSpan.FromSeconds(15);
+        private static readonly TimeSpan MaxLeaseTime = TimeSpan.FromSeconds(60);
+
+        private readonly CloudBlobClient _cloudBlobClient;
+        private readonly string _containerName;
+        private readonly string _basePath;
+
+        public CloudBlobLeaseService(CloudBlobClient cloudBlobClient, string containerName, string basePath)
+        {
+            _cloudBlobClient = cloudBlobClient ?? throw new ArgumentNullException(nameof(cloudBlobClient));
+            _containerName = containerName ?? throw new ArgumentNullException(nameof(containerName));
+            _basePath = string.IsNullOrEmpty(basePath) ? string.Empty : basePath.TrimEnd('/') + '/';
+        }
+
+        public async Task<LeaseResult> TryAcquireAsync(string resourceName, TimeSpan leaseTime, CancellationToken cancellationToken)
+        {
+            var blob = GetBlob(resourceName);
+            try
+            {
+                return await TryAcquireAsync(blob, leaseTime, cancellationToken);
+            }
+            catch (StorageException ex) when (ex.RequestInformation?.HttpStatusCode == (int)HttpStatusCode.NotFound)
+            {
+                // The lease file does not exist. Try to create it and lease it.
+                return await TryCreateAndAcquireAsync(blob, leaseTime, cancellationToken);
+            }
+        }
+
+        private CloudBlockBlob GetBlob(string resourceName)
+        {
+            var container = _cloudBlobClient.GetContainerReference(_containerName);
+            return container.GetBlockBlobReference($"{_basePath}{resourceName}");
+        }
+
+        public async Task<bool> ReleaseAsync(string resourceName, string leaseId, CancellationToken cancellationToken)
+        {
+            var blob = GetBlob(resourceName);
+            try
+            {
+                await blob.ReleaseLeaseAsync(
+                    AccessCondition.GenerateLeaseCondition(leaseId),
+                    cancellationToken);
+                return true;
+            }
+            catch (StorageException ex) when (ex.RequestInformation?.HttpStatusCode == (int)HttpStatusCode.Conflict)
+            {
+                return false;
+            }
+        }
+
+        private async Task<LeaseResult> TryCreateAndAcquireAsync(CloudBlockBlob blob, TimeSpan leaseTime, CancellationToken cancellationToken)
+        {
+            try
+            {
+                // Use an empty blob for the lease blob. The contents are not important. Only the lease state (managed
+                // by Azure Blob Storage) is important.
+                await blob.UploadFromByteArrayAsync(
+                    Array.Empty<byte>(),
+                    index: 0,
+                    count: 0,
+                    cancellationToken: cancellationToken);
+
+                return await TryAcquireAsync(blob, leaseTime, cancellationToken);
+            }
+            catch (StorageException ex) when (ex.RequestInformation?.HttpStatusCode == (int)HttpStatusCode.PreconditionFailed)
+            {
+                // The file has already created and leased by someone else.
+                return LeaseResult.Failure();
+            }
+        }
+
+        private async Task<LeaseResult> TryAcquireAsync(CloudBlockBlob blob, TimeSpan leaseTime, CancellationToken cancellationToken)
+        {
+            if (leaseTime < MinLeaseTime || leaseTime > MaxLeaseTime)
+            {
+                throw new ArgumentOutOfRangeException(
+                    nameof(leaseTime),
+                    "The lease time must be between 15 and 60 seconds, inclusive.");
+            }
+
+            try
+            {
+                var leaseId = await blob.AcquireLeaseAsync(
+                    leaseTime: leaseTime,
+                    proposedLeaseId: null,
+                    cancellationToken: cancellationToken);
+
+                return LeaseResult.Success(leaseId);
+            }
+            catch (StorageException ex) when (ex.RequestInformation?.HttpStatusCode == (int)HttpStatusCode.Conflict)
+            {
+                // The lease has already been acquired by someone else.
+                return LeaseResult.Failure();
+            }
+        }
+    }
+}
---
+++ tests/Validation.Common.Job.Tests/Leases/BlobStorageFixture.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Validation.Common.Job.Tests.Leases
+{
+    public class BlobStorageFixture : IDisposable
+    {
+        private const string ConnectionStringName = "TestStorageConnectionString";
+
+        public static string SkipReason
+        {
+            get
+            {
+                if (GetEnvironmentVariable(ConnectionStringName, required: false) == null)
+                {
+                    return $"The {ConnectionStringName} environment variable needs to be defined to run Azure Blob " +
+                        $"Storage integration tests.";
+                }
+
+                return null;
+            }
+        }
+
+        public BlobStorageFixture()
+        {
+            TestRunId = Guid.NewGuid().ToString();
+            ConnectionString = GetEnvironmentVariable(ConnectionStringName, required: true);
+
+            GetContainerReference().CreateIfNotExists();
+        }
+
+        public string TestRunId { get; }
+        public string ConnectionString { get; }
+
+        public void Dispose()
+        {
+            GetContainerReference().DeleteIfExists();
+        }
+
+        private CloudBlobContainer GetContainerReference()
+        {
+            var account = CloudStorageAccount.Parse(ConnectionString);
+            var blobClient = account.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference(TestRunId);
+            return container;
+        }
+
+        private static string GetEnvironmentVariable(string name, bool required)
+        {
+            var value = Environment.GetEnvironmentVariable(name);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                if (required)
+                {
+                    throw new InvalidOperationException($"The environment variable '{name}' must be defined.");
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            return value;
+        }
+    }
+}
