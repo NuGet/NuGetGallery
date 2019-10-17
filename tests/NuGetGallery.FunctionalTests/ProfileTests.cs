55344e83447e1fa9834892644b75866e589efb3a
Loic Sharma <sharma.loic@gmail.com>
2019-10-17 16:17:46 -0700

    Add hard tests

---
+++ tests/NuGetGallery.FunctionalTests/ProfileTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NuGetGallery.FunctionalTests
+{
+    public class ProfileTests : GalleryTestBase
+    {
+        public ProfileTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+        {
+        }
+
+        [Fact]
+        [Description("Load a profile's avatar")]
+        [Priority(1)]
+        [Category("P1Tests")]
+        public async Task GetProfileAvatarAsync()
+        {
+            // Act
+            using (var httpClient = new HttpClient())
+            using (var response = await httpClient.GetAsync(UrlHelper.GetAvatarUrl("microsoft")))
+            {
+                // Assert
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+    }
+}
