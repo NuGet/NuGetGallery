5574b248ceeeda27b54ea57eb05605f8345eaa29
Scott Bommarito <scottbom@microsoft.com>
2019-10-31 10:06:02 -0700

    .NET Foundation
    Co-Authored-By: Xavier Decoster <xavierdecoster@users.noreply.github.com>

--- src/GitHubVulnerabilities2Db/Properties/AssemblyInfo.cs
+++ src/GitHubVulnerabilities2Db/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("GitHubVulnerabilities2Db")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany(".NET Foundation")]
 [assembly: AssemblyProduct("GitHubVulnerabilities2Db")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
