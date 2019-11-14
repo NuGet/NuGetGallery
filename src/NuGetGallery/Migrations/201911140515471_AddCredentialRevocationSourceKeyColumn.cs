9d400d4cb4e9d84aeb6b416fa14cafe299d9c1c2
zhhyu <Zhaohua.Yu@microsoft.com>
2019-11-13 21:32:21 -0800

    Add Migration File

---
+++ src/NuGetGallery/Migrations/201911140515471_AddCredentialRevocationSourceKeyColumn.cs
@@ -0,0 +1,18 @@
+﻿namespace NuGetGallery.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCredentialRevocationSourceKeyColumn : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Credentials", "RevocationSourceKey", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Credentials", "RevocationSourceKey");
+        }
+    }
+}
