ff9d3edaa16c8bf68a4eee9ef451425b45989220
Andrei Grigorev <angrigor@microsoft.com>
2019-11-19 17:09:40 -0800

    Dropped the log flush job.

--- src/NuGetGallery/App_Start/AppActivator.cs
+++ src/NuGetGallery/App_Start/AppActivator.cs
@@ -250,11 +250,6 @@ namespace NuGetGallery
                 indexingJobFactory.RegisterBackgroundJobs(jobs, configuration);
             }
 
-            if (configuration.CollectPerfLogs)
-            {
-                jobs.Add(CreateLogFlushJob());
-            }
-
             if (configuration.StorageType == StorageType.AzureStorage)
             {
                 var cloudDownloadCountService = DependencyResolver.Current.GetService<IDownloadCountService>() as CloudDownloadCountService;
@@ -276,31 +271,6 @@ namespace NuGetGallery
                 _jobManager.Fail(e => ErrorLog.GetDefault(null).Log(new Error(e)));
                 _jobManager.Start();
             }
-        }
-
-        private static ProcessPerfEvents CreateLogFlushJob()
-        {
-            var logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", "Logs");
-            try
-            {
-                if (RoleEnvironment.IsAvailable)
-                {
-                    var resource = RoleEnvironment.GetLocalResource("Logs");
-                    if (resource != null)
-                    {
-                        logDirectory = Path.Combine(resource.RootPath);
-                    }
-                }
-            }
-            catch
-            {
-                // Meh, so Azure isn't available...
-            }
-            return new ProcessPerfEvents(
-                TimeSpan.FromSeconds(10),
-                logDirectory,
-                new[] { "ExternalSearchService" },
-                timeout: TimeSpan.FromSeconds(10));
         }
 
         private static void BackgroundJobsStop()
