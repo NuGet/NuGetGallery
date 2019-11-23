7f8dda3b2f098a65d02c3921cde7b10a6d0baf20
Andrei Grigorev <angrigor@microsoft.com>
2019-11-22 16:54:23 -0800

    Deployment label logging.

---
+++ src/NuGetGallery/Telemetry/DeploymentLabelEnricher.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using NuGetGallery.Configuration;
+
+namespace NuGetGallery
+{
+    public class DeploymentLabelEnricher : ITelemetryInitializer
+    {
+        private const string DeploymentLabel = "DeploymentLabel";
+        private readonly string _deploymentLabel;
+
+        public DeploymentLabelEnricher(IAppConfiguration appConfiguration)
+        {
+            _deploymentLabel = appConfiguration.DeploymentLabel;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (_deploymentLabel == null)
+            {
+                return;
+            }
+
+            if (!(telemetry is ISupportProperties itemTelemetry))
+            {
+                return;
+            }
+
+            if (itemTelemetry.Properties.ContainsKey(DeploymentLabel))
+            {
+                return;
+            }
+
+            itemTelemetry.Properties.Add(DeploymentLabel, _deploymentLabel);
+        }
+    }
+}
