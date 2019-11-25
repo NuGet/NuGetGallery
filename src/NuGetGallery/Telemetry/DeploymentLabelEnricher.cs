55b6891ebc31bcbde4ddc55d70b1efce3c44e9ff
Andrei Grigorev <angrigor@microsoft.com>
2019-11-25 15:07:54 -0800

    null check.

--- src/NuGetGallery/Telemetry/DeploymentLabelEnricher.cs
+++ src/NuGetGallery/Telemetry/DeploymentLabelEnricher.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
@@ -15,6 +16,11 @@ namespace NuGetGallery
 
         public DeploymentLabelEnricher(IAppConfiguration appConfiguration)
         {
+            if (appConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(appConfiguration));
+            }
+            
             _deploymentLabel = appConfiguration.DeploymentLabel;
         }
 
