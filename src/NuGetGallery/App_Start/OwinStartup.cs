7e4caf76b1eda1d439da1df788108e8aa177aadb
Andrei Grigorev <angrigor@microsoft.com>
2019-11-25 13:29:55 -0800

    Conditional adding of the `DeploymentLabelEnricher`.

--- src/NuGetGallery/App_Start/OwinStartup.cs
+++ src/NuGetGallery/App_Start/OwinStartup.cs
@@ -96,7 +96,10 @@ namespace NuGetGallery
 
                 // Add enrichers
                 TelemetryConfiguration.Active.TelemetryInitializers.Add(new DeploymentIdTelemetryEnricher());
-                TelemetryConfiguration.Active.TelemetryInitializers.Add(new DeploymentLabelEnricher(config.Current));
+                if (config.Current.DeploymentLabel != null)
+                {
+                    TelemetryConfiguration.Active.TelemetryInitializers.Add(new DeploymentLabelEnricher(config.Current));
+                }
                 TelemetryConfiguration.Active.TelemetryInitializers.Add(new ClientInformationTelemetryEnricher());
 
                 var telemetryProcessorChainBuilder = TelemetryConfiguration.Active.TelemetryProcessorChainBuilder;
