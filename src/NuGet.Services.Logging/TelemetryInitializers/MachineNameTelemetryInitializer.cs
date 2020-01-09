d74bea9921e980dcc7d93c20a8b0951a12cdfde1
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2020-01-09 17:24:18 +0100

    Port MachineNameTelemetryInitializer from NuGet.ApplicationInsights.Owin (#330)

---
+++ src/NuGet.Services.Logging/TelemetryInitializers/MachineNameTelemetryInitializer.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace NuGet.Services.Logging
+{
+    public class MachineNameTelemetryInitializer
+        : SupportPropertiesTelemetryInitializer
+    {
+        public MachineNameTelemetryInitializer()
+            : base("MachineName", TryGetMachineName())
+        {
+        }
+
+        private static string TryGetMachineName()
+        {
+            try
+            {
+                return Environment.MachineName;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
