NuGet/NuGet.Jobs

Add SplitLargeFiles tool to split big log files (#867)

src/SplitLargeFiles/FileProperties.cs
@@ -1,0 +1,178 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.GZip;
+
+namespace NuGet.Jobs.SplitLargeFiles
+{
+    public class FileProperties
+    {
+        private const int BufferSize = 16 * 1024;
+
+        public FileProperties(string path, long size, bool isGzipped, Encoding encoding, int lineCount)
+        {
+            Path = path;
+            Size = size;
+            IsGzipped = isGzipped;
+            Encoding = encoding;
+            LineCount = lineCount;
+        }
+
+        public string Path { get; }
+        public long Size { get; }
+        public bool IsGzipped { get; }
+        public Encoding Encoding { get; }
+        public int LineCount { get; }
+
+        public static FileProperties FromFileStream(FileStream stream)
+        {
+            var path = stream.Name;
+            var size = stream.Length;
+            var isGzipped = DetectGzip(stream);
+            var encoding = DetectEncoding(isGzipped, stream);
+            var lineCount = CountLines(isGzipped, encoding, stream);
+
+            return new FileProperties(
+                path,
+                size,
+                isGzipped,
+                encoding,
+                lineCount);
+        }
+
+        private static byte[] GetLeadingBytes(Stream stream, int count)
+        {
+            var initialPosition = stream.Position;
+            stream.Position = 0;
+            var buffer = new byte[count];
+            var read = stream.Read(buffer, 0, buffer.Length);
+            var bytes = new byte[read];
+            Buffer.BlockCopy(buffer, 0, bytes, 0, read);
+            stream.Position = initialPosition;
+            return bytes;
+        }
+
+        private static bool DetectGzip(Stream stream)
+        {
+            var header = GetLeadingBytes(stream, 2);
+            if (header.Length < 2 || header[0] != 0x1f || header[1] != 0x8b)
+            {
+                return false;
+            }
+
+            var initialPosition = stream.Position;
+            stream.Position = 0;
+            try
+            {
+                using (var gzipStream = new GZipInputStream(stream) { IsStreamOwner = false })
+                {
+                    var buffer = new byte[128];
+                    gzipStream.Read(buffer, 0, buffer.Length);
+                }
+            }
+            catch (InvalidDataException)
+            {
+                return false;
+            }
+            stream.Position = initialPosition;
+
+            return true;
+        }
+
+        private static Encoding DetectEncoding(bool isGzipped, Stream stream)
+        {
+            return ProcesStream(
+                isGzipped,
+                stream,
+                readStream =>
+                {
+                    using (var reader = new StreamReader(
+                        readStream,
+                        Encoding.UTF8,
+                        detectEncodingFromByteOrderMarks: true,
+                        bufferSize: BufferSize,
+                        leaveOpen: true))
+                    {
+                        reader.Peek();
+                        return reader.CurrentEncoding;
+                    }
+                });
+        }
+
+        private static int CountLines(bool isGzipped, Encoding encoding, Stream stream)
+        {
+            return ProcesStream(
+                isGzipped,
+                stream,
+                readStream =>
+                {
+                    Console.Write("Counting lines...");
+                    var lineCount = 0;
+                    using (var reader = new StreamReader(
+                        readStream,
+                        encoding,
+                        detectEncodingFromByteOrderMarks: false,
+                        bufferSize: BufferSize,
+                        leaveOpen: true))
+                    {
+                        string line;
+                        var stopwatch = Stopwatch.StartNew();
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            lineCount++;
+                            if (lineCount % 1_000_000 == 0)
+                            {
+                                Console.WriteLine();
+                                var percent = 1.0 * reader.BaseStream.Position / stream.Length;
+                                var totalTimeEst = stopwatch.ElapsedTicks / percent;
+                                var remainingTicks = (long)((1 - percent) * totalTimeEst);
+                                var remainingMins = TimeSpan.FromTicks(remainingTicks).TotalMinutes;
+                                Console.Write($"[{percent:P} - ETA {remainingMins:F2}m] {lineCount} lines");
+                            }
+                            else if (lineCount % 25_000 == 0)
+                            {
+                                Console.Write(".");
+                            }
+                        }
+                    }
+
+                    Console.WriteLine();
+                    return lineCount;
+                });
+            
+        }
+
+        private static T ProcesStream<T>(bool isGzipped, Stream stream, Func<Stream, T> processAsync)
+        {
+            var initialPosition = stream.Position;
+            stream.Position = 0;
+
+            var readStream = stream;
+            if (isGzipped)
+            {
+                readStream = new GZipInputStream(stream) { IsStreamOwner = false };
+            }
+
+            var output = default(T);
+            try
+            {
+                output = processAsync(readStream);
+            }
+            finally
+            {
+                if (isGzipped)
+                {
+                    readStream.Dispose();
+                }
+            }
+
+            stream.Position = initialPosition;
+            return output;
+        }
+    }
+}

src/SplitLargeFiles/Program.cs
@@ -1,0 +1,150 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using ICSharpCode.SharpZipLib.GZip;
+
+namespace NuGet.Jobs.SplitLargeFiles
+{
+    public class Program
+    {
+        private const string GzipExtension = ".gz";
+
+        public static int Main(string[] args)
+        {
+            var desiredFileSize = 20 * 1024 * 1024;
+            var fileIndexFormat = "_{0:D4}";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("The first parameter should be the path to the large file to split.");
+                Console.WriteLine($"The optional second parameter should be the desired file size in bytes. Default is {desiredFileSize} bytes.");
+                Console.WriteLine($"The optional third parameter is the file index suffix. Default is '{fileIndexFormat}'.");
+                return 1;
+            }
+
+            var inputPath = args[0];
+            
+            if (args.Length > 1)
+            {
+                desiredFileSize = int.Parse(args[1]);
+            }
+
+            if (args.Length > 2)
+            {
+                fileIndexFormat = args[2];
+            }
+
+            Console.WriteLine("Input parameters:");
+            Console.WriteLine();
+            Console.WriteLine($"  Input path: {inputPath}");
+            Console.WriteLine($"  Desired output file size (bytes): {desiredFileSize}");
+            Console.WriteLine($"  File Index Format: {fileIndexFormat} (ex: '{string.Format(fileIndexFormat, 0)}')");
+            Console.WriteLine();
+
+            if (!File.Exists(inputPath))
+            {
+                Console.WriteLine("The input file does not exist.");
+                return 1;
+            }
+
+            using (var inputStream = new FileStream(inputPath, FileMode.Open))
+            {
+
+                Console.WriteLine("Gathering data about the input file...");
+                var properties = FileProperties.FromFileStream(inputStream);
+
+                // Generate the output path format.
+                var outputPathFormat = GenerateOutputPathFormat(inputPath, fileIndexFormat, properties.IsGzipped);
+
+                // This is a rough estimate assuming that each line compresses to the same length.
+                var fileCount = properties.Size / desiredFileSize;
+                if (fileCount == 0)
+                {
+                    fileCount = 1;
+                }
+                var linesPerFile = properties.LineCount / fileCount;
+
+                Console.WriteLine();
+                Console.WriteLine($"  Output path format: {outputPathFormat}");
+                Console.WriteLine();
+                Console.WriteLine($"  Input File encoding: {properties.Encoding}");
+                Console.WriteLine($"  Is gzipped: {properties.IsGzipped}");
+                Console.WriteLine($"  Input file size (bytes): {properties.Size}");
+                Console.WriteLine($"  Input file line count: {properties.LineCount}");
+                Console.WriteLine();
+                Console.WriteLine($"  Output file count: {fileCount}");
+                Console.WriteLine($"  Output file line count: {linesPerFile}");
+                Console.WriteLine();
+
+                Console.WriteLine("Splitting up files...");
+
+                Stream readStream = inputStream;
+                if (properties.IsGzipped)
+                {
+                    readStream = new GZipInputStream(inputStream);
+                }
+
+                using (var reader = new StreamReader(readStream, properties.Encoding))
+                {
+                    for (var fileIndex = 0; fileIndex < fileCount; fileIndex++)
+                    {
+                        var isLastFile = fileIndex >= fileCount - 1;
+                        var outputPath = string.Format(outputPathFormat, fileIndex);
+
+                        Console.Write($"[{fileIndex + 1}/{fileCount}] Writing {outputPath}...");
+                        WriteOutputFile(properties, reader, linesPerFile, outputPath, isLastFile);
+                        Console.WriteLine(" done.");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The file has been split up.");
+
+            return 0;
+        }
+
+        private static string GenerateOutputPathFormat(string inputPath, string fileIndexFormat, bool isGzipped)
+        {
+            var directory = Path.GetDirectoryName(inputPath);
+            var fileName = Path.GetFileName(inputPath);
+
+            var gzipSuffix = string.Empty;
+            if (isGzipped && inputPath.EndsWith(GzipExtension, StringComparison.OrdinalIgnoreCase))
+            {
+                gzipSuffix = Path.GetExtension(fileName);
+                fileName = Path.GetFileNameWithoutExtension(fileName);
+            }
+
+            var withoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            var extension = Path.GetExtension(fileName);
+
+            return Path.Combine(directory, withoutExtension + fileIndexFormat + extension + gzipSuffix);
+        }
+
+        private static void WriteOutputFile(FileProperties properties, StreamReader reader, long linesPerFile, string outputPath, bool isLastFile)
+        {
+            using (var outputStream = new FileStream(outputPath, FileMode.Create))
+            {
+                Stream writeStream = outputStream;
+                if (properties.IsGzipped)
+                {
+                    writeStream = new GZipOutputStream(writeStream);
+                }
+
+                var lineIndex = 0;
+                using (var writer = new StreamWriter(writeStream, properties.Encoding))
+                {
+                    string line;
+                    while (((!isLastFile && lineIndex < linesPerFile) || isLastFile) &&
+                           (line = reader.ReadLine()) != null)
+                    {
+                        lineIndex++;
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}

src/SplitLargeFiles/Properties/AssemblyInfo.cs
@@ -1,0 +1,9 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Jobs.SplitLargeFiles")]
+[assembly: ComVisible(false)]
+[assembly: Guid("afb33a62-c437-4c32-a965-d6cc5ee50540")]

NuGet/NuGetGallery

src/SplitLargeFiles/FileProperties.cs
@@ -1,0 +1,177 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using ICSharpCode.SharpZipLib.GZip;
+
+namespace NuGet.Tools.SplitLargeFiles
+{
+    public class FileProperties
+    {
+        private const int BufferSize = 16 * 1024;
+
+        public FileProperties(string path, long size, bool isGzipped, Encoding encoding, int lineCount)
+        {
+            Path = path;
+            Size = size;
+            IsGzipped = isGzipped;
+            Encoding = encoding;
+            LineCount = lineCount;
+        }
+
+        public string Path { get; }
+        public long Size { get; }
+        public bool IsGzipped { get; }
+        public Encoding Encoding { get; }
+        public int LineCount { get; }
+
+        public static FileProperties FromFileStream(FileStream stream)
+        {
+            var path = stream.Name;
+            var size = stream.Length;
+            var isGzipped = DetectGzip(stream);
+            var encoding = DetectEncoding(isGzipped, stream);
+            var lineCount = CountLines(isGzipped, encoding, stream);
+
+            return new FileProperties(
+                path,
+                size,
+                isGzipped,
+                encoding,
+                lineCount);
+        }
+
+        private static byte[] GetLeadingBytes(Stream stream, int count)
+        {
+            var initialPosition = stream.Position;
+            stream.Position = 0;
+            var buffer = new byte[count];
+            var read = stream.Read(buffer, 0, buffer.Length);
+            var bytes = new byte[read];
+            Buffer.BlockCopy(buffer, 0, bytes, 0, read);
+            stream.Position = initialPosition;
+            return bytes;
+        }
+
+        private static bool DetectGzip(Stream stream)
+        {
+            var header = GetLeadingBytes(stream, 2);
+            if (header.Length < 2 || header[0] != 0x1f || header[1] != 0x8b)
+            {
+                return false;
+            }
+
+            var initialPosition = stream.Position;
+            stream.Position = 0;
+            try
+            {
+                using (var gzipStream = new GZipInputStream(stream) { IsStreamOwner = false })
+                {
+                    var buffer = new byte[128];
+                    gzipStream.Read(buffer, 0, buffer.Length);
+                }
+            }
+            catch (InvalidDataException)
+            {
+                return false;
+            }
+            stream.Position = initialPosition;
+
+            return true;
+        }
+
+        private static Encoding DetectEncoding(bool isGzipped, Stream stream)
+        {
+            return ProcesStream(
+                isGzipped,
+                stream,
+                readStream =>
+                {
+                    using (var reader = new StreamReader(
+                        readStream,
+                        Encoding.UTF8,
+                        detectEncodingFromByteOrderMarks: true,
+                        bufferSize: BufferSize,
+                        leaveOpen: true))
+                    {
+                        reader.Peek();
+                        return reader.CurrentEncoding;
+                    }
+                });
+        }
+
+        private static int CountLines(bool isGzipped, Encoding encoding, Stream stream)
+        {
+            return ProcesStream(
+                isGzipped,
+                stream,
+                readStream =>
+                {
+                    Console.Write("Counting lines...");
+                    var lineCount = 0;
+                    using (var reader = new StreamReader(
+                        readStream,
+                        encoding,
+                        detectEncodingFromByteOrderMarks: false,
+                        bufferSize: BufferSize,
+                        leaveOpen: true))
+                    {
+                        string line;
+                        var stopwatch = Stopwatch.StartNew();
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            lineCount++;
+                            if (lineCount % 1_000_000 == 0)
+                            {
+                                Console.WriteLine();
+                                var percent = 1.0 * reader.BaseStream.Position / stream.Length;
+                                var totalTimeEst = stopwatch.ElapsedTicks / percent;
+                                var remainingTicks = (long)((1 - percent) * totalTimeEst);
+                                var remainingMins = TimeSpan.FromTicks(remainingTicks).TotalMinutes;
+                                Console.Write($"[{percent:P} - ETA {remainingMins:F2}m] {lineCount} lines");
+                            }
+                            else if (lineCount % 25_000 == 0)
+                            {
+                                Console.Write(".");
+                            }
+                        }
+                    }
+
+                    Console.WriteLine();
+                    return lineCount;
+                });
+            
+        }
+
+        private static T ProcesStream<T>(bool isGzipped, Stream stream, Func<Stream, T> processAsync)
+        {
+            var initialPosition = stream.Position;
+            stream.Position = 0;
+
+            var readStream = stream;
+            if (isGzipped)
+            {
+                readStream = new GZipInputStream(stream) { IsStreamOwner = false };
+            }
+
+            var output = default(T);
+            try
+            {
+                output = processAsync(readStream);
+            }
+            finally
+            {
+                if (isGzipped)
+                {
+                    readStream.Dispose();
+                }
+            }
+
+            stream.Position = initialPosition;
+            return output;
+        }
+    }
+}

src/SplitLargeFiles/Program.cs
@@ -1,0 +1,157 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using ICSharpCode.SharpZipLib.GZip;
+
+namespace NuGet.Tools.SplitLargeFiles
+{
+    public class Program
+    {
+        private const string GzipExtension = ".gz";
+
+        public static int Main(string[] args)
+        {
+            var desiredFileSize = 20 * 1024 * 1024;
+            var fileIndexFormat = "_{0:D4}";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("The first parameter should be the path to the large file to split.");
+                Console.WriteLine($"The optional second parameter should be the desired file size in bytes. Default is {desiredFileSize} bytes.");
+                Console.WriteLine($"The optional third parameter is the file index suffix. Default is '{fileIndexFormat}'.");
+                return 1;
+            }
+
+            var inputPath = args[0];
+            
+            if (args.Length > 1 && !int.TryParse(args[1], out desiredFileSize))
+            {
+                Console.WriteLine($"Unable to parse desired file size: {args[1]}");
+                return 1;
+            }
+
+            if (desiredFileSize < 1)
+            {
+                Console.WriteLine("The desired file size must be larger than zero.");
+                return 1;
+            }
+
+            if (args.Length > 2)
+            {
+                fileIndexFormat = args[2];
+            }
+
+            Console.WriteLine("Input parameters:");
+            Console.WriteLine();
+            Console.WriteLine($"  Input path: {inputPath}");
+            Console.WriteLine($"  Desired output file size (bytes): {desiredFileSize}");
+            Console.WriteLine($"  File Index Format: {fileIndexFormat} (ex: '{string.Format(fileIndexFormat, 0)}')");
+            Console.WriteLine();
+
+            if (!File.Exists(inputPath))
+            {
+                Console.WriteLine("The input file does not exist.");
+                return 1;
+            }
+
+            using (var inputStream = new FileStream(inputPath, FileMode.Open))
+            {
+
+                Console.WriteLine("Gathering data about the input file...");
+                var properties = FileProperties.FromFileStream(inputStream);
+
+                // Generate the output path format.
+                var outputPathFormat = GenerateOutputPathFormat(inputPath, fileIndexFormat, properties.IsGzipped);
+
+                // This is a rough estimate assuming that each line compresses to the same length.
+                var fileCount = properties.Size / desiredFileSize;
+                if (fileCount == 0)
+                {
+                    fileCount = 1;
+                }
+                var linesPerFile = properties.LineCount / fileCount;
+
+                Console.WriteLine();
+                Console.WriteLine($"  Output path format: {outputPathFormat}");
+                Console.WriteLine();
+                Console.WriteLine($"  Input File encoding: {properties.Encoding}");
+                Console.WriteLine($"  Is gzipped: {properties.IsGzipped}");
+                Console.WriteLine($"  Input file size (bytes): {properties.Size}");
+                Console.WriteLine($"  Input file line count: {properties.LineCount}");
+                Console.WriteLine();
+                Console.WriteLine($"  Output file count: {fileCount}");
+                Console.WriteLine($"  Output file line count: {linesPerFile}");
+                Console.WriteLine();
+
+                Console.WriteLine("Splitting up files...");
+
+                Stream readStream = inputStream;
+                if (properties.IsGzipped)
+                {
+                    readStream = new GZipInputStream(inputStream);
+                }
+
+                using (var reader = new StreamReader(readStream, properties.Encoding))
+                {
+                    for (var fileIndex = 0; fileIndex < fileCount; fileIndex++)
+                    {
+                        var isLastFile = fileIndex >= fileCount - 1;
+                        var outputPath = string.Format(outputPathFormat, fileIndex);
+
+                        Console.Write($"[{fileIndex + 1}/{fileCount}] Writing {outputPath}...");
+                        WriteOutputFile(properties, reader, linesPerFile, outputPath, isLastFile);
+                        Console.WriteLine(" done.");
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The file has been split up.");
+
+            return 0;
+        }
+
+        private static string GenerateOutputPathFormat(string inputPath, string fileIndexFormat, bool isGzipped)
+        {
+            var directory = Path.GetDirectoryName(inputPath);
+            var fileName = Path.GetFileName(inputPath);
+
+            var gzipSuffix = string.Empty;
+            if (isGzipped && inputPath.EndsWith(GzipExtension, StringComparison.OrdinalIgnoreCase))
+            {
+                gzipSuffix = Path.GetExtension(fileName);
+                fileName = Path.GetFileNameWithoutExtension(fileName);
+            }
+
+            var withoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            var extension = Path.GetExtension(fileName);
+
+            return Path.Combine(directory, withoutExtension + fileIndexFormat + extension + gzipSuffix);
+        }
+
+        private static void WriteOutputFile(FileProperties properties, StreamReader reader, long linesPerFile, string outputPath, bool isLastFile)
+        {
+            using (var outputStream = new FileStream(outputPath, FileMode.Create))
+            {
+                Stream writeStream = outputStream;
+                if (properties.IsGzipped)
+                {
+                    writeStream = new GZipOutputStream(writeStream);
+                }
+
+                var lineIndex = 0;
+                using (var writer = new StreamWriter(writeStream, properties.Encoding))
+                {
+                    string line;
+                    while (((!isLastFile && lineIndex < linesPerFile) || isLastFile) &&
+                           (line = reader.ReadLine()) != null)
+                    {
+                        lineIndex++;
+                        writer.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}

src/SplitLargeFiles/Properties/AssemblyInfo.cs
@@ -1,0 +1,9 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Tools.SplitLargeFiles")]
+[assembly: ComVisible(false)]
+[assembly: Guid("afb33a62-c437-4c32-a965-d6cc5ee50540")]