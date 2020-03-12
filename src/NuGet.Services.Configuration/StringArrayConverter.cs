4d9a60fa2494f325dbb941b8011e303e6f6714b1
Joel Verhagen <jver@microsoft.com>
2020-03-12 10:07:04 -0700

    Add StringArrayConverter to handle arrays in Gallery-style config (#338)
    Progress on https://github.com/NuGet/Engineering/issues/3020

---
+++ src/NuGet.Services.Configuration/StringArrayConverter.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved. 
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information. 
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace NuGet.Services.Configuration
+{
+    public class StringArrayConverter : ArrayConverter
+    {
+        private static readonly string[] EmptyArray = new string[0];
+
+        public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+        {
+            if (sourceType == typeof(string))
+            {
+                return true;
+            }
+
+            return base.CanConvertFrom(context, sourceType);
+        }
+
+        public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+        {
+            var s = value as string;
+            if (s != null)
+            {
+                if (s == string.Empty)
+                {
+                    return EmptyArray;
+                }
+                else
+                {
+                    return s.Split(';');
+                }
+            }
+
+            return base.ConvertFrom(context, culture, value);
+        }
+    }
+}
