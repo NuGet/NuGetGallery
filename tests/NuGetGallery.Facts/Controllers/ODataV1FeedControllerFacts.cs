b6c61bc42b262c8ceea82e35daf3a477b98152fa
Andrei Grigorev <angrigor@microsoft.com>
2020-03-18 15:51:33 -0700

    using order

--- tests/NuGetGallery.Facts/Controllers/ODataV1FeedControllerFacts.cs
+++ tests/NuGetGallery.Facts/Controllers/ODataV1FeedControllerFacts.cs
@@ -1,15 +1,15 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Moq;
 using NuGet.Services.Entities;
 using NuGetGallery.Configuration;
 using NuGetGallery.OData;
-using Moq;
 using Xunit;
-using System;
 
 namespace NuGetGallery.Controllers
 {
