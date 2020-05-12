e8acf7d79ebaf19e561565a865f9be14b99a5f36
drewgillies <drewgil@microsoft.com>
2020-05-12 17:49:36 +1000

    Force a number of significant figures in Y axis values

--- src/NuGetGallery/ViewModels/StatisticsPackagesViewModel.cs
+++ src/NuGetGallery/ViewModels/StatisticsPackagesViewModel.cs
@@ -142,22 +142,37 @@ namespace NuGetGallery
             return (amount / total).ToString("P0", CultureInfo.CurrentCulture);
         }
 
-        public string DisplayShortNumber(long number)
+        public string DisplayShortNumber(double number, int sigFigures = 3)
         {
             var numDiv = 0;
 
             while (number >= 1000)
             {
-                number = number / 1000;
+                number /= 1000;
                 numDiv++;
             }
 
-            if (numDiv >= _magnitudeAbbreviations.Length)
+            // Find a rounding factor based on size, and round to sigFigures, e.g. for 3 sig figs, 1.774545 becomes 1.77.
+            var placeValues = Math.Ceiling(Math.Log10(number));
+            var roundingFactor = Math.Pow(10, sigFigures - placeValues);
+            var roundedNum = Math.Round(number * roundingFactor) / roundingFactor;
+
+            // Pad from right with zeroes to sigFigures length, so for 3 sig figs, 1.6 becomes 1.60
+            var formattedNum = roundedNum.ToString("F" + sigFigures);
+            var desiredLength = formattedNum.Contains('.') ? sigFigures + 1 : sigFigures;
+            if (formattedNum.Length > desiredLength)
             {
-                return number + $"10^{numDiv*3}";
+                formattedNum = formattedNum.Substring(0, desiredLength);
             }
 
-            return number + _magnitudeAbbreviations[numDiv];
+            formattedNum = formattedNum.TrimEnd('.');
+
+            if (numDiv >= _magnitudeAbbreviations.Length)
+            {
+                return formattedNum + $"10^{numDiv*3}";
+            }
+            
+            return formattedNum + _magnitudeAbbreviations[numDiv];
         }
     }
 }
