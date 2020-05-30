798bde43adb1f12e6be0b99a96c2faf4643c85c6
David Cueva Cortez <t-dacue@microsoft.com>
2020-05-30 01:28:24 +0000

    change cache time to 60 min

--- src/NuGetGallery/Controllers/PackagesController.cs
+++ src/NuGetGallery/Controllers/PackagesController.cs
@@ -958,7 +958,7 @@ namespace NuGetGallery
                     cacheDependentsCacheKey,
                     dependents,
                     null,
-                    DateTime.UtcNow.AddMinutes(5),
+                    DateTime.UtcNow.AddMinutes(60),
                     Cache.NoSlidingExpiration,
                     CacheItemPriority.Default, null);
 
