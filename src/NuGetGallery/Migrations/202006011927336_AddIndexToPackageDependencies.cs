a765777eb088d2ceb812326961eedc23425bbf5e
David Cueva Cortez <t-dacue@microsoft.com>
2020-06-01 20:22:28 +0000

    interim commit

---
+++ src/NuGetGallery/Migrations/202006011927336_AddIndexToPackageDependencies.cs
@@ -0,0 +1,16 @@
+﻿namespace NuGetGallery.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIndexToPackageDependencies : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
