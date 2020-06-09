5e3a1d5ab5119a134c386b5d68a00593cc7f74ab
David Cueva Cortez <t-dacue@microsoft.com>
2020-06-09 19:52:16 +0000

    added bunch of hacks

--- src/NuGetGallery/Migrations/202006011927336_AddIndexToPackageDependencies.cs
+++ src/NuGetGallery/Migrations/202006011927336_AddIndexToPackageDependencies.cs
@@ -6,7 +6,21 @@
     {
         public override void Up()
         {
-            Sql("CREATE NONCLUSTERED INDEX [IX_PackageDependencies_Id] ON [dbo].[PackageDependencies] ([Id]) INCLUDE ([PackageKey]) WITH (ONLINE = ON)");
+            // "WITH (ONLINE = ON)" is not supported on all editions of SQL Server. We want to create the index in the background
+            // when we are deploying to our live environment on Azure (which supports online index creation).
+            // Editions: https://docs.microsoft.com/en-us/sql/t-sql/functions/serverproperty-transact-sql?view=sql-server-ver15#arguments
+            // We used sp_executesql because it is blocked on SQL that does not support "WITH (ONLINE = ON)".
+            Sql(@"IF SERVERPROPERTY ('edition') = 'SQL Azure'
+                BEGIN
+                    EXECUTE sp_executesql N'CREATE NONCLUSTERED INDEX [IX_PackageDependencies_Id] ON [dbo].[PackageDependencies] ([Id])
+                    INCLUDE ([PackageKey])
+                    WITH (ONLINE = ON)'
+                END
+                ELSE
+                BEGIN
+                    CREATE NONCLUSTERED INDEX [IX_PackageDependencies_Id] ON [dbo].[PackageDependencies] ([Id])
+                    INCLUDE ([PackageKey])
+                END");
         }
 
         public override void Down()
