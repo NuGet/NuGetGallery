031347ac7fc822497f8d8223893b27639e3db683
Lynn Dai <lind@microsoft.com>
2020-07-14 17:21:04 +0000

    add column in package table

---
+++ src/NuGetGallery/Migrations/20200713010635_ReadmeChanges.cs
@@ -0,0 +1,18 @@
+﻿
+namespace NuGetGallery.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class ReadmeChanges : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Packages", "EmbeddedReadmeFileType", c => c.Int(nullable: false));
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Packages", "EmbeddedReadmeFileType");
+        }
+    }
+}
