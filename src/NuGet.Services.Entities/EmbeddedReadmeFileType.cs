f0fdd66034dcd8b4619f82343896cb1dbd1c5c69
Lynn Dai <lind@microsoft.com>
2020-07-14 17:21:04 +0000

    3268 add column EmbeddedReadmeFileType to Package tables

---
+++ src/NuGet.Services.Entities/EmbeddedReadmeFileType.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGet.Services.Entities
+{
+    /// <summary>
+    /// Specifies the type of the readme file used in the package
+    /// </summary>
+    public enum EmbeddedReadmeFileType
+    {
+        /// <summary>
+        /// Indicates that the package has no embedded readme file.
+        /// </summary>
+        Absent = 0,
+
+        /// <summary>
+        /// Indicates that the embedded readme file is markdown.
+        /// </summary>
+        Markdown = 1,
+    }
+}
