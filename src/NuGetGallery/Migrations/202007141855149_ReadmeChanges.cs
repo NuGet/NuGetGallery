648ae4240944869ca63a9121817dd46deb92a6fd
Lynn Dai <lind@microsoft.com>
2020-07-14 17:21:04 +0000

    3268 add column EmbeddedReadmeFileType to Package tables

---
+++ src/NuGetGallery/Migrations/202007141855149_ReadmeChanges.cs
@@ -0,0 +1,17 @@
+﻿namespace NuGetGallery.Migrations
+{
+    using System.Data.Entity.Migrations;
+    
+    public partial class ReadmeChanges : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Packages", "EmbeddedReadmeFileType", c => c.Int(nullable: false));
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Packages", "EmbeddedReadmeFileType");
+        }
+    }
+}
