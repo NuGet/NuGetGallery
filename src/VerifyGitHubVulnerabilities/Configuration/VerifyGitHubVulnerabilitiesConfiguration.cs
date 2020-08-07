6297797b158894f72596b403bf074f4cf63491c3
drewgillies <drewgil@microsoft.com>
2020-08-07 17:11:31 +1000

    Move PVA Verify command to its own job, use Key Vault secrets for db auth

---
+++ src/VerifyGitHubVulnerabilities/Configuration/VerifyGitHubVulnerabilitiesConfiguration.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace VerifyGitHubVulnerabilities.Configuration
+{
+    public class VerifyGitHubVulnerabilitiesConfiguration
+    {
+        /// <summary>
+        /// GitHub's v4 GraphQL API endpoint.
+        /// </summary>
+        public Uri GitHubGraphQLQueryEndpoint { get; set; } = new Uri("https://api.github.com/graphql");
+
+        /// <summary>
+        /// The personal access token to use to authenticate with GitHub.
+        /// </summary>
+        public string GitHubPersonalAccessToken { get; set; }
+    }
+}
