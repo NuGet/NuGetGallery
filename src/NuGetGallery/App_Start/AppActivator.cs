1a712c158c072c29962b7e8b74dfdfed06c4f8d2
Joel Verhagen <jver@microsoft.com>
2020-08-20 15:14:05 -0700

    Revert "Revert "Move to Knockout 3.5.1 and fix admin JS errors""
    This reverts commit f0cd5c07fb07e612c4aa83ec4230701ff17ebb14.

--- src/NuGetGallery/App_Start/AppActivator.cs
+++ src/NuGetGallery/App_Start/AppActivator.cs
@@ -132,7 +132,7 @@ namespace NuGetGallery
                 .Include("~/Scripts/gallery/jquery-3.4.1.js")
                 .Include("~/Scripts/gallery/jquery.validate-1.16.0.js")
                 .Include("~/Scripts/gallery/jquery.validate.unobtrusive-3.2.6.js")
-                .Include("~/Scripts/gallery/knockout-3.4.2.js")
+                .Include("~/Scripts/gallery/knockout-3.5.1.js")
                 .Include("~/Scripts/gallery/bootstrap.js")
                 .Include("~/Scripts/gallery/moment-2.18.1.js")
                 .Include("~/Scripts/gallery/common.js")
