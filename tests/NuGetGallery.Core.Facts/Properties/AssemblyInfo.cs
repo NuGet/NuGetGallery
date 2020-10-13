9f07360a3af7618a9a807a43e3ef78ec492620cf
Lynn Dai <lind@microsoft.com>
2020-10-13 14:49:22 -0700

    resolve this conflict

--- tests/NuGetGallery.Core.Facts/Properties/AssemblyInfo.cs
+++ tests/NuGetGallery.Core.Facts/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 using System.Reflection;
 
-[assembly: AssemblyTitle("NuGetGallery.Facts")]
+[assembly: AssemblyTitle("NuGetGallery.Core.Facts")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
