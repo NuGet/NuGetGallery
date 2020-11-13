fd7fe136828f2e3b6e0b756f08d1c260cd729dc1
Joel Verhagen <jver@microsoft.com>
2020-11-13 12:13:51 -0800

    Add restricted API for soft deleting packages  (#8290)
    Address https://github.com/NuGet/Engineering/issues/3512

--- src/NuGetGallery.Services/Configuration/FeatureFlagService.cs
+++ src/NuGetGallery.Services/Configuration/FeatureFlagService.cs
@@ -40,6 +40,7 @@ namespace NuGetGallery
         private const string PackageRenamesFeatureName = GalleryPrefix + "PackageRenames";
         private const string EmbeddedReadmeFlightName = GalleryPrefix + "EmbeddedReadmes";
         private const string LicenseMdRenderingFlightName = GalleryPrefix + "LicenseMdRendering";
+        private const string DeletePackageApiFlightName = GalleryPrefix + "DeletePackageApi";
 
         private const string ODataV1GetAllNonHijackedFeatureName = GalleryPrefix + "ODataV1GetAllNonHijacked";
         private const string ODataV1GetAllCountNonHijackedFeatureName = GalleryPrefix + "ODataV1GetAllCountNonHijacked";
@@ -283,6 +284,11 @@ namespace NuGetGallery
         public bool IsODataV2SearchCountNonHijackedEnabled()
         {
             return _client.IsEnabled(ODataV2SearchCountNonHijackedFeatureName, defaultValue: true);
+        }
+
+        public bool IsDeletePackageApiEnabled(User user)
+        {
+            return _client.IsEnabled(DeletePackageApiFlightName, user, defaultValue: false);
         }
     }
 }
