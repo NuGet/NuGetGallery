8b62800f14ac9642c64400a99be6c700532264cb
C. Augusto Proiete <augusto@proiete.com>
2021-03-01 20:21:47 -0400

    CakeBuildManagerExtensionsFacts: Update tests to handle null or empty tags

--- tests/NuGetGallery.Facts/Extensions/CakeBuildManagerExtensionsFacts.cs
+++ tests/NuGetGallery.Facts/Extensions/CakeBuildManagerExtensionsFacts.cs
@@ -1,172 +1,167 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections;
+using System.Collections.Generic;
 using Xunit;
 
 namespace NuGetGallery.Extensions
 {
     public class CakeBuildManagerExtensionsFacts
     {
-        public class GivenACakeAddin
+        public class TheMethodGetCakeInstallPackageCommand
         {
-            [Fact]
-            public void ReturnsAnAddinDirective()
+            public class GivenAPackageWithTheCakeAddinTag
             {
-                // Arrange
-                var model = new DisplayPackageViewModel
+                [Fact]
+                public void ReturnsAnAddinDirective()
                 {
-                    Id = "Cake.7zip", 
-                    Version = "1.0.0",
-                    Tags = new[] { "cake-addin" }
-                };
+                    var model = new DisplayPackageViewModel
+                    {
+                        Id = "Cake.7zip",
+                        Version = "1.0.0",
+                        Tags = new[] { "cake-addin" },
+                    };
 
-                // act
-                var actual = model.GetCakeInstallPackageCommand();
+                    var actual = model.GetCakeInstallPackageCommand();
 
-                // assert
-                Assert.Equal("#addin nuget:?package=Cake.7zip&version=1.0.0", actual);
+                    Assert.Equal("#addin nuget:?package=Cake.7zip&version=1.0.0", actual);
+                }
+
+                [Fact]
+                public void ReturnsAnAddinDirectiveWithPrerelease()
+                {
+                    var model = new DisplayPackageViewModel
+                    {
+                        Id = "Cake.7zip",
+                        Version = "1.0.0",
+                        Tags = new[] { "cake-addin" },
+                        Prerelease = true,
+                    };
+
+                    var actual = model.GetCakeInstallPackageCommand();
+
+                    Assert.Equal("#addin nuget:?package=Cake.7zip&version=1.0.0&prerelease", actual);
+                }
             }
 
-            [Fact]
-            public void ReturnsAnAddinDirectiveWithPrerelease()
+            public class GivenAPackageWithTheCakeModuleTag
             {
-                // Arrange
-                var model = new DisplayPackageViewModel
+                [Fact]
+                public void ReturnsAModuleDirective()
                 {
-                    Id = "Cake.7zip",
-                    Version = "1.0.0",
-                    Tags = new[] { "cake-addin" },
-                    Prerelease = true
-                };
+                    var model = new DisplayPackageViewModel
+                    {
+                        Id = "Cake.BuildSystems.Module",
+                        Version = "1.0.0",
+                        Tags = new[] {"cake-module"},
+                    };
 
-                // act
-                var actual = model.GetCakeInstallPackageCommand();
+                    var actual = model.GetCakeInstallPackageCommand();
 
-                // assert
-                Assert.Equal("#addin nuget:?package=Cake.7zip&version=1.0.0&prerelease", actual);
+                    Assert.Equal("#module nuget:?package=Cake.BuildSystems.Module&version=1.0.0", actual);
+                }
+
+                [Fact]
+                public void ReturnsAModuleDirectiveWithPrerelease()
+                {
+                    var model = new DisplayPackageViewModel
+                    {
+                        Id = "Cake.BuildSystems.Module",
+                        Version = "1.0.0",
+                        Tags = new[] { "cake-module" },
+                        Prerelease = true,
+                    };
+
+                    var actual = model.GetCakeInstallPackageCommand();
+
+                    Assert.Equal("#module nuget:?package=Cake.BuildSystems.Module&version=1.0.0&prerelease", actual);
+                }
+            }
+
+            public class GivenAPackageWithTheCakeRecipeTag
+            {
+                [Fact]
+                public void ReturnsALoadDirective()
+                {
+                    var model = new DisplayPackageViewModel
+                    {
+                        Id = "Cake.Recipe",
+                        Version = "1.0.0",
+                        Tags = new[] { "cake-recipe" },
+                    };
+
+                    var actual = model.GetCakeInstallPackageCommand();
+
+                    Assert.Equal("#load nuget:?package=Cake.Recipe&version=1.0.0", actual);
+                }
+
+                [Fact]
+                public void ReturnsALoadDirectiveWithPrerelease()
+                {
+                    var model = new DisplayPackageViewModel
+                    {
+                        Id = "Cake.Recipe",
+                        Version = "1.0.0",
+                        Tags = new[] { "cake-recipe" },
+                        Prerelease = true,
+                    };
+
+                    var actual = model.GetCakeInstallPackageCommand();
+
+                    Assert.Equal("#load nuget:?package=Cake.Recipe&version=1.0.0&prerelease", actual);
+                }
+            }
+
+            public class GivenAPackageWithoutAnyKnownCakeTags
+            {
+                [Theory]
+                [ClassData(typeof(PackageTagsNotKnownToBeCakeExtensions))]
+                public void ReturnsMultipleDirectives(string[] tags)
+                {
+                    var model = new DisplayPackageViewModel
+                    {
+                        Id = "Polly",
+                        Version = "1.0.0",
+                        Tags = tags,
+                    };
+
+                    var actual = model.GetCakeInstallPackageCommand();
+
+                    Assert.Contains("#addin nuget:?package=Polly&version=1.0.0", actual);
+                    Assert.Contains("#tool nuget:?package=Polly&version=1.0.0", actual);
+                }
+
+                [Theory]
+                [ClassData(typeof(PackageTagsNotKnownToBeCakeExtensions))]
+                public void ReturnsMultipleDirectivesWithPrerelease(string[] tags)
+                {
+                    var model = new DisplayPackageViewModel
+                    {
+                        Id = "Polly",
+                        Version = "1.0.0",
+                        Tags = tags,
+                        Prerelease = true,
+                    };
+
+                    var actual = model.GetCakeInstallPackageCommand();
+
+                    Assert.Contains("#addin nuget:?package=Polly&version=1.0.0&prerelease", actual);
+                    Assert.Contains("#tool nuget:?package=Polly&version=1.0.0&prerelease", actual);
+                }
             }
         }
 
-        public class GivenACakeModule
+        public class PackageTagsNotKnownToBeCakeExtensions : IEnumerable<object[]>
         {
-            [Fact]
-            public void ReturnsAModuleDirective()
+            public IEnumerator<object[]> GetEnumerator()
             {
-                // Arrange
-                var model = new DisplayPackageViewModel
-                {
-                    Id = "Cake.BuildSystems.Module",
-                    Version = "1.0.0",
-                    Tags = new[] { "cake-module" }
-                };
-
-                // act
-                var actual = model.GetCakeInstallPackageCommand();
-
-                // assert
-                Assert.Equal("#module nuget:?package=Cake.BuildSystems.Module&version=1.0.0", actual);
+                yield return new object[] { null };
+                yield return new object[] { new string[0] };
+                yield return new object[] { new[] { "json" } };
             }
 
-            [Fact]
-            public void ReturnsAModuleDirectiveWithPrerelease()
-            {
-                // Arrange
-                var model = new DisplayPackageViewModel
-                {
-                    Id = "Cake.BuildSystems.Module",
-                    Version = "1.0.0",
-                    Tags = new[] { "cake-module" },
-                    Prerelease = true
-                };
-
-                // act
-                var actual = model.GetCakeInstallPackageCommand();
-
-                // assert
-                Assert.Equal("#module nuget:?package=Cake.BuildSystems.Module&version=1.0.0&prerelease", actual);
-            }
-        }
-
-        public class GivenACakeRecipe
-        {
-            [Fact]
-            public void ReturnsALoadDirective()
-            {
-                // Arrange
-                var model = new DisplayPackageViewModel
-                {
-                    Id = "Cake.Recipe",
-                    Version = "1.0.0",
-                    Tags = new[] { "cake-recipe" }
-                };
-
-                // act
-                var actual = model.GetCakeInstallPackageCommand();
-
-                // assert
-                Assert.Equal("#load nuget:?package=Cake.Recipe&version=1.0.0", actual);
-            }
-
-            [Fact]
-            public void ReturnsALoadDirectiveWithPrerelease()
-            {
-                // Arrange
-                var model = new DisplayPackageViewModel
-                {
-                    Id = "Cake.Recipe",
-                    Version = "1.0.0",
-                    Tags = new[] { "cake-recipe" },
-                    Prerelease = true
-                };
-
-                // act
-                var actual = model.GetCakeInstallPackageCommand();
-
-                // assert
-                Assert.Equal("#load nuget:?package=Cake.Recipe&version=1.0.0&prerelease", actual);
-            }
-        }
-
-        public class GivenANonCakePackage
-        {
-            [Fact]
-            public void ReturnsMultipleDirectives()
-            {
-                // Arrange
-                var model = new DisplayPackageViewModel
-                {
-                    Id = "Polly",
-                    Version = "1.0.0",
-                    Tags = new[] { "" }
-                };
-
-                // act
-                var actual = model.GetCakeInstallPackageCommand();
-
-                // assert
-                Assert.Contains("#addin nuget:?package=Polly&version=1.0.0", actual);
-                Assert.Contains("#tool nuget:?package=Polly&version=1.0.0", actual);
-            }
-
-            [Fact]
-            public void ReturnsALoadDirectiveWithPrerelease()
-            {
-                // Arrange
-                var model = new DisplayPackageViewModel
-                {
-                    Id = "Polly",
-                    Version = "1.0.0",
-                    Tags = new[] { "" },
-                    Prerelease = true
-                };
-
-                // act
-                var actual = model.GetCakeInstallPackageCommand();
-
-                // assert
-                Assert.Contains("#addin nuget:?package=Polly&version=1.0.0&prerelease", actual);
-                Assert.Contains("#tool nuget:?package=Polly&version=1.0.0&prerelease", actual);
-            }
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         }
     }
 }
