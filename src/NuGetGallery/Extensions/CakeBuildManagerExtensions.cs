65907b43a8a05354b53319e7e47a119e64b6c7f7
C. Augusto Proiete <augusto@proiete.com>
2021-03-03 20:08:43 -0400

    Implementation of Cake tab with instructions for NuGet packages (#8434)
    * Add installation instructions for Cake for Addin, Module, Recipe
    
    Co-authored-by: Nils Andresen <nils@nils-andresen.de>
    
    Related to https://github.com/NuGet/NuGetGallery/issues/8381

---
+++ src/NuGetGallery/Extensions/CakeBuildManagerExtensions.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+
+namespace NuGetGallery
+{
+    public static class CakeBuildManagerExtensions
+    {
+        public static bool IsCakeExtension(this DisplayPackageViewModel model)
+        {
+            return IsCakeAddin(model) || IsCakeModule(model) || IsCakeRecipe(model);
+        }
+
+        public static string GetCakeInstallPackageCommand(this DisplayPackageViewModel model)
+        {
+            var reference = $"nuget:?package={model.Id}&version={model.Version}";
+
+            if (model.Prerelease)
+            {
+                reference += "&prerelease";
+            }
+
+            if (IsCakeAddin(model))
+            {
+                return $"#addin {reference}";
+            }
+
+            if (IsCakeModule(model))
+            {
+                return $"#module {reference}";
+            }
+
+            if (IsCakeRecipe(model))
+            {
+                return $"#load {reference}";
+            }
+
+            return string.Join(Environment.NewLine,
+                $"// Install {model.Id} as a Cake Addin",
+                $"#addin {reference}",
+                "",
+                $"// Install {model.Id} as a Cake Tool",
+                $"#tool {reference}"
+            );
+        }
+
+        private static bool IsCakeAddin(ListPackageItemViewModel model) =>
+            model.Tags?.Contains("cake-addin", StringComparer.OrdinalIgnoreCase) ?? false;
+
+        private static bool IsCakeModule(ListPackageItemViewModel model) =>
+            model.Tags?.Contains("cake-module", StringComparer.OrdinalIgnoreCase) ?? false;
+
+        private static bool IsCakeRecipe(ListPackageItemViewModel model) =>
+            model.Tags?.Contains("cake-recipe", StringComparer.OrdinalIgnoreCase) ?? false;
+    }
+}
