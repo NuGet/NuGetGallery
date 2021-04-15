015d88077bc5c1bf91c3c8434c4c47dc51a781e7
Drew Gillies <drewgil@microsoft.com>
2021-04-15 14:03:13 +1000

    adjust backfill logic - remove redundant catch, process null TFMs

--- src/GalleryTools/Commands/BackfillTfmMetadataCommand.cs
+++ src/GalleryTools/Commands/BackfillTfmMetadataCommand.cs
@@ -53,7 +53,11 @@ namespace GalleryTools.Commands
                 // See https://github.com/NuGet/NuGet.Client/blob/ba008e14611f4fa518c2d02ed78dfe5969e4a003/src/NuGet.Core/NuGet.Frameworks/FrameworkNameProvider.cs#L487                }
                 try
                 {
-                    supportedTFMs.Add(tfm.ToShortNameOrNull());
+                    var tfmToAdd = tfm.ToShortNameOrNull();
+                    if (!string.IsNullOrEmpty(tfmToAdd))
+                    {
+                        supportedTFMs.Add(tfmToAdd);
+                    }
                 }
                 catch
                 {
