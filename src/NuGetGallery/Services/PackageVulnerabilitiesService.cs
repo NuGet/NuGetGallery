cc43c2c1486474af7da46e131ebc2e2036cdd460
Loïc Sharma <737941+loic-sharma@users.noreply.github.com>
2021-04-29 12:27:33 -0700

    Revert "change sql get vulnerabilities by package id (#8540)"
    This reverts commit a2ab1c7052e55431c3d46073d41e6da3ca966c48.

--- src/NuGetGallery/Services/PackageVulnerabilitiesService.cs
+++ src/NuGetGallery/Services/PackageVulnerabilitiesService.cs
@@ -3,8 +3,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using NuGet.Services.Entities;
 
@@ -21,15 +21,26 @@ namespace NuGetGallery
 
         public IReadOnlyDictionary<int, IReadOnlyList<PackageVulnerability>> GetVulnerabilitiesById(string id)
         {
-            var packageKeyAndVulnerability = _entitiesContext.VulnerableRanges
-                .Include(x => x.Vulnerability)
-                .Where(x => x.PackageId == id)
-                .SelectMany(x => x.Packages.Select(p => new {PackageKey = p.Key, x.Vulnerability}))
-                .GroupBy(pv => pv.PackageKey, pv => pv.Vulnerability)
-                .ToDictionary(pv => pv.Key, pv => pv.ToList().AsReadOnly() as IReadOnlyList<PackageVulnerability>);
+            var result = new Dictionary<int, List<PackageVulnerability>>();
+            var packagesMatchingId = _entitiesContext.Packages
+                .Where(p => p.PackageRegistration != null && p.PackageRegistration.Id == id)
+                .Include($"{nameof(Package.VulnerablePackageRanges)}.{nameof(VulnerablePackageVersionRange.Vulnerability)}");
+            foreach (var package in packagesMatchingId)
+            {
+                if (package.VulnerablePackageRanges == null)
+                {
+                    continue;
+                }
 
-            return !packageKeyAndVulnerability.Any() ? null
-                : new ReadOnlyDictionary<int, IReadOnlyList<PackageVulnerability>>(packageKeyAndVulnerability);
+                if (package.VulnerablePackageRanges.Any())
+                {
+                    result.Add(package.Key,
+                        package.VulnerablePackageRanges.Select(vr => vr.Vulnerability).ToList());
+                }
+            }
+
+            return !result.Any() ? null :
+                result.ToDictionary(kv => kv.Key, kv => kv.Value as IReadOnlyList<PackageVulnerability>);
         }
 
         public bool IsPackageVulnerable(Package package)
