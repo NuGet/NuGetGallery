2b06a7ec3d2601a8ec1b7c7726b0321eb6452889
Joel Verhagen <jver@microsoft.com>
2021-08-20 21:26:24 -0700

    Start on models, view, and initial controller logic

---
+++ src/NuGetGallery/Areas/Admin/Controllers/PackageOwnershipController.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using NuGet.Services.Entities;
+using NuGetGallery.Areas.Admin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace NuGetGallery.Areas.Admin.Controllers
+{
+    public class PackageOwnershipController : AdminControllerBase
+    {
+        private readonly IPackageService _packageService;
+
+        public PackageOwnershipController(IPackageService packageService)
+        {
+            _packageService = packageService ?? throw new ArgumentNullException(nameof(packageService));
+        }
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ViewResult ValidateInput(PackageOwnershipChangesInput input)
+        {
+            // Find all package registrations
+            var packageIds = input
+                .PackageIds
+                .Split(new[] { '\n' })
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrEmpty(x));
+
+            var packageRegistrations = new List<PackageRegistration>();
+            foreach (var packageId in packageIds)
+            {
+                var packageRegistration = _packageService.FindPackageRegistrationById(packageId);
+                if (packageRegistration == null)
+                {
+                    return ShowError(nameof(PackageOwnershipChangesInput.PackageIds), $"The package ID '{packageId}' does not exist.");
+                }
+                packageRegistrations.Add(packageRegistration);
+            }
+
+            if (packageRegistrations.Count == 0)
+            {
+                return ShowError(nameof(PackageOwnershipChangesInput.PackageIds), "You must provide at least one valid package ID.");
+            }
+
+            return View();
+        }
+
+        private ViewResult ShowError(string key, string message)
+        {
+            ModelState.AddModelError(key, message);
+            return View(nameof(Index));
+        }
+    }
+}
---
+++ src/NuGetGallery/Areas/Admin/ViewModels/PackageOwnershipViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NuGetGallery.Areas.Admin.ViewModels
+{
+    public enum PackageOwnershipStateType
+    {
+        /// <summary>
+        /// This user is an owner of the package and will not be modified.
+        /// </summary>
+        ExistingOwner,
+
+        /// <summary>
+        /// This user has an existing ownership request for the package and will not be modified.
+        /// </summary>
+        ExistingOwnerRequest,
+
+        /// <summary>
+        /// This user is already an owner of the package so no request needs to be sent.
+        /// </summary>
+        AlreadyOwner,
+
+        /// <summary>
+        /// This user already has an existing ownership request for the package so no request needs to be sent.
+        /// </summary>
+        AlreadyOwnerRequest,
+
+        /// <summary>
+        /// This user is not currently an owner so an ownership request will be sent.
+        /// </summary>
+        NewOwnerRequest,
+
+        /// <summary>
+        /// This user is currently an owner of the package and will be removed.
+        /// </summary>
+        RemoveOwner,
+
+        /// <summary>
+        /// This user current has an ownership request which will be removed.
+        /// </summary>
+        RemoveOwnerRequest,
+    }
+
+    public class PackageOwnershipState
+    {
+        public PackageOwnershipState(string username, PackageOwnershipStateType type)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+            Type = type;
+        }
+
+        public string Username { get; }
+        public PackageOwnershipStateType Type { get; }
+    }
+
+
+    public class PackageRegistrationOwnershipChangeViewModel
+    {
+        public PackageRegistrationOwnershipChangeViewModel(string id, bool requestorHasPermissions, IReadOnlyList<PackageOwnershipState> ownershipStates)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+            RequestorHasPermissions = requestorHasPermissions;
+            OwnershipStates = ownershipStates ?? throw new ArgumentNullException(nameof(ownershipStates));
+        }
+
+        /// <summary>
+        /// The package ID this ownership change relates to.
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// Whether or not the request has permissions to make ownership changes on this package.
+        /// </summary>
+        public bool RequestorHasPermissions { get; }
+
+        /// <summary>
+        /// The state of all current, added, and removed owners on this package registration.
+        /// </summary>
+        public IReadOnlyList<PackageOwnershipState> OwnershipStates { get; }
+    }
+
+    public class PackageOwnershipChangesInput
+    {
+        [Required(ErrorMessage = "You must provide at least one package ID.")]
+        public string PackageIds { get; set; }
+
+        [Required(ErrorMessage = "You must provide a requestor username.")]
+        public string Requestor { get; set; }
+
+        public string AddOwners { get; set; }
+
+        public string RemoveOwners { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public class PackageOwnershipChangesViewModel
+    {
+        public PackageOwnershipChangesViewModel(
+            PackageOwnershipChangesInput input,
+            string requestorUsername,
+            string message,
+            IReadOnlyList<PackageRegistrationOwnershipChangeViewModel> packageRegistrationOwnershipChanges)
+        {
+            Input = input ?? throw new ArgumentNullException(nameof(requestorUsername));
+            RequestorUsername = requestorUsername ?? throw new ArgumentNullException(nameof(requestorUsername));
+            Message = message;
+            PackageRegistrationOwnershipChanges = packageRegistrationOwnershipChanges ?? throw new ArgumentNullException(nameof(packageRegistrationOwnershipChanges));
+        }
+
+        public PackageOwnershipChangesInput Input { get; }
+        public string RequestorUsername { get; }
+        public string Message { get; }
+        public IReadOnlyList<PackageRegistrationOwnershipChangeViewModel> PackageRegistrationOwnershipChanges { get; }
+    }
+}
