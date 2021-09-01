342b55cd63b7748965962621f7a831c25c5eb66a
Joel Verhagen <jver@microsoft.com>
2021-09-01 09:55:56 -0700

    Address comment

--- src/NuGetGallery.Services/Providers/IUrlHelper.cs
+++ src/NuGetGallery.Services/Providers/IUrlHelper.cs
@@ -10,8 +10,19 @@ using System.Web.Mvc;
 
 namespace NuGetGallery
 {
+    /// <summary>
+    /// Provides a way for the service layer (NuGetGallery.Services) to produce URLs. In an ASP.NET MVC web project
+    /// this would be implemented by wrapping <see cref="UrlHelper"/> and an extension methods on that type.
+    /// </summary>
     public interface IUrlHelper
     {
+        /// <summary>
+        /// Produce a URL to the package details (a.k.a. display package) page given a package ID and optional version.
+        /// </summary>
+        /// <param name="id">The package ID to link to.</param>
+        /// <param name="version">The specific package version to link to. Can be null.</param>
+        /// <param name="relativeUrl">True to return a relative URL, false to return an absolute URL.</param>
+        /// <returns>The relative or absolute URL as a string.</returns>
         string Package(string id, string version, bool relativeUrl);
     }
 }
