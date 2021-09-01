560985c2ced0fa8ee71ac29d99220af8fd31d0a9
Joel Verhagen <jver@microsoft.com>
2021-09-01 12:21:30 -0700

    Add comments

--- src/NuGetGallery.Services/Providers/IUrlHelper.cs
+++ src/NuGetGallery.Services/Providers/IUrlHelper.cs
@@ -19,8 +19,33 @@ namespace NuGetGallery
         /// <param name="relativeUrl">True to return a relative URL, false to return an absolute URL.</param>
         /// <returns>The relative or absolute URL as a string.</returns>
         string Package(string id, string version, bool relativeUrl);
+
+        /// <summary>
+        /// Produces a URL to the package ownership request confirmation page.
+        /// </summary>
+        /// <param name="id">The package ID for the ownership request.</param>
+        /// <param name="username">The username of the ownership request recipient (new owner).</param>
+        /// <param name="confirmationCode">The confirmation code (secret) associated with the request.</param>
+        /// <param name="relativeUrl">True to return a relative URL, false to return an absolute URL.</param>
+        /// <returns>The relative or absolute URL as a string.</returns>
         string ConfirmPendingOwnershipRequest(string id, string username, string confirmationCode, bool relativeUrl);
+
+        /// <summary>
+        /// Produces a URL to the package ownership request rejection page.
+        /// </summary>
+        /// <param name="id">The package ID for the ownership request.</param>
+        /// <param name="username">The username of the ownership request recipient (new owner).</param>
+        /// <param name="confirmationCode">The confirmation code (secret) associated with the request.</param>
+        /// <param name="relativeUrl">True to return a relative URL, false to return an absolute URL.</param>
+        /// <returns>The relative or absolute URL as a string.</returns>
         string RejectPendingOwnershipRequest(string id, string username, string confirmationCode, bool relativeUrl);
+
+        /// <summary>
+        /// Produces a URL to manage the ownership of an existing package.
+        /// </summary>
+        /// <param name="id">The package ID to manage ownership for.</param>
+        /// <param name="relativeUrl">True to return a relative URL, false to return an absolute URL.</param>
+        /// <returns>The relative or absolute URL as a string.</returns>
         string ManagePackageOwnership(string id, bool relativeUrl);
     }
 }
