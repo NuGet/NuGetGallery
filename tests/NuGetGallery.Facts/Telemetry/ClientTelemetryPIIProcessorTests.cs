d00117ebb27dd94f7448ef63869626df0e95b9cc
Andrei Grigorev <angrigor@microsoft.com>
2021-09-14 15:10:27 -0700

    Clarified RegisterUIRoutes calls

--- tests/NuGetGallery.Facts/Telemetry/ClientTelemetryPIIProcessorTests.cs
+++ tests/NuGetGallery.Facts/Telemetry/ClientTelemetryPIIProcessorTests.cs
@@ -24,7 +24,7 @@ namespace NuGetGallery.Telemetry
             {
                 _currentRoutes = new RouteCollection();
                 Routes.RegisterApiV2Routes(_currentRoutes);
-                Routes.RegisterUIRoutes(_currentRoutes, true);
+                Routes.RegisterUIRoutes(_currentRoutes, adminPanelEnabled: true);
             }
         }
 
