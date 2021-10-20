92d2a66ef95a1772e748872d77f34a6e951ffdc8
Advay Tandon <82980589+advay26@users.noreply.github.com>
2021-10-20 16:58:01 -0700

    Updated nuget.exe download link to a version with PackageSourceMapping (v6.0.0-preview4) (#373)

--- build/common.ps1
+++ build/common.ps1
@@ -441,7 +441,7 @@ Function Install-NuGet {
 
             Trace-Log 'Downloading nuget.exe'
             Invoke-WebRequest `
-                https://dist.nuget.org/win-x86-commandline/v5.8.0/nuget.exe `
+                https://dist.nuget.org/win-x86-commandline/v6.0.0-preview4/nuget.exe `
                 -UseBasicParsing `
                 -OutFile $NuGetExe
             
