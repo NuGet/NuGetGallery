2d67c70a5b32633444cbc396b5e4cc9d616ce84a
Daniel Jacinto <dajacint@microsoft.com>
2021-11-08 15:25:50 -0800

    Added documentation on service interface.

--- src/NuGetGallery.Core/Frameworks/IFrameworkCompatibilityService.cs
+++ src/NuGetGallery.Core/Frameworks/IFrameworkCompatibilityService.cs
@@ -8,6 +8,14 @@ namespace NuGetGallery.Frameworks
 {
     public interface IFrameworkCompatibilityService
     {
+        /// <summary>
+        /// Computes a set of compatible target frameworks from a list of target frameworks.
+        /// </summary>
+        /// <param name="frameworks">List of frameworks.</param>
+        /// <returns>A set of computed compatible target frameworks.</returns>
+        /// <remarks>
+        /// Every element on the returned set is compatible with at least one of the target frameworks from the input.
+        /// </remarks>
         ISet<NuGetFramework> GetCompatibleFrameworks(IEnumerable<NuGetFramework> frameworks);
     }
 }
