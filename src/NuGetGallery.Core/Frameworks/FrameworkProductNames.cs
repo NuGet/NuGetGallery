54cc349eca89d4ac1bf34625c362ed524fd125dc
Daniel Jacinto <dajacint@microsoft.com>
2021-11-15 14:11:12 -0800

    framework product names added.

---
+++ src/NuGetGallery.Core/Frameworks/FrameworkProductNames.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGetGallery.Frameworks
+{
+    public static class FrameworkProductNames
+    {
+        public const string Net = ".NET";
+        public const string NetCore = ".NET Core";
+        public const string NetStandard = ".NET Standard";
+        public const string NetFramework = ".NET Framework";
+        public const string WindowsPhone = "Windows Phone";
+        public const string WindowsStore = "Windows Store";
+        public const string NetMicroFramework = ".NET Micro Framework";
+        public const string UWP = "Universal Windows Platform";
+    }
+}
