9781b873b98270bac46743449332e8460550b379
Daniel Jacinto <dajacint@microsoft.com>
2021-11-16 18:20:23 -0800

    PackageFrameworkCompatibilityTableData documentation.

--- src/NuGetGallery.Core/Frameworks/PackageFrameworkCompatibilityTableData.cs
+++ src/NuGetGallery.Core/Frameworks/PackageFrameworkCompatibilityTableData.cs
@@ -7,7 +7,15 @@ namespace NuGetGallery.Frameworks
 {
     public class PackageFrameworkCompatibilityTableData
     {
+        /// <summary>
+        /// Compatible framework.
+        /// </summary>
         public NuGetFramework Framework { get; set; }
+
+        /// <summary>
+        /// <see langword="true"/> if the <see cref="Framework"/> was computed from <see cref="IFrameworkCompatibilityService"/>.<br></br>
+        /// <see langword="false"/> if the <see cref="Framework"/> was retrieved from the package asset frameworks.
+        /// </summary>
         public bool IsComputed { get; set; }
     }
 }
