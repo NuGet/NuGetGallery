b8b1a85f7e0655adf0e873d3a9fac560ea3380a0
Shishir H <shishirh@microsoft.com>
2022-01-20 19:30:30 -0800

    Add client name interpretation for new stats pipeline. (#944)
    * Add client name to category translation support
    
    * Address feedback
    
    * add references for functions
    
    * fix the func name

---
+++ src/Stats.LogInterpretation/ClientCategories.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stats.LogInterpretation
+{
+    /// <summary>
+    /// Built from the SQL functions here: https://github.com/NuGet/NuGet.Jobs/tree/main/src/Stats.Warehouse/Programmability/Functions
+    /// </summary>
+    public static class ClientCategories
+    {
+        public const string NuGet = "NuGet";
+        public const string WebMatrix = "WebMatrix";
+        public const string NuGetPackageExplorer = "NuGet Package Explorer";
+        public const string Script = "Script";
+        public const string Crawler = "Crawler";
+        public const string Mobile = "Mobile";
+        public const string Browser = "Browser";
+        public const string Unknown = "Unknown";
+    }
+}
