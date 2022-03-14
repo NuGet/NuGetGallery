7de88c598f41511c0689e9bdf6eca7ac13a4db6c
Joel Verhagen <jver@microsoft.com>
2022-03-14 12:25:17 -0700

    Add namespace and package prefix search tools to namespace reservation admin panel
    Progress on https://github.com/NuGet/Engineering/issues/4269

--- src/NuGetGallery/Areas/Admin/ViewModels/ReservedNamespaceViewModel.cs
+++ src/NuGetGallery/Areas/Admin/ViewModels/ReservedNamespaceViewModel.cs
@@ -1,13 +1,16 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
+using NuGet.Services.Entities;
+
 namespace NuGetGallery.Areas.Admin.ViewModels
 {
     public sealed class ReservedNamespaceViewModel
     {
-        /// <summary>
-        /// Prefix search query.
-        /// </summary>
-        public string PrefixSearchQuery { get; set; }
+        public string ReservedNamespacesQuery { get; set; }
+        public IReadOnlyCollection<ReservedNamespace> ReservedNamespaces { get; set; }
+        public string PackageRegistrationsQuery { get; set; }
+        public IReadOnlyCollection<PackageRegistration> PackageRegistrations { get; set; }
     }
 }
