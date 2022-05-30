33be4939d1e0fe3e26068615576a2fa9d655e452
Ryu Yu <ryuyu@microsoft.com>
2022-05-29 17:06:32 -0700

    Add migration.

---
+++ src/NuGetGallery/Migrations/202205300006043_Int64PackageDownloadCount.cs
@@ -0,0 +1,20 @@
+﻿namespace NuGetGallery.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Int64PackageDownloadCount : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.PackageRegistrations", "DownloadCount", c => c.Long(nullable: false));
+            AlterColumn("dbo.Packages", "DownloadCount", c => c.Long(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Packages", "DownloadCount", c => c.Int(nullable: false));
+            AlterColumn("dbo.PackageRegistrations", "DownloadCount", c => c.Int(nullable: false));
+        }
+    }
+}
