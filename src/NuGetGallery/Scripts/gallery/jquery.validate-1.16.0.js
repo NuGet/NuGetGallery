5fd09327f9b69c2d3777001551755cf7be65e2de
Riad <1711217+RiadGahlouz@users.noreply.github.com>
2022-06-27 13:01:10 -0700

    Fix A11y bugs (#9148)
    * no aria-required for labels
    
    * Contrast and tabbing bugs
    
    * Nit
    
    * Revert "Nit"
    
    This reverts commit d7e381a9fd7c6a652e1860ccf90417625e47548d.
    
    * reNit
    
    Co-authored-by: Advay Tandon <82980589+advay26@users.noreply.github.com>

--- src/NuGetGallery/Scripts/gallery/jquery.validate-1.16.0.js
+++ src/NuGetGallery/Scripts/gallery/jquery.validate-1.16.0.js
@@ -429,7 +429,8 @@
 
                 // Add aria-required to any Static/Data/Class required fields before first validation
                 // Screen readers require this attribute to be present before the initial submission http://www.w3.org/TR/WCAG-TECHS/ARIA2.html
-                $(this.currentForm).find("[required], [data-rule-required], .required").attr("aria-required", "true");
+                // We exclude labels since it's not accessible
+                $(this.currentForm).find("[required], [data-rule-required], .required").not("label").attr("aria-required", "true");
             },
 
             // http://jqueryvalidation.org/Validator.form/
