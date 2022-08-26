b2bccefbebbd9a04f748a0409b1d12bc5bafad44
Joel Verhagen <jver@microsoft.com>
2022-08-26 17:20:58 -0400

    Add a constant dimension to package push metrics (#9213)
    This allows us to measure package push SLO all as one % instead of splitting it by user tenant. Splitting by user tenant has proven to be very noisy and not an accurate representation of how we see push availability on NuGet.org.
    
    Progress on https://github.com/NuGet/Engineering/issues/4524

--- src/NuGetGallery/Telemetry/CustomerResourceIdEnricher.cs
+++ src/NuGetGallery/Telemetry/CustomerResourceIdEnricher.cs
@@ -13,6 +13,7 @@ namespace NuGetGallery
     public class CustomerResourceIdEnricher : ITelemetryInitializer
     {
         private const string CustomerResourceId = "CustomerResourceId";
+        private const string CustomerResourceIdConstant = "CustomerResourceIdConstant";
         private const string Prefix = "/tenants/";
         private static readonly string Empty = Prefix + Guid.Empty.ToString();
 
@@ -49,6 +50,15 @@ namespace NuGetGallery
             var tenantId = httpContext?.User?.Identity.GetTenantIdOrNull();
             var customerResourceId = tenantId != null ? Prefix + tenantId : Empty;
             itemTelemetry.Properties[CustomerResourceId] = customerResourceId;
+
+            // This is necessary for the NuGet SLO/SLO pipeline to signal that there is a single "tenant" on NuGet.org
+            // when measuring package push availability. This is an alernative approach to splitting package push
+            // availability by authenticated user tenant (the "CustomerResourceId" property added above). The drawback
+            // for using the authenticated user tenant is that it splits the availability measurement by tenant, leading
+            // to very noisy results since the total volume per user tenant is quite low.
+            //
+            // Any constant value can be used here.
+            itemTelemetry.Properties[CustomerResourceIdConstant] = nameof(NuGetGallery);
         }
 
         protected virtual HttpContextBase GetHttpContext() => HttpContextBaseExtensions.GetCurrent();
