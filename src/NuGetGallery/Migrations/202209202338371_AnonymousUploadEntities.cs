08b7b0c2836a0d903b1d9f812be1e48b1464ecd2
Ryu Yu <ryuyu@microsoft.com>
2022-09-20 16:39:22 -0700

    Adding Migrations for AnonymousUpload support.

---
+++ src/NuGetGallery/Migrations/202209202338371_AnonymousUploadEntities.cs
@@ -0,0 +1,20 @@
+﻿namespace NuGetGallery.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AnonymousUploadEntities : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.PackageRegistrations", "TemporaryId", c => c.String());
+            AddColumn("dbo.Packages", "ClaimKey", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Packages", "ClaimKey");
+            DropColumn("dbo.PackageRegistrations", "TemporaryId");
+        }
+    }
+}
