1c9a57878161a0b66f86e322596b58671efc5440
Daniel Jacinto <dajacint@microsoft.com>
2023-04-02 23:43:28 -0700

    nit.

--- tests/NuGetGallery.Facts/Infrastructure/Lucene/ExternalSearchServiceFacts.cs
+++ tests/NuGetGallery.Facts/Infrastructure/Lucene/ExternalSearchServiceFacts.cs
@@ -1,14 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using NuGet.Services.Entities;
-using NuGetGallery.Helpers;
-using NuGetGallery.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Linq;
+using Newtonsoft.Json.Linq;
+using NuGetGallery.Helpers;
 using Xunit;
 
 namespace NuGetGallery.Infrastructure.Search
