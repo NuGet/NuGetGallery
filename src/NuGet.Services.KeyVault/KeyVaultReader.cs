2a23db4ea121139c356eab5336c7978949c714f0
Erick Yondon <8766776+erdembayar@users.noreply.github.com>
2023-08-09 15:14:11 -0700

    Try again with DefaultAzureCredential

--- src/NuGet.Services.KeyVault/KeyVaultReader.cs
+++ src/NuGet.Services.KeyVault/KeyVaultReader.cs
@@ -61,7 +61,7 @@ namespace NuGet.Services.KeyVault
             TokenCredential credential = null;
             if (_configuration.UseManagedIdentity)
             {
-                credential = new ManagedIdentityCredential(_configuration.ClientId);
+                credential = new DefaultAzureCredential();
             }
             else
             {
