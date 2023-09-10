98642ad4ddb288a452d03ad7e4a9290a20a62188
Ryu Yu <ryuyu@microsoft.com>
2023-09-10 10:17:57 -0700

    Update AssemblyInfo

--- src/NuGet.Services.Github/Properties/AssemblyInfo.cs
+++ src/NuGet.Services.Github/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NuGet.Services.Github")]
+[assembly: AssemblyTitle("NuGet.Services.GitHub")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NuGet.Services.Github")]
+[assembly: AssemblyProduct("NuGet.Services.GitHub")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
