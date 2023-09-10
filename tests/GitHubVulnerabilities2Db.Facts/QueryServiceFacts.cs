52811ed957aaa36d6f4f135a72047cc33572ab63
Ryu Yu <ryuyu@microsoft.com>
2023-09-10 12:55:06 -0700

    Fix small mistake in reference updates.

--- tests/GitHubVulnerabilities2Db.Facts/QueryServiceFacts.cs
+++ tests/GitHubVulnerabilities2Db.Facts/QueryServiceFacts.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
-using NuGet.Services.GitHub.Configuration;
+using GitHubVulnerabilities2Db.Configuration;
 using NuGet.Services.GitHub.GraphQL;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
