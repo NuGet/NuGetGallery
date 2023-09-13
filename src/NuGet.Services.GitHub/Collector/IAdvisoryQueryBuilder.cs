d25217605dba726e709268ff9ec299ec4b38f06a
Ryu Yu <ryuyu@microsoft.com>
2023-09-13 14:02:03 -0700

    update casing. Remove unneeded files

---
+++ src/NuGet.Services.GitHub/Collector/IAdvisoryQueryBuilder.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using NuGet.Services.GitHub.GraphQL;
+
+namespace NuGet.Services.GitHub.Collector
+{
+    public interface IAdvisoryQueryBuilder
+    {
+        int GetMaximumResultsPerRequest();
+        string CreateSecurityAdvisoriesQuery(DateTimeOffset? updatedSince = null, string afterCursor = null);
+        string CreateSecurityAdvisoryQuery(SecurityAdvisory advisory);
+    }
+}
