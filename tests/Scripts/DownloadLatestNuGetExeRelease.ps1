f323789b318e8938542db207e4abc103ef53b601
Joel Verhagen <jver@microsoft.com>
2023-09-14 00:10:00 -0400

    Use a fixed nuget.exe

--- tests/Scripts/DownloadLatestNuGetExeRelease.ps1
+++ tests/Scripts/DownloadLatestNuGetExeRelease.ps1
@@ -12,6 +12,6 @@ if (Test-Path $targetNugetExePath) {
 
 Write-Host "nuget.exe not found in $Directory"
 
-$sourceNugetExeUrl = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+$sourceNugetExeUrl = "https://dist.nuget.org/win-x86-commandline/v6.7.0/nuget.exe"
 Write-Host "Downloading nuget.exe from $sourceNugetExeUrl"
 Invoke-WebRequest $sourceNugetExeUrl -OutFile $targetNugetExePath
