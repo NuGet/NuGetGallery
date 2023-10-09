ea40b7ae103c859c45ad4330ac66191d9d125695
Advay Tandon <82980589+advay26@users.noreply.github.com>
2023-10-09 16:39:49 -0700

    Admin Panel for Popularity Transfers (#9668)
    * first draft
    
    * got it to work
    
    * fixed css
    
    * fixed css stuff
    
    * fixed an admin page title
    
    * addressed feedback
    
    * added additional validations
    
    * Replace arrow symbol in success message
    
    Co-authored-by: Joel Verhagen <jver@microsoft.com>
    
    * Addressed smore feedback
    
    * remove redundant EF commit
    
    ---------
    
    Co-authored-by: Joel Verhagen <jver@microsoft.com>

---
+++ src/NuGetGallery/Areas/Admin/Controllers/PopularityTransferController.cs
@@ -0,0 +1,189 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using NuGet.Services.Entities;
+using NuGetGallery.Areas.Admin.ViewModels;
+
+namespace NuGetGallery.Areas.Admin.Controllers
+{
+    public class PopularityTransferController : AdminControllerBase
+    {
+        private readonly IPackageService _packageService;
+        private readonly IPackageRenameService _packageRenameService;
+        private readonly IEntityRepository<PackageRename> _packageRenameRepository;
+        private readonly IEntitiesContext _entitiesContext;
+
+        public PopularityTransferController(
+            IPackageService packageService,
+            IPackageRenameService packageRenameService,
+            IEntityRepository<PackageRename> packageRenameRepository,
+            IEntitiesContext entitiesContext)
+        {
+            _packageService = packageService ?? throw new ArgumentNullException(nameof(packageService));
+            _packageRenameService = packageRenameService ?? throw new ArgumentNullException(nameof(packageRenameService));
+            _packageRenameRepository = packageRenameRepository ?? throw new ArgumentNullException(nameof(packageRenameRepository));
+            _entitiesContext = entitiesContext ?? throw new ArgumentNullException(nameof(entitiesContext));
+        }
+
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ValidateInputs(string packagesFromInput, string packagesToInput)
+        {
+            if (string.IsNullOrEmpty(packagesFromInput) || string.IsNullOrEmpty(packagesToInput))
+            {
+                return Json(HttpStatusCode.BadRequest, "Package IDs in the 'From' or 'To' fields cannot be null or empty.", JsonRequestBehavior.AllowGet);
+            }
+
+            var packagesFrom = packagesFromInput
+                                        .Split(null) // all whitespace
+                                        .Where(id => id != string.Empty)
+                                        .Select(id => id.Trim())
+                                        .ToList();
+            var packagesTo = packagesToInput
+                                        .Split(null) // all whitespace
+                                        .Where(id => id != string.Empty)
+                                        .Select(id => id.Trim())
+                                        .ToList();
+
+            if (packagesFrom.Count != packagesTo.Count)
+            {
+                return Json(HttpStatusCode.BadRequest, "There must be an equal number of Package IDs in the 'From' and 'To' fields.", JsonRequestBehavior.AllowGet);
+            }
+
+            var result = new PopularityTransferViewModel();
+            HashSet<int> inputKeys = new HashSet<int>();
+
+            for (int i = 0; i < packagesFrom.Count; i++)
+            {
+                var packageFrom = _packageService.FindPackageRegistrationById(packagesFrom[i]);
+                var packageTo = _packageService.FindPackageRegistrationById(packagesTo[i]);
+
+                // check for invalid package ids
+                if (packageFrom is null || !packageFrom.Packages.Any())
+                {
+                    return Json(HttpStatusCode.BadRequest,
+                                $"Could not find a package with the Package ID: {packagesFrom[i]}",
+                                JsonRequestBehavior.AllowGet);
+                }
+                if (packageTo is null || !packageTo.Packages.Any())
+                {
+                    return Json(HttpStatusCode.BadRequest,
+                                $"Could not find a package with the Package ID: {packagesTo[i]}",
+                                JsonRequestBehavior.AllowGet);
+                }
+
+                // check for duplicate package ids
+                if (!inputKeys.Add(packageFrom.Key))
+                {
+                    return Json(HttpStatusCode.BadRequest,
+                                $"{packageFrom.Id} appears twice. Please remove duplicate Package IDs from the 'From' and 'To' fields.",
+                                JsonRequestBehavior.AllowGet);
+                }
+                if (!inputKeys.Add(packageTo.Key))
+                {
+                    return Json(HttpStatusCode.BadRequest,
+                                $"{packageTo.Id} appears twice. Please remove duplicate Package IDs from the 'From' and 'To' fields.",
+                                JsonRequestBehavior.AllowGet);
+                }
+
+                // create validated input result
+                var input = new PopularityTransferItem(CreatePackageSearchResult(packageFrom.Packages.First()),
+                                                       CreatePackageSearchResult(packageTo.Packages.First()),
+                                                       packageFrom.Key,
+                                                       packageTo.Key);
+
+                result.ValidatedInputs.Add(input);
+
+                // check for existing entries in the PackageRename table for the 'From' packages
+                var existingRenamesFrom = _packageRenameService.GetPackageRenames(packageFrom);
+
+                if (existingRenamesFrom.Any())
+                {
+                    if (existingRenamesFrom.Count == 1)
+                    {
+                        var existingRenamesMessage = $"{packageFrom.Id} already has 1 entry in the PackageRenames table. This will be removed with this operation.";
+                        result.ExistingPackageRenames.Add(existingRenamesMessage);
+                    }
+                    else
+                    {
+                        var existingRenamesMessage = $"{packageFrom.Id} already has {existingRenamesFrom.Count} entries in the PackageRenames table. These will be removed with this operation.";
+                        result.ExistingPackageRenames.Add(existingRenamesMessage);
+                    }
+                }
+
+                // check for existing entries in the PackageRename table for the 'To' packages
+                var existingRenamesTo = _packageRenameService.GetPackageRenames(packageTo);
+
+                if (existingRenamesTo.Any())
+                {
+                    var existingRenamesMessage = $"{packageTo.Id} already has entries in the PackageRenames table. This popularity transfer will result in a new transitive relationship. Please look at the PackageRenames table and verify your input before proceeding.";
+                    result.ExistingPackageRenames.Insert(0, existingRenamesMessage);
+                }
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> ExecutePopularityTransfer(List<PopularityTransferItem> confirmedInputs)
+        {
+            var newPackageRenames = new List<PackageRename>();
+            var previousPackageRenames = new List<PackageRename>();
+            
+            // keeping track of this so we can pad the success message
+            int maxIdLength = 0;
+
+            foreach (var input in confirmedInputs)
+            {
+                newPackageRenames.Add(new PackageRename
+                                        {
+                                            FromPackageRegistrationKey = input.FromKey,
+                                            ToPackageRegistrationKey = input.ToKey,
+                                            TransferPopularity = true
+                                        });
+
+                var previousRenames = _packageRenameService.GetPackageRenames(_packageService.FindPackageRegistrationById(input.FromId));
+                previousPackageRenames.AddRange(previousRenames);
+
+                maxIdLength = Math.Max(maxIdLength, input.FromId.Length);
+            }
+
+            var result = new PopularityTransferViewModel();
+
+            _packageRenameRepository.DeleteOnCommit(previousPackageRenames);
+            _packageRenameRepository.InsertOnCommit(newPackageRenames);
+
+            await _entitiesContext.SaveChangesAsync();
+
+            result.SuccessMessage = GenerateSuccessMessage(confirmedInputs, maxIdLength);
+
+            return Json(HttpStatusCode.OK, result, JsonRequestBehavior.AllowGet);
+        }
+
+        private string GenerateSuccessMessage(List<PopularityTransferItem> confirmedInputs, int maxIdLength)
+        {
+            var message = "Popularity transfer(s) executed successfully:\n\n";
+
+            foreach (var input in confirmedInputs)
+            {
+                message += $"\t{input.FromId.PadRight(maxIdLength)}  ➡️  {input.ToId}\n";
+            }
+
+            message += "\nIt can take up to 30 minutes for the popularity transfer(s) to be applied.";
+
+            return message;
+        }
+    }
+}
---
+++ src/NuGetGallery/Areas/Admin/ViewModels/PopularityTransferViewModel.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NuGetGallery.Areas.Admin.ViewModels
+{
+    public class PopularityTransferViewModel
+    {
+        public PopularityTransferViewModel()
+        {
+            ValidatedInputs = new List<PopularityTransferItem>();
+            ExistingPackageRenames = new List<string>();
+            SuccessMessage = string.Empty;
+        }
+
+        public List<PopularityTransferItem> ValidatedInputs { get; set; }
+        public List<string> ExistingPackageRenames { get; set; }
+        public string SuccessMessage { get; set; } = string.Empty;
+    }
+
+    public class PopularityTransferItem
+    {
+        public PopularityTransferItem()
+        {
+            FromOwners = new List<UserViewModel>();
+            ToOwners = new List<UserViewModel>();
+        }
+        
+        public PopularityTransferItem(
+            PackageSearchResult packageFrom, 
+            PackageSearchResult packageTo,
+            int fromKey,
+            int toKey)
+        {
+            FromId = packageFrom.PackageId;
+            FromUrl = UrlHelperExtensions.Package(new UrlHelper(HttpContext.Current.Request.RequestContext), packageFrom.PackageId);
+            FromDownloads = packageFrom.DownloadCount;
+            FromOwners = packageFrom.Owners;
+            FromKey = fromKey;
+
+            ToId = packageTo.PackageId;
+            ToUrl = UrlHelperExtensions.Package(new UrlHelper(HttpContext.Current.Request.RequestContext), packageTo.PackageId);
+            ToDownloads = packageTo.DownloadCount;
+            ToOwners = packageTo.Owners;
+            ToKey = toKey;
+        }
+
+        public string FromId { get; set; }
+        public string FromUrl { get; set; }
+        public long FromDownloads { get; set; }
+        public IReadOnlyList<UserViewModel> FromOwners { get; set; } = new List<UserViewModel>();
+        public int FromKey { get; set; }
+
+        public string ToId { get; set; }
+        public string ToUrl { get; set; }
+        public long ToDownloads { get; set; }
+        public IReadOnlyList<UserViewModel> ToOwners { get; set; } = new List<UserViewModel>();
+        public int ToKey { get; set; }
+    }
+}
