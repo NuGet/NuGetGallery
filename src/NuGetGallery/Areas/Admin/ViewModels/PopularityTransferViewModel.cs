75615820c2f7dd11d77b1f59429a58af910aea85
Advay Tandon <82980589+advay26@users.noreply.github.com>
2023-11-17 11:13:12 -0800

    Fixing popularity transfer download count bug, surfacing additional validation info (#9710)
    * changed to package registration download count
    
    * added more validation information
    
    * cleaned up and simplified controller and view model
    
    * cleaned up and simplified the view page
    
    * Fixed download count formatting
    
    * Added missing validation case that could have resulted in a transitive relationship

--- src/NuGetGallery/Areas/Admin/ViewModels/PopularityTransferViewModel.cs
+++ src/NuGetGallery/Areas/Admin/ViewModels/PopularityTransferViewModel.cs
@@ -1,9 +1,12 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NuGet.Services.Entities;
 
 namespace NuGetGallery.Areas.Admin.ViewModels
 {
@@ -12,12 +15,18 @@ namespace NuGetGallery.Areas.Admin.ViewModels
         public PopularityTransferViewModel()
         {
             ValidatedInputs = new List<PopularityTransferItem>();
-            ExistingPackageRenames = new List<string>();
+            ExistingPackageRenamesConflict = new List<PopularityTransferItem>();
+            ExistingPackageRenamesTransitive = new List<PopularityTransferItem>();
+            ExistingPackageRenamesMessagesConflict = new List<string>();
+            ExistingPackageRenamesMessagesTransitive = new List<string>();
             SuccessMessage = string.Empty;
         }
 
         public List<PopularityTransferItem> ValidatedInputs { get; set; }
-        public List<string> ExistingPackageRenames { get; set; }
+        public List<PopularityTransferItem> ExistingPackageRenamesConflict { get; set; }
+        public List<PopularityTransferItem> ExistingPackageRenamesTransitive { get; set; }
+        public List<string> ExistingPackageRenamesMessagesConflict { get; set; }
+        public List<string> ExistingPackageRenamesMessagesTransitive { get; set; }
         public string SuccessMessage { get; set; } = string.Empty;
     }
 
@@ -28,35 +37,49 @@ namespace NuGetGallery.Areas.Admin.ViewModels
             FromOwners = new List<UserViewModel>();
             ToOwners = new List<UserViewModel>();
         }
-        
-        public PopularityTransferItem(
-            PackageSearchResult packageFrom, 
-            PackageSearchResult packageTo,
-            int fromKey,
-            int toKey)
-        {
-            FromId = packageFrom.PackageId;
-            FromUrl = UrlHelperExtensions.Package(new UrlHelper(HttpContext.Current.Request.RequestContext), packageFrom.PackageId);
-            FromDownloads = packageFrom.DownloadCount;
-            FromOwners = packageFrom.Owners;
-            FromKey = fromKey;
 
-            ToId = packageTo.PackageId;
-            ToUrl = UrlHelperExtensions.Package(new UrlHelper(HttpContext.Current.Request.RequestContext), packageTo.PackageId);
-            ToDownloads = packageTo.DownloadCount;
-            ToOwners = packageTo.Owners;
-            ToKey = toKey;
+        public PopularityTransferItem(PackageRegistration packageFrom, PackageRegistration packageTo)
+        {
+            FromId = packageFrom.Id;
+            FromUrl = UrlHelperExtensions.Package(new UrlHelper(HttpContext.Current.Request.RequestContext), packageFrom.Id);
+            FromDownloads = packageFrom.DownloadCount.ToNuGetNumberString();
+            FromOwners = packageFrom
+                            .Owners
+                            .Select(u => u.Username)
+                            .OrderBy(u => u, StringComparer.OrdinalIgnoreCase)
+                            .Select(u => new UserViewModel
+                                            {
+                                                Username = u,
+                                                ProfileUrl = UrlHelperExtensions.User(new UrlHelper(HttpContext.Current.Request.RequestContext), u),
+                                            })
+                            .ToList();
+            FromKey = packageFrom.Key;
+
+            ToId = packageTo.Id;
+            ToUrl = UrlHelperExtensions.Package(new UrlHelper(HttpContext.Current.Request.RequestContext), packageTo.Id);
+            ToDownloads = packageTo.DownloadCount.ToNuGetNumberString();
+            ToOwners = packageTo
+                            .Owners
+                            .Select(u => u.Username)
+                            .OrderBy(u => u, StringComparer.OrdinalIgnoreCase)
+                            .Select(u => new UserViewModel
+                                            {
+                                                Username = u,
+                                                ProfileUrl = UrlHelperExtensions.User(new UrlHelper(HttpContext.Current.Request.RequestContext), u),
+                                            })
+                            .ToList();
+            ToKey = packageTo.Key;
         }
 
         public string FromId { get; set; }
         public string FromUrl { get; set; }
-        public long FromDownloads { get; set; }
+        public string FromDownloads { get; set; }
         public IReadOnlyList<UserViewModel> FromOwners { get; set; } = new List<UserViewModel>();
         public int FromKey { get; set; }
 
         public string ToId { get; set; }
         public string ToUrl { get; set; }
-        public long ToDownloads { get; set; }
+        public string ToDownloads { get; set; }
         public IReadOnlyList<UserViewModel> ToOwners { get; set; } = new List<UserViewModel>();
         public int ToKey { get; set; }
     }
