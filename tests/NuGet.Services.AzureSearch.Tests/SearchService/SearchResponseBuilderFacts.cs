5a1eaadd04f49d0eb4730d7ac4a1755c22420a36
Maria Ghiondea <11394867+mariaghiondea@users.noreply.github.com>
2024-02-21 14:07:15 -0800

    Update SearchResponseBuilderFacts.cs in include using for serialization
    Update SearchResponseBuilderFacts.cs in include using for serialization so it includes the JsonIgnoreCondition enum

--- tests/NuGet.Services.AzureSearch.Tests/SearchService/SearchResponseBuilderFacts.cs
+++ tests/NuGet.Services.AzureSearch.Tests/SearchService/SearchResponseBuilderFacts.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Azure.Search.Documents;
 using Azure.Search.Documents.Models;
 using Microsoft.Extensions.Options;
