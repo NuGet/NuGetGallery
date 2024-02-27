7bb035ce51aaa19bb8e5cdee90ec26458f8c3fd7
Lana Parezanin <lparezanin@microsoft.com>
2024-02-27 10:14:25 -0800

    Downloads page changed

--- src/NuGetGallery/Scripts/gallery/page-downloads.js
+++ src/NuGetGallery/Scripts/gallery/page-downloads.js
@@ -7,16 +7,16 @@
 
     var chevronIcon;
     var olderVersionsElement;
-
+    
     function makeOlderVersionsCollapsible() {
-        const listContainer = document.getElementById('win-x86-versions').children[1];   //children[0] is the headline, children[1] is the list of versions
-        const listContainerParent = document.getElementById('win-x86-versions');
+        const listContainer = document.getElementById('nuget-exe').children[1];   //children[0] is the headline, children[1] is the list of versions
+        const listContainerParent = document.getElementById('nuget-exe');
         olderVersionsElement = document.createElement('div');
 
         // We want to display 2 versions to the users. Others should be collapsed.
         const allVersions = listContainer.querySelectorAll('li');
         var collapsedVersions = Array.from(allVersions).slice(2);
-
+        
         // If the first two versions in json file are the same, we only want to show the first.
         // The following code checks if they're the same, and if so, removes the second.
         // It checks if they're the same based on the last word, which is a version identifier.
@@ -33,7 +33,7 @@
         if (firstVersionLastWord === secondVersionLastWord) {
             listContainer.removeChild(secondElement);
         }
-
+        
         olderVersionsElement.setAttribute('class', 'older-versions-dropdown');
         olderVersionsElement.innerHTML = 'Older versions ' +
             '<button class="toggle-older-versions-button"' +
@@ -45,8 +45,7 @@
             'type="button">' +
             '<i class="ms-Icon ms-Icon--ChevronDown"' +
             'id="olderVersionsToggleChevron"></i>' +
-            '</button>' +
-            '<p> These versions are no longer supported and might have vulnerabilities. </p>';
+            '</button>';
 
         const versionsList = document.createElement('ul');
 
