54a3fc1d31f1799c1523de3f2b0db3e240612768
Lynn Dai <lynndaidai@gmail.com>
2024-03-25 20:51:43 -0700

    add interface

--- src/VerifyMicrosoftPackage/Fakes/FakeFeatureFlagService.cs
+++ src/VerifyMicrosoftPackage/Fakes/FakeFeatureFlagService.cs
@@ -129,5 +129,7 @@ namespace NuGet.VerifyMicrosoftPackage.Fakes
         public bool IsDisplayPackageReadmeWarningEnabled(User user) => throw new NotImplementedException();
 
         public bool IsFrameworkFilteringEnabled(User user) => throw new NotImplementedException();
+
+        public bool IsDisplayNuGetTrendsLinksEnabled() => throw new NotImplementedException();
     }
 }
