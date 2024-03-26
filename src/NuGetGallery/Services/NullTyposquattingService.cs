953caa0465c20697d02578f2fef830c8cbac4833
Erick Yondon <8766776+erdembayar@users.noreply.github.com>
2024-03-26 12:02:10 -0700

    Add PR comment

--- src/NuGetGallery/Services/NullTyposquattingService.cs
+++ src/NuGetGallery/Services/NullTyposquattingService.cs
@@ -1,18 +1,20 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+
 namespace NuGetGallery.Services
 {
     public class ExactMatchTyposquattingServiceHelper : ITyposquattingServiceHelper
     {
         public bool IsDistanceLessThanOrEqualToThreshold(string uploadedPackageId, string packageId)
         {
-            return uploadedPackageId.ToLowerInvariant() == packageId.ToLowerInvariant();
+            return StringComparer.OrdinalIgnoreCase.Equals(uploadedPackageId, packageId);
         }
 
         public bool IsDistanceLessThanOrEqualToThresholdWithNormalizedPackageId(string uploadedPackageId, string normalizedPackageId)
         {
-            return uploadedPackageId.ToLowerInvariant() == normalizedPackageId.ToLowerInvariant();
+            return StringComparer.OrdinalIgnoreCase.Equals(uploadedPackageId, normalizedPackageId);
         }
 
         public string NormalizeString(string packageId)
