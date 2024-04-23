fd89a5dfe479c8ee7424d99bca547026ca65594d
Martin Ruiz <martin.ruiz.mares@gmail.com>
2024-04-23 13:19:27 -0700

    test changing the test

--- tests/NuGetGallery.FunctionalTests/StaticAssets/StaticAssetsTests.cs
+++ tests/NuGetGallery.FunctionalTests/StaticAssets/StaticAssetsTests.cs
@@ -43,8 +43,6 @@ namespace NuGetGallery.FunctionalTests.StaticAssets
                 "Content/gallery/css/site.min.css",
                 new[]
                 {
-                    "Content/gallery/css/bootstrap.min.css",
-                    "Content/gallery/css/bootstrap-theme.min.css",
                     "Content/gallery/css/fabric.css",
                 }
             },
