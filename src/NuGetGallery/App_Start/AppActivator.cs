d22dff20949ccf760359fbf03173592f5d66e48e
Martin Ruiz <martin.ruiz.mares@gmail.com>
2024-04-23 10:43:29 -0700

    fix tests

--- src/NuGetGallery/App_Start/AppActivator.cs
+++ src/NuGetGallery/App_Start/AppActivator.cs
@@ -118,8 +118,8 @@ namespace NuGetGallery
             // Add scripts bundles
             var newStyleBundle = new StyleBundle("~/Content/gallery/css/site.min.css");
             newStyleBundle
-                .Include("~/Content/gallery/css/bootstrap.min.css")
-                .Include("~/Content/gallery/css/bootstrap-theme.min.css")
+                .Include("~/Content/gallery/css/bootstrap.css")
+                .Include("~/Content/gallery/css/bootstrap-theme.css")
                 .Include("~/Content/gallery/css/fabric.css");
             BundleTable.Bundles.Add(newStyleBundle);
 
