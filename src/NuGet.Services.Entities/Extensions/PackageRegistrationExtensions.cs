976f67c988ea8683608a59d0c1918c4061c60ea7
Joel Verhagen <joel.verhagen@gmail.com>
2024-06-23 11:28:56 -0400

    Move user certificate validation out of Entities project
    Move to new ServerCommon

--- src/NuGet.Services.Entities/Extensions/PackageRegistrationExtensions.cs
+++ src/NuGet.Services.Entities/Extensions/PackageRegistrationExtensions.cs
@@ -56,46 +56,9 @@ namespace NuGet.Services.Entities
             return HasAnyCertificate(requiredSigner);
         }
 
-        /// <summary>
-        /// Determines if the certificate with specified thumbprint is valid for signing the specified package registration.
-        /// </summary>
-        /// <param name="packageRegistration">A package registration.</param>
-        /// <param name="thumbprint">A certificate thumbprint.</param>
-        /// <returns>A flag indicating whether the certificate is acceptable for signing.</returns>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="packageRegistration" />
-        /// is <c>null</c>.</exception>
-        /// <exception cref="ArgumentException">Thrown if <paramref name="thumbprint" /> is <c>null</c>
-        /// or empty.</exception>
-        public static bool IsAcceptableSigningCertificate(this PackageRegistration packageRegistration, string thumbprint)
-        {
-            if (packageRegistration == null)
-            {
-                throw new ArgumentNullException(nameof(packageRegistration));
-            }
-
-            if (string.IsNullOrWhiteSpace(thumbprint))
-            {
-                throw new ArgumentException(Strings.ArgumentCannotBeNullOrEmpty, nameof(thumbprint));
-            }
-
-            var requiredSigner = packageRegistration.RequiredSigners.FirstOrDefault();
-
-            if (requiredSigner == null)
-            {
-                return packageRegistration.Owners.Any(owner => CanUseCertificate(owner, thumbprint));
-            }
-
-            return CanUseCertificate(requiredSigner, thumbprint);
-        }
-
         private static bool HasAnyCertificate(User user)
         {
             return user.UserCertificates.Any() || user.UserCertificatePatterns.Any();
-        }
-
-        private static bool CanUseCertificate(User user, string thumbprint)
-        {
-            return user.UserCertificates.Any(uc => string.Equals(uc.Certificate.Thumbprint, thumbprint, StringComparison.OrdinalIgnoreCase));
         }
     }
 }
