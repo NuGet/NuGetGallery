3d09ca39fd9759930745ef8e98a1e702012c1ffa
Andrei Grigorev <angrigor@microsoft.com>
2024-07-12 20:51:12 -0700

    MSI support.

--- src/NuGet.Jobs.Common/StorageAccountExtensions.cs
+++ src/NuGet.Jobs.Common/StorageAccountExtensions.cs
@@ -4,6 +4,8 @@
 using System;
 using Autofac;
 using Autofac.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using NuGetGallery;
 
@@ -11,12 +13,52 @@ namespace NuGet.Jobs
 {
     public static class StorageAccountHelper
     {
+        private const string StorageUseManagedIdentityProperyName = "Storage_UseManagedIdentity";
+        private const string StorageManagedIdentityClientIdProperyName = "Storage_ManagedIdentityClientId";
+
+        public static IServiceCollection ConfigureStorageMsi(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            if (serviceCollection == null)
+            {
+                throw new ArgumentNullException(nameof(serviceCollection));
+            }
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            string useManagedIdentityStr = configuration[StorageUseManagedIdentityProperyName];
+            string managedIdentityClientId = configuration[StorageManagedIdentityClientIdProperyName];
+            bool useManagedIdentity = false;
+            if (!string.IsNullOrWhiteSpace(useManagedIdentityStr))
+            {
+                useManagedIdentity = bool.Parse(useManagedIdentityStr);
+            }
+            return serviceCollection.Configure<StorageMsiConfiguration>(storageConfiguration =>
+            {
+                storageConfiguration.UseManagedIdentity = useManagedIdentity;
+                storageConfiguration.ManagedIdentityClientId = managedIdentityClientId;
+            });
+        }
+
         public static CloudBlobClientWrapper CreateCloudBlobClient(
+            this IServiceProvider serviceProvider,
             string storageConnectionString,
             bool readAccessGeoRedundant = false,
             TimeSpan? requestTimeout = null)
         {
-            return new CloudBlobClientWrapper(
+            if (serviceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(serviceProvider));
+            }
+            if (string.IsNullOrWhiteSpace(storageConnectionString))
+            {
+                throw new ArgumentException($"{nameof(storageConnectionString)} cannot be null or empty.", nameof(storageConnectionString));
+            }
+
+            var msiConfiguration = serviceProvider.GetRequiredService<IOptions<StorageMsiConfiguration>>().Value;
+            return CreateCloudBlobClient(
+                msiConfiguration,
                 storageConnectionString,
                 readAccessGeoRedundant,
                 requestTimeout);
@@ -29,16 +71,58 @@ namespace NuGet.Jobs
             TimeSpan? requestTimeout = null)
             where TConfiguration : class, new()
         {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+            if (getConnectionString == null)
+            {
+                throw new ArgumentNullException(nameof(getConnectionString));
+            }
+
             return builder.Register(c =>
             {
                 var options = c.Resolve<IOptionsSnapshot<TConfiguration>>();
                 string storageConnectionString = getConnectionString(options.Value);
                 bool readAccessGeoRedundant = getReadAccessGeoRedundant?.Invoke(options.Value) ?? false;
+                var msiConfiguration = c.Resolve<IOptions<StorageMsiConfiguration>>().Value;
                 return CreateCloudBlobClient(
+                    msiConfiguration,
                     storageConnectionString,
                     readAccessGeoRedundant,
                     requestTimeout);
             });
+        }
+
+        private static CloudBlobClientWrapper CreateCloudBlobClient(
+            StorageMsiConfiguration msiConfiguration,
+            string storageConnectionString,
+            bool readAccessGeoRedundant = false,
+            TimeSpan? requestTimeout = null)
+        {
+            if (msiConfiguration.UseManagedIdentity)
+            {
+                if (string.IsNullOrWhiteSpace(msiConfiguration.ManagedIdentityClientId))
+                {
+                    return CloudBlobClientWrapper.UsingDefaultAzureCredential(
+                        storageConnectionString,
+                        readAccessGeoRedundant: readAccessGeoRedundant,
+                        requestTimeout: requestTimeout);
+                }
+                else
+                {
+                    return CloudBlobClientWrapper.UsingMsi(
+                        storageConnectionString,
+                        msiConfiguration.ManagedIdentityClientId,
+                        readAccessGeoRedundant,
+                        requestTimeout);
+                }
+            }
+
+            return new CloudBlobClientWrapper(
+                storageConnectionString,
+                readAccessGeoRedundant,
+                requestTimeout);
         }
     }
 }
