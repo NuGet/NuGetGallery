0ed61d24b4773b2087bd607549a2b37345705e80
Andrei Grigorev <angrigor@microsoft.com>
2024-08-01 16:30:38 -0700

    Post-rebase fixup

--- src/NuGet.Services.AzureSearch/DependencyInjectionExtensions.cs
+++ src/NuGet.Services.AzureSearch/DependencyInjectionExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -19,6 +19,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Rest;
+using NuGet.Jobs;
 using NuGet.Protocol;
 using NuGet.Services.AzureSearch.Auxiliary2AzureSearch;
 using NuGet.Services.AzureSearch.AuxiliaryFiles;
