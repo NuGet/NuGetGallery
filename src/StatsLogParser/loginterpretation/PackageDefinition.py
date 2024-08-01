db60f1c2ae5376e8780d07dff62537f6fd7ae21e
Claire Novotny <cnov@microsoft.com>
2024-08-01 14:52:53 -0400

    Initial impl of from_request_url

--- src/StatsLogParser/loginterpretation/PackageDefinition.py
+++ src/StatsLogParser/loginterpretation/PackageDefinition.py
@@ -1,24 +1,56 @@
+import urllib.parse
 
 
-class PackageDefinition(object):
+class PackageDefinition:
 
     NUGET_EXTENSION = ".nupkg"
+    VERSION_QUERY_PARAMETER = "packageVersion"
 
-    def __init__(self):
-        self.packageId = ''
-        self.packageVersion = ''
+    def __init__(self, package_id=None, package_version=None):
+        self.package_id = package_id.strip() if package_id else None
+        self.package_version = package_version.strip() if package_version else None
 
-    def __init__(self, packageId, packageVersion):
-        self.packageId = packageId.strip()
-        self.packageVersion = packageVersion.strip()
+    @staticmethod
+    def from_request_url(request_url):
+        if not request_url or not request_url.lower().endswith(
+            PackageDefinition.NUGET_EXTENSION
+        ):
+            return None
 
-    def fromRequestUrl(requestUrl):
-        # requestUrl = "https://api.nuget.org/v3-flatcontainer/microsoft.extensions.configuration/2.2.0/microsoft.extensions.configuration.2.2.0.nupkg"
-        # packageId = "microsoft.extensions.configuration"
-        # packageVersion = "2.2.0"
-        # Split the requestUrl by '/'
-        # Extract the packageId and packageVersion
-        # return PackageDefinition(packageId, packageVersion)
-        pass
+        resolution_options = []
 
+        parsed = urllib.parse.urlparse(request_url)
+        url_segments = [segment for segment in parsed.path.split("/") if segment]
 
+        file_name = url_segments[-1]
+        file_name = file_name[: -len(PackageDefinition.NUGET_EXTENSION)]
+
+        # Special handling for flat container
+        if len(url_segments) > 3:
+            package_id_container = url_segments[-3]
+            package_version_container = url_segments[-2]
+
+            if (
+                file_name.lower()
+                == f"{package_id_container}.{package_version_container}".lower()
+            ):
+                resolution_options.append(
+                    PackageDefinition(package_id_container, package_version_container)
+                )
+
+        # Look for it in the query string
+        if not resolution_options:
+            version_param = urllib.parse.parse_qs(parsed.query)[
+                PackageDefinition.VERSION_QUERY_PARAMETER
+            ]
+            if version_param:
+                # remove it from the file name and use that as the package id
+
+                parsed_id = file_name[: -len(version_param)]
+                # sanity check to make sure the version param matches what's in the file name
+                if file_name.lower() == f"{parsed_id}.{version_param}".lower():
+                    resolution_options.append(
+                        PackageDefinition(parsed_id, version_param)
+                    )
+
+        return resolution_options
