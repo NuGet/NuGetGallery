7b80fac101f054aaa62cac0855d662959c65da57
Claire Novotny <cnov@microsoft.com>
2024-08-07 14:09:52 -0400

    Syntax updates and decorator for static init

--- python/StatsLogParser/loginterpretation/useragentparser.py
+++ python/StatsLogParser/loginterpretation/useragentparser.py
@@ -9,28 +9,33 @@ import yaml
 
 UserAgent = namedtuple('UserAgent', ['family', 'major', 'minor', 'patch'])
 
+def initializestatic(cls):
+    """Decorator to initialize static variables of a class."""
+    cls.__static_init__()
+    return cls
 
+@initializestatic
 class UserAgentParser:
-
+    """UserAgentParser class to parse user agent string."""
     DEFAULT_PARSER_DATA = USER_AGENT_PARSERS
     KNOWN_CLIENTS_DATA: list[user_agent_parser.UserAgentParser]
     KNOWN_CLIENTS_IN_CHINA_DATA: list[user_agent_parser.UserAgentParser]
 
     @classmethod
-    def _initialize_class(cls):
+    def __static_init__(cls):
         cls.KNOWN_CLIENTS_DATA = cls._load_known_clients_parser()
         cls.KNOWN_CLIENTS_IN_CHINA_DATA = cls._load_known_clients_in_china_parser()
 
     @staticmethod
     def _load_known_clients_parser():
         yaml_content = UserAgentParser._read_known_clients_yaml()
-        return UserAgentParser._create_parser_from_yaml(yaml_content)
+        return UserAgentParser._create_parser_data_from_yaml(yaml_content)
 
     @staticmethod
-    def _load_known_clients_in_china_parser(self):
+    def _load_known_clients_in_china_parser():
         yaml_content = UserAgentParser._read_known_clients_yaml()
         patched_yaml = UserAgentParser._add_support_for_china_cdn(yaml_content)
-        return UserAgentParser._create_parser_from_yaml(patched_yaml)
+        return UserAgentParser._create_parser_data_from_yaml(patched_yaml)
 
     @staticmethod
     def _add_support_for_china_cdn(yaml_content):
@@ -73,8 +78,7 @@ class UserAgentParser:
 
     @staticmethod
     def parse(user_agent_string):
-        """Parse using known clients parser, then known clients in China parser, then default parser.
-        """
+        """Parse using known clients parser, then known clients in China parser, then default parser."""
         entry = UserAgentParser._lookup(user_agent_string)
 
         if entry is not None:
@@ -94,12 +98,10 @@ class UserAgentParser:
 
     @staticmethod
     def _parse_user_agent_with_parsers(user_agent_string: str, parsers: list[user_agent_parser.UserAgentParser]) -> UserAgent:
-            for uaParser in parsers:
-                family, v1, v2, v3 = uaParser.Parse(user_agent_string)
-                if family:
-                    break
+        for uaParser in parsers:
+            family, v1, v2, v3 = uaParser.Parse(user_agent_string)
+            if family:
+                break
 
-            family = family or "Other"
-            return UserAgent(family, v1 or None, v2 or None, v3 or None)
-
-    _initialize_class()
+        family = family or "Other"
+        return UserAgent(family, v1 or None, v2 or None, v3 or None)
