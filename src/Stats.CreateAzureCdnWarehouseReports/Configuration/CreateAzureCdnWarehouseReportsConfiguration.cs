6bf49bb133bbe2e177a4248911679fc1861081a2
Joel Verhagen <jver@microsoft.com>
2024-08-22 09:07:23 -0700

    Delete unused code in CreateAzureCdnWarehouseReportsJob (#10151)
    The only thing this job does now is create stats-totals.json

--- src/Stats.CreateAzureCdnWarehouseReports/Configuration/CreateAzureCdnWarehouseReportsConfiguration.cs
+++ src/Stats.CreateAzureCdnWarehouseReports/Configuration/CreateAzureCdnWarehouseReportsConfiguration.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 namespace Stats.CreateAzureCdnWarehouseReports
@@ -9,18 +9,10 @@ namespace Stats.CreateAzureCdnWarehouseReports
 
         public string AzureCdnCloudStorageContainerName { get; set; }
 
-        public string DataStorageAccount { get; set; }
-
-        public string DataContainerName { get; set; }
-
         public string AdditionalGalleryTotalsStorageAccount { get; set; }
 
         public string AdditionalGalleryTotalsStorageContainerName { get; set; }
 
         public int? CommandTimeOut { get; set; }
-
-        public int? PerPackageReportDegreeOfParallelism { get; set; }
-
-        public string ReportName { get; set; }
     }
 }
