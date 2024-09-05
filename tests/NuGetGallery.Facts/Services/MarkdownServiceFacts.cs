e7825011abd2e11244faea6170b7b64aa1627ac4
Daniel Cazzulino <daniel@cazzulino.com>
2024-09-05 17:16:02 -0300

    Add support for markdown > alerts via markdig
    Markdig supports alert blocks as documented at https://github.com/xoofx/markdig/blob/master/src/Markdig.Tests/Specs/AlertBlockSpecs.md.
    
    Bumping markdig and adding `.UseAlertBlocks()` now renders the expected html.
    
    Fixes #10125

--- tests/NuGetGallery.Facts/Services/MarkdownServiceFacts.cs
+++ tests/NuGetGallery.Facts/Services/MarkdownServiceFacts.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -105,12 +105,23 @@ namespace NuGetGallery
             [InlineData("![](http://www.otherurl.net/fake.jpg)", "<p><img src=\"https://www.otherurl.net/fake.jpg\" alt=\"\" /></p>", true, false)]
             [InlineData("![](http://www.otherurl.net/fake.jpg)", "<p><img src=\"https://www.otherurl.net/fake.jpg\" class=\"img-fluid\" alt=\"alternate text is missing from this package README image\" /></p>", true, true)]
             [InlineData("## License\n\tLicensed under the Apache License, Version 2.0 (the \"License\");", "<h3 id=\"license\">License</h3>\n<pre><code>Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);\n</code></pre>", false, true)]
+            [InlineData(
+                """
+                > [!NOTE]
+                > This is a note
+                """,
+                """
+                <div class="markdown-alert markdown-alert-note alert alert-primary" role="alert">
+                <p class="mb-0">This is a note</p>
+                </div>
+                """,
+                false, true)]
             public void ConvertsMarkdownToHtml(string originalMd, string expectedHtml, bool imageRewriteExpected, bool isMarkdigMdRenderingEnabled)
             {
                 _featureFlagService.Setup(x => x.IsMarkdigMdRenderingEnabled()).Returns(isMarkdigMdRenderingEnabled);
                 _featureFlagService.Setup(x => x.IsImageAllowlistEnabled()).Returns(false);
                 var readMeResult = _markdownService.GetHtmlFromMarkdown(originalMd);
-                Assert.Equal(expectedHtml, readMeResult.Content);
+                Assert.Equal(expectedHtml.Replace("\r\n", "\n"), readMeResult.Content.Replace("\r\n", "\n"));
                 Assert.Equal(imageRewriteExpected, readMeResult.ImagesRewritten);
             }
 
