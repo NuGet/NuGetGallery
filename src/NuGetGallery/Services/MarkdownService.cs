af6daf5341cc91a043adcd5248d45b34db67884e
Daniel Cazzulino <daniel@cazzulino.com>
2024-09-05 17:16:02 -0300

    Add support for markdown > alerts via markdig
    Markdig supports alert blocks as documented at https://github.com/xoofx/markdig/blob/master/src/Markdig.Tests/Specs/AlertBlockSpecs.md.
    
    Bumping markdig and adding `.UseAlertBlocks()` now renders the expected html.
    
    Fixes #10125

--- src/NuGetGallery/Services/MarkdownService.cs
+++ src/NuGetGallery/Services/MarkdownService.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -208,6 +208,7 @@ namespace NuGetGallery
                 .UseTaskLists()
                 .UseEmojiAndSmiley()
                 .UseAutoLinks()
+                .UseAlertBlocks()
                 .UseReferralLinks("noopener noreferrer nofollow")
                 .UseAutoIdentifiers()
                 .UseEmphasisExtras(EmphasisExtraOptions.Strikethrough)
