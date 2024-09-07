3352f0edf319517e7a55d17843be1f4353a5111e
Andrei Grigorev <angrigor@microsoft.com>
2024-09-06 18:44:08 -0700

    Testing a hack enhancement.

--- tests/NuGetGallery.FunctionalTests.Core/GalleryConfiguration.cs
+++ tests/NuGetGallery.FunctionalTests.Core/GalleryConfiguration.cs
@@ -37,7 +37,18 @@ namespace NuGetGallery.FunctionalTests
                 ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
 
                 // This method is a workaround for binding redirect issues. Please check the implementation for more information.
+                EnsureRedirectedAssembliesLoaded();
                 RedirectAssembly("Newtonsoft.Json");
+                RedirectAssembly("Azure.Core");
+                RedirectAssembly("System.Diagnostics.DiagnosticSource");
+                RedirectAssembly("System.Runtime.CompilerServices.Unsafe");
+                RedirectAssembly("System.Buffers");
+                RedirectAssembly("System.Memory");
+                RedirectAssembly("System.Security.Cryptography.ProtectedData");
+                RedirectAssembly("System.Text.Encodings.Web");
+                RedirectAssembly("System.Text.Json");
+                RedirectAssembly("System.Threading.Tasks.Extensions");
+                RedirectAssembly("System.ValueTuple");
 
                 // Load the configuration without injection. This allows us to read KeyVault configuration.
                 var uninjectedBuilder = new ConfigurationBuilder()
@@ -73,6 +84,34 @@ namespace NuGetGallery.FunctionalTests
             }
         }
 
+        private static void EnsureRedirectedAssembliesLoaded()
+        {
+            // The following comment is an observation-based speculation and can be at various degrees of wrong.
+            // For the RedirectAssembly function to work, we need to load versions of redirected assemblies into
+            // app domain that we know exist.
+            // To achieve that we need to touch something in the redirected assemblies to cause them loaded right now
+            // assuming this assembly depends on correct and available versions.
+            // Just mentioning a type from an assembly will cause it to be loaded, so we have
+            // control over which versions do get loaded into app domain, assuming this is
+            // the first time types from those assemblies are mentioned during current execution.
+            // If we don't do that, then we leave the assembly version resolution to a chance of when it is going
+            // to be accessed first.
+            // Full type names are used to not litter the using section on top of the file.
+            // The list of assemblies is taken from compiler-generated binding redirects for the test DLLs.
+
+            Newtonsoft.Json.JsonConverter jc; // Newtonsoft.Json
+            System.Diagnostics.DiagnosticSource ds; // System.Diagnostics.DiagnosticSource
+            Azure.Core.TokenCredential tc; // Azure.Core
+            var e = System.Runtime.CompilerServices.Unsafe.NullRef<object>; // System.Runtime.CompilerServices.Unsafe it only exports single static type, so need to reference a method
+            System.Buffers.ArrayPool<object> pool; // System.Buffers
+            System.Memory<object> m; // System.Memory
+            var p = System.Security.Cryptography.ProtectedData.Protect; // System.Security.Cryptography.ProtectedData, maybe
+            System.Text.Encodings.Web.TextEncoder te; // System.Text.Encodings.Web
+            System.Text.Json.JsonDocument jd; // System.Text.Json
+            System.Threading.Tasks.ValueTask t; // System.Threading.Tasks.Extensions
+            System.ValueTuple<object, object> vt; // System.ValueTuple, maybe
+        }
+
         /// <summary>
         /// Source: https://stackoverflow.com/a/32698357
         /// </summary>
@@ -82,7 +121,7 @@ namespace NuGetGallery.FunctionalTests
              * where loaded on the AppDomain, this led to an issue where a package needed an specific assembly version 
              * and the loaded assembly didn't contain a certain implementation. To fix this we are loading the last assembly 
              * for the specified name (which at least for this case it's the valid one) at runtime.
-             * This issue appeared on the FunciontalTests solution, so for the LoadTests project we only needed to 
+             * This issue appeared on the FunctionalTests solution, so for the LoadTests project we only needed to 
              * add the dependentAssembly specified on the app.config file.
              * Providing dependentAssembly did not work for WebUiTests projects since they use QTAgent (internal stuff for legacy mstest) 
              * that has it's own configuration file with their own binding redirects.
