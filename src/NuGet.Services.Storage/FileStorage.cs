f8c582992f52e6039eb4af771c32c133645181bd
Lana Parezanin <lparezanin@microsoft.com>
2024-10-21 17:22:34 -0700

    Added TopLevel

--- src/NuGet.Services.Storage/FileStorage.cs
+++ src/NuGet.Services.Storage/FileStorage.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -57,6 +57,9 @@ namespace NuGet.Services.Storage
             return Task.FromResult<IEnumerable<StorageListItem>>(List(getMetadata));
         }
 
+        public override Task<IEnumerable<StorageListItem>> ListTopLevelAsync(bool getMetadata, CancellationToken cancellationToken) =>
+            ListAsync(getMetadata, cancellationToken);
+        
         public string Path
         { 
             get;
