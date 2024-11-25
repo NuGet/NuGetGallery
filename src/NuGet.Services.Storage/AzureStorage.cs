7c7e01e1b48a72f9847c05f3e180339bfd4159c0
Lanaparezanin <lanaparezanin@gmail.com>
2024-11-25 10:34:32 -0800

    Revert "Bugfix"
    This reverts commit 777b59a86ad0579e667d4adc8ca0a0658de91aad.

--- src/NuGet.Services.Storage/AzureStorage.cs
+++ src/NuGet.Services.Storage/AzureStorage.cs
@@ -197,7 +197,7 @@ namespace NuGet.Services.Storage
 
         public override async Task<IEnumerable<StorageListItem>> ListTopLevelAsync(bool getMetadata, CancellationToken cancellationToken)
         {
-            var prefix = _path.Trim('/') + '/';
+            var thepath = _path.Trim('/') + '/';
             var blobTraits = new BlobTraits();
             if (getMetadata)
             {
@@ -206,7 +206,7 @@ namespace NuGet.Services.Storage
 
             var blobList = new List<StorageListItem>();
 
-            await foreach (BlobHierarchyItem blob in _directory.GetBlobsByHierarchyAsync(traits: blobTraits, prefix: prefix, delimiter: "/"))
+            await foreach (BlobHierarchyItem blob in _directory.GetBlobsByHierarchyAsync(traits: blobTraits, prefix: thepath, delimiter: "/"))
             {
                 if (!blob.IsPrefix)
                 {
