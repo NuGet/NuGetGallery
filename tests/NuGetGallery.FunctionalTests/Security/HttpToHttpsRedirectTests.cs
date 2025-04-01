7bd23f9d47b303b9e21b5bd6c1a38c8aea5da091
Andrei Grigorev <angrigor@microsoft.com>
2025-04-01 12:32:30 -0700

    Printing URL

--- tests/NuGetGallery.FunctionalTests/Security/HttpToHttpsRedirectTests.cs
+++ tests/NuGetGallery.FunctionalTests/Security/HttpToHttpsRedirectTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -107,6 +107,7 @@ namespace NuGetGallery.FunctionalTests.Security
         [Category("AppServiceTests")]
         public async Task AllUrlsRedirect(HttpMethod method, string url)
         {
+            Console.WriteLine($"Method: {method}, url: {url}");
             Uri uri = ForceHttp(url);
             await VerifyHttpResponseStatus(r =>
             {
