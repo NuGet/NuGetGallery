c7f14db4097b1c70e0ecd2e33668a7d6913f93ef
Erik Ejlskov Jensen <eej@delegate.dk>
2025-04-20 15:37:48 +0200

    fix up

--- src/NuGetGallery.Services/PackageManagement/PackageService.cs
+++ src/NuGetGallery.Services/PackageManagement/PackageService.cs
@@ -504,7 +504,6 @@ namespace NuGetGallery
                 .Include(p => p.PackageRegistration)
                 .Include(p => p.PackageRegistration.Owners)
                 .Include(p => p.PackageRegistration.RequiredSigners)
-                .Include(p => p.PackageRegistration.Packages)
                 .ToList();
         }
 
