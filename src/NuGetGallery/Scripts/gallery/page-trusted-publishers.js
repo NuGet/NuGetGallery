19105ec64fcc70fe11a9787c7b48095f02f938e1
ET <evgenyt@microsoft.com>
2025-06-17 14:53:47 -0700

    Implementing Create section

--- src/NuGetGallery/Scripts/gallery/page-trusted-publishers.js
+++ src/NuGetGallery/Scripts/gallery/page-trusted-publishers.js
@@ -24,6 +24,22 @@
             }
         }
 
+        var _gitHubDetails = {};
+        _gitHubDetails.Initialize = function (self) {
+            self.GitHub_RepositoryOwner = ko.observable();
+            self.GitHub_Repository = ko.observable();
+            self.GitHub_WorkflowFile = ko.observable();
+            self.GitHub_Environment = ko.observable();
+        }
+
+        _gitHubDetails.Update = function (self, data) {
+            const details = data.PublisherName !== "GitHub" ? {} : data.PublisherDetails || {};
+            self.GitHub_RepositoryOwner(details.RepositoryOwner || '');
+            self.GitHub_Repository(details.Repository || '');
+            self.GitHub_WorkflowFile(details.WorkflowFile || '');
+            self.GitHub_Environment(details.Environment || '');
+        }
+
         function TrustedPublisherViewModel(parent, packageOwners, data) {
             var self = this;
             data = data || {};
@@ -31,30 +47,22 @@
             // Common properties
             this.Key = ko.observable(0);
             this.Description = ko.observable();
+            this.PendingDescription = ko.observable();
             this.Owner = ko.observable();
             this.PublisherName = ko.observable();
             
-            // Publisher-specific details
-            this.PublisherDetails = ko.observable();
+            // Provider specific properties
+            _gitHubDetails.Initialize(this);
             
-            this._UpdateData = function(data) {
+            this._UpdateData = function (data) {
                 this.Key(data.Key || 0);
                 this.Description(data.Description || null);
+                this.PendingDescription(data.Description || null);
                 this.Owner(data.Owner || null);
                 this.PublisherName(data.PublisherName || null);
-                
-                // Create appropriate publisher details based on type
-                this.PublisherDetails(null);
-                const publisherDetails = data.PublisherDetails;
-                if (data.PublisherName === 'GitHub') {
-                    this.PublisherDetails({
-                        RepositoryOwner: publisherDetails.RepositoryOwner || '',
-                        Repository: publisherDetails.Repository || '',
-                        WorkflowFile: publisherDetails.WorkflowFile || '',
-                        Environment: publisherDetails.Environment || '',
-                        Name: "GitHub",
-                    });
-                }
+
+                // Provider specific properties
+                _gitHubDetails.Update(this, data);
             };
             
             this.PackageOwners = packageOwners;
@@ -123,7 +131,7 @@
                 $.validator.unobtrusive.parse($form);
                 var $validator = $form.validate();
 
-                // Immediately validate the description and glob pattern.
+                // Immediately validate the description
                 $validator.submitted[self.DescriptionId()] = null;
             }
 
@@ -272,12 +280,12 @@
         function TrustedPublisherListViewModel(initialData) {
             var self = this;
 
-            var trustedPublisher = $.map(initialData.TrustedPublishers, function (data) {
+            var trustedPublishers = $.map(initialData.TrustedPublishers, function (data) {
                 return new TrustedPublisherViewModel(self, initialData.PackageOwners, data);
             });
             var newTrustedPublisher = new TrustedPublisherViewModel(self, initialData.PackageOwners);
 
-            this.TrustedPublishers = ko.observableArray(trustedPublisher);
+            this.TrustedPublishers = ko.observableArray(trustedPublishers);
             this.NewTrustedPublisher = ko.observable(newTrustedPublisher);
             this.Error = ko.observable();
 
