a32a725f7401240d8f579c64ddf2bb2fa77715de
ET <evgenyt@microsoft.com>
2025-07-16 11:44:43 -0700

    Move GitHub OIDC params to service layer to be reused on back end

--- src/NuGetGallery/ViewModels/GitHubPolicyDetailsViewModel.cs
+++ src/NuGetGallery/ViewModels/GitHubPolicyDetailsViewModel.cs
@@ -5,10 +5,12 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
-using System.Globalization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NuGet.Services.Entities;
+using NuGetGallery.Services.Authentication;
+#nullable enable
+
 
 namespace NuGetGallery
 {
@@ -22,15 +24,12 @@ namespace NuGetGallery
     public sealed class GitHubPolicyDetailsViewModel : TrustedPublisherPolicyDetailsViewModel
     {
         public const int ValidationExpirationDays = 7;
-        private string _repositoryOwner = string.Empty;
-        private string _repositoryOwnerId = string.Empty;
-        private string _repository = string.Empty;
-        private string _repositoryId = string.Empty;
-        private string _workflowFile = string.Empty;
-        private string _environment = string.Empty;
 
-        public GitHubPolicyDetailsViewModel()
+        private readonly GitHubCriteria _criteria;
+
+        public GitHubPolicyDetailsViewModel(GitHubCriteria criteria)
         {
+            _criteria = criteria ?? new GitHubCriteria();
         }
 
         public override FederatedCredentialType PublisherType => FederatedCredentialType.GitHubActions;
@@ -41,17 +40,17 @@ namespace NuGetGallery
         [Required]
         public string RepositoryOwner
         {
-            get => _repositoryOwner;
-            set => _repositoryOwner = value?.Trim() ?? string.Empty;
+            get => _criteria.RepositoryOwner;
+            set => _criteria.RepositoryOwner = value;
         }
 
         /// <summary>
         /// GitHub repository owner id. Obtained from GitHub.
         /// </summary>
-        public string RepositoryOwnerId
+        public string? RepositoryOwnerId
         {
-            get => _repositoryOwnerId;
-            set => _repositoryOwnerId = value?.Trim() ?? string.Empty;
+            get => _criteria.RepositoryOwnerId ?? string.Empty;
+            set => _criteria.RepositoryOwnerId = value;
         }
 
         /// <summary>
@@ -60,17 +59,17 @@ namespace NuGetGallery
         [Required]
         public string Repository
         {
-            get => _repository;
-            set => _repository = value?.Trim() ?? string.Empty;
+            get => _criteria.Repository;
+            set => _criteria.Repository = value;
         }
 
         /// <summary>
         /// GitHub repository id. Obtained from GitHub.
         /// </summary>
-        public string RepositoryId
+        public string? RepositoryId
         {
-            get => _repositoryId;
-            set => _repositoryId = value?.Trim() ?? string.Empty;
+            get => _criteria.RepositoryId;
+            set => _criteria.RepositoryId = value;
         }
 
         /// <summary>
@@ -79,23 +78,18 @@ namespace NuGetGallery
         [Required]
         public string WorkflowFile
         {
-            get => _workflowFile;
-            set => _workflowFile = value?.Trim() ?? string.Empty;
+            get => _criteria.WorkflowFile;
+            set => _criteria.WorkflowFile = value;
         }
 
         /// <summary>
         /// GitHub Action environment name, e.g. production.
         /// </summary>
-        public string Environment
+        public string? Environment
         {
-            get => _environment;
-            set => _environment = value?.Trim() ?? string.Empty;
+            get => _criteria.Environment;
+            set => _criteria.Environment = value;
         }
-
-        /// <summary>
-        /// GitHub policy is considered validated when owner and repo IDs are set.
-        /// </summary>
-        public bool IsPermanentlyEnabled => !string.IsNullOrEmpty(RepositoryOwnerId) && !string.IsNullOrEmpty(RepositoryId);
 
         /// <summary>
         /// UTC date and time when the publisher details need to be validated by.
@@ -105,7 +99,16 @@ namespace NuGetGallery
         /// The policy can be created without these IDs, and later validated upon first use
         /// or user manually updating the policy.
         /// </remarks>
-        public DateTimeOffset? ValidateByDate { get; set; }
+        public DateTimeOffset? ValidateByDate
+        {
+            get => _criteria.ValidateByDate;
+            set => _criteria.ValidateByDate = value;
+        }
+
+        /// <summary>
+        /// GitHub policy is considered validated when owner and repo IDs are set.
+        /// </summary>
+        public bool IsPermanentlyEnabled => !string.IsNullOrEmpty(RepositoryOwnerId) && !string.IsNullOrEmpty(RepositoryId);
 
         public int EnabledDaysLeft
         {
@@ -126,16 +129,15 @@ namespace NuGetGallery
             }
         }
 
-        public override string Validate()
+        public override string? Validate()
         {
             InitializeValidateByDate();
             return ValidateInternal();
         }
 
-        private string ValidateInternal()
+        private string? ValidateInternal()
         {
             var errors = new List<string>();
-
 
             if (string.IsNullOrEmpty(RepositoryOwner))
             {
@@ -161,7 +163,7 @@ namespace NuGetGallery
             if (!IsPermanentlyEnabled)
             {
                 // Make sure we have both or nothing, owner and repo IDs. Round date to the hour.
-                _repositoryOwnerId = _repositoryId = string.Empty;
+                RepositoryOwnerId = RepositoryId = string.Empty;
                 DateTimeOffset date = DateTimeOffset.UtcNow + TimeSpan.FromDays(ValidationExpirationDays);
                 ValidateByDate = new DateTimeOffset(date.Year, date.Month, date.Day, date.Hour, 0, 0, TimeSpan.Zero);
             }
@@ -174,110 +176,58 @@ namespace NuGetGallery
         /// <inheritdoc />
         public override TrustedPublisherPolicyDetailsViewModel Update(string viewJson)
         {
-            var model = new GitHubPolicyDetailsViewModel();
-            model._repositoryOwner = _repositoryOwner;
-            model._repositoryOwnerId = _repositoryOwnerId;
-            model._repository = _repository;
-            model._repositoryId = _repositoryId;
-            model._workflowFile = _workflowFile;
-            model._environment = _environment;
-            model.ValidateByDate = ValidateByDate;
-
+            var model = new GitHubPolicyDetailsViewModel(_criteria.Clone());
             model.UpdateFromViewJson(viewJson);
             return model;
         }
 
         public static GitHubPolicyDetailsViewModel FromViewJson(string json)
         {
-            var model = new GitHubPolicyDetailsViewModel();
+            var model = new GitHubPolicyDetailsViewModel(new GitHubCriteria());
             model.UpdateFromViewJson(json);
             return model;
         }
 
         private void UpdateFromViewJson(string json)
-        { 
+        {
             var properties = JObject.Parse(json);
 
             // MUST MATCH GitHub details serialization in page-trusted-publishing.js.
             if (properties.TryGetValue(nameof(RepositoryOwner), out var owner))
             {
-                RepositoryOwner = owner?.ToString();
+                RepositoryOwner = owner.ToString();
             }
             if (properties.TryGetValue(nameof(RepositoryOwnerId), out var ownerId))
             {
-                RepositoryOwnerId = ownerId?.ToString();
+                RepositoryOwnerId = ownerId.ToString();
             }
 
             if (properties.TryGetValue(nameof(Repository), out var repository))
             {
-                Repository = repository?.ToString();
+                Repository = repository.ToString();
             }
             if (properties.TryGetValue(nameof(RepositoryId), out var repositoryId))
             {
-                RepositoryId = repositoryId?.ToString();
+                RepositoryId = repositoryId.ToString();
             }
 
             if (properties.TryGetValue(nameof(WorkflowFile), out var workflowFile))
             {
-                WorkflowFile = workflowFile?.ToString();
+                WorkflowFile = workflowFile.ToString();
             }
             if (properties.TryGetValue(nameof(Environment), out var environment))
             {
-                Environment = environment?.ToString();
+                Environment = environment.ToString();
             }
         }
 
-        public override string ToDatabaseJson()
-        {
-            // When storing in database we want to serialize the object differently than when passing to JavaScript.
-            // This gives us flexibility to change the model without breaking existing data.
-            var properties = new Dictionary<string, object>
-            {
-                { "owner", RepositoryOwner },
-                { "repository", Repository },
-                { "workflow", WorkflowFile },
-            };
-
-            if (!string.IsNullOrEmpty(RepositoryOwnerId))
-            {
-                properties["ownerId"] = RepositoryOwnerId;
-            }
-            if (!string.IsNullOrEmpty(RepositoryId))
-            {
-                properties["repositoryId"] = RepositoryId;
-            }
-            if (!string.IsNullOrEmpty(Environment))
-            {
-                properties["environment"] = Environment;
-            }
-            if (ValidateByDate.HasValue)
-            {
-                properties["validateBy"] = properties["validateBy"] = ValidateByDate.Value.ToString("yyyy-MM-ddTHH:00:00Z"); // UTC format with hour precision
-            }
-
-            // Serialize to JSON
-            string json = JsonConvert.SerializeObject(properties, Formatting.None);
-            return json;
-        }
+        public override string ToDatabaseJson() => _criteria.ToDatabaseJson();
 
         public static GitHubPolicyDetailsViewModel FromDatabaseJson(string json)
         {
+            var criteria = GitHubCriteria.FromDatabaseJson(json);
             var properties = JObject.Parse(json);
-            var model = new GitHubPolicyDetailsViewModel
-            {
-                RepositoryOwner = properties["owner"]?.ToString(),
-                RepositoryOwnerId = properties["ownerId"]?.ToString(),
-
-                Repository = properties["repository"]?.ToString(),
-                RepositoryId = properties["repositoryId"]?.ToString(),
-
-                WorkflowFile = properties["workflow"]?.ToString(),
-                Environment = properties["environment"]?.ToString(),
-
-                // ValidateByDate = DateTimeOffset.TryParseExact(properties["validateBy"]?.ToString(), "yyyy-MM-ddTHH:00:00Z", null, DateTimeStyles.AssumeUniversal, out var validateBy) ? validateBy : null,
-                ValidateByDate = DateTimeOffset.TryParse(properties["validateBy"]?.ToString(), null, DateTimeStyles.AssumeUniversal, out var validateBy) ? validateBy : null,
-            };
-
+            var model = new GitHubPolicyDetailsViewModel(criteria);
             if (model.ValidateInternal() is string error)
             {
                 throw new InvalidOperationException($"Invalid GitHub policy details: {error}");
