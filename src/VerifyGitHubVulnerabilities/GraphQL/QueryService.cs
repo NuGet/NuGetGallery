4843cd57d92aee144ab67d51145d496f4a0519bd
Joel Verhagen <jver@microsoft.com>
2025-08-27 13:47:40 -0400

    Add retries to GraphQL HTTP requests in VerifyGitHubVulnerabilities

--- src/VerifyGitHubVulnerabilities/GraphQL/QueryService.cs
+++ src/VerifyGitHubVulnerabilities/GraphQL/QueryService.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -11,6 +11,7 @@ using NuGet.Services.GitHub.GraphQL;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using VerifyGitHubVulnerabilities.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace VerifyGitHubVulnerabilities.GraphQL
 {
@@ -23,13 +24,16 @@ namespace VerifyGitHubVulnerabilities.GraphQL
 
         private readonly VerifyGitHubVulnerabilitiesConfiguration _configuration;
         private readonly HttpClient _client;
+        private readonly ILogger<QueryService> _logger;
 
         public QueryService(
             VerifyGitHubVulnerabilitiesConfiguration configuration,
-            HttpClient client)
+            HttpClient client,
+            ILogger<QueryService> logger)
         {
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
             _client = client ?? throw new ArgumentNullException(nameof(client));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public async Task<QueryResponse> QueryAsync(string query, CancellationToken token)
@@ -45,10 +49,26 @@ namespace VerifyGitHubVulnerabilities.GraphQL
 
         private async Task<string> MakeWebRequestAsync(string query, CancellationToken token)
         {
-            using (var request = CreateRequest(query))
-            using (var response = await _client.SendAsync(request, token))
+            var attempt = 0;
+            const int maxAttempts = 5;
+            while (true)
             {
-                return await response.Content.ReadAsStringAsync();
+                try
+                {
+                    attempt++;
+                    using (var request = CreateRequest(query))
+                    using (var response = await _client.SendAsync(request, token))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        return await response.Content.ReadAsStringAsync();
+                    }
+                }
+                catch (Exception ex) when (!token.IsCancellationRequested && attempt < maxAttempts)
+                {
+                    var delay = TimeSpan.FromSeconds(3);
+                    _logger.LogWarning(ex, "Failed attempt {Attempt} to query GitHub GraphQL API. Waiting {Seconds} seconds.", attempt, delay.TotalSeconds);
+                    await Task.Delay(delay, token);
+                }
             }
         }
 
