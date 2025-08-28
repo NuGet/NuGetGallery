1c171225fa1bd02ea0413fcf5ffe3119ad90983f
Joel Verhagen <jver@microsoft.com>
2025-08-27 17:53:54 -0700

    Add retry to GraphQL query service, unify implementations (#10562)
    * Add retries to GraphQL HTTP requests in VerifyGitHubVulnerabilities
    
    * Add retry to QueryService, unify copied implementation

--- tests/GitHubVulnerabilities2Db.Facts/QueryServiceFacts.cs
+++ tests/GitHubVulnerabilities2Db.Facts/QueryServiceFacts.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
@@ -9,126 +9,190 @@ using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using GitHubVulnerabilities2Db.Configuration;
-using NuGet.Services.GitHub.GraphQL;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using NuGet.Services.GitHub.GraphQL;
+using NuGetGallery.TestUtils;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace GitHubVulnerabilities2Db.Facts
 {
-    public class QueryServiceFacts
-    {
-        private readonly GitHubVulnerabilities2DbConfiguration _configuration;
-        private readonly QueryServiceHttpClientHandler _handler;
-        private readonly QueryService _service;
+	public class QueryServiceFacts
+	{
+		private readonly GitHubVulnerabilities2DbConfiguration _configuration;
+		private readonly MockHandler _mockHandler;
+		private readonly QueryService _service;
+		private Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handlerFunction;
 
-        public QueryServiceFacts()
-        {
-            _configuration = new GitHubVulnerabilities2DbConfiguration();
-            _handler = new QueryServiceHttpClientHandler();
-            _service = new QueryService(
-                _configuration, new HttpClient(_handler));
-        }
+		public QueryServiceFacts(ITestOutputHelper output)
+		{
+			_configuration = new GitHubVulnerabilities2DbConfiguration
+			{
+				GitHubPersonalAccessToken = "fake-token",
+				UserAgent = "NuGet.GitHubVulnerabilities2Db",
+				RetryDelay = TimeSpan.Zero,
+			};
+			_handlerFunction = (r, c) => Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+			{
+				Content = new StringContent("{}")
+			});
+			_mockHandler = new MockHandler((r, c) => _handlerFunction(r, c));
 
-        [Fact]
-        public async Task Success()
-        {
+			var loggerFactory = new LoggerFactory().AddXunit(output);
+			_service = new QueryService(_configuration, new HttpClient(_mockHandler), loggerFactory.CreateLogger<QueryService>());
+		}
+
+		[Fact]
+		public async Task QueryAsync_ReturnsDeserializedResponse_OnSuccess()
+		{
+			// Arrange
+			var expected = new QueryResponse { Data = new QueryResponseData() };
+			_handlerFunction = (req, ct) => Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+			{
+				Content = new StringContent(JsonConvert.SerializeObject(expected))
+			});
+
+			// Act
+			var result = await _service.QueryAsync("query { test }", CancellationToken.None);
+
+			// Assert
+			Assert.NotNull(result);
+			Assert.NotNull(result.Data);
+		}
+
+		[Fact]
+		public async Task QueryAsync_RetriesOn5xx_ThenSucceeds()
+		{
+			// Arrange
+			var callCount = 0;
+			_handlerFunction = async (req, ct) =>
+			{
+				await Task.Yield();
+				callCount++;
+				if (callCount < 3)
+				{
+					return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+				}
+				return new HttpResponseMessage(HttpStatusCode.OK)
+				{
+					Content = new StringContent(JsonConvert.SerializeObject(new QueryResponse { Data = new QueryResponseData() }))
+				};
+			};
+
+			// Act
+			var result = await _service.QueryAsync("query { test }", CancellationToken.None);
+
+			// Assert
+			Assert.NotNull(result);
+			Assert.Equal(3, callCount);
+		}
+
+		[Fact]
+		public async Task QueryAsync_RetriesOn5xx_ThenFails()
+		{
+			// Arrange
+			var callCount = 0;
+			_handlerFunction = async (req, ct) =>
+			{
+				await Task.Yield();
+				callCount++;
+				return new HttpResponseMessage(HttpStatusCode.InternalServerError);
+			};
+
+			// Act & Assert
+			await Assert.ThrowsAsync<HttpRequestException>(() => _service.QueryAsync("query { test }", CancellationToken.None));
+			Assert.Equal(5, callCount);
+		}
+
+		[Fact]
+		public async Task QueryAsync_DoesNotRetryOn4xx()
+		{
+			// Arrange
+			var callCount = 0;
+			_handlerFunction = (req, ct) =>
+			{
+				callCount++;
+				return Task.FromResult(new HttpResponseMessage(HttpStatusCode.BadRequest));
+			};
+
+			// Act & Assert
+			await Assert.ThrowsAsync<HttpRequestException>(() => _service.QueryAsync("query { test }", CancellationToken.None));
+			Assert.Equal(1, callCount);
+		}
+
+		[Fact]
+		public async Task QueryAsync_ThrowsIfCancelled()
+		{
+			// Arrange
+			var cts = new CancellationTokenSource();
+			cts.Cancel();
+			_handlerFunction = (req, ct) =>
+			{
+				ct.ThrowIfCancellationRequested();
+				return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK));
+			};
+
+			// Act & Assert
+			await Assert.ThrowsAnyAsync<OperationCanceledException>(() => _service.QueryAsync("query { test }", cts.Token));
+		}
+
+		[Fact]
+		public async Task QueryAsync_SetsUserAgentAndAuthorizationHeaders()
+		{
+			// Arrange
+			HttpRequestMessage capturedRequest = null;
+			_handlerFunction = (req, ct) =>
+			{
+				capturedRequest = req;
+				return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+				{
+					Content = new StringContent(JsonConvert.SerializeObject(new QueryResponse { Data = new QueryResponseData() }))
+				});
+			};
+
+			// Act
+			await _service.QueryAsync("query { test }", CancellationToken.None);
+
+			// Assert
+			Assert.NotNull(capturedRequest);
+			Assert.Equal("Bearer", capturedRequest.Headers.Authorization?.Scheme);
+			Assert.Equal(_configuration.GitHubPersonalAccessToken, capturedRequest.Headers.Authorization?.Parameter);
+			Assert.Contains(_configuration.UserAgent, capturedRequest.Headers.UserAgent.ToString());
+		}
+
+		[Fact]
+		public async Task QueryAsync_ErrorStatusCodeIsRejected()
+		{
             // Arrange
-            var query = "someString";
-            _handler.ExpectedQueryContent = (new JObject { ["query"] = query }).ToString();
-
-            _handler.ExpectedEndpoint = new Uri("https://graphQL.net");
-            _configuration.GitHubGraphQLQueryEndpoint = _handler.ExpectedEndpoint;
-
-            _handler.ExpectedApiKey = "patpatpat";
-            _configuration.GitHubPersonalAccessToken = _handler.ExpectedApiKey;
-
-            var response = new QueryResponse();
-            _handler.ResponseMessage = new HttpResponseMessage
+            var callCount = 0;
+            _handlerFunction = (req, ct) =>
             {
-                Content = new StringContent(JsonConvert.SerializeObject(response))
+                callCount++;
+                return Task.FromResult(new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("{}")
+                });
             };
 
-            // Act
-            await _service.QueryAsync(query, new CancellationToken());
+			// Act & Assert
+			await Assert.ThrowsAsync<HttpRequestException>(() => _service.QueryAsync("query { test }", CancellationToken.None));
+            Assert.Equal(1, callCount);
+		}
 
-            // Assert
-            Assert.True(_handler.WasCalled);
-        }
+		[Fact]
+		public async Task QueryAsync_ErrorResponseJsonIsRejected()
+		{
+			// Arrange
+			var response = new QueryResponse { Errors = new List<QueryError> { new QueryError { Message = "Query = not great" } } };
+			_handlerFunction = (req, ct) => Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+			{
+				Content = new StringContent(JsonConvert.SerializeObject(response))
+			});
 
-        [Fact]
-        public async Task ErrorStatusCodeIsRejected()
-        {
-            // Arrange
-            var query = "someString";
-            _handler.ExpectedQueryContent = (new JObject { ["query"] = query }).ToString();
-
-            _handler.ExpectedEndpoint = new Uri("https://graphQL.net");
-            _configuration.GitHubGraphQLQueryEndpoint = _handler.ExpectedEndpoint;
-
-            _handler.ExpectedApiKey = "patpatpat";
-            _configuration.GitHubPersonalAccessToken = _handler.ExpectedApiKey;
-
-            var response = new QueryResponse();
-            _handler.ResponseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest)
-            {
-                Content = new StringContent(JsonConvert.SerializeObject(response))
-            };
-
-            // Act & Assert
-            var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.QueryAsync(query, new CancellationToken()));
-            Assert.True(_handler.WasCalled);
-            Assert.Equal("The GitHub GraphQL response returned status code 400 Bad Request. Response body:\r\n{\"Data\":null,\"Errors\":null}", ex.Message);
-        }
-
-        [Fact]
-        public async Task ErrorResponseJsonIsRejected()
-        {
-            // Arrange
-            var query = "someString";
-            _handler.ExpectedQueryContent = (new JObject { ["query"] = query }).ToString();
-
-            _handler.ExpectedEndpoint = new Uri("https://graphQL.net");
-            _configuration.GitHubGraphQLQueryEndpoint = _handler.ExpectedEndpoint;
-
-            _handler.ExpectedApiKey = "patpatpat";
-            _configuration.GitHubPersonalAccessToken = _handler.ExpectedApiKey;
-
-            var response = new QueryResponse { Errors = new List<QueryError> { new QueryError { Message = "Query = not great" } } };
-            _handler.ResponseMessage = new HttpResponseMessage(HttpStatusCode.OK)
-            {
-                Content = new StringContent(JsonConvert.SerializeObject(response))
-            };
-
-            // Act & Assert
-            var ex = await Assert.ThrowsAsync<InvalidOperationException>(() => _service.QueryAsync(query, new CancellationToken()));
-            Assert.True(_handler.WasCalled);
-            Assert.Equal("The GitHub GraphQL response returned errors in the response JSON. Response body:\r\n{\"Data\":null,\"Errors\":[{\"Message\":\"Query = not great\"}]}", ex.Message);
-        }
-
-        private class QueryServiceHttpClientHandler : HttpClientHandler
-        {
-            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-            {
-                WasCalled = true;
-
-                Assert.Equal(HttpMethod.Post, request.Method);
-                Assert.Equal(ExpectedEndpoint, request.RequestUri);
-                Assert.Equal(ExpectedQueryContent, request.Content.ReadAsStringAsync().Result);
-                Assert.Equal(
-                    new AuthenticationHeaderValue("Bearer", ExpectedApiKey),
-                    request.Headers.Authorization);
-
-                Assert.Contains(ProductInfoHeaderValue.Parse(QueryService.UserAgent), request.Headers.UserAgent);
-                return Task.FromResult(ResponseMessage);
-            }
-
-            public Uri ExpectedEndpoint { get; set; }
-            public string ExpectedQueryContent { get; set; }
-            public string ExpectedApiKey { get; set; }
-            public HttpResponseMessage ResponseMessage { get; set; }
-
-            public bool WasCalled { get; private set; }
-        }
-    }
+			// Act & Assert
+			await Assert.ThrowsAsync<InvalidOperationException>(() => _service.QueryAsync("query { test }", CancellationToken.None));
+		}
+	}
 }
