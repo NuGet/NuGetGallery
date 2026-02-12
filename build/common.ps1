5d2a7ef554f0056c8813a5ccdbe79c1b2ac069f8
Joel Verhagen <jver@microsoft.com>
2026-02-11 20:11:22 -0500

    Revert "Try simple"
    This reverts commit da4b181828e7f416910da587473954cc68902628.

--- build/common.ps1
+++ build/common.ps1
@@ -200,19 +200,24 @@ Function Sign-Binaries {
         [int]$BuildNumber = (Get-BuildNumber),
         [string]$MSBuildVersion = $DefaultMSBuildVersion,
         [string[]]$ProjectsToSign = $null,
-        [switch]$BinLog
+        [switch]$BinLog,
+        [string]$RepositoryRootDirectory = $null
     )
 
-    if ($ProjectsToSign -eq $null) {
+    if ($null -eq $ProjectsToSign) {
         $repositoryDir = [IO.Path]::GetDirectoryName($PSScriptRoot)
         $defaultProjectsToSign = Join-Path $repositoryDir "src\**\*.csproj"
         $ProjectsToSign = @($defaultProjectsToSign)
     }
 
     $projectsToSignProperty = $ProjectsToSign -join ';'
+    $msbuildProperties = "/p:ProjectsToSign=`"$projectsToSignProperty`""
+    if ($RepositoryRootDirectory) {
+        $msbuildProperties += ";RepositoryRootDirectory=`"$RepositoryRootDirectory`""
+    }
 
     $ProjectPath = Join-Path $PSScriptRoot "sign-binaries.proj"
-    Build-Solution $Configuration $BuildNumber -MSBuildVersion "$MSBuildVersion" $ProjectPath -MSBuildProperties "/p:ProjectsToSign=`"$projectsToSignProperty`"" -BinLog:$BinLog
+    Build-Solution $Configuration $BuildNumber -MSBuildVersion "$MSBuildVersion" $ProjectPath -MSBuildProperties $msbuildProperties -BinLog:$BinLog
 }
 
 Function Sign-Packages {
