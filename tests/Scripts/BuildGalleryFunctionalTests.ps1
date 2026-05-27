21bdcb86b3b0271c9312c70d532a92cd436438c1
Pranathi Rao Bora <prabora@microsoft.com>
2026-05-27 15:47:01 -0700

    Upgraded functional tests projects and dependencies to .NET 10

--- tests/Scripts/BuildGalleryFunctionalTests.ps1
+++ tests/Scripts/BuildGalleryFunctionalTests.ps1
@@ -7,33 +7,21 @@ $parentDir = Resolve-Path (Join-Path $PSScriptRoot "..")
 $repoDir = Resolve-Path (Join-Path $parentDir "..")
 
 # Required tools
-$BuiltInVsWhereExe = "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-$VsInstallationPath = & $BuiltInVsWhereExe -latest -prerelease -property installationPath
-$msbuild = Join-Path $VsInstallationPath "MSBuild\Current\Bin\msbuild"
 $nuget = Join-Path $parentDir "nuget.exe"
 & (Join-Path $PSScriptRoot "DownloadLatestNuGetExeRelease.ps1") $parentDir
 
 Write-Host "##[group]Restoring and building functional tests"
-Write-Host "Restoring solution tools"
-& $nuget install (Join-Path $repoDir "packages.config") -SolutionDirectory $repoDir -NonInteractive -ExcludeVersion
 
-# Restore packages
-Write-Host "Restoring solution"
+# Restore and build using dotnet CLI
+Write-Host "Restoring and building solution"
 $solutionPath = Join-Path $repoDir "NuGetGallery.FunctionalTests.sln"
-& $nuget restore $solutionPath -NonInteractive
-if ($LASTEXITCODE -ne 0) {
-    throw "Failed to restore packages!"
-}
-
-# Build the solution
-Write-Host "Building solution"
-& $msbuild $solutionPath "/p:Configuration=$Configuration" ('/p:VSINSTALLDIR=' + $VsInstallationPath)
+& dotnet build $solutionPath --configuration $Configuration
 if ($LASTEXITCODE -ne 0) {
     throw "Failed to build solution!"
 }
 
 Write-Host "Copying nuget.exe to functional tests directory"
-$functionalTestsDirectory = Join-Path $parentDir "NuGetGallery.FunctionalTests\bin\$Configuration\net8.0"
+$functionalTestsDirectory = Join-Path $parentDir "NuGetGallery.FunctionalTests\bin\$Configuration\net10.0"
 Copy-Item $nuget $functionalTestsDirectory
 Write-Host "##[endgroup]"
 
