cc11ac55d2dbf440450ec26a2759a8d7d25729ab
Lanaparezanin <lanaparezanin@gmail.com>
2026-08-12 14:46:53 -0700

    First commit

--- src/NuGetGallery/ViewModels/ApiKeyListViewModel.cs
+++ src/NuGetGallery/ViewModels/ApiKeyListViewModel.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
@@ -11,5 +11,6 @@ namespace NuGetGallery
         public int ExpirationInDaysForApiKeyV1 { get; set; }
         public IList<ApiKeyOwnerViewModel> PackageOwners { get; set; }
         public bool IsDeprecationApiEnabled { get; set; }
+        public bool IsApiKeyExpirationRestricted { get; set; }
     }
 }
