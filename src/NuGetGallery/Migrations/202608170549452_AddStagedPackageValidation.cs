5d491de5d81321c78ade13a00d39701870b9772a
James Parsons <59936622+japarson@users.noreply.github.com>
2026-08-26 12:32:52 -0700

    Remove sdl delete during migration

--- src/NuGetGallery/Migrations/202608170549452_AddStagedPackageValidation.cs
+++ src/NuGetGallery/Migrations/202608170549452_AddStagedPackageValidation.cs
@@ -7,11 +7,6 @@
     {
         public override void Up()
         {
-            Sql(@"
-                DELETE p
-                FROM [dbo].[Packages] AS p
-                INNER JOIN [dbo].[StagedPackages] AS s ON s.[PackageKey] = p.[Key]");
-
             AddColumn("dbo.StagedPackages", "BlobETag", c => c.String(nullable: false, maxLength: 256));
             AddColumn("dbo.StagedPackages", "Status", c => c.Int(nullable: false));
             AddColumn("dbo.StagedPackages", "ValidationTrackingId", c => c.Guid(nullable: false));
