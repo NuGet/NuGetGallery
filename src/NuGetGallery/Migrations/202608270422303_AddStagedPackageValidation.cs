88a5922b46006be73cd961043370c520d8b2162a
James Parsons <59936622+japarson@users.noreply.github.com>
2026-08-27 16:15:37 -0700

    Add unique key to validation migration

--- src/NuGetGallery/Migrations/202608270422303_AddStagedPackageValidation.cs
+++ src/NuGetGallery/Migrations/202608270422303_AddStagedPackageValidation.cs
@@ -7,16 +7,22 @@
     {
         public override void Up()
         {
+            DropPrimaryKey("dbo.StagedPackages");
+            AddColumn("dbo.StagedPackages", "Key", c => c.Int(nullable: false, identity: true));
             AddColumn("dbo.StagedPackages", "BlobETag", c => c.String(nullable: false, maxLength: 256));
             AddColumn("dbo.StagedPackages", "Status", c => c.Int(nullable: false));
             AddColumn("dbo.StagedPackages", "RowVersion", c => c.Binary(nullable: false, fixedLength: true, timestamp: true, storeType: "rowversion"));
+            AddPrimaryKey("dbo.StagedPackages", "Key");
         }
         
         public override void Down()
         {
+            DropPrimaryKey("dbo.StagedPackages");
             DropColumn("dbo.StagedPackages", "RowVersion");
             DropColumn("dbo.StagedPackages", "Status");
             DropColumn("dbo.StagedPackages", "BlobETag");
+            DropColumn("dbo.StagedPackages", "Key");
+            AddPrimaryKey("dbo.StagedPackages", "PackageKey");
         }
     }
 }
