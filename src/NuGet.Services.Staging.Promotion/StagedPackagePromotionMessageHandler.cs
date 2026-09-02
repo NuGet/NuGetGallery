dd3949147100b5db9a05d1e6ad27901300675390
James Parsons <59936622+japarson@users.noreply.github.com>
2026-09-02 12:03:06 -0700

    Add staged package promotion worker happy path

---
+++ src/NuGet.Services.Staging.Promotion/StagedPackagePromotionMessageHandler.cs
@@ -0,0 +1,155 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using NuGet.Services.Entities;
+using NuGet.Services.ServiceBus;
+using NuGetGallery;
+using NuGetGallery.Packaging;
+
+namespace NuGet.Services.Staging.Promotion
+{
+    public class StagedPackagePromotionMessageHandler : IMessageHandler<StagedPackagePromotionMessage>
+    {
+        private readonly IEntityRepository<StagedPackage> _stagedPackageRepository;
+        private readonly ICorePackageService _packageService;
+        private readonly IStagingBlobService _stagingBlobService;
+        private readonly ICoreFileStorageService _packageFileStorageService;
+        private readonly IFileMetadataService _packageFileMetadataService;
+        private readonly ICoreLicenseFileService _licenseFileService;
+        private readonly ICoreReadmeFileService _readmeFileService;
+
+        public StagedPackagePromotionMessageHandler(
+            IEntityRepository<StagedPackage> stagedPackageRepository,
+            ICorePackageService packageService,
+            IStagingBlobService stagingBlobService,
+            ICoreFileStorageService packageFileStorageService,
+            IFileMetadataService packageFileMetadataService,
+            ICoreLicenseFileService licenseFileService,
+            ICoreReadmeFileService readmeFileService)
+        {
+            _stagedPackageRepository = stagedPackageRepository ?? throw new ArgumentNullException(nameof(stagedPackageRepository));
+            _packageService = packageService ?? throw new ArgumentNullException(nameof(packageService));
+            _stagingBlobService = stagingBlobService ?? throw new ArgumentNullException(nameof(stagingBlobService));
+            _packageFileStorageService = packageFileStorageService ?? throw new ArgumentNullException(nameof(packageFileStorageService));
+            _packageFileMetadataService = packageFileMetadataService ?? throw new ArgumentNullException(nameof(packageFileMetadataService));
+            _licenseFileService = licenseFileService ?? throw new ArgumentNullException(nameof(licenseFileService));
+            _readmeFileService = readmeFileService ?? throw new ArgumentNullException(nameof(readmeFileService));
+        }
+
+        public async Task<bool> HandleAsync(StagedPackagePromotionMessage message)
+        {
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            var stagedPackage = _stagedPackageRepository
+                .GetAll()
+                .Include(candidate => candidate.Package.PackageRegistration.Owners)
+                .SingleOrDefault(candidate => candidate.Key == message.StagedPackageKey);
+            if (!CanPromote(stagedPackage, message.PromotionId))
+            {
+                return true;
+            }
+
+            var package = stagedPackage.Package;
+            var streamMetadata = await GetStreamMetadataAsync(stagedPackage);
+            await PublishPackageAsync(package, stagedPackage);
+            await ExtractPackageContentAsync(package, stagedPackage);
+            await CompletePromotionAsync(package, stagedPackage, streamMetadata);
+
+            return true;
+        }
+
+        private static bool CanPromote(StagedPackage stagedPackage, Guid promotionId)
+        {
+            return stagedPackage != null
+                && stagedPackage.Status == StagedPackageStatus.Promoting
+                && stagedPackage.ActivePromotionId == promotionId
+                && stagedPackage.Package.PackageStatusKey == PackageStatus.Staged
+                && !string.IsNullOrWhiteSpace(stagedPackage.ValidatedBlobPath)
+                && !string.IsNullOrWhiteSpace(stagedPackage.ValidatedBlobETag)
+                && stagedPackage.Package.PackageRegistration.Owners.Any(owner => owner.Key == stagedPackage.OwnerKey);
+        }
+
+        private async Task<PackageStreamMetadata> GetStreamMetadataAsync(StagedPackage stagedPackage)
+        {
+            using (var packageStream = await _stagingBlobService.OpenPackageFileAsync(stagedPackage.ValidatedBlobPath, stagedPackage.ValidatedBlobETag))
+            {
+                return new PackageStreamMetadata
+                {
+                    HashAlgorithm = CoreConstants.Sha512HashAlgorithmId,
+                    Hash = CryptographyService.GenerateHash(packageStream, CoreConstants.Sha512HashAlgorithmId),
+                    Size = packageStream.Length,
+                };
+            }
+        }
+
+        private async Task PublishPackageAsync(Package package, StagedPackage stagedPackage)
+        {
+            var sourceUri = await _stagingBlobService.GetPackageReadUriAsync(stagedPackage.ValidatedBlobPath, stagedPackage.ValidatedBlobETag);
+
+            var packageFileName = FileNameHelper.BuildFileName(
+                package.PackageRegistration.Id,
+                package.NormalizedVersion,
+                _packageFileMetadataService.FileSavePathTemplate,
+                _packageFileMetadataService.FileExtension);
+
+            await _packageFileStorageService.CopyFileAsync(
+                sourceUri,
+                _packageFileMetadataService.FileFolderName,
+                packageFileName,
+                AccessConditionWrapper.GenerateIfNotExistsCondition());
+
+            await _packageFileStorageService.SetPropertiesAsync(
+                _packageFileMetadataService.FileFolderName,
+                packageFileName,
+                (_, properties) =>
+                {
+                    if (!string.Equals(properties.CacheControl, CoreConstants.DefaultCacheControl, StringComparison.OrdinalIgnoreCase))
+                    {
+                        properties.CacheControl = CoreConstants.DefaultCacheControl;
+                        return Task.FromResult(true);
+                    }
+
+                    return Task.FromResult(false);
+                });
+        }
+
+        private async Task ExtractPackageContentAsync(Package package, StagedPackage stagedPackage)
+        {
+            if (package.EmbeddedLicenseType == EmbeddedLicenseFileType.Absent && !package.HasEmbeddedReadme)
+            {
+                return;
+            }
+
+            using (var packageStream = await _stagingBlobService.OpenPackageFileAsync(stagedPackage.ValidatedBlobPath, stagedPackage.ValidatedBlobETag))
+            {
+                if (package.EmbeddedLicenseType != EmbeddedLicenseFileType.Absent)
+                {
+                    await _licenseFileService.ExtractAndSaveLicenseFileAsync(package, packageStream);
+                }
+
+                if (package.HasEmbeddedReadme)
+                {
+                    await _readmeFileService.ExtractAndSaveReadmeFileAsync(package, packageStream);
+                }
+            }
+        }
+
+        private async Task CompletePromotionAsync(Package package, StagedPackage stagedPackage, PackageStreamMetadata streamMetadata)
+        {
+            await _stagedPackageRepository.ExecuteInTransactionAsync(async () =>
+            {
+                await _packageService.UpdatePackageStreamMetadataAsync(package, streamMetadata, commitChanges: false);
+                await _packageService.UpdatePackageStatusAsync(package, PackageStatus.Available, commitChanges: false);
+                _stagedPackageRepository.DeleteOnCommit(stagedPackage);
+                await _stagedPackageRepository.CommitChangesAsync();
+            });
+        }
+    }
+}
---
+++ tests/NuGet.Services.Staging.Promotion.Tests/StagedPackagePromotionMessageHandlerFacts.cs
@@ -0,0 +1,266 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using NuGet.Services.Entities;
+using NuGetGallery;
+using Xunit;
+
+namespace NuGet.Services.Staging.Promotion.Tests
+{
+    public class StagedPackagePromotionMessageHandlerFacts
+    {
+        [Fact]
+        public async Task PublishesExactValidatedPackageAndRemovesStagingRow()
+        {
+            var context = new TestContext();
+
+            var handled = await context.Target.HandleAsync(context.Message);
+
+            Assert.True(handled);
+            Assert.Equal(PackageStatus.Available, context.Package.PackageStatusKey);
+            Assert.Equal(CoreConstants.Sha512HashAlgorithmId, context.Package.HashAlgorithm);
+            Assert.Equal(context.Content.Length, context.Package.PackageFileSize);
+            using (var sha512 = SHA512.Create())
+            {
+                Assert.Equal(
+                    Convert.ToBase64String(sha512.ComputeHash(context.Content)),
+                    context.Package.Hash);
+            }
+            context.StagingBlobService.Verify(
+                x => x.OpenPackageFileAsync(
+                    context.StagedPackage.ValidatedBlobPath,
+                    context.StagedPackage.ValidatedBlobETag),
+                Times.Once);
+            context.PackageFileStorageService.Verify(
+                x => x.CopyFileAsync(
+                    context.SourceUri,
+                    CoreConstants.Folders.PackagesFolderName,
+                    "example.package.1.2.3.nupkg",
+                    It.Is<IAccessCondition>(condition => condition.IfNoneMatchETag == "*")),
+                Times.Once);
+            Assert.Equal(CoreConstants.DefaultCacheControl, context.PublicBlobProperties.Object.CacheControl);
+            context.PackageService.Verify(
+                x => x.UpdatePackageStreamMetadataAsync(
+                    context.Package,
+                    It.IsAny<NuGetGallery.Packaging.PackageStreamMetadata>(),
+                    false),
+                Times.Once);
+            context.PackageService.Verify(
+                x => x.UpdatePackageStatusAsync(context.Package, PackageStatus.Available, false),
+                Times.Once);
+            context.StagedPackageRepository.Verify(
+                x => x.ExecuteInTransactionAsync(It.IsAny<Func<Task>>()),
+                Times.Once);
+            context.StagedPackageRepository.Verify(x => x.DeleteOnCommit(context.StagedPackage), Times.Once);
+            context.StagedPackageRepository.Verify(x => x.CommitChangesAsync(), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(InvalidState.MissingStagedPackage)]
+        [InlineData(InvalidState.NotPromoting)]
+        [InlineData(InvalidState.DifferentPromotion)]
+        [InlineData(InvalidState.PackageNotStaged)]
+        [InlineData(InvalidState.MissingValidatedBlob)]
+        [InlineData(InvalidState.OwnerNoLongerOwnsPackage)]
+        public async Task ConsumesMessageWithoutPublishingWhenPromotionIsNotCurrent(InvalidState state)
+        {
+            var context = new TestContext();
+            context.Apply(state);
+
+            var handled = await context.Target.HandleAsync(context.Message);
+
+            Assert.True(handled);
+            context.PackageFileStorageService.Verify(
+                x => x.CopyFileAsync(
+                    It.IsAny<Uri>(),
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<IAccessCondition>()),
+                Times.Never);
+            context.StagedPackageRepository.Verify(
+                x => x.ExecuteInTransactionAsync(It.IsAny<Func<Task>>()),
+                Times.Never);
+        }
+
+        [Fact]
+        public async Task ExtractsEmbeddedLicenseAndReadmeFromValidatedPackage()
+        {
+            var context = new TestContext();
+            context.Package.EmbeddedLicenseType = EmbeddedLicenseFileType.PlainText;
+            context.Package.EmbeddedReadmeType = EmbeddedReadmeFileType.Markdown;
+            context.Package.HasReadMe = true;
+
+            await context.Target.HandleAsync(context.Message);
+
+            context.LicenseFileService.Verify(
+                x => x.ExtractAndSaveLicenseFileAsync(context.Package, It.IsAny<Stream>()),
+                Times.Once);
+            context.ReadmeFileService.Verify(
+                x => x.ExtractAndSaveReadmeFileAsync(context.Package, It.IsAny<Stream>()),
+                Times.Once);
+            context.StagingBlobService.Verify(
+                x => x.OpenPackageFileAsync(
+                    context.StagedPackage.ValidatedBlobPath,
+                    context.StagedPackage.ValidatedBlobETag),
+                Times.Exactly(2));
+        }
+
+        public enum InvalidState
+        {
+            MissingStagedPackage,
+            NotPromoting,
+            DifferentPromotion,
+            PackageNotStaged,
+            MissingValidatedBlob,
+            OwnerNoLongerOwnsPackage,
+        }
+
+        private class TestContext
+        {
+            public TestContext()
+            {
+                Content = Encoding.UTF8.GetBytes("validated package content");
+                SourceUri = new Uri("https://example.test/staging/validated.nupkg");
+                PromotionId = Guid.NewGuid();
+                var owner = new User { Key = 23, Username = "owner" };
+                Package = new Package
+                {
+                    Key = 11,
+                    NormalizedVersion = "1.2.3",
+                    PackageStatusKey = PackageStatus.Staged,
+                    PackageRegistration = new PackageRegistration
+                    {
+                        Id = "Example.Package",
+                        Owners = new List<User> { owner },
+                        Packages = new List<Package>(),
+                    },
+                };
+                Package.PackageRegistration.Packages.Add(Package);
+                StagedPackage = new StagedPackage
+                {
+                    Key = 42,
+                    PackageKey = Package.Key,
+                    Package = Package,
+                    OwnerKey = owner.Key,
+                    Owner = owner,
+                    Status = StagedPackageStatus.Promoting,
+                    ActivePromotionId = PromotionId,
+                    ValidatedBlobPath = "example.package/1.2.3/validated.nupkg",
+                    ValidatedBlobETag = "\"validated\"",
+                    UploadedBlobPath = "example.package/1.2.3/uploaded.nupkg",
+                    UploadedBlobETag = "\"uploaded\"",
+                    UploadHash = "upload-hash",
+                };
+                Message = new StagedPackagePromotionMessage(PromotionId, StagedPackage.Key);
+                StagedPackages = new List<StagedPackage> { StagedPackage };
+
+                StagedPackageRepository = new Mock<IEntityRepository<StagedPackage>>();
+                StagedPackageRepository
+                    .Setup(x => x.GetAll())
+                    .Returns(() => StagedPackages.AsQueryable());
+                StagedPackageRepository
+                    .Setup(x => x.ExecuteInTransactionAsync(It.IsAny<Func<Task>>()))
+                    .Returns((Func<Task> action) => action());
+
+                PackageService = new Mock<ICorePackageService>();
+                PackageService
+                    .Setup(x => x.UpdatePackageStreamMetadataAsync(Package, It.IsAny<NuGetGallery.Packaging.PackageStreamMetadata>(), false))
+                    .Returns<Package, NuGetGallery.Packaging.PackageStreamMetadata, bool>((package, metadata, _) =>
+                    {
+                        package.Hash = metadata.Hash;
+                        package.HashAlgorithm = metadata.HashAlgorithm;
+                        package.PackageFileSize = metadata.Size;
+                        return Task.CompletedTask;
+                    });
+                PackageService
+                    .Setup(x => x.UpdatePackageStatusAsync(Package, PackageStatus.Available, false))
+                    .Returns<Package, PackageStatus, bool>((package, status, _) =>
+                    {
+                        package.PackageStatusKey = status;
+                        return Task.CompletedTask;
+                    });
+
+                StagingBlobService = new Mock<IStagingBlobService>();
+                StagingBlobService
+                    .Setup(x => x.OpenPackageFileAsync(StagedPackage.ValidatedBlobPath, StagedPackage.ValidatedBlobETag))
+                    .ReturnsAsync(() => new MemoryStream(Content));
+                StagingBlobService
+                    .Setup(x => x.GetPackageReadUriAsync(StagedPackage.ValidatedBlobPath, StagedPackage.ValidatedBlobETag))
+                    .ReturnsAsync(SourceUri);
+
+                PublicBlobProperties = new Mock<ICloudBlobProperties>();
+                PublicBlobProperties.SetupProperty(x => x.CacheControl, "private");
+                PackageFileStorageService = new Mock<ICoreFileStorageService>();
+                PackageFileStorageService
+                    .Setup(x => x.SetPropertiesAsync(
+                        CoreConstants.Folders.PackagesFolderName,
+                        "example.package.1.2.3.nupkg",
+                        It.IsAny<Func<Lazy<Task<Stream>>, ICloudBlobProperties, Task<bool>>>()))
+                    .Returns<string, string, Func<Lazy<Task<Stream>>, ICloudBlobProperties, Task<bool>>>(
+                        (_, __, update) => update(null, PublicBlobProperties.Object));
+
+                LicenseFileService = new Mock<ICoreLicenseFileService>();
+                ReadmeFileService = new Mock<ICoreReadmeFileService>();
+                Target = new StagedPackagePromotionMessageHandler(
+                    StagedPackageRepository.Object,
+                    PackageService.Object,
+                    StagingBlobService.Object,
+                    PackageFileStorageService.Object,
+                    new PackageFileMetadataService(),
+                    LicenseFileService.Object,
+                    ReadmeFileService.Object);
+            }
+
+            public void Apply(InvalidState state)
+            {
+                switch (state)
+                {
+                    case InvalidState.MissingStagedPackage:
+                        StagedPackages.Clear();
+                        break;
+                    case InvalidState.NotPromoting:
+                        StagedPackage.Status = StagedPackageStatus.Ready;
+                        break;
+                    case InvalidState.DifferentPromotion:
+                        StagedPackage.ActivePromotionId = Guid.NewGuid();
+                        break;
+                    case InvalidState.PackageNotStaged:
+                        Package.PackageStatusKey = PackageStatus.Available;
+                        break;
+                    case InvalidState.MissingValidatedBlob:
+                        StagedPackage.ValidatedBlobPath = null;
+                        break;
+                    case InvalidState.OwnerNoLongerOwnsPackage:
+                        Package.PackageRegistration.Owners.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(state));
+                }
+            }
+
+            public byte[] Content { get; }
+            public Uri SourceUri { get; }
+            public Guid PromotionId { get; }
+            public Package Package { get; }
+            public StagedPackage StagedPackage { get; }
+            public StagedPackagePromotionMessage Message { get; }
+            public List<StagedPackage> StagedPackages { get; }
+            public Mock<IEntityRepository<StagedPackage>> StagedPackageRepository { get; }
+            public Mock<ICorePackageService> PackageService { get; }
+            public Mock<IStagingBlobService> StagingBlobService { get; }
+            public Mock<ICoreFileStorageService> PackageFileStorageService { get; }
+            public Mock<ICloudBlobProperties> PublicBlobProperties { get; }
+            public Mock<ICoreLicenseFileService> LicenseFileService { get; }
+            public Mock<ICoreReadmeFileService> ReadmeFileService { get; }
+            public StagedPackagePromotionMessageHandler Target { get; }
+        }
+    }
+}
