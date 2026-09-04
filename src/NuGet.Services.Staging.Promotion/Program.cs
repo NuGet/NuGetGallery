496730af095aada4f4bdcb4d6694fc0f3d0092d6
James Parsons <59936622+japarson@users.noreply.github.com>
2026-09-03 21:39:36 -0700

    Switch to async main

--- src/NuGet.Services.Staging.Promotion/Program.cs
+++ src/NuGet.Services.Staging.Promotion/Program.cs
@@ -1,15 +1,16 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Threading.Tasks;
 using NuGet.Jobs;
 
 namespace NuGet.Services.Staging.Promotion
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            JobRunner.RunOnce(new Job(), args).GetAwaiter().GetResult();
+            await JobRunner.RunOnce(new Job(), args);
         }
     }
 }
